e3a8be946ff8176a43173e77b017986d789a2b76
Andy Creeth <acreeth@gmail.com>
2017-02-01 15:36:04 -0800

    Stop including twitchalerts css via webpack

--- app/app.js
+++ app/app.js
@@ -4,9 +4,6 @@ import store from './store';
 
 require('./app.less');
 
-// This is the 
-require('./twitchalerts.css');
-
 document.addEventListener('DOMContentLoaded', function() {
   new Vue({
     el: '#app',
