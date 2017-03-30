bee11a1c6e1effe07f8a96ba1453b3b668f4ab6d
Michael Fabian Dirks <info@xaymar.com>
2017-03-30 07:15:59 +0200

    renderer: Implement shared memory interface

--- app/util/SourceFrameStream.js
+++ app/util/SourceFrameStream.js
@@ -1,9 +1,19 @@
 const net = window.require('net');
 const { ipcRenderer } = window.require('electron');
+const boost = window.require('node-boost');
 
-import SourceFrameHeader from './SourceFrameHeader.js';
+import SourceFrame from './SourceFrame.js';
 import _ from 'lodash';
 import store from '../store';
+
+function handleListenerReacquire(p_obj, p_event, p_id, p_bufferName) {
+  p_obj.handleListenerReacquire(p_event, p_id, p_bufferName);
+}
+
+function handleListenerFlip(p_obj, p_event, p_id) {
+  p_obj.handleListenerFlip(p_event, p_id);
+}
+
 
 class SourceFrameStream {
 
@@ -11,17 +21,6 @@ class SourceFrameStream {
     // Object for keeping track of active streams.
     // Keyed by source id
     this.sourceStreams = {};
-
-    // All headers will be read into this buffer
-    this.header = new SourceFrameHeader();
-
-    // This is the buffer we are currently trying to fill
-    // from the TCP socket.
-    this.currentBuffer = this.header.buffer;
-
-    // This represents how far into the current buffer
-    // we are.
-    this.currentOffset = 0;
   }
 
   /*                         *
@@ -32,130 +31,76 @@ class SourceFrameStream {
   // A subscribed id will be returned, which can be
   // used to unsubscribe.
   subscribe(sourceId, callback) {
-    // Ensure we are connected to the main process
-    this.ensureSocketConnection();
-
-    // Ensure we are streaming the requested source
-    this.ensureSourceStream(sourceId);
+    if (!this.sourceStreams[sourceId]) {
+      this.sourceStreams[sourceId] = {
+        subscribers: new Map(),
+        memory: null,
+        region: null,
+        data: null
+      }
+    }
 
     let subscriberId = _.uniqueId();
     let stream = this.sourceStreams[sourceId];
 
-    stream.subscribers[subscriberId] = callback;
+    // IPC Signal
+    ipcRenderer.send('listenerRegister', sourceId, store.state.sources.sources[sourceId].name);
+    ipcRenderer.on('listenerReacquire', (p_event, p_id, p_bufferName) => {
+      handleListenerReacquire(this, p_event, p_id, p_bufferName);
+    });
+    ipcRenderer.on('listenerFlip', (p_event, p_id) => {
+      handleListenerFlip(this, p_event, p_id);
+    });
 
+    stream.subscribers.set(subscriberId, callback);
     return subscriberId;
   }
 
   // sourceId isn't technically required, but it makes
   // it faster/easier to find their subscriber id
   unsubscribe(sourceId, subscriberId) {
+    // ToDo: sourceId is undefined?
+    console.log(sourceId);
+
     let stream = this.sourceStreams[sourceId];
 
-    delete stream.subscribers[subscriberId];
+    // IPC Signal
+    ipcRenderer.send('listenerUnregister', sourceId);
 
-    // TODO: We may want to unsubscribe from frames entirely
-    // once the last subscriber has unsubscribed.
+    stream.subscribers.delete(subscriberId);
   }
 
   /*         *
    * PRIVATE *
    *         */
 
-  // The socket is started lazily
-  ensureSocketConnection() {
-    if (!this.connected) {
-      this.connected = true;
+  handleListenerReacquire(p_event, p_id, p_bufferName) {
+    console.log('listenerReacquire', p_id)
+    let stream = this.sourceStreams[p_id];
 
-      let socket = new net.Socket();
-
-      socket.on('data', chunk => {
-        this.handleChunk(chunk);
-      });
-
-      socket.connect(ipcRenderer.sendSync('getSocketPath'));
+    try {
+      stream.memory = new boost.interprocess.shared_memory(p_bufferName, 0, boost.interprocess.shared_memory_flags.Open);
+      stream.region = new boost.interprocess.mapped_region(stream.memory);
+      stream.data = new SourceFrame(stream.region.buffer());
+    } catch (exc) {
+      console.error(exc);
+      return;
     }
+
+    console.log(stream.data);
   }
 
-  // Streams for each source are started lazily
-  ensureSourceStream(sourceId) {
-    if (!this.sourceStreams[sourceId]) {
-      this.sourceStreams[sourceId] = {
-        subscribers: {},
-        frameBuffer: null
-      };
-
-      let sourceName = store.state.sources.sources[sourceId].name;
-
-      ipcRenderer.send('subscribeToSource', {
-        name: sourceName,
-        id: sourceId
+  handleListenerFlip(p_event, p_id) {
+    console.log('listenerFlip', p_id)
+    let stream = this.sourceStreams[p_id];
+    stream.subscribers.forEach((p_value, p_key, p_map) => {
+      p_value({
+        width: stream.data.width,
+        height: stream.data.height,
+        format: stream.data.format,
+        frameBuffer: stream.data.front_buffer()
       });
-    }
-  }
-
-  processBuffer() {
-    // If we just processed a source frame
-    if (this.currentSourceId) {
-      let stream = this.sourceStreams[this.currentSourceId];
-
-      _.each(stream.subscribers, callback => {
-        _.defer(callback, {
-          width: this.header.width,
-          height: this.header.height,
-          format: this.header.format,
-          frameBuffer: stream.frameBuffer
-        });
-      });
-
-      this.currentSourceId = null;
-      this.currentBuffer = this.header.buffer;
-
-    // Otherwise we processed a header
-    } else {
-      this.currentSourceId = this.header.id.toString();
-      let stream = this.sourceStreams[this.currentSourceId];
-
-      // If the frame buffer does not exist yet, or is not correctly
-      // sized for the incoming frame, allocate a new framebuffer
-      if (!stream.frameBuffer || (stream.frameBuffer.length !== this.header.frameLength)) {
-        stream.frameBuffer = new Uint8Array(this.header.frameLength);
-      }
-
-      this.currentBuffer = stream.frameBuffer;
-
-      let source = store.state.sources.sources[this.currentSourceId];
-
-      // If the width of the incoming frame is different,
-      // let the vuex store know about the new source size.
-      if ((source.width !== this.header.width) || source.height !== this.header.height) {
-        store.dispatch({
-          type: 'setSourceSize',
-          sourceId: source.id,
-          width: this.header.width,
-          height: this.header.height
-        });
-      }
-    }
-
-    this.currentOffset = 0;
-  }
-
-  handleChunk(chunk) {
-    if ((chunk.length + this.currentOffset) >= this.currentBuffer.length) {
-      let boundary = this.currentBuffer.length - this.currentOffset;
-
-      let finalChunk = chunk.subarray(0, boundary);
-      let overflow = chunk.subarray(boundary);
-
-      this.currentBuffer.set(finalChunk, this.currentOffset);
-
-      this.processBuffer();
-
-      this.handleChunk(overflow);
-    } else {
-      this.currentBuffer.set(chunk, this.currentOffset);
-      this.currentOffset += chunk.length;
-    }
+    });
   }
 }
 
