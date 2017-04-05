783aaf1f2b5379e1683d788d552fc4cdabfd80cb
eddyStreamlabs <eddy@streamlabs.com>
2017-04-04 17:36:45 -0700

    resize for source properties preview

--- app/components/SourcePropertiesPreview.vue
+++ app/components/SourcePropertiesPreview.vue
@@ -1,11 +1,12 @@
 <template>
-<canvas ref="canvas"/>
+<canvas ref="canvas"
+        @resize="onResize"/>
 </template>
 
 <script>
 import WebGLRenderer from '../util/WebGLRenderer.js';
 import SourceFrameStream from '../util/SourceFrameStream.js';
-
+import Obs from '../api/Obs.js';
 export default {
 
   props: ['sourceId'],
@@ -25,11 +26,36 @@ export default {
         );
       }
     });
+    window.addEventListener('resize', this.onResize);
   },
 
   beforeDestroy() {
     SourceFrameStream.unsubscribe(this.sourceId, this.subId);
-  }
+    window.removeEventListener('resize', this.onResize);
+  },
 
+  methods: {
+    onResize() {
+      var rect = this.$refs.canvas.getBoundingClientRect();
+
+      Obs.resizeDisplay(
+        'Preview Window',
+        rect.width,
+        rect.height
+      );
+
+      Obs.moveDisplay(
+        'Preview Window',
+        rect.left,
+        rect.top
+      );
+
+      this.$store.dispatch({
+        type: 'setVideoRenderedSize',
+        width: this.$refs.canvas.offsetWidth,
+        height: this.$refs.canvas.offsetHeight
+      });
+    },
+  }
 };
 </script>
