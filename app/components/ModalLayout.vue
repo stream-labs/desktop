3a3dfe5f0ded33c0fd0f44e8f5f0d8a5eba15840
Andy Creeth <acreeth@gmail.com>
2017-04-10 17:02:25 -0700

    Fix source properties preview

--- app/components/ModalLayout.vue
+++ app/components/ModalLayout.vue
@@ -3,6 +3,11 @@
   <div class="ModalLayout-spacer"/>
   <title-bar :window-title="title" class="ModalLayout-titleBar"/>
   <div
+    class="ModalLayout-fixed"
+    :style="fixedStyle">
+    <slot name="fixed"/>
+  </div>
+  <div
     class="ModalLayout-content"
     :style="contentStyle">
     <slot name="content"/>
@@ -36,8 +41,13 @@ export default {
 
     Object.assign(contentStyle, this.contentStyles);
 
+    let fixedStyle = {
+      height: (this.fixedSectionHeight || 0).toString() + 'px'
+    };
+
     return {
-      contentStyle
+      contentStyle,
+      fixedStyle
     };
   },
 
@@ -72,7 +82,10 @@ export default {
     'cancelHandler',
 
     // Additional CSS styles for the content section
-    'contentStyles'
+    'contentStyles',
+
+    // The height of the fixed section
+    'fixedSectionHeight'
   ]
 
 };
@@ -91,6 +104,10 @@ export default {
 
 .ModalLayout-titleBar {
   border-bottom: 1px solid #eee;
+  flex-shrink: 0;
+}
+
+.ModalLayout-fixed {
   flex-shrink: 0;
 }
 
