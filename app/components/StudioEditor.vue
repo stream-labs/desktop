c6a4432490c0e458a64b16c196d177363df06afd
Andy Creeth <acreeth@gmail.com>
2017-04-13 13:00:12 -0700

    move everything into StudioEditor

--- app/components/StudioEditor.vue
+++ app/components/StudioEditor.vue
@@ -1,24 +1,74 @@
 <template>
-<div class="StudioEditor">
-  <studio-editor-display/>
-</div>
+<div
+  class="StudioEditor-display"
+  ref="display"
+  @mousedown="handleMouseDown"
+  @mouseup="handleMouseUp"
+  @mousemove="handleMouseMove"/>
 </template>
 
 <script>
-import StudioEditorDisplay from './StudioEditorDisplay.vue';
-import _ from 'lodash';
+import Obs from '../api/Obs.js';
+const { webFrame, screen, remote } = window.require('electron');
 
 export default {
 
-  components: {
-    StudioEditorDisplay
+  mounted() {
+    Obs.createDisplay('Main Window');
+    this.onResize();
+
+    remote.getCurrentWindow().show();
+
+    window.addEventListener('resize', this.onResize);
+  },
+
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize);
+
+    Obs.destroyDisplay('Main Window');
+  },
+
+  methods: {
+    onResize() {
+      const display = this.$refs.display;
+      const rect = display.getBoundingClientRect();
+      const factor = webFrame.getZoomFactor() * screen.getPrimaryDisplay().scaleFactor;
+
+      Obs.resizeDisplay(
+        'Main Window',
+        rect.width * factor,
+        rect.height * factor
+      );
+
+      Obs.moveDisplay(
+        'Main Window',
+        rect.left * factor,
+        rect.top * factor
+      );
+    },
+
+    /*****************
+    * Mouse Handling *
+    *****************/
+
+    handleMouseDown(event) {
+
+    },
+
+    handleMouseUp(event) {
+
+    },
+
+    handleMouseMove(event) {
+
+    }
   }
 
 };
 </script>
 
 <style lang="less" scoped>
-.StudioEditor {
+.StudioEditor-display {
   position: relative;
   flex-grow: 1;
   background-color: #222;
