37661b749e37e671e6f43691d347b12b30e568be
Andy Creeth <acreeth@gmail.com>
2017-04-27 15:05:10 -0700

    fix text input

--- app/components/shared/forms/TextInput.vue
+++ app/components/shared/forms/TextInput.vue
@@ -5,7 +5,7 @@
     type="text"
     :value="value.currentValue"
     :disabled="!value.enabled"
-    @change="$emit('input', value)"
+    @change="onInputHandler"
   />
 </div>
 </template>
@@ -13,7 +13,17 @@
 <script>
 import Input from './Input.vue';
 
-let TextInput = Input.extend({});
+let TextInput = Input.extend({
+
+  methods: {
+
+    onInputHandler(event) {
+      this.$emit('input', Object.assign({}, this.value, { currentValue: event.target.value }));
+    }
+
+  }
+
+});
 TextInput.obsType = 'OBS_PROPERTY_EDIT_TEXT';
 export default TextInput;
 
