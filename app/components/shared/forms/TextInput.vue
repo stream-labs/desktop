ef66107763316978abc86e52c13a64fc987361f7
Alexander Nazarov <kadabrik91@gmail.com>
2017-05-05 12:32:32 -0700

    implement masked text inputs (#131)

--- app/components/shared/forms/TextInput.vue
+++ app/components/shared/forms/TextInput.vue
@@ -1,12 +1,20 @@
 <template>
 <div class="input">
   <label>{{ value.description }}</label>
-  <input
-    type="text"
-    :value="value.currentValue"
-    :disabled="value.enabled === 0"
-    @change="onInputHandler"
-  />
+  <div class="ButtonInput">
+    <input
+      :type="textVisible ? 'text' : 'password'"
+      :value="value.currentValue"
+      :disabled="value.enabled === 0"
+      @change="onInputHandler"
+    />
+    <button
+      class="button button--default"
+      v-if="value.masked"
+      @click="toggleVisible">
+      {{ textVisible ? 'Hide' : 'Show' }}
+    </button>
+  </div>
 </div>
 </template>
 
@@ -15,7 +23,16 @@ import Input from './Input.vue';
 
 let TextInput = Input.extend({
 
+  data() {
+    return {
+      textVisible: !this.value.masked
+    };
+  },
+
   methods: {
+    toggleVisible() {
+      this.textVisible = !this.textVisible;
+    },
 
     onInputHandler(event) {
       this.$emit('input', Object.assign({}, this.value, { currentValue: event.target.value }));
@@ -28,3 +45,19 @@ TextInput.obsType = 'OBS_PROPERTY_EDIT_TEXT';
 export default TextInput;
 
 </script>
+
+<style lang="less" scoped>
+.ButtonInput {
+  display: flex;
+  flex-direction: row;
+
+  >input {
+    flex-grow: 1;
+  }
+
+  >button {
+    margin-left: 10px;
+    width: 80px;
+  }
+}
+</style>
