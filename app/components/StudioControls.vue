6bc4e36a313caa845fc44e31679e0284ea39b727
Andy Creeth <acreeth@gmail.com>
2017-05-30 15:34:31 -0700

    get rid of theme selector (#179)

--- app/components/StudioControls.vue
+++ app/components/StudioControls.vue
@@ -1,21 +1,18 @@
 <template>
 <div class="studioControls row expanded">
-  <theme-selector class="studioControls-panel small-3 columns" />
-  <scene-selector class="studioControls-panel small-3 columns" />
-  <source-selector class="studioControls-panel small-3 columns" />
-  <mixer class="studioControls-panel small-3 columns" />
+  <scene-selector class="studioControls-panel small-4 columns" />
+  <source-selector class="studioControls-panel small-4 columns" />
+  <mixer class="studioControls-panel small-4 columns" />
 </div>
 </template>
 
 <script>
-import ThemeSelector from './ThemeSelector.vue';
 import SceneSelector from './SceneSelector.vue';
 import SourceSelector from './SourceSelector.vue';
 import Mixer from './Mixer.vue';
 
 export default {
   components: {
-    ThemeSelector,
     SceneSelector,
     SourceSelector,
     Mixer
