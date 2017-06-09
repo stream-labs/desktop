bc235dd865819700c041e93a25da9abacbb56920
Andy Creeth <acreeth@gmail.com>
2017-06-09 13:57:39 -0700

    add persistent stateful service

--- app/services/customization.ts
+++ app/services/customization.ts
@@ -1,10 +1,11 @@
-import { StatefulService, mutation } from './stateful-service';
+import { PersistentStatefulService } from './persistent-stateful-service';
+import { mutation } from './stateful-service';
 
 interface ICustomizationServiceState {
   nightMode: boolean;
 }
 
-export class CustomizationService extends StatefulService<ICustomizationServiceState> {
+export class CustomizationService extends PersistentStatefulService<ICustomizationServiceState> {
 
   // TODO: Saving and laoding of state
   static initialState = {
