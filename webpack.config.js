e944ee1e96733e1c298dc122725050534690ec68
Alexander Nazarov <kadabrik91@gmail.com>
2017-06-20 19:17:32 -0700

    add video tag support (#263)
    * add video tag support

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,9 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           esModule: true,
-          loaders: {
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          transformToRequire: {
+            video: 'src',
+            source: 'src'
           }
         }
       },
