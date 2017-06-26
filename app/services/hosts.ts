8f0912bdf86feda9d6f401631989b6235c8224c3
Andy Creeth <acreeth@gmail.com>
2017-06-26 10:12:57 -0700

    update comment

--- app/services/hosts.ts
+++ app/services/hosts.ts
@@ -1,7 +1,8 @@
 import { Service } from './service';
 
 // Hands out hostnames to the rest of the app. Eventually
-// will allow overriding based on environment
+// we should allow overriding this value. But for now we
+// are just keeping the value in one place.
 export class HostsService extends Service {
 
   get streamlabs() {
