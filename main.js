69664e162aa363bc8725dd2f30632c4c7f36af1b
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2017-07-13 12:25:35 -0700

    pass slobs config path even if obs config files are currently in use (#329)

--- main.js
+++ main.js
@@ -133,10 +133,9 @@ function startApp() {
   if (startupConfig && startupConfig.obsMode) {
     obs.OBS_API_setOBS_currentProfile(startupConfig.profile);
     obs.OBS_API_setOBS_currentSceneCollection(startupConfig.sceneCollection);
-    obs.OBS_API_setPathConfigDirectory('');
-  } else {
-    obs.OBS_API_setPathConfigDirectory(app.getPath('userData'));
-  }
+  } 
+
+  obs.OBS_API_setPathConfigDirectory(app.getPath('userData'));
 
   obs.OBS_API_openAllModules();
   obs.OBS_API_initAllModules();
