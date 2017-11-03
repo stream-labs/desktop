c219af8dbfa07f17d6f5a7e61f54926606dbc39e
Andy Creeth <acreeth@gmail.com>
2017-11-03 15:39:23 -0700

    upgrade node obs

--- bin/install-node-obs.js
+++ bin/install-node-obs.js
@@ -2,7 +2,7 @@
  * This script downloads and exctracts the specified release of node-obs
  */
 
-const NODE_OBS_VERSION = '0.1.26';
+const NODE_OBS_VERSION = '0.1.27';
 
 const https = require('https');
 const fs = require('fs');
