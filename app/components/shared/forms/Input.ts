932424803eba3c8b81c7314fa201e35b7a6c48b9
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2017-11-08 15:33:47 -0800

    Boolean settings properties (#39)
    * node-obs returns boolean for boolean values
    
    * node-obs returns boolean for boolean values, also for the current value

--- app/components/shared/forms/Input.ts
+++ app/components/shared/forms/Input.ts
@@ -166,12 +166,9 @@ export function obsValuesToInputValues(
     }
 
     prop.value = obsValue;
-
-    if (options.boolIsString) {
-      prop.masked = obsProp.masked === 'true';
-      prop.enabled = obsProp.enabled === 'true';
-      prop.visible = obsProp.visible === 'true';
-    }
+    prop.masked = !!obsProp.masked;
+    prop.enabled = !!obsProp.enabled;
+    prop.visible = !!obsProp.visible;
 
     if (options.disabledFields && options.disabledFields.includes(prop.name)) {
       prop.enabled = false;
@@ -202,7 +199,7 @@ export function obsValuesToInputValues(
 
     } else if (obsProp.type === 'OBS_PROPERTY_BOOL') {
 
-      if (options.boolIsString) prop.value = prop.value === 'true';
+      prop.value = !!prop.value;
 
     } else if (['OBS_PROPERTY_INT', 'OBS_PROPERTY_FLOAT', 'OBS_PROPERTY_DOUBLE'].includes(obsProp.type)) {
 
