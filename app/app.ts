5dc2613efc68b4a5bdf4ab329426a88a8289455c
Andy Creeth <acreeth@gmail.com>
2017-11-29 12:16:53 -0800

    Add interactive deploy script, switch to sentry.io for renderer exceptions (#83)
    * Added sentry, working on new release script
    
    * written up to deploy, needs testing on fork
    
    * uncommented release script
    
    * Integrate sentry with release script
    
    * temp disable tests
    
    * re-enable stash
    
    * use sentry correctly
    
    * sentry issues
    
    * Properly upload sentry artifacts
    
    * upload source map as regular file
    
    * more quick tweaks
    
    * release from origin branches
    
    * branch syncing
    
    * update comment
    
    * final changes to get ready for production
    
    * remove deploy script
    
    * update readme

--- app/app.ts
+++ app/app.ts
@@ -13,22 +13,39 @@ import { AppService } from './services/app';
 import { ServicesManager } from './services-manager';
 import Utils from './services/utils';
 import electron from 'electron';
+import Raven from 'raven-js';
+import RavenVue from 'raven-js/plugins/vue';
 
 const { ipcRenderer, remote } = electron;
 
 const slobsVersion = remote.process.env.SLOBS_VERSION;
 
 if (remote.process.env.NODE_ENV === 'production') {
-  const bt = require('backtrace-js');
+  Raven
+    .config('https://6971fa187bb64f58ab29ac514aa0eb3d@sentry.io/251674', {
+      release: slobsVersion,
+      dataCallback: data => {
+        // Because our URLs are local files and not publicly
+        // accessible URLs, we simply truncate and send only
+        // the filename.  Unfortunately sentry's electron support
+        // isn't that great, so we do this hack.
+        // Some discussion here: https://github.com/getsentry/sentry/issues/2708
+        const normalize = (filename: string) => {
+          const splitArray = filename.split('/');
+          return splitArray[splitArray.length - 1];
+        };
 
-  bt.initialize({
-    endpoint: 'https://streamlabs.sp.backtrace.io:6098',
-    token: 'e3f92ff3be69381afe2718f94c56da4644567935cc52dec601cf82b3f52a06ce',
-    attributes: {
-      version: slobsVersion,
-      processType: 'renderer'
-    }
-  });
+        data.exception.values[0].stacktrace.frames.forEach((frame: any) => {
+          frame.filename = normalize(frame.filename);
+        });
+
+        data.culprit = data.exception.values[0].stacktrace.frames[0].filename;
+
+        return data;
+      }
+    })
+    .addPlugin(RavenVue, Vue)
+    .install();
 }
 
 electron.crashReporter.start({
