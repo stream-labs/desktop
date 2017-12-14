eb17af46cba2ed99273a0831f85338577be030f1
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2017-12-14 13:06:41 -0800

    Bump facemask to v0.3.8 (#119)

--- bin/install-node-obs.js
+++ bin/install-node-obs.js
@@ -13,7 +13,7 @@ const colors = require('colors/safe');
 
 // CONFIGURATION
 const NODE_OBS_VERSION = '0.1.31';
-const FACE_MASK_VERSION = '0.3.4';
+const FACE_MASK_VERSION = '0.3.8';
 
 // This is the main function
 async function runScript() {
