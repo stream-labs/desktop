0b3eb6b64d87f715c43878790ab9a79b16588275
eddyStreamlabs <eddy@streamlabs.com>
2017-12-15 14:38:43 -0800

    Minor changes

--- test/onboarding.js
+++ test/onboarding.js
@@ -20,6 +20,7 @@ useSpectron({ skipOnboarding: false });
 test('Adding some starter widgets', async t => {
   const app = t.context.app;
   await focusMain(t);
+
   const widgetToken = 'SomeWidgetToken';
   const platform = {
     type: 'twitch',
@@ -27,34 +28,45 @@ test('Adding some starter widgets', async t => {
     token: 'SomeToken',
     id: 'SomeId'
   };
+
   // Wait for the auth screen to appear
   await app.client.isExisting('button=Twitch');
+
   await app.webContents.send('testing-fakeAuth', {
     widgetToken,
     platform
   });
+
   // This will only show up if OBS is installed
   if (await t.context.app.client.isExisting('button=Start Fresh')) {
     await t.context.app.client.click('button=Start Fresh');
   }
+
   // Select and deselect some widgets
+
   await app.client.click('div=Event List');
   await app.client.click('button=Remove Widget');
+
   await app.client.click('div=Chatbox');
   await app.client.click('button=Add Widget');
+
   await app.client.click('div=Donation Goal');
   await app.client.click('button=Add Widget');
+
   await app.client.click('button=Add 4 Widgets');
   await app.client.click('a=Setup later');
+
   t.true(await app.client.isExisting('li=Alert Box'));
   t.false(await app.client.isExisting('li=Event List'));
   t.true(await app.client.isExisting('li=The Jar'));
   t.true(await app.client.isExisting('li=Chat Box'));
   t.false(await app.client.isExisting('li=Donation Ticker'));
   t.true(await app.client.isExisting('li=Donation Goal'));
+
   await selectSource(t, 'Alert Box');
   await clickSourceProperties(t);
   await focusChild(t);
+
   t.true(await app.client.isExisting('label=Widget Type'));
 });
 
@@ -63,16 +75,9 @@ test('Obs-importer', async t => {
   const sh = require('shelljs');
 
   const app = t.context.app;
-  const zipExe = path.resolve( __dirname, '..', '..', 'node_modules', '7zip-bin-win', 'x64', '7za.exe');
+  const zipExe = path.resolve('node_modules', '7zip-bin-win', 'x64', '7za.exe');
   const userData = t.context.cacheDir;
-  const obsStudioCache = path.resolve(
-    __dirname,
-    '..',
-    '..',
-    'test',
-    'ressources',
-    'obs-studio.zip'
-  );
+  const obsStudioCache = path.resolve('test', 'ressources', 'obs-studio.zip');
 
   let result = sh.exec(`"${zipExe}" x "${obsStudioCache}" -o"${userData}"`);
 
