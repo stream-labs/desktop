06bf758ca4160762540599b67607460a4446be8b
Zachary Lund <zachary.lund@streamlabs.com>
2018-01-17 19:57:01 -0800

    Fix overlay migration to autocorrect missing style setting

--- app/components/shared/forms/GoogleFontSelector.vue.ts
+++ app/components/shared/forms/GoogleFontSelector.vue.ts
@@ -30,6 +30,27 @@ export default class GoogleFontSelector extends Input<IGoogleFont> {
 
   selectedStyle = '';
 
+  /* TrueType/OpenType defines fonts to have a
+   * preferred family name. For instance Arial Black
+   * has a preferred family name of 'Arial' and a 
+   * preferred subfamily name of 'Black'. 
+   *
+   * GDI+ and consequently the plugin doesn't 
+   * understand this at all. GDI+ only understands 
+   * the following for styles: 
+   *
+   * `Bold | Italic | Bold Italic | Regular
+   *
+   * As a result, anything that isn't part of the style must
+   * be part of the family name. For example, in the eyes of
+   * GDI+, Arial Black has a family name of 'Arial Black' and
+   * a subfamily name of Regular. 
+   * The plugin itself takes the family name as the style
+   * as a bitmask of flags */
+  actualFamily = '';
+
+  actualStyle: number = 0;
+
   loading = true;
 
   created() {
@@ -67,7 +88,6 @@ export default class GoogleFontSelector extends Input<IGoogleFont> {
       const style = family.styles[0];
 
       this.updateStyles();
-      this.value.face = familyName;
       this.setStyle(style.name);
     });
   }
@@ -78,6 +98,12 @@ export default class GoogleFontSelector extends Input<IGoogleFont> {
 
     this.fontLibraryService.findStyle(this.selectedFamily, styleName).then(style => {
       this.fontLibraryService.downloadFont(style.file).then(fontPath => {
+        [this.actualFamily, this.actualStyle] = 
+          this.fontLibraryService.getSettingsFromFont(this.selectedFamily, style.name);
+
+        this.value.face = this.actualFamily;
+        this.value.flags = this.actualStyle;
+
         this.emitInput({ ...this.value, path: fontPath });
         this.loading = false;
       });
--- app/services/font-library.ts
+++ app/services/font-library.ts
@@ -3,13 +3,17 @@ import path from 'path';
 import fs from 'fs';
 import https from 'https';
 import electron from 'electron';
+import { EFontStyle } from 'obs-studio-node';
 
+export interface IFamilyWithStyle {
+  family: IFontFamily;
+  style: IFontStyle;
+}
 
 export interface IFontStyle {
   name: string;
   file: string;
 }
-
 
 export interface IFontFamily {
   name: string;
@@ -45,12 +49,45 @@ export class FontLibraryService extends Service {
     return Promise.resolve(this.manifest);
   }
 
+  getSettingsFromFont(familyName: string, style: string): [string, number] {
+    /* Scrape style string remnants onto actual family name
+     * until we're left with nothing but plugin compatible terms. */
+    const styleSplit = style.split(' ');
+    let actualFamily = familyName;
+    let actualStyle = 0;
+
+    for (let i = 0; i < styleSplit.length; ++i) {
+      switch (styleSplit[i]) {
+        case 'Regular':
+          if (i !== styleSplit.length - 1) {
+            console.log(`Weird font style detected: ${style}`);
+          }
+
+          break;
+        case 'Italic':
+          if (i !== styleSplit.length - 1) {
+            console.log(`Weird font style detected: ${style}`);
+          }
+
+          actualStyle |= EFontStyle.Italic;
+          break;
+        case 'Bold':
+          actualStyle |= EFontStyle.Bold;
+          break;
+        default:
+          actualFamily += ` ${styleSplit[i]}`;
+      }
+    }
+
+    return [actualFamily, actualStyle];
+  }
+
   /* This scans the entire manifest (which is slow)
    * to find a filename, then finds the family name
    * in the parent. This also doesn't take into account
    * font files yet. This function is used mostly for
    * upgrading config schema */
-  findFontFile(baseFileName: string): Promise<IFontFamily> {
+  findFontFile(baseFileName: string): Promise<IFamilyWithStyle> {
     return this.getManifest().then(manifest => {
       for (let i = 0; i < manifest.families.length; ++i) {
         const family = manifest.families[i];
@@ -59,7 +96,7 @@ export class FontLibraryService extends Service {
           const style = family.styles[k];
 
           if (baseFileName === style.file) {
-            return family;
+            return { family, style };
           }
         }
       }
