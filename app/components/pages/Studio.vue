028569102f5e3c0fe2714d15564988c7e4f10279
Alexander Nazarov <kadabrik91@gmail.com>
2018-01-23 09:06:47 -0800

    preformance mode (#195)
    * preformance mode
    
    * fix menu item
    
    * review fixes

--- app/components/pages/Studio.vue
+++ app/components/pages/Studio.vue
@@ -1,6 +1,13 @@
 <template>
 <div class="studio-page">
-  <studio-editor />
+  <studio-editor v-if="previewEnabled" />
+  <div v-if="!previewEnabled" class="no-preview">
+    <div class="message">
+      Preview is disabled in performance mode
+      <div class="button button--action button--sm" @click="enablePreview">Disable Performance Mode</div>
+    </div>
+
+  </div>
   <studio-controls />
 </div>
 </template>
@@ -8,8 +15,27 @@
 <script lang="ts" src="./Studio.vue.ts"></script>
 
 <style lang="less" scoped>
+@import "../../styles/index";
+
 .studio-page {
   display: flex;
   flex-direction: column;
+}
+
+.no-preview {
+  position: relative;
+  flex-grow: 1;
+  background-color: @navy-secondary;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .message {
+    max-width: 50%;
+    .button {
+      margin-top: 20px;
+      display: block
+    }
+  }
 }
 </style>
