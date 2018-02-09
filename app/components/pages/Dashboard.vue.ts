3d1bf17f07084c0ddc40af697d791e26024d7b14
Andy Creeth <acreeth@gmail.com>
2018-02-09 13:44:07 -0800

    fix no subPage case, and close child window when opening widget settings

--- app/components/pages/Dashboard.vue.ts
+++ app/components/pages/Dashboard.vue.ts
@@ -6,17 +6,13 @@ import { Inject } from '../../util/injector';
 @Component({})
 export default class Dashboard extends Vue {
   @Inject() userService: UserService;
-
   @Prop() params: Dictionary<string>;
 
   get loggedIn() {
     return this.userService.isLoggedIn();
   }
 
-  subPage: String = '';
-
   get dashboardUrl() {
-    const url = this.userService.dashboardUrl(this.params.subPage);
-    return url;
+    return this.userService.dashboardUrl(this.params.subPage || '');
   }
 }
