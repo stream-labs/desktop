ade0db55f490155776137e58986ea6bf40bd5201
Andy Creeth <acreeth@gmail.com>
2018-04-20 11:14:37 -0700

    move remote control into its own settings category

--- app/services/settings/settings.ts
+++ app/services/settings/settings.ts
@@ -113,7 +113,7 @@ export class SettingsService extends StatefulService<ISettingsState>
   getCategories(): string[] {
     let categories = nodeObs.OBS_settings_getListCategories();
     categories = categories
-      .concat(['Overlays','Notifications', 'Appearance']);
+      .concat(['Overlays','Notifications', 'Appearance', 'Remote Control']);
 
     // we decided to not expose API settings for production version yet
     if (this.advancedSettingEnabled()) categories = categories.concat(['API', 'Experimental']);
