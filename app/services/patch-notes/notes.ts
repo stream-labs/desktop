c687cfe340fbc2e22d03c7629e7861eeb4d2fb08
Andy Creeth <acreeth@gmail.com>
2018-05-08 12:58:09 -0700

    patch notes for 0.9.0

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,18 +1,15 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.8.17',
-  title: 'Projectors and Bugfixes',
+  version: '0.9.0',
+  title: 'Studio Mode',
   notes: [
-    'Projectors: Right click on a source to pop out a preview of that source.  '
-      + 'You can also pop out a preview of your streaming output.  All projectors can be made fullscreen.',
-    'Filters can now be added to scenes, and they will be saved properly',
-    'Switched from legacy to current Twitch chat.  Dark mode is not enabled by default in this chat.  If you ' +
-      'want to use dark mode chat, you can click the gear in the lower left of the chat pane.',
-    'The shift button can be used to select a range of sources',
-    'Clicking the "Refresh Chat" button will now navigate back to the chat for your channel',
-    'Navigating to a different tab in settings no longer moves the window',
-    'Notifications settings will now save properly when exiting',
-    'Allow reordering multiple sources at once'
+    'Studio Mode: This feature allows you make changes to your scenes in the background, and switch ' +
+      'to them on your stream when you are ready.  Click the "Studio Mode" button in the top right to get started!',
+    'Removing a scene will now ask for confirmation',
+    'Fixed an issue where right clicking a selection of multiple sources would not work',
+    'Fixed an issue where copy/pasting would not preserve folder structure',
+    'Made a number of minor visual tweaks throughout the app',
+    'The library has been renamed to "Themes"'
   ]
 };
