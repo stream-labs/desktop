f79d2326a6a77f47472ff92dac272aafcdee6805
Andy Creeth <acreeth@gmail.com>
2018-05-09 11:11:38 -0700

    start stubbing in service

---
+++ app/services/media-backup.ts
@@ -0,0 +1,11 @@
+import { StatefulService } from 'services/stateful-service';
+
+interface IMediaBackupState {
+
+}
+
+export class MediaBackupService extends StatefulService<IMediaBackupState> {
+
+  
+
+}
