3390c76618bb7856c48970aa29b9ed5421618b35
Andy Creeth <acreeth@gmail.com>
2018-05-18 13:46:39 -0700

    add audio source hiding (#455)

--- app/components/Mixer.vue.ts
+++ app/components/Mixer.vue.ts
@@ -3,6 +3,7 @@ import { Component } from 'vue-property-decorator';
 import { AudioService } from '../services/audio';
 import { Inject } from '../util/injector';
 import MixerItem from './MixerItem.vue';
+import { Menu } from 'util/menus/Menu';
 
 @Component({
   components: { MixerItem }
@@ -17,7 +18,18 @@ export default class Mixer extends Vue {
     this.audioService.showAdvancedSettings();
   }
 
+  handleRightClick() {
+    const menu = new Menu();
+    menu.append({
+      label: 'Unhide All',
+      click: () => this.audioService.unhideAllSourcesForCurrentScene()
+    });
+    menu.popup();
+  }
+
   get audioSources() {
-    return this.audioService.getSourcesForCurrentScene();
+    return this.audioService.getSourcesForCurrentScene().filter(source => {
+      return !source.mixerHidden;
+    });
   }
 }
