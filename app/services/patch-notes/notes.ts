483120ae5168781344e7f38ae1a4aa3b4d023317
Andy Creeth <acreeth@gmail.com>
2018-06-06 09:55:52 -0700

    remove a period

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -7,7 +7,7 @@ export const notes: IPatchNotes = {
     "We've been working with a team of international streamers to translate Streamlabs OBS " +
       'into as many different languages as possible.  Today we are launching with 14 different ' +
       'languages with more on the way.  You can find a language selector in Settings => General. ' +
-      'Please bear with us as we continue to improve and complete our translations.',
+      'Please bear with us as we continue to improve and complete our translations',
     'Our audio mixer has been given an overhaul with new multi-channel ' +
       'volume level displays, as well as color coded zones to help you dial in the perfect volume.',
     'You can now pop out the recent events view on the live page into a separate window',
