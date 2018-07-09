988a05f10b55643b5e56e73b57d24726edb93343
Sean Beyer <sean.beyer@streamlabs.com>
2018-07-09 11:00:51 -0700

    Add styling to widget textarea (#542)

--- app/components/shared/widget-inputs/WTextAreaInput.vue
+++ app/components/shared/widget-inputs/WTextAreaInput.vue
@@ -4,7 +4,19 @@
       :placeholder="metadata.placeholder"
       :value="value"
       @input="emitInput($event.target.value)"
+      class="text-area radius"
   />
 </template>
 
 <script lang="ts" src="./WTextAreaInput.vue.ts"></script>
+
+<style lang="less" scoped>
+@import "../../../styles/index";
+
+.night-theme {
+  .text-area:focus {
+    background-color: @night-hover;
+    border-color: @teal-light-opac;
+  }
+}
+</style>
