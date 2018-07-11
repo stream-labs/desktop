bfd526f431741f600cb9987e4a61b3d2d250e0a2
Andy Creeth <acreeth@gmail.com>
2018-07-11 13:50:13 -0700

    patch notes 0.9.4

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,16 +1,15 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.9.3',
-  title: 'Fixes and Tweaks',
+  version: '0.9.4',
+  title: 'Transitions Overhaul',
   notes: [
-    'You can now copy scenes and filters across scene collections',
-    'Dragging and dropping or copy-pasting media, text, or URLs will now create a source of the appropriate type',
-    'Source filters can now be reordered',
-    'We updated translations for a handful of languages',
-    'Fixed a bug where scene themes would import with multiple widgets of the same type',
-    'Fixed a bug where switching scenes would sometimes end the stream',
-    'Fixed a bug where media sources would sometimes revert to their old backed up media upon restarting the app',
-    'Fixed a bug where streaming and recording at the same time using the QSV encoder was not working'
+    "We've completely overhauled transitions from the ground up. You can create as many transitions as you want, " +
+      'and set up connections to automatically use certain transitions between scenes of your choice.',
+    'Added a confirmation dialog when deleting sources',
+    'Fixed an issue where the active scene collection would be changed when restoring from cloud backup',
+    'Fixed an issue where animated gifs were not properly restored from cloud backup',
+    'Fixed an issue where logging into a different account would still display chat for the previous account',
+    'Fixed some crashes relating to the SLOBS chat window and starting a raid'
   ]
 };
