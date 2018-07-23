8bd22769c3cc8b31c81455a43f1ba41888b3ecfc
Oat <nitsorn.oat@gmail.com>
2018-07-23 12:40:24 -0700

    minor

--- app/components/shared/widget-inputs/WTextInput.vue
+++ app/components/shared/widget-inputs/WTextInput.vue
@@ -25,3 +25,28 @@
     }
   }
 </style>
+
+// import { Component, Prop } from 'vue-property-decorator';
+// import { IWInputMetadata, WInput } from './WInput';
+
+// interface IWTextMetadata extends IWInputMetadata {
+//   placeholder: string;
+//   validate: string;
+// }
+
+// @Component({
+// })
+// export default class WTextAreaInput extends WInput<string, IWTextMetadata> {
+
+//   @Prop()
+//   value: string;
+
+//   @Prop({ default: () => ({}) })
+//   metadata: IWTextMetadata;
+
+//   getValidations() {
+//     return {
+//       ...super.getValidations(),
+//     };
+//   }
+// }
