98924ff3a48c88801f9be57767ef87b6e8fc66fa
Oat <nitsorn.oat@gmail.com>
2018-07-23 12:42:44 -0700

    minor

--- app/components/shared/widget-inputs/WTextInput.vue
+++ app/components/shared/widget-inputs/WTextInput.vue
@@ -1,12 +1,12 @@
 <template>
   <span class="w-text-input">
     <input
-      type="text"
-      :placeholder="metadata.placeholder"
-      :value="value"
-      @input="emitInput($event.target.value)"
-      :name="uuid"
-      v-validate="validate"
+        type="text"
+        :placeholder="metadata.placeholder"
+        :value="value"
+        @input="emitInput($event.target.value)"
+        :name="uuid"
+        v-validate="validate"
     />
     <span class="input-error" v-show="errors.first(uuid)">
       {{ errors.first(uuid) }}
@@ -25,28 +25,3 @@
     }
   }
 </style>
-
-// import { Component, Prop } from 'vue-property-decorator';
-// import { IWInputMetadata, WInput } from './WInput';
-
-// interface IWTextMetadata extends IWInputMetadata {
-//   placeholder: string;
-//   validate: string;
-// }
-
-// @Component({
-// })
-// export default class WTextAreaInput extends WInput<string, IWTextMetadata> {
-
-//   @Prop()
-//   value: string;
-
-//   @Prop({ default: () => ({}) })
-//   metadata: IWTextMetadata;
-
-//   getValidations() {
-//     return {
-//       ...super.getValidations(),
-//     };
-//   }
-// }
