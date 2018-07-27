278b2d404060fb48deba79bd77f426286da24d44
Oat <nitsorn.oat@gmail.com>
2018-07-27 15:37:12 -0700

    convert value from string to number before emitting

--- app/components/shared/inputs/NumberInput.vue.ts
+++ app/components/shared/inputs/NumberInput.vue.ts
@@ -1,15 +1,26 @@
 import { Component, Prop } from 'vue-property-decorator';
 import { BaseInput } from './BaseInput';
 import { INumberMetadata } from './index';
+import { String } from '../../../../node_modules/aws-sdk/clients/ssm';
 
 @Component({})
-export default class NumberInput extends BaseInput<number|string, INumberMetadata> {
+export default class NumberInput extends BaseInput<number | string, INumberMetadata> {
 
   @Prop()
   value: number | string; // the string type is for empty field
 
   @Prop()
   metadata: INumberMetadata;
+
+  emitInput(eventData: String) {
+    if (!isNaN(parseFloat(eventData))) {
+      // if is a string of valid number
+      // convert to number
+      super.emitInput(parseFloat(eventData));
+      return;
+    }
+    super.emitInput(eventData);
+  }
 
   getValidations() {
     return {
