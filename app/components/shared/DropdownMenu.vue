2da91df9e1d1cd8b5f5821c51b254b688411f857
Oat <nitsorn.oat@gmail.com>
2018-07-30 13:35:14 -0700

    minor

--- app/components/shared/DropdownMenu.vue
+++ app/components/shared/DropdownMenu.vue
@@ -170,14 +170,28 @@
 }
 
 
-
-
-
-
-
-
-
-
-
-
 </style>
+
+
+// import Vue from 'vue';
+// import { Component, Prop } from 'vue-property-decorator';
+// import Popper from 'vue-popperjs';
+
+// @Component({
+//   components: { Popper }
+// })
+// export default class DropdownMenu extends Vue {
+
+//   @Prop()
+//   title: string;
+
+//   @Prop()
+//   placement: string;
+
+//   @Prop()
+//   icon: string;
+
+//   get getPlacement() {
+//     return this.placement || 'bottom-start';
+//   }
+// }
