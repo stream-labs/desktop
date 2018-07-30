7f2ac92e764328345da3aea2b43056c7f8078122
Sean Beyer <sean.beyer@streamlabs.com>
2018-07-30 16:10:16 -0700

    Add donation ticker widget settings

--- app/services-manager.ts
+++ app/services-manager.ts
@@ -82,6 +82,7 @@ import { DonationGoalService } from 'services/widget-settings/donation-goal';
 import { FollowerGoalService } from 'services/widget-settings/follower-goal';
 import { ViewerCountService } from 'services/widget-settings/viewer-count';
 import { StreamBossService } from 'services/widget-settings/stream-boss';
+import { DonationTickerService } from 'services/widget-settings/donation-ticker';
 import { CreditsService } from 'services/widget-settings/credits';
 import { EventListService } from 'services/widget-settings/event-list';
 
@@ -166,6 +167,7 @@ export class ServicesManager extends Service {
     ChatBoxService,
     ViewerCountService,
     StreamBossService,
+    DonationTickerService,
     CreditsService,
     EventListService,
     MediaGalleryService
