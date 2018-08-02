5bb90c4e2634b829a03b5e12beb1d26949825e4c
Sean Beyer <sean.beyer@streamlabs.com>
2018-08-02 14:15:52 -0700

    Add multi-platform support for the jar

--- app/components/widgets/TipJar.vue
+++ app/components/widgets/TipJar.vue
@@ -2,46 +2,44 @@
 <widget-window v-if="wData" ref="layout" v-model="tabName">
 
   <div slot="description">
-    {{ $t('Include your channel\'s chat into your stream, and make it look pretty while you\'re at it.') }}
+    {{ $t('Following in the footsteps of some other similar Twitch services, we\'ve decided to make our own take on a jar that catches bits, tips, and more. Copy the Widget URL into your streaming software, or launch it and capture it.') }}
   </div>
 
   <div slot="settings">
-    <form-group title="Enabled Events">
-      <bool-input title="Tips &amp; Donations" v-model="wData.settings.types.tips.enabled"/>
-      <bool-input title="Twitch Follows" v-model="wData.settings.types.twitch_follows.enabled"/>
-      <bool-input title="Twitch Bits/Cheers" v-model="wData.settings.types.twitch_bits.enabled"/>
-      <bool-input title="Twitch Subs" v-model="wData.settings.types.twitch_subs.enabled"/>
-      <bool-input title="Twitch Resubs" v-model="wData.settings.types.twitch_resubs.enabled"/>
-    </form-group>
-    <form-group title="Jar Image">
-      <image-picker
-        :metadata="{ options: inputOptions }"
-        v-model="wData.settings.jar.type"
+    <form-group :title="$t('Enabled Events')">
+      <bool-input
+        v-for="key in Object.keys(wData.settings.types)"
+        :key="key"
+        :title="titleFromKey(key)"
+        v-model="wData.settings.types[key].enabled"
       />
     </form-group>
-    <form-group title="Text">
-      <bool-input title="Show Text" v-model="wData.settings.text.show"/>
+    <form-group :title="$t('Jar Image')">
+      <image-picker :metadata="{ options: inputOptions }" v-model="wData.settings.jar.type"/>
+    </form-group>
+    <form-group :title="$t('Text')">
+      <bool-input :title="$t('Show Text')" v-model="wData.settings.text.show"/>
     </form-group>
     <form-group type="fontFamily" :value="wData.settings.text.font"/>
-    <form-group title="Text Color" type="color" v-model="wData.settings.text.color" :metadata="{ tooltip: textColorTooltip }"/>
-    <form-group title="Font Size" type="fontSize" v-model="wData.settings.text_size"/>
-    <form-group title="Minimum Bits">
+    <form-group :title="$t('Text Color')" type="color" v-model="wData.settings.text.color" :metadata="{ tooltip: textColorTooltip }"/>
+    <form-group :title="$t('Font Size')" type="fontSize" v-model="wData.settings.text_size"/>
+    <form-group v-if="wData.settings.types.twitch_bits" :title="$t('Minimum Bits')">
       <number-input v-model="wData.settings.types.twitch_bits.minimum_amount" :metadata="{ required: true, min: 1 }"/>
     </form-group>
-    <form-group title="Minimum Tips">
+    <form-group :title="$t('Minimum Tips')">
       <number-input v-model="wData.settings.types.tips.minimum_amount" :metadata="{ required: true, min: 1 }"/>
     </form-group>
-    <form-group title="Background Color" type="color" v-model="wData.settings.background_color" :metadata="{ description: backgroundColorDescription }" />
-    <form-group title="Twitch Follows">
+    <form-group :title="$t('Background Color')" type="color" v-model="wData.settings.background_color" :metadata="{ description: backgroundColorDescription }" />
+    <form-group v-for="key in mediaGalleryInputs" :key="key" :title="titleFromKey(key)">
       <media-gallery-input
-        :metadata="{ fileName: fileNameFromHref(wData.settings.types.twitch_follows.image_src), clearImage: wData.defaultImage.twitch_account }"
-        v-model="wData.settings.types.twitch_follows.image_src"
+        :metadata="{ fileName: fileNameFromHref(wData.settings.types[key].image_src), clearImage: wData.defaultImage[`${platform}_account`] }"
+        v-model="wData.settings.types[key].image_src"
       />
     </form-group>
     <form-group
       v-for="tier in wData.settings.types.tips.tiers"
       :key="tier.minimum_amount"
-      :title="`Tips over ${tier.minimum_amount}`"
+      :title="`${$t('Tips over')} ${tier.minimum_amount}`"
     >
       <media-gallery-input
         :metadata="{ fileName: fileNameFromHref(tier.image_src), clearImage: tier.clear_image }"
--- app/components/widgets/TipJar.vue.ts
+++ app/components/widgets/TipJar.vue.ts
@@ -1,14 +1,38 @@
 import { Component } from 'vue-property-decorator';
+import { Inject } from '../../util/injector';
 import WidgetWindow from 'components/windows/WidgetWindow.vue';
 import WidgetSettings from './WidgetSettings.vue';
 import {
   TipJarService,
   ITipJarData
 } from 'services/widget-settings/tip-jar';
+import { UserService } from 'services/user';
 import { inputComponents } from 'components/shared/inputs';
 import FormGroup from 'components/shared/inputs/FormGroup.vue';
 
 import { $t } from 'services/i18n';
+
+const nameMap = () => ({
+  tips: $t('Tips & Donations'),
+  twitch_follows: $t('Twitch Follows'),
+  mixer_follows: $t('Mixer Follows'),
+  twitch_bits: $t('Twitch Bits'),
+  twitch_subs: $t('Twitch Subs'),
+  mixer_subscriptions: $t('Mixer Subscriptions'),
+  twitch_resubs: $t('Twitch Resubs'),
+  youtube_subscribers: $t('Youtube Subscriptions'),
+  youtube_sponsors: $t('Youtube Sponsors'),
+  youtube_superchats: $t('Youtube Super Chats'),
+  periscope_superhearts: $t('Periscope Super Hearts'),
+  picarto_follows: $t('Picarto Follows'),
+  picarto_subscriptions: $t('Picarto Subscriptions')
+});
+
+const mediaGalleryInputs = {
+  twitch: ['twitch_follows'],
+  youtube: ['youtube_subscribers', 'youtube_sponsors'],
+  mixer: ['mixer_subscriptions', 'mixer_follows']
+};
 
 @Component({
   components: {
@@ -18,6 +42,8 @@ import { $t } from 'services/i18n';
   }
 })
 export default class TipJar extends WidgetSettings<ITipJarData, TipJarService> {
+  @Inject() userService: UserService;
+
   textColorTooltip = $t('A hex code for the base text color.');
 
   backgroundColorDescription = $t(
@@ -30,6 +56,19 @@ export default class TipJar extends WidgetSettings<ITipJarData, TipJarService> {
   fileNameFromHref(href: string) {
     if (!href) return null;
     return decodeURIComponent(href.split(/[\\/]/).pop());
+  }
+
+  titleFromKey(key: string) {
+    return nameMap()[key];
+  }
+
+  get platform() {
+    return this.userService.platform.type;
+  }
+
+  get mediaGalleryInputs() {
+    if (!mediaGalleryInputs[this.platform]) return [];
+    return mediaGalleryInputs[this.platform];
   }
 
   afterFetch() {
