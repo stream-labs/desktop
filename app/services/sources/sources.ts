6400c7b99e5166f9860b8edd6ba6b5cdbe980f7d
Alexander Nazarov <kadabrik91@gmail.com>
2018-08-02 12:00:54 -0700

    Preserve previous window component (#572)

--- app/services/sources/sources.ts
+++ app/services/sources/sources.ts
@@ -449,6 +449,7 @@ export class SourcesService extends StatefulService<ISourcesState> implements IS
   showNameSource(sourceType: TSourceType, propertiesManager?: TPropertiesManager) {
     this.windowsService.showWindow({
       componentName: 'NameSource',
+      preservePrevWindow: true,
       queryParams: { sourceType, propertiesManager },
       size: {
         width: 400,
