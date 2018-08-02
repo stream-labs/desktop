wenyu826/streamlabs-obs

Render new tipjar settings

app/services/sources/sources.ts
@@ -384,7 +384,8 @@ export class SourcesService extends StatefulService<ISourcesState> implements IS
         WidgetType.DonationGoal,
         WidgetType.FollowerGoal,
         WidgetType.ChatBox,
-        WidgetType.ViewerCount
+        WidgetType.ViewerCount,
+        WidgetType.TipJar
       ];
 
       if (isWidget) {

stream-labs/desktop

app/services/sources/sources.ts
@@ -389,7 +389,8 @@ export class SourcesService extends StatefulService<ISourcesState> implements IS
         WidgetType.DonationTicker,
         WidgetType.Credits,
         WidgetType.EventList,
-        WidgetType.StreamBoss
+        WidgetType.StreamBoss,
+        WidgetType.TipJar
       ];
 
       if (isWidget) {