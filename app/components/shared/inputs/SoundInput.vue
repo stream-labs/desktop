8174d1793406aca753abfb2c4302640412742ce4
Sean Beyer <sean.beyer@streamlabs.com>
2018-08-06 10:39:33 -0700

    Build skeleton

---
+++ app/components/shared/inputs/SoundInput.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <div class="media-box radius">
+      <div class="url-uploader radius" v-if="showUrlUpload">
+        <form-group :title="$t('Image URL')" >
+          <text-input v-model="url" :metadata="{ placeholder: `${$t('Example')}: https://yoururl.com/image/Streamlabs` }" />
+        </form-group>
+        <button class="button button--action" @click="uploadUrl">{{ $t('Submit') }}</button>
+      </div>
+      <div class="footer">
+        <span>{{ fileName || 'Default' }}</span>
+        <div>
+          <i @click="clearSound" class="icon-close" />
+          <i @click="previewSound" class="fa fa-search-plus" />
+          <i @click="toggleUrlUpload" class="icon-link" />
+          <span @click="updateValue" class="change-media">{{ $t('Change Media') }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./SoundInput.vue.ts"></script>
+
+<style lang="less" scoped>
+@import "../../../styles/index";
+
+.media-box {
+  position: relative;
+  width: 100%;
+  height: 20px;
+  background-color: @day-secondary;
+}
+
+.footer {
+  display: flex;
+  position: absolute;
+  width: auto;
+  bottom: 1px;
+  left: 1px;
+  right: 1px;
+  height: 30px;
+  justify-content: space-between;
+
+  span, i {
+    position: relative;
+    top: -12px;
+  }
+
+  i {
+    margin-left: 10px;
+  }
+
+  .icon-close {
+    color: @red;
+  }
+}
+
+.change-media, i {
+  transition: 0.1s all linear;
+  &:hover {
+    cursor: pointer;
+    color: @white;
+  }
+}
+
+.url-uploader {
+  background-color: @day-primary;
+  padding: 12px;
+  position: absolute;
+  top: 1px;
+  bottom: 1px;
+  right: 1px;
+  left: 1px;
+
+  button {
+    position: absolute;
+    bottom: 25px;
+    right: 12px;
+  }
+}
+
+.change-media {
+  text-transform: uppercase;
+  color: @night-text;
+}
+
+.night-theme {
+  .media-box {
+    background-color: @night-secondary;
+    border-color: @night-secondary;
+  }
+
+  .url-uploader {
+    background-color: @night-primary;
+  }
+}
+</style>
