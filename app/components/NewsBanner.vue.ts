1dda1edb5bd57a67fe92b0f1f3a03e01cb170589
Sean Beyer <sean.beyer@streamlabs.com>
2018-08-08 17:01:30 -0700

    Add news-banner service

--- app/components/NewsBanner.vue.ts
+++ app/components/NewsBanner.vue.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue';
+import { NewsBannerService} from 'services/news-banner';
+import { Inject } from 'util/injector';
 
 export default class NewsBanner extends Vue {
-
+  @Inject() newsBannerService: NewsBannerService;
 }
---
+++ app/services/news-banner.ts
@@ -0,0 +1,9 @@
+import { StatefulService, mutation } from './stateful-service';
+import { NavigationService } from './navigation';
+import { UserService } from './user';
+import { Inject } from '../util/injector';
+import electron from 'electron';
+
+export class NewsBannerService extends StatefulService<{}> {
+
+}
