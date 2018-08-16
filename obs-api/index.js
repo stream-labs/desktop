c24999706da1d2f2bc4aec4a8b55b8cc9f5f3ab7
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2018-08-17 01:58:15 +0200

    obs-api: Fix formatting

--- obs-api/index.js
+++ obs-api/index.js
@@ -4,5 +4,5 @@ var obs = window['require']('obs-studio-node');
 
 /* Use for...in operator to perfectly mirror the osn module */
 for (let entry in obs) {
-    exports[entry] = obs[entry];
+  exports[entry] = obs[entry];
 }
