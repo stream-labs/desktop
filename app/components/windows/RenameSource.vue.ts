04a030bcec221a8c1f32fe3bd571c2dccc1c6a2d
Andy Creeth <acreeth@gmail.com>
2018-08-24 10:16:39 -0700

    remove NameSource window path for creating sources

---
+++ app/components/windows/RenameSource.vue.ts
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import { Inject } from '../../util/injector';
+import ModalLayout from '../ModalLayout.vue';
+import { WindowsService } from '../../services/windows';
+import { IScenesServiceApi } from '../../services/scenes';
+import { ISourcesServiceApi, TSourceType, TPropertiesManager } from '../../services/sources';
+import { WidgetsService, WidgetDefinitions, WidgetType } from '../../services/widgets';
+import { $t } from 'services/i18n';
+
+@Component({
+  components: { ModalLayout }
+})
+export default class RenameSource extends Vue {
+  @Inject() sourcesService: ISourcesServiceApi;
+  @Inject() scenesService: IScenesServiceApi;
+  @Inject() widgetsService: WidgetsService;
+  @Inject() windowsService: WindowsService;
+
+  options: {
+    sourceId?: string,
+  }  = this.windowsService.getChildWindowQueryParams();
+
+  name = '';
+  error = '';
+
+  disabled = false;
+
+  mounted() {
+    const source = this.sourcesService.getSource(this.options.sourceId);
+    this.name = source.name;
+  }
+
+  submit() {
+    if (!this.name) {
+      this.error = $t('The source name is required');
+    } else {
+      this.sourcesService.getSource(this.options.sourceId).setName(this.name);
+      this.windowsService.closeChildWindow();
+    }
+  }
+
+}
--- app/services/sources/sources-api.ts
+++ app/services/sources/sources-api.ts
@@ -65,8 +65,6 @@ export interface ISourcesServiceApi {
   showSourceProperties(sourceId: string): void;
   showShowcase(): void;
   showAddSource(sourceType: TSourceType): void;
-  showNameSource(sourceType: TSourceType): void;
-  showNameWidget(widgetType: WidgetType): void;
   sourceAdded: Observable<ISource>;
   sourceUpdated: Observable<ISource>;
   sourceRemoved: Observable<ISource>;
