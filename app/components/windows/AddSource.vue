ab5754df01922e8541728ef2d9c0e278df673eb0
Andy Creeth <acreeth@gmail.com>
2018-08-24 10:26:34 -0700

    remove unneeded disabled

--- app/components/windows/AddSource.vue
+++ app/components/windows/AddSource.vue
@@ -65,7 +65,6 @@
       <div class="columns small-12 buttons">
         <button
           @click="addExisting"
-          :disabled="false"
           class="button button--action">
           {{ $t('Add Existing Source') }}
         </button>
