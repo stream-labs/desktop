844ddc0b4e1e1020647c0a1d243283f35da828e4
Oat <nitsorn.oat@gmail.com>
2018-09-10 11:05:14 -0700

    refactor formgroup in mediashare to v-formgroup

--- app/components/widgets/MediaShare.vue.ts
+++ app/components/widgets/MediaShare.vue.ts
@@ -8,7 +8,7 @@ import WidgetWindow from 'components/windows/WidgetWindow.vue';
 import WidgetSettings from './WidgetSettings.vue';
 
 import { inputComponents } from './inputs';
-import FormGroup from 'components/shared/inputs/FormGroup.vue';
+import VFormGroup from 'components/shared/inputs/VFormGroup.vue';
 import CodeEditor from './CodeEditor.vue';
 
 import { $t } from 'services/i18n';
@@ -16,7 +16,7 @@ import { $t } from 'services/i18n';
 @Component({
   components: {
     WidgetWindow,
-    FormGroup,
+    VFormGroup,
     CodeEditor,
     ...inputComponents
   }
