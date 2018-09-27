b26fd56b2aaf28924c6b210a0eab7bc28a983494
Zachary Lund <zachary.lund@streamlabs.com>
2018-09-27 10:24:30 -0700

    Update electron to 3.0.1

--- main.js
+++ main.js
@@ -289,25 +289,27 @@ app.setPath('userData', path.join(app.getPath('appData'), 'slobs-client'));
 app.setAsDefaultProtocolClient('slobs');
 
 // This ensures that only one copy of our app can run at once.
-const shouldQuit = app.makeSingleInstance(argv => {
-  // Check for protocol links in the argv of the other process
-  argv.forEach(arg => {
-    if (arg.match(/^slobs:\/\//)) {
-      mainWindow.send('protocolLink', arg);
+const appLock = app.requestSingleInstanceLock();
+
+if (appLock) {
+  app.on('second-instance', (event, argv) => {
+    // Check for protocol links in the argv of the other process
+    argv.forEach(arg => {
+      if (arg.match(/^slobs:\/\//)) {
+        mainWindow.send('protocolLink', arg);
+      }
+    });
+
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) {
+        mainWindow.restore();
+      }
+
+      mainWindow.focus();
     }
   });
-
-  // Someone tried to run a second instance, we should focus our window.
-  if (mainWindow) {
-    if (mainWindow.isMinimized()) {
-      mainWindow.restore();
-    }
-
-    mainWindow.focus();
-  }
-});
-
-if (shouldQuit) {
+} else {
   app.exit();
 }
 
