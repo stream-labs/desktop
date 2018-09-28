29c0a16df19ba958a848150396501ccbd4c1c75f
Mike <sir@stendec.me>
2018-09-28 13:36:06 -0400

    FrankerFaceZ Settings Support (#860)
    * Add a new window for FrankerFaceZ settings.
    
    * Add a button to Appearance Settings for opening the new FFZ Settings window, visible when FFZ is enabled.
    
    * Add a hook to the new-window handler for chat so that, when FFZ attempts to open its settings in a pop-out window, we can intercept that and show the new FFZ Settings window.
    
    * Sanity check the results of url.parse to make sure the new-window handler doesn't error in the FFZ Settings code.
    
    * Remove done since I ended up not using child window for the FFZ Settings window.
    
    * Omit the type from two booleans with default values.

---
+++ app/components/windows/FFZSettings.vue
@@ -0,0 +1,21 @@
+<template>
+<modal-layout :show-controls="false">
+  <div slot="content" class="ffz-settings">
+    <webview
+      class="ffz-settings-container"
+      id="ffz-settings"
+      ref="ffzSettings"
+      :src="popoutURL"
+      />
+  </div>
+</modal-layout>
+</template>
+
+<script lang="ts" src="./FFZSettings.vue.ts"></script>
+
+<style lang="less">
+.ffz-settings-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
