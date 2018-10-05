61b2f9927933b3f70f237b04af9d8465460d9058
Oat <nitsorn.oat@gmail.com>
2018-10-05 09:54:31 -0700

    removed unrequested apis

--- app/services/platform-apps/api/modules/notifications.ts
+++ app/services/platform-apps/api/modules/notifications.ts
@@ -65,10 +65,6 @@ export class NotificationsModule extends Module {
   constructor() {
     super();
 
-    this.notificationsService.notificationPushed.subscribe(notification => {
-      this.notificationPushed.next(notification);
-    });
-
     this.notificationsService.notificationRead.subscribe(ids => {
       this.notificationRead.next(ids);
     });
@@ -76,9 +72,6 @@ export class NotificationsModule extends Module {
 
   moduleName = 'Notifications';
   permissions = [EApiPermissions.Notifications];
-
-  @apiEvent()
-  notificationPushed = new Subject<INotification>();
 
   @apiEvent()
   notificationRead = new Subject<number[]>();
@@ -89,57 +82,7 @@ export class NotificationsModule extends Module {
   }
 
   @apiMethod()
-  getNotification(ctx: IApiContext, id: number): INotification {
-    return this.notificationsService.getNotification(id);
-  }
-
-  @apiMethod()
-  getAll(ctx: IApiContext, type?: ENotificationType): INotification[] {
-    return this.notificationsService.getAll(type);
-  }
-
-  @apiMethod()
-  getUnread(ctx: IApiContext, type?: ENotificationType): INotification[] {
-    return this.notificationsService.getUnread(type);
-  }
-
-  @apiMethod()
-  getRead(ctx: IApiContext, type?: ENotificationType): INotification[] {
-    return this.notificationsService.getRead(type);
-  }
-
-  @apiMethod()
-  getSettings(): INotificationsSettings {
-    return this.notificationsService.getSettings();
-  }
-
-  @apiMethod()
-  setSettings(ctx: IApiContext, patch: Partial<INotificationsSettings>): void {
-    return this.notificationsService.setSettings(patch);
-  }
-
-  @apiMethod()
-  restoreDefaultSettings(): void {
-    return this.notificationsService.restoreDefaultSettings();
-  }
-
-  @apiMethod()
   markAsRead(ctx: IApiContext, id: number): void {
     return this.notificationsService.markAsRead(id);
-  }
-
-  @apiMethod()
-  markAllAsRead(): void {
-    return this.notificationsService.markAllAsRead();
-  }
-
-  @apiMethod()
-  applyAction(ctx: IApiContext, notificationId: number): void {
-    return this.notificationsService.applyAction(notificationId);
-  }
-
-  @apiMethod()
-  showNotifications(): void {
-    return this.notificationsService.showNotifications();
   }
 }
