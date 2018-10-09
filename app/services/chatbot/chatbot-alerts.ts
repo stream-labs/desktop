e4d7c35089362640ebf6abf7d497e09cea3e1f4f
Oat <nitsorn.oat@gmail.com>
2018-10-09 15:46:03 -0700

    wip

--- app/services/chatbot/chatbot-alerts.ts
+++ app/services/chatbot/chatbot-alerts.ts
@@ -1,175 +1,29 @@
 import Vue from 'vue';
 import { PersistentStatefulService } from '../persistent-stateful-service';
-import { UserService } from 'services/user';
 import { Inject } from 'util/injector';
-import { handleErrors, authorizedHeaders } from 'util/requests';
 import { mutation } from '../stateful-service';
-import { ChatbotCommonService } from './chatbot-common';
-import {
-  MediaShareService,
-  IMediaShareData,
-  IMediaShareBan
-} from 'services/widget-settings/media-share';
-import io from 'socket.io-client';
+import { ChatbotBaseApiService } from './chatbot-base';
 
 import {
-  IChatbotAuthResponse,
-  IChatbotErrorResponse,
-  IChatbotStatusResponse,
-  ChatbotClients,
-  ICustomCommand,
-  IDefaultCommand,
-  IChatbotTimer,
-  IDafaultCommandsResponse,
-  ICustomCommandsResponse,
-  ITimersResponse,
   IChatbotAPIPostResponse,
-  IChatbotAPIPutResponse,
-  IChatbotAPIDeleteResponse,
-  ICommandVariablesResponse,
   IChatAlertsResponse,
-  ICapsProtectionResponse,
-  ISymbolProtectionResponse,
-  ILinkProtectionResponse,
-  IWordProtectionResponse,
-  IQuotesResponse,
-  ChatbotSettingSlug,
-  IQuote,
-  IQuotePreferencesResponse,
-  IQueuePreferencesResponse,
-  IQueueStateResponse,
-  IQueueEntriesResponse,
-  IQueuePickedResponse,
-  IChatbotSocketAuthResponse,
-  ChatbotSocketRoom,
-  ISongRequestPreferencesResponse,
-  ISongRequestResponse
 } from './chatbot-interfaces';
 
 // state
 interface IChatbotAlertsApiServiceState {
-  // v1
-  apiToken: string;
-  socketToken: string;
-  globallyEnabled: boolean;
-  defaultCommandsResponse: IDafaultCommandsResponse;
-  customCommandsResponse: ICustomCommandsResponse;
-  timersResponse: ITimersResponse;
-  commandVariablesResponse: ICommandVariablesResponse;
   chatAlertsResponse: IChatAlertsResponse;
-  capsProtectionResponse: ICapsProtectionResponse;
-  symbolProtectionResponse: ISymbolProtectionResponse;
-  linkProtectionResponse: ILinkProtectionResponse;
-  wordProtectionResponse: IWordProtectionResponse;
-
-  // v2
-  quotesResponse: IQuotesResponse;
-  quotePreferencesResponse: IQuotePreferencesResponse;
-  queuePreferencesResponse: IQueuePreferencesResponse;
-  queueStateResponse: IQueueStateResponse;
-  queueEntriesResponse: IQueueEntriesResponse;
-  queuePickedResponse: IQueuePickedResponse;
-  songRequestPreferencesResponse: ISongRequestPreferencesResponse;
-  songRequestResponse: ISongRequestResponse;
 }
 
-export class ChatbotAlertsApiService extends PersistentStatefulService<
-  IChatbotAlertsApiServiceState
-> {
-  @Inject() userService: UserService;
-  @Inject() chatbotCommonService: ChatbotCommonService;
-  @Inject() mediaShareService: MediaShareService;
-
-  apiUrl = 'https://chatbot-api.streamlabs.com/';
-  socketUrl = 'https://chatbot-io.streamlabs.com';
-  version = 'api/v1/';
+export class ChatbotAlertsApiService extends PersistentStatefulService<IChatbotAlertsApiServiceState> {
+  @Inject() chatbotBaseApiService: ChatbotBaseApiService;
+  api = this.chatbotBaseApiService.api;
 
   static defaultState: IChatbotAlertsApiServiceState = {
-    apiToken: null,
-    socketToken: null,
-    globallyEnabled: false,
-    defaultCommandsResponse: {
-      commands: {},
-      'link-protection': {},
-      giveaway: {}
-    },
-    customCommandsResponse: {
-      pagination: {
-        current: 1,
-        total: 1
-      },
-      data: []
-    },
-    commandVariablesResponse: [],
-    timersResponse: {
-      pagination: {
-        current: 1,
-        total: 1
-      },
-      data: []
-    },
     chatAlertsResponse: {
       enabled: false,
       settings: null
     },
-    capsProtectionResponse: {
-      enabled: false,
-      settings: null
-    },
-    symbolProtectionResponse: {
-      enabled: false,
-      settings: null
-    },
-    linkProtectionResponse: {
-      enabled: false,
-      settings: null
-    },
-    wordProtectionResponse: {
-      enabled: false,
-      settings: null
-    },
-    quotesResponse: {
-      pagination: {
-        current: 1,
-        total: 1
-      },
-      data: []
-    },
-    quotePreferencesResponse: {
-      enabled: false,
-      settings: null
-    },
-    queuePreferencesResponse: {
-      enabled: false,
-      settings: null
-    },
-    queueStateResponse: {
-      status: 'Closed'
-    },
-    queueEntriesResponse: {
-      pagination: {
-        current: 1,
-        total: 1
-      },
-      data: []
-    },
-    queuePickedResponse: {
-      pagination: {
-        current: 1,
-        total: 1
-      },
-      data: []
-    },
-    songRequestPreferencesResponse: {
-      banned_media: [],
-      settings: null
-    },
-    songRequestResponse: {
-      enabled: false,
-      settings: null
-    }
   };
-
 
   //
   // GET requests
@@ -196,7 +50,6 @@ export class ChatbotAlertsApiService extends PersistentStatefulService<
   //
   // Mutations
   //
-
   @mutation()
   private UPDATE_CHAT_ALERTS(response: IChatAlertsResponse) {
     Vue.set(this.state, 'chatAlertsResponse', response);
