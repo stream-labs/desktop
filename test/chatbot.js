1cf4ae99a1352d9c883c557c285074097f011f3f
Oat <nitsorn.oat@gmail.com>
2018-10-12 15:16:04 -0700

    wip

--- test/chatbot.js
+++ test/chatbot.js
@@ -10,16 +10,23 @@ import { sleep } from './helpers/sleep';
 
 useSpectron();
 
+// test('Log into chatbot', async t => {
+//   // t.true(await logIn(t));
+//   // const app = t.context.app;
+//   const apiClient = await getClient();
+//   const navigationService = apiClient.getResource('NavigationService');
+//   t.true(await navigationService.navigate('Dashboard'));
+//   // await app.client.click('.nav-item.Commands');
+//   // await app.client.click('.nav-item.Custom_Commands');
+//   // await app.client.click('.button--add-command');
+//   // await navigateToCustomCommandsTab(t);
+//   // await openCustomCommandWindow(t);
+//   // t.true();
+// });
+
 test('Log into chatbot', async t => {
-  // t.true(await logIn(t));
-  // const app = t.context.app;
-  const apiClient = await getClient();
-  const navigationService = apiClient.getResource('NavigationService');
-  t.true(await navigationService.navigate('Dashboard'));
-  // await app.client.click('.nav-item.Commands');
-  // await app.client.click('.nav-item.Custom_Commands');
-  // await app.client.click('.button--add-command');
-  // await navigateToCustomCommandsTab(t);
-  // await openCustomCommandWindow(t);
-  // t.true();
+  const client = await getClient();
+  await logIn(t);
+  const app = t.context.app;
+  const navigationService = client.getResource('NavigationService');
 });
