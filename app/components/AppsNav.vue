4c1a79640525d123cac91de33262cc784d83916a
Oat <nitsorn.oat@gmail.com>
2018-10-13 15:57:51 -0700

    finished

---
+++ app/components/AppsNav.vue
@@ -0,0 +1,120 @@
+<template>
+<div>
+  <div class="apps-nav">
+    <div
+      v-if="hasPrev"
+      @click="scrollLeft"
+      class="apps-nav-control flex has-prev">
+      <i class="icon-down icon-left"></i>
+      <span>...</span>
+    </div>
+    <div
+      ref="app_tabs"
+      @scroll="calculateScrolls"
+      class="apps-tab__container"
+      :class="{
+        'has-next': hasNext,
+        'has-prev': hasPrev
+      }"
+    >
+      <span
+        v-for="(app, index) in topNavApps"
+        :key="index"
+        @click="navigateApp(app.id)"
+        class="app-tab"
+        :class="{ 'is-active': page === 'PlatformAppContainer' && navigationService.state.params.appId === app.id }">
+        <span> {{index}}-{{ app.manifest.name }}</span>
+      </span>
+    </div>
+    <div
+      v-if="hasNext"
+      @click="scrollRight"
+      class="apps-nav-control flex has-next">
+      <span>...</span>
+      <i class="icon-down icon-right"></i>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts" src="./AppsNav.vue.ts"></script>
+
+<style lang="less" scoped>
+@import "../styles/index";
+
+.apps-nav {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  .padding-h-sides();
+  position: relative;
+  max-width:  none;
+  background-color: @day-secondary;
+  border-bottom: 1px solid @day-border;
+  flex: 0 0 35px;
+  z-index: 1;
+}
+
+.apps-tab__container {
+  display: inline-block;
+  overflow-x: auto;
+  white-space: nowrap;
+  overflow-y: hidden;
+  margin-bottom: -4px;
+
+  &.has-prev {
+    margin-left: 15px;
+  }
+  &.has-next {
+    margin-right: 15px;
+  }
+
+}
+
+.apps-nav-control {
+  cursor: pointer;
+
+  &.has-prev {
+    margin-left: 8px;
+     i {
+      margin-right: 5px;
+    }
+  }
+  &.has-next {
+    margin-right: 8px;
+     i {
+      margin-left: 5px;
+    }
+  }
+}
+
+.apps-tab__container::-webkit-scrollbar {
+  display: none;
+  width: 0;
+  height: 0;
+}
+
+
+.app-tab {
+  padding: 8px 10px;
+  opacity: 0.8;
+  font-weight: bold;
+  cursor: pointer;
+  // width: 150px;
+  overflow:  hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  display: inline-block;
+
+  &.is-active {
+    opacity: 1;
+  }
+}
+
+.night-theme {
+  .apps-nav {
+    background-color: @night-primary;
+    border-color: @night-border;
+  }
+}
+</style>
---
+++ app/components/AppsNav.vue.ts
@@ -0,0 +1,89 @@
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+import { Inject } from 'util/injector';
+import { NavigationService } from 'services/navigation';
+import { PlatformAppsService, EAppPageSlot } from 'services/platform-apps';
+
+@Component({})
+export default class AppsNav extends Vue {
+  @Inject() platformAppsService: PlatformAppsService;
+  @Inject() navigationService: NavigationService;
+
+  $refs: {
+    app_tabs: HTMLDivElement
+  }
+
+  isMounted = false;
+
+  appTabsContainer: HTMLDivElement = null;
+  canScroll = false;
+  hasNext = false;
+  hasPrev = false;
+
+  private scrollIncrement = 100;
+
+  created() {
+    window.addEventListener('resize', this.calculateScrolls);
+  }
+
+  destroyed() {
+    window.removeEventListener('resize', this.calculateScrolls);
+  }
+
+  mounted() {
+    this.isMounted = true;
+    this.appTabsContainer = this.$refs.app_tabs;
+    this.calculateScrolls();
+  }
+
+  scrollLeft() {
+    this.appTabsContainer.scrollLeft = this.appTabsContainer.scrollLeft - this.scrollIncrement;
+  }
+
+  scrollRight() {
+    this.appTabsContainer.scrollLeft = this.appTabsContainer.scrollLeft + this.scrollIncrement;
+  }
+
+  calculateScrolls() {
+    if (!this.isMounted) return false;
+    this.canScroll = this.appTabsContainer.scrollWidth > this.appTabsContainer.clientWidth;
+    this.hasPrev = this.appTabsContainer.scrollLeft > 0;
+    let scrollRight = this.appTabsContainer.scrollWidth -
+      (this.appTabsContainer.scrollLeft + this.appTabsContainer.clientWidth);
+
+    this.hasNext = scrollRight > 0;
+  }
+
+  get topNavApps() {
+    let x: any[] = [];
+    let i = 10;
+
+    while (i > 0) {
+      x = x.concat(this.platformAppsService.enabledApps.filter(app => {
+        return !!app.manifest.pages.find(page => {
+          return page.slot === EAppPageSlot.TopNav;
+        });
+      }));
+      i--;
+    }
+
+    return x;
+  }
+
+  // get topNavApps() {
+  //   return this.platformAppsService.enabledApps.filter(app => {
+  //     return !!app.manifest.pages.find(page => {
+  //       return page.slot === EAppPageSlot.TopNav;
+  //     });
+  //   });
+  // }
+
+  get page() {
+    return this.navigationService.state.currentPage;
+  }
+
+  navigateApp(appId: string) {
+    this.navigationService.navigate('PlatformAppContainer', { appId });
+  }
+
+}
