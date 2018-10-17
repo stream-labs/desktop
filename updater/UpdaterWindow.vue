786a0eb320e5de0a64634658414a994c7856b1df
Zachary Lund <zachary.lund@streamlabs.com>
2018-10-17 13:17:05 -0700

    Add bootstrap for new updater (#919)
    * Add bootstrap for new updater
    
    * Update package.json and yarn.lock for bootstrap
    
    * Remove progress bar from status window to reduce complication

--- updater/UpdaterWindow.vue
+++ updater/UpdaterWindow.vue
@@ -4,30 +4,6 @@
     <i class="UpdaterWindow-icon fas fa-sync-alt fa-spin"/>
     {{ message }}
   </div>
-
-  <div v-if="(percentComplete !== null) && !installing && !error">
-    <div class="UpdaterWindow-progressPercent">
-      {{ percentComplete }}% complete
-    </div>
-    <div class="UpdaterWindow-progressBarContainer">
-      <div
-        class="UpdaterWindow-progressBar"
-        :style="{ width: percentComplete + '%' }"/>
-    </div>
-  </div>
-
-  <div class="UpdaterWindow-issues" v-if="installing">
-    This may take a few minutes.  If you are having issues updating, please
-    <span class="UpdaterWindow-link" @click="download">
-      download a fresh installer.
-    </span>
-  </div>
-  <div class="UpdaterWindow-issues" v-if="error">
-    There was an error updating. Please
-    <span class="UpdaterWindow-link" @click="download">
-      download a fresh installer.
-    </span>
-  </div>
 </div>
 </template>
 
@@ -35,47 +11,11 @@
 const { remote, ipcRenderer } = window.require('electron');
 
 export default {
-
   data() {
     return {
       message: 'Checking for updates',
-      installing: false,
-      error: false,
-      percentComplete: null
     };
-  },
-
-  mounted() {
-    ipcRenderer.on('autoUpdate-pushState', (event, data) => {
-      if (data.version) {
-        this.message = `Downloading version ${data.version}`;
-      }
-
-      if (data.percent) {
-        this.percentComplete = Math.floor(data.percent);
-      }
-
-      if (data.installing) {
-        this.installing = true;
-        this.message = `Installing version ${data.version}`;
-      }
-
-      if (data.error) {
-        this.error = true;
-        this.message = 'Something went wrong';
-      }
-    });
-
-    ipcRenderer.send('autoUpdate-getState');
-  },
-
-  methods: {
-    download() {
-      remote.shell.openExternal('https://streamlabs.com/streamlabs-obs');
-      remote.app.quit();
-    }
   }
-
 };
 </script>
 
@@ -96,34 +36,6 @@ export default {
 
 .UpdaterWindow-icon {
   margin-right: 8px;
-}
-
-.UpdaterWindow-progressBarContainer {
-  position: relative;
-  margin-top: 40px;
-  background-color: #09161D;
-  border-radius: 4px;
-  overflow: hidden;
-}
-
-.UpdaterWindow-progressBar {
-  height: 8px;
-  background-color: #31c3a2;
-}
-
-.UpdaterWindow-progressPercent {
-  width: 100%;
-  text-align: center;
-  color: #BDC2C4;
-  font-size: 14px;
-  margin: 16px 0 24px 0;
-}
-
-.UpdaterWindow-issues {
-  font-size: 13px;
-  font-weight: 300;
-  padding-top: 25px;
-  -webkit-app-region: no-drag;
 }
 
 .UpdaterWindow-link {
