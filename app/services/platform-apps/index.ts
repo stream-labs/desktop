5a464bb9617833714cb5259f2df528fc014930f6
Andy Creeth <acreeth@gmail.com>
2018-10-22 14:58:48 -0700

    remove extra whitespace

--- app/services/platform-apps/index.ts
+++ app/services/platform-apps/index.ts
@@ -485,9 +485,6 @@ export class PlatformAppsService extends
 
   exposeAppApi(appId: string, webContentsId: number) {
     const app = this.getApp(appId);
-
-
-
     const api = this.apiManager.getApi(app);
 
     // Namespace under v1 for now.  Eventually we may want to add
