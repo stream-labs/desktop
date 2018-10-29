09fdc76dbb07581b7db008342f97acdd871040b7
Andy Creeth <acreeth@gmail.com>
2018-10-29 15:30:21 -0700

    remove old logic

--- app/components/TopNav.vue.ts
+++ app/components/TopNav.vue.ts
@@ -116,9 +116,6 @@ export default class TopNav extends Vue {
   }
 
   get appStoreVisible() {
-    return (this.platformAppsService.state.storeVisible && this.isUserLoggedIn) ||
-      this.featureIsEnabled(this.availableFeatures.platform);
-
     return (this.platformAppsService.state.storeVisible
       || this.featureIsEnabled(this.availableFeatures.platform))
       && this.userService.isLoggedIn()
