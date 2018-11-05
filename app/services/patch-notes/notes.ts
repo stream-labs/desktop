7a0a77b7f8b0db3f616ecdc8d0cb839cb332d886
Andy Creeth <acreeth@gmail.com>
2018-11-05 08:24:45 -0800

    patch notes 0.11.2

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,19 +1,16 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.11.1',
-  title: 'New Widget Settings, Bug Fixes 🔧',
+  version: '0.11.2',
+  title: 'Bug Fixes 🔧',
   showChest: false,
   notes: [
-    'Improved the design of widget settings windows',
-    'Fixed stream settings not correctly saving when unicode is present in the username',
-    'Fixed bitrate/settings visually not changing when going live',
-    'Fixed optimized game encoder not being detected in certain cases ',
-    'Fixed a potential crash when ending the stream',
-    'Fixed adding new transition properties window when changing transition type',
-    'Fixed scene collections failing to load and being erased in some cases',
-    'Fixed projectors showing a titlebar when in fullscreen',
-    'Fixed an issue where you could not duplicate a scene',
-    'Improve overall application responsiveness (most notably in loading scene collections)'
+    'Fixed as issue that would sometimes result in a "Failed to load scene collection" message',
+    'Fixed visual flickering in volmeters',
+    'Fixed a source of crashes on Windows 7',
+    'Fixed various input bugs in widget settings',
+    'Added hotkey support for media hotkeys',
+    'Fixed an issue that sometimes caused projectors to pop up as a white screen',
+    'Fixed a bug that would cause sources to move while they are being cropped'
   ]
 };
