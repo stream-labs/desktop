7cea0c4c6b32b03a2093b12b1a2a6c2ec767ea61
Adrian Perez <adrian@adrianperez.org>
2018-11-14 14:16:15 -0800

    feat(init): display error message & exit if OBS API fails to init

---
+++ test/services/app/app.ts
@@ -0,0 +1,15 @@
+import test from 'ava';
+import { EVideoCodes } from 'obs-studio-node';
+import { apiInitErrorResultToMessage as rtm } from '../../../app/services/app';
+
+test('returns user-friendly error for ModuleNotFound status', t => {
+  t.regex(rtm(EVideoCodes.ModuleNotFound), /DirectX could not be found/);
+});
+
+test('returns user-friendly error for NotSupported status', t => {
+  t.regex(rtm(EVideoCodes.NotSupported), /Failed to initialize OBS/);
+});
+
+test('returns generic error string for Fail, InvalidParam and CurrentlyActive results', t => {
+  t.regex(rtm(EVideoCodes.Fail), /unknown error/);
+});
