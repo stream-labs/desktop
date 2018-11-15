b9aefa9bf56406b25a7e813faf4cc8e6a3d7efed
Alexander Nazarov <kadabrik91@gmail.com>
2018-11-15 10:22:25 -0800

    Fix app deloading (#1014)

--- app/services/app/app.ts
+++ app/services/app/app.ts
@@ -148,17 +148,17 @@ export class AppService extends StatefulService<IAppState> {
     this.tcpServerService.stopListening();
 
     window.setTimeout(async () => {
-      obs.NodeObs.OBS_Volmeter_ReleaseVolmeters();
-      obs.NodeObs.OBS_Fader_ReleaseFaders();
       await this.sceneCollectionsService.deinitialize();
       this.performanceMonitorService.stop();
       this.transitionsService.shutdown();
       this.windowsService.closeAllOneOffs();
       await this.fileManagerService.flushAll();
-      this.crashReporterService.endShutdown();
+      obs.NodeObs.OBS_Volmeter_ReleaseVolmeters();
+      obs.NodeObs.OBS_Fader_ReleaseFaders();
       obs.NodeObs.OBS_service_removeCallback();
       obs.NodeObs.OBS_API_destroyOBS_API();
       obs.IPC.disconnect();
+      this.crashReporterService.endShutdown();
       electron.ipcRenderer.send('shutdownComplete');
     }, 300);
   }
