76df02459c5b69381cf534788198163d33ab93c7
Andy Creeth <acreeth@gmail.com>
2018-11-29 10:26:05 -0800

    patch notes 0.11.4

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,16 +1,22 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.11.2',
-  title: 'Bug Fixes 🔧',
+  version: '0.11.4',
+  title: 'Bug Fixes and Stability',
   showChest: false,
   notes: [
-    'Fixed an issue that would sometimes result in a "Failed to load scene collection" message',
-    'Fixed visual flickering in volmeters',
-    'Fixed a source of crashes on Windows 7',
-    'Fixed various input bugs in widget settings',
-    'Added hotkey support for media hotkeys',
-    'Fixed an issue that sometimes caused projectors to pop up as a white screen',
-    'Fixed a bug that would cause sources to move while they are being cropped'
+    'Added Alert box settings directly integrated into source properties',
+    'Crash-handler: Added a new module that will monitor the application processes ' +
+      'in order to have the possibility to not end the stream when certain crashes occur',
+    'Fixed some memory leaks',
+    'Fixed an issue where recording quality was not correctly applied in some cases',
+    'Added "Same as stream" option in advanced output recording settings',
+    'Fixed importing advanced audio settings from OBS',
+    'Fixed empty server list which switching from a service type to another',
+    'Fixed some application crashes on shutdown',
+    'Fixed a potential source of crashes on startup',
+    'Updated the browser source to utilize hardware acceleration',
+    'Fixed the notification system reporting wrong values for skipped and lagged frames values in some cases',
+    'Fixed an issue where streaming would not start in some cases'
   ]
 };
