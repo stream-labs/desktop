54e2358a0a4f99e1b38c82732e16e2274a4f440c
Sean Beyer <sean.beyer@streamlabs.com>
2018-12-11 12:43:48 -0800

    Show events depending on platform

--- app/components/widgets/EventList.vue.ts
+++ app/components/widgets/EventList.vue.ts
@@ -11,6 +11,29 @@ import { AnimationInput } from './inputs';
 import VFormGroup from 'components/shared/inputs/VFormGroup.vue';
 import { $t } from 'services/i18n';
 import ValidatedForm from 'components/shared/inputs/ValidatedForm.vue';
+import { Inject } from 'util/injector';
+import { UserService } from 'services/user';
+
+const eventsByPlatform = (platform: string) => ({
+  twitch: [
+    { key: 'show_follows', title: $t('Follows') },
+    { key: 'show_subscriptions', title: $t('Subscriptions') },
+    { key: 'show_resubs', title: $t('Show Resubs') },
+    { key: 'show_sub_tiers', title: $t('Show Sub Tiers') },
+    { key: 'show_hosts', title: $t('Hosts') },
+    { key: 'show_bits', title: $t('Bits') },
+    { key: 'show_raids', title: $t('Raids') }
+  ],
+  facebook: [],
+  youtube: [
+    { key: 'show_subscriptions', title: $t('Subscriptions') },
+    { key: 'show_sponsors', title: $t('Members') },
+    { key: 'show_fanfundings', title: $t('Super Chats') }
+  ],
+  mixer: [
+    { key: 'show_resubs', title: $t('Show Resubs') },
+  ]
+}[platform]);
 
 @Component({
   components: {
@@ -22,11 +45,26 @@ import ValidatedForm from 'components/shared/inputs/ValidatedForm.vue';
   }
 })
 export default class EventList extends WidgetSettings<IEventListData, EventListService> {
+  @Inject() userService: UserService;
+
   get themeMetadata() {
     return Object.keys(this.wData.themes).map((theme) => ({
       title: this.wData.themes[theme].label,
       value: theme
     }));
+  }
+
+  get eventsForPlatform() {
+    const baseEvents = [{ key: 'show_donations', title: $t('Donations') }, { key: 'show_merch', title: $t('Merch') }];
+    return eventsByPlatform(this.userService.platform.type).concat(baseEvents);
+  }
+
+  get isTwitch() {
+    return this.userService.platform.type === 'twitch';
+  }
+
+  get isMixer() {
+    return this.userService.platform.type === 'mixer';
   }
 
   textColorTooltip = $t('A hex code for the base text color.');
