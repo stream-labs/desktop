5eab1a148a887d09ef13200abe0e72c876b1dc08
Sean Beyer <sean.beyer@streamlabs.com>
2018-12-14 15:30:45 -0800

    Clarify scheduling messaging
    Also style links to distinguish them from copy

--- app/components/windows/EditStreamInfo.vue.ts
+++ app/components/windows/EditStreamInfo.vue.ts
@@ -366,7 +366,8 @@ export default class EditStreamInfo extends Vue {
       title: $t('Scheduled Date'),
       dateFormat: 'MM/DD/YYYY',
       placeholder:'MM/DD/YYYY',
-      description: this.isFacebook ? $t('Please schedule no further than 7 days in advance.') : undefined
+      description: this.isFacebook ?
+        $t('Please schedule no further than 7 days in advance and no sooner than 10 minutes in advance.') : undefined
     };
   }
 
