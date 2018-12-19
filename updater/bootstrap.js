bef7cb235a57bc50972e4b7cc5607352587f3cd3
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2018-12-19 12:52:27 -0800

    Handle the case when spawing the updater results in an error (#1184)

--- updater/bootstrap.js
+++ updater/bootstrap.js
@@ -304,6 +304,11 @@ async function entry(info) {
         shell: true
     });
 
+    cp.on('error', (error) => {
+        console.log('Error spawning');
+        console.log('Error : ' + error)
+    });
+
     return true;
 }
 
