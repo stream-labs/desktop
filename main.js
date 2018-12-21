14bbce89f29ad2bf4ed9c4ccef9c2f1c8d0c0e14
Andy Creeth <acreeth@gmail.com>
2018-12-21 10:31:36 -0800

    disable vue dev tools (#1192)

--- main.js
+++ main.js
@@ -285,8 +285,8 @@ function startApp() {
     // interference with certain NodeJS APIs, expecially asynchronous
     // IO from the renderer process.  Enable at your own risk.
 
-    const devtoolsInstaller = require('electron-devtools-installer');
-    devtoolsInstaller.default(devtoolsInstaller.VUEJS_DEVTOOLS);
+    // const devtoolsInstaller = require('electron-devtools-installer');
+    // devtoolsInstaller.default(devtoolsInstaller.VUEJS_DEVTOOLS);
 
     // setTimeout(() => {
       //openDevTools();
