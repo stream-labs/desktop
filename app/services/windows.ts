828b69aeab51ac11ffc74ca53624660d0bfa68e6
Adrian Perez <adrian@adrianperez.org>
2019-01-07 15:11:22 -0800

    fix(windows): avoid creating window bigger than current display res
    Override window size to avoid creating a window bigger than the current display.

--- app/services/windows.ts
+++ app/services/windows.ts
@@ -203,6 +203,22 @@ export class WindowsService extends StatefulService<IWindowsState> {
     // This prevents "snapping" behavior when navigating settings
     if (options.componentName !== this.state.child.componentName) options.center = true;
 
+    // Override `options.size` when what is passed in is bigger than the current display
+    if (options.size) {
+      const { width: screenWidth, height: screenHeight } = electron.screen.getDisplayMatching(
+        this.windows.main.getBounds(),
+      ).workAreaSize;
+
+      const SCREEN_PERCENT = 0.75;
+
+      if (options.size.width > screenWidth || options.size.height > screenHeight) {
+        options.size = {
+          width: screenWidth * SCREEN_PERCENT,
+          height: screenHeight * SCREEN_PERCENT,
+        };
+      }
+    }
+
     ipcRenderer.send('window-showChildWindow', options);
     this.updateChildWindowOptions(options);
   }
