dc42666419fb3ab3151811d33a03df9dbf28e97b
Sean Beyer <sean.beyer@streamlabs.com>
2019-01-07 15:55:17 -0800

    Migrate ObsBoolInput to base inputs

--- app/components/obs/inputs/ObsBoolInput.vue.ts
+++ app/components/obs/inputs/ObsBoolInput.vue.ts
@@ -1,12 +1,24 @@
 import { Component, Prop } from 'vue-property-decorator';
 import { ObsInput, IObsInput, TObsType } from './ObsInput';
+import VFormGroup from '../../shared/inputs/VFormGroup.vue';
+import { metadata } from '../../shared/inputs';
+import { BoolInput } from '../../shared/inputs/inputs';
 
-@Component
+@Component({
+  components: { VFormGroup, BoolInput },
+})
 class ObsBoolInput extends ObsInput<IObsInput<boolean>> {
   static obsType: TObsType;
 
   @Prop()
   value: IObsInput<boolean>;
+
+  get metadata() {
+    return metadata.bool({
+      title: this.value.showDescription !== false ? this.value.description : null,
+      disabled: !this.value.enabled,
+    });
+  }
 
   handleClick() {
     if (!this.value.enabled) return;
