2be7fd79c197da80dde9798c0b051dec0cf779ae
Andy Creeth <acreeth@gmail.com>
2019-01-09 14:09:48 -0800

    ez rollout script

---
+++ bin/set-rollout.js
@@ -0,0 +1,92 @@
+const inq = require('inquirer');
+const colors = require('colors/safe');
+const sh = require('shelljs');
+const https = require('https');
+const cp = require('child_process');
+const path = require('path');
+
+function info(msg) {
+  sh.echo(colors.magenta(msg));
+}
+
+function important(msg) {
+  sh.echo(colors.cyan(msg));
+}
+
+function error(msg) {
+  sh.echo(colors.red(`ERROR: ${msg}`));
+}
+
+async function setRollout(version, rollout, bucket) {
+  const proc = cp.fork(
+    path.resolve(__dirname, 'aws-wrapper.js'),
+    [
+      path.resolve(__dirname, 'set-chance.js'),
+      '--s3-bucket', bucket,
+      '--version', version,
+      '--chance', rollout
+    ]
+  );
+  await new Promise(resolve => {
+    proc.on('exit', resolve);
+  });
+}
+
+(async () => {
+  info('|-------------------------------------------|');
+  info('| Streamlabs OBS Interactive Rollout Script |');
+  info('|-------------------------------------------|');
+
+  const channel = (await inq.prompt({
+    type: 'list',
+    name: 'channel',
+    message: 'Which channel would you like to modify?',
+    choices: [
+      {
+        name: 'preview',
+        value: 'preview'
+      },
+      {
+        name: 'live',
+        value: 'latest'
+      }
+    ]
+  })).channel;
+
+  const version = await new Promise((resolve, reject) => {
+    https.get(`https://slobs-cdn.streamlabs.com/${channel}.json`, res => {
+      res.on('data', body => {
+        resolve(JSON.parse(body).version);
+      });
+    });
+  });
+
+  important(`The latest version is ${version}`);
+
+  const rollout = await new Promise((resolve, reject) => {
+    https.get(`https://slobs-cdn.streamlabs.com/${version}.chance`, res => {
+      res.on('data', body => {
+        resolve(JSON.parse(body).chance);
+      });
+    });
+  });
+
+  important(`${version} is at ${rollout}% rollout`);
+  info('Rollout can be raised or lowered.');
+  info('Lowering rollout to 0 will stop propagation of the update');
+
+  const newRollout = (await inq.prompt({
+    type: 'input',
+    name: 'newRollout',
+    message: `Please enter the new rollout percentage (0 - 100)`,
+    filter: ans => parseInt(ans, 10)
+  })).newRollout;
+
+  info(`Setting ${newRollout}% rollout for version ${version}...`);
+
+  await setRollout(version, newRollout, 'streamlabs-obs');
+  await setRollout(version, newRollout, 'slobs-cdn.streamlabs.com');
+
+})().then(() => {
+  sh.exit(0);
+});
