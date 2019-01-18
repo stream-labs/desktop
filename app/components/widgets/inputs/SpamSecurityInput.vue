8759e9fb581b7186d169a43324279189203d8a91
Sean Beyer <sean.beyer@streamlabs.com>
2019-01-18 14:33:17 -0800

    Pass function in first-class

--- app/components/widgets/inputs/SpamSecurityInput.vue
+++ app/components/widgets/inputs/SpamSecurityInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <slider-input :metadata="spamMetadata" :value="localValue" @input="value => updateLocalValue(value)" />
+  <slider-input :metadata="spamMetadata" :value="localValue" @input="updateLocalValue" />
 </template>
 
 <script lang="ts" src="./SpamSecurityInput.vue.ts"></script>
