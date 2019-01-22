b89313349ffa4aa9cff0f5b141cfa458a22b311a
Sean Beyer <sean.beyer@streamlabs.com>
2019-01-22 12:24:17 -0800

    Fix list input errors (#1272)

--- app/components/obs/inputs/ObsListInput.vue.ts
+++ app/components/obs/inputs/ObsListInput.vue.ts
@@ -24,8 +24,7 @@ class ObsListInput extends ObsInput<IObsListInput<TObsValue>> {
   @Prop({ default: false })
   loading: boolean;
 
-  onInputHandler(option: IObsListOption<string> | string) {
-    const value = typeof option === 'string' ? option : option.value;
+  onInputHandler(value: string) {
     this.emitInput({ ...this.value, value });
   }
 
@@ -42,16 +41,6 @@ class ObsListInput extends ObsInput<IObsListInput<TObsValue>> {
       name: this.value.name,
       options: this.value.options.map(opt => ({ title: opt.description, value: opt.value })),
     };
-  }
-
-  get currentValue() {
-    const option = this.value.options.find((opt: IObsListOption<string>) => {
-      return this.value.value === opt.value;
-    });
-
-    if (option) return option;
-    if (this.allowEmpty) return '';
-    return this.value.options[0];
   }
 }
 
