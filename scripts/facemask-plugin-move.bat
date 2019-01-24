519bc8af0589a81a29810eca679654e9bbf4b083
Branka Lakic <brankalakic@gmail.com>
2019-01-24 13:13:42 -0800

    Put facemask plugin moving into a script

---
+++ scripts/facemask-plugin-move.bat
@@ -0,0 +1,5 @@
+del .\\node_modules\\obs-studio-node\\obs-plugins\\64bit\\facemask-plugin* && ^
+rmdir /s /Q .\\node_modules\\obs-studio-node\\data\\obs-plugins\\facemask-plugin && ^
+move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\obs-plugins\\* .\\node_modules\\obs-studio-node\\obs-plugins\\64bit && ^
+move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\data\\obs-plugins\\facemask-plugin .\\node_modules\\obs-studio-node\\data\\obs-plugins && ^
+rmdir /s /Q .\\node_modules\\facemask-plugin
