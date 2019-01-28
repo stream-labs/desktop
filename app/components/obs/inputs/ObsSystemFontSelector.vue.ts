3ef3f39f84f62858d324713d11d1df1a65863a66
Sean Beyer <sean.beyer@streamlabs.com>
2019-01-28 14:07:52 -0800

    Prevent slider from unintentionally setting font properties

--- app/components/obs/inputs/ObsSystemFontSelector.vue.ts
+++ app/components/obs/inputs/ObsSystemFontSelector.vue.ts
@@ -111,6 +111,7 @@ export default class ObsSystemFontSelector extends ObsInput<IObsInput<IObsFont>>
   // Values that are left blank will be filled with
   // the currently selected value.
   setFont(args: IObsFont) {
+    // if (args.size === this.value.value.size) return;
     const fontObj = { ...args };
 
     // If we want to properly apply a system font, path must be null
