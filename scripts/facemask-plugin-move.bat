6ed9d9b6529f59a878a390faf53976fe4a6aec0d
glaba13 <glaba13@freeuni.edu.ge>
2019-02-08 04:31:56 -0500

    Removed tabs from facemask move script

--- scripts/facemask-plugin-move.bat
+++ scripts/facemask-plugin-move.bat
@@ -1,7 +1,7 @@
 IF EXIST .\\node_modules\\facemask-plugin (
-    del .\\node_modules\\obs-studio-node\\obs-plugins\\64bit\\facemask-plugin* && ^
-	rmdir /s /Q .\\node_modules\\obs-studio-node\\data\\obs-plugins\\facemask-plugin && ^
-	move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\obs-plugins\\* .\\node_modules\\obs-studio-node\\obs-plugins\\64bit && ^
-	move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\data\\obs-plugins\\facemask-plugin .\\node_modules\\obs-studio-node\\data\\obs-plugins && ^
-	rmdir /s /Q .\\node_modules\\facemask-plugin
+del .\\node_modules\\obs-studio-node\\obs-plugins\\64bit\\facemask-plugin* && ^
+rmdir /s /Q .\\node_modules\\obs-studio-node\\data\\obs-plugins\\facemask-plugin && ^
+move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\obs-plugins\\* .\\node_modules\\obs-studio-node\\obs-plugins\\64bit && ^
+move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\data\\obs-plugins\\facemask-plugin .\\node_modules\\obs-studio-node\\data\\obs-plugins && ^
+rmdir /s /Q .\\node_modules\\facemask-plugin
 )
