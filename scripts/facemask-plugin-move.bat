9145f6f920154e7ba585e8e0b25f29062c0abb75
glaba13 <glaba13@freeuni.edu.ge>
2019-02-13 03:09:58 -0500

    Remove unnecessary and operators

--- scripts/facemask-plugin-move.bat
+++ scripts/facemask-plugin-move.bat
@@ -1,8 +1,8 @@
 IF EXIST .\\node_modules\\facemask-plugin\\NUL (
-IF EXIST .\\node_modules\\obs-studio-node\\obs-plugins\\64bit\\facemask-plugin* del .\\node_modules\\obs-studio-node\\obs-plugins\\64bit\\facemask-plugin* && ^
-IF EXIST .\\node_modules\\obs-studio-node\\data\\obs-plugins\\facemask-plugin\\NUL rmdir /s /Q .\\node_modules\\obs-studio-node\\data\\obs-plugins\\facemask-plugin && ^
-move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\obs-plugins\\* .\\node_modules\\obs-studio-node\\obs-plugins\\64bit && ^
-move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\data\\obs-plugins\\facemask-plugin .\\node_modules\\obs-studio-node\\data\\obs-plugins && ^
+IF EXIST .\\node_modules\\obs-studio-node\\obs-plugins\\64bit\\facemask-plugin* del .\\node_modules\\obs-studio-node\\obs-plugins\\64bit\\facemask-plugin* 
+IF EXIST .\\node_modules\\obs-studio-node\\data\\obs-plugins\\facemask-plugin\\NUL rmdir /s /Q .\\node_modules\\obs-studio-node\\data\\obs-plugins\\facemask-plugin
+move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\obs-plugins\\* .\\node_modules\\obs-studio-node\\obs-plugins\\64bit 
+move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\data\\obs-plugins\\facemask-plugin .\\node_modules\\obs-studio-node\\data\\obs-plugins 
 rmdir /s /Q .\\node_modules\\facemask-plugin
 ECHO Facemask Files Moved Successfully
 )
