c9e330bf8125f4ca1c691d20a61c4a6edde3a559
Sean Beyer <sean.beyer@streamlabs.com>
2019-02-14 12:40:49 -0800

    Add ValidatedForm to GenericForm

--- app/components/obs/inputs/GenericForm.vue.ts
+++ app/components/obs/inputs/GenericForm.vue.ts
@@ -2,15 +2,22 @@ import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import { IObsInput, TObsValue } from './ObsInput';
 import { propertyComponentForType } from './Components';
+import ValidatedForm from 'components/shared/inputs/ValidatedForm.vue';
 
-@Component({})
+@Component({ components: { ValidatedForm } })
 export default class GenericForm extends Vue {
   @Prop()
   value: IObsInput<TObsValue>[];
 
+  $refs: {
+    form: ValidatedForm;
+  };
+
   propertyComponentForType = propertyComponentForType;
 
-  onInputHandler(value: IObsInput<TObsValue>, index: number) {
+  async onInputHandler(value: IObsInput<TObsValue>, index: number) {
+    this.$refs.form.validate();
+
     const newValue = [].concat(this.value);
     newValue.splice(index, 1, value);
     this.$emit('input', newValue, index);
