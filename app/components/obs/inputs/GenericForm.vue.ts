8e1c7f9d833aa24173e1b8da1c85a85e6109ab56
Sean Beyer <sean.beyer@streamlabs.com>
2019-02-15 14:03:32 -0800

    Fix janky number input errors (#1383)
    * Add ValidatedForm to GenericForm
    
    * Allow validations to run properly
    
    * Display error text properly

--- app/components/obs/inputs/GenericForm.vue.ts
+++ app/components/obs/inputs/GenericForm.vue.ts
@@ -2,15 +2,22 @@ import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import { IObsInput, TObsValue } from './ObsInput';
 import { propertyComponentForType } from './Components';
+import ValidatedForm from 'components/shared/inputs/ValidatedForm.vue';
 
-@Component({})
+@Component({ components: { ValidatedForm } })
 export default class GenericForm extends Vue {
   @Prop()
   value: IObsInput<TObsValue>[];
 
+  $refs: {
+    form: ValidatedForm;
+  };
+
   propertyComponentForType = propertyComponentForType;
 
-  onInputHandler(value: IObsInput<TObsValue>, index: number) {
+  async onInputHandler(value: IObsInput<TObsValue>, index: number) {
+    if (await this.$refs.form.validateAndGetErrorsCount()) return;
+
     const newValue = [].concat(this.value);
     newValue.splice(index, 1, value);
     this.$emit('input', newValue, index);
