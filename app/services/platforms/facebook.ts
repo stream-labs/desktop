57b308fc8edf4997747bd13d0bb6455e1f8bf487
Alexander Nazarov <kadabrik91@gmail.com>
2019-02-15 13:49:20 -0800

    fix facebook games search

--- app/services/platforms/facebook.ts
+++ app/services/platforms/facebook.ts
@@ -263,7 +263,7 @@ export class FacebookService extends StatefulService<IFacebookServiceState>
   }
 
   @requiresToken()
-  searchGames(searchString: string): Promise<IGame[]> {
+  async searchGames(searchString: string): Promise<IGame[]> {
     if (searchString.length < 2) return;
     const url = `${this.apiBase}/v3.2/search?type=game&q=${searchString}`;
     const headers = this.getHeaders();
