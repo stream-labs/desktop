faafc930491003c5bbf4b052186897c167d9bc93
glaba13 <glaba13@freeuni.edu.ge>
2019-02-15 18:39:48 -0500

    Revert of Revert facemask dependecy and Add create directory

---
+++ scripts/facemask-plugin-move.bat
@@ -0,0 +1,9 @@
+IF EXIST .\\node_modules\\facemask-plugin\\slobs\\NUL (
+IF EXIST .\\node_modules\\obs-studio-node\\obs-plugins\\64bit\\facemask-plugin* del .\\node_modules\\obs-studio-node\\obs-plugins\\64bit\\facemask-plugin* 
+IF EXIST .\\node_modules\\obs-studio-node\\data\\obs-plugins\\facemask-plugin\\NUL rmdir /s /Q .\\node_modules\\obs-studio-node\\data\\obs-plugins\\facemask-plugin
+move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\obs-plugins\\* .\\node_modules\\obs-studio-node\\obs-plugins\\64bit 
+move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\data\\obs-plugins\\facemask-plugin .\\node_modules\\obs-studio-node\\data\\obs-plugins 
+rmdir /s /Q .\\node_modules\\facemask-plugin
+mkdir .\\node_modules\\facemask-plugin
+ECHO Facemask Files Moved Successfully
+)
