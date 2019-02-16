e5a6f01a2f1727735afccda086eaa0a6e0e303e2
glaba13 <glaba13@freeuni.edu.ge>
2019-02-15 20:08:46 -0500

    Update version and include package

--- scripts/facemask-plugin-move.bat
+++ scripts/facemask-plugin-move.bat
@@ -3,7 +3,6 @@ IF EXIST .\\node_modules\\obs-studio-node\\obs-plugins\\64bit\\facemask-plugin* 
 IF EXIST .\\node_modules\\obs-studio-node\\data\\obs-plugins\\facemask-plugin\\NUL rmdir /s /Q .\\node_modules\\obs-studio-node\\data\\obs-plugins\\facemask-plugin
 move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\obs-plugins\\* .\\node_modules\\obs-studio-node\\obs-plugins\\64bit 
 move .\\node_modules\\facemask-plugin\\slobs\\RelWithDebInfo\\data\\obs-plugins\\facemask-plugin .\\node_modules\\obs-studio-node\\data\\obs-plugins 
-rmdir /s /Q .\\node_modules\\facemask-plugin
-mkdir .\\node_modules\\facemask-plugin
+rmdir /s /Q .\\node_modules\\facemask-plugin\\slobs
 ECHO Facemask Files Moved Successfully
 )
