6dff934052cd688b036f76d5d746024c91993bdc
Sean Beyer <sean.beyer@streamlabs.com>
2019-02-20 15:44:53 -0800

    Add TODO

--- app/components/shared/inputs/NumberInput.vue.ts
+++ app/components/shared/inputs/NumberInput.vue.ts
@@ -31,6 +31,7 @@ export default class NumberInput extends BaseInput<number | string, INumberMetad
 
     if (this.options.min !== void 0 && Number(value) < this.options.min) {
       this.timeout = window.setTimeout(() => this.updateValue(`${this.options.min}`), 1000);
+      // TODO: Remove early return and re-implement proper validations
       return;
     }
 
