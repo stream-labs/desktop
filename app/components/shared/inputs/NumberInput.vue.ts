876319725a6c0732d247ebab4a8bd6f22a6329fc
Sean Beyer <sean.beyer@streamlabs.com>
2019-02-20 15:31:06 -0800

    Fix NumberInput crash
    This is a bandaid to give more time to investigate issues with our input validator

--- app/components/shared/inputs/NumberInput.vue.ts
+++ app/components/shared/inputs/NumberInput.vue.ts
@@ -31,6 +31,7 @@ export default class NumberInput extends BaseInput<number | string, INumberMetad
 
     if (this.options.min !== void 0 && Number(value) < this.options.min) {
       this.timeout = window.setTimeout(() => this.updateValue(`${this.options.min}`), 1000);
+      return;
     }
 
     if (this.options.max !== void 0 && Number(value) > this.options.max) {
