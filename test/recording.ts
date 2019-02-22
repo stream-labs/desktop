28d34bcb0cbc1150a0a616814b35a091fa1c6f87
Adrian Perez <adrian@adrianperez.org>
2019-02-21 16:56:57 -0800

    test(recording): add file detection

--- test/recording.ts
+++ test/recording.ts
@@ -1,11 +1,14 @@
 import * as path from 'path';
 import { tmpdir } from 'os';
 import { mkdtemp, readdir } from 'fs-extra';
+import { Magic, MAGIC_MIME_TYPE } from 'mmmagic';
 import { focusChild, focusMain, test, useSpectron } from './helpers/spectron';
 import { setFormDropdown, setFormInput } from './helpers/spectron/forms';
 import { sleep } from './helpers/sleep';
 
 useSpectron({ appArgs: '--nosync' });
+
+const magic = new Magic(MAGIC_MIME_TYPE);
 
 test('Recording', async t => {
   const tmpDir = await mkdtemp(path.join(tmpdir(), `slobs-recording-`));
@@ -46,12 +49,37 @@ test('Recording', async t => {
 
     // Stop recording
     await app.client.click('.record-button');
-    await sleep(1000);
+    await sleep(2000);
   }
 
   // Check that every file was created
   const files = await readdir(tmpDir);
-
   // M3U8 creates multiple TS files in addition to the catalog itself.
   t.true(files.length >= formats.length, `Files that were created:\n${files.join('\n')}`);
+
+  await sleep(2000);
+  // Perform file detection for every file and ensure they're somewhat-valid videos
+  for (const file of files) {
+    const info = await detectFile(path.join(tmpDir, file).replace('\\', '\\\\'));
+    const mime = info.toString();
+
+    // Unfortunately, TS files don't seem to be detected correctly.
+    if (path.extname(file) === '.ts' && mime === 'application/octet-stream') {
+      return;
+    }
+
+    t.regex(mime, /^video/, `${file} was not detected as a video file`);
+  }
 });
+
+const detectFile = (file: string) => {
+  return new Promise((resolve, reject) => {
+    magic.detectFile(file, (err: Error, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
