6f016bc8a6ca17a2e450a90b04f9efae3f24b965
DESKTOP-FGF6CBD\Peter <peter.kucharski@streamlabs.com>
2019-02-22 12:48:22 -0800

    Add more data to FM settings form

--- app/components/windows/FacemaskSettings.vue.ts
+++ app/components/windows/FacemaskSettings.vue.ts
@@ -3,7 +3,7 @@ import { Component } from 'vue-property-decorator';
 import { Inject } from 'util/injector';
 import ModalLayout from '../ModalLayout.vue';
 import { BoolInput, ListInput, ToggleInput, SliderInput } from 'components/shared/inputs/inputs';
-import { ProgressBar } from 'streamlabs-beaker';
+import { ProgressBar, ItemGrid, VirtualItem } from 'streamlabs-beaker';
 import HFormGroup from 'components/shared/inputs/HFormGroup.vue';
 import { FacemasksService } from 'services/facemasks';
 
@@ -13,6 +13,15 @@ interface IInputDeviceSelection {
   selected?: boolean;
 }
 
+interface IFacemaskSelection {
+  uuid: string;
+  type: string;
+  tier: number;
+  name: string;
+  selectionCount: number;
+  selected: boolean;
+}
+
 @Component({
   components: {
     ModalLayout,
@@ -20,6 +29,8 @@ interface IInputDeviceSelection {
     BoolInput,
     ListInput,
     ProgressBar,
+    ItemGrid,
+    VirtualItem,
     ToggleInput,
     SliderInput,
   },
@@ -28,8 +39,33 @@ export default class FacemaskSettings extends Vue {
   @Inject() facemasksService: FacemasksService;
   updatingInfo = false;
   enabledModel = this.facemasksService.state.settings.enabled;
+  donationsEnabledModel = this.facemasksService.state.settings.donations_enabled;
+  subsEnabledModel = this.facemasksService.state.settings.subs_enabled;
   durationModel = this.facemasksService.state.settings.duration;
   videoInputModel = this.facemasksService.state.device.value;
+  userT2masks = this.facemasksService.state.settings.userT2masks;
+  userT3masks = this.facemasksService.state.settings.userT3masks;
+
+  t2AvailableMasks = this.facemasksService.state.settings.t2masks.map(mask => {
+    return {
+      uuid: mask.uuid,
+      type: mask.type,
+      tier: mask.tier,
+      name: mask.name,
+      selected: this.userT2masks.indexOf(mask.uuid) !== -1 ? true : false,
+    } as IFacemaskSelection;
+  });
+
+  t3AvailableMasks = this.facemasksService.state.settings.t3masks.map(mask => {
+    return {
+      uuid: mask.uuid,
+      type: mask.type,
+      tier: mask.tier,
+      name: mask.name,
+      selected: this.userT3masks.indexOf(mask.uuid) !== -1 ? true : false,
+    } as IFacemaskSelection;
+  });
+
   inputDevices = this.facemasksService.getInputDevicesList().map(device => {
     return {
       title: device.name,
@@ -41,7 +77,11 @@ export default class FacemaskSettings extends Vue {
     this.updatingInfo = true;
     const newSettings = {
       enabled: this.enabledModel,
+      donations_enabled: this.donationsEnabledModel,
+      subs_enabled: this.subsEnabledModel,
       duration: this.durationModel,
+      userT2masks: this.formatMaskSelections(this.t2AvailableMasks),
+      userT3masks: this.formatMaskSelections(this.t3AvailableMasks),
       device: this.facemasksService.getInputDevicesList().filter(device => {
         return device.value === this.videoInputModel;
       })[0],
@@ -54,8 +94,38 @@ export default class FacemaskSettings extends Vue {
     }
   }
 
+  formatMaskSelections(masks: IFacemaskSelection[]) {
+    return masks
+      .filter(mask => {
+        return mask.selected;
+      })
+      .map(mask => {
+        return {
+          uuid: mask.uuid,
+          tier: mask.tier,
+        };
+      });
+  }
+
   cancel() {
     return;
+  }
+
+  clickMask(mask: IFacemaskSelection, options: IFacemaskSelection[]) {
+    if (mask.selected) {
+      mask.selected = false;
+    } else if (!mask.selected) {
+      if (this.validateSelectedMaskCount(options)) {
+        mask.selected = true;
+      }
+    }
+  }
+
+  validateSelectedMaskCount(masks: IFacemaskSelection[]) {
+    const length = masks.filter(mask => {
+      return mask.selected;
+    }).length;
+    return length < 3;
   }
 
   onVideoInputSelect(webcam: IInputDeviceSelection) {
