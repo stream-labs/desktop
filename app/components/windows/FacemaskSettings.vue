71f86a2b9767be2bd3e0e2f6040e4315116d8ad8
DESKTOP-FGF6CBD\Peter <peter.kucharski@streamlabs.com>
2019-02-22 12:48:01 -0800

    Update fm settings form WIP

--- app/components/windows/FacemaskSettings.vue
+++ app/components/windows/FacemaskSettings.vue
@@ -4,7 +4,7 @@
   title="Face Mask Settings">
     <div slot="content">
       <div class="row">
-          <h-form-group :title="$t('Face Masks Enabled')">
+        <h-form-group :title="$t('Face Masks Enabled')">
           <toggle-input
             v-model="enabledModel"
             name="enabled"
@@ -13,22 +13,68 @@
         </h-form-group>
       </div>
       <div class="row">
-          <h-form-group :title="$t('Download Progress')">
+        <h-form-group :title="$t('Download Progress')">
           <progress-bar :progressComplete="downloadProgress"></progress-bar>
         </h-form-group>
       </div>
       <div class="row">
-          <h-form-group :title="$t('Alert Duration')">
+        <h-form-group :title="$t('Alert Duration')">
           <slider-input v-model="durationModel" :metadata="{min: 8, max: 60}"/>
         </h-form-group>
       </div>
       <div class="row">
-          <h-form-group :title="$t('Video Device')">
+        <h-form-group :title="$t('Video Device')">
           <list-input
             @input="onVideoInputSelect"
             v-model="videoInputModel"
             :metadata="videoInputMetadata"
           />
+        </h-form-group>
+      </div>
+      <div class="row">
+        <h-form-group :title="$t('Face Mask Donations')">
+          <toggle-input
+            v-model="donationsEnabledModel"
+            name="donationsEnabled"
+            :metadata="{ title: $t('Enable Face Mask Donations') }"
+          />
+        </h-form-group>
+      </div>
+      <div class="row">
+        <h-form-group :title="$t('Sub Masks')">
+          <toggle-input
+            v-model="subsEnabledModel"
+            name="subsEnabled"
+            :metadata="{ title: $t('Enable Sub Masks') }"
+          />
+        </h-form-group>
+      </div>
+      <div class="row">
+        <h-form-group :title="$t('T2 Sub Masks')">
+          <item-grid>
+            <virtual-item
+              v-for="(mask) in t2AvailableMasks"
+              :key="mask.uuid"
+              :preview="`http://facemasks-cdn.streamlabs.com/${mask.uuid}.png`"
+              :name="mask.name"
+              :selected="mask.selected"
+              @click="clickMask(mask, t2AvailableMasks)"
+            ></virtual-item>
+          </item-grid>
+        </h-form-group>
+      </div>
+      <div class="row">
+        <h-form-group :title="$t('T3 Sub Masks')">
+          <item-grid>
+            <virtual-item
+              v-for="(mask) in t3AvailableMasks"
+              :key="mask.uuid"
+              :preview="`http://facemasks-cdn.streamlabs.com/${mask.uuid}.png`"
+              :name="mask.name"
+              :selected="mask.selected"
+              @click="clickMask(mask, t3AvailableMasks)"
+            ></virtual-item>
+          </item-grid>
         </h-form-group>
       </div>
     </div>
