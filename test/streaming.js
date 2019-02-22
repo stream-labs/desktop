c9407c5ecf1f0a3502b9e65a1544fb53fa9b25f7
Alexander Nazarov <kadabrik91@gmail.com>
2019-02-21 16:11:21 -0800

    disable mixer tests

--- test/streaming.js
+++ test/streaming.js
@@ -89,34 +89,34 @@ test('Streaming to Facebook', async t => {
   t.pass();
 });
 
+// TODO: the auth-token for Mixer get stale very fast
+// test('Streaming to Mixer', async t => {
+//
+//   // login into the account
+//   if (!(await logIn(t, 'mixer'))) return;
+//   const app = t.context.app;
+//
+//   // open EditStreamInfo window
+//   await focusMain(t);
+//   await app.client.click('button=Go Live');
+//
+//   // set stream info, and start stream
+//   await focusChild(t);
+//   await fillForm(t, 'editStreamForm', {
+//     stream_title: 'SLOBS Test Stream',
+//     game: 'PLAYERUNKNOWN\'S BATTLEGROUNDS',
+//   });
+//
+//   await app.client.click('button=Confirm & Go Live');
+//
+//   // check we're streaming
+//   await focusMain(t);
+//   await app.client.waitForExist('button=End Stream', 10 * 1000);
+//   t.pass();
+// });
 
-test('Streaming to Mixer', async t => {1
 
-  // login into the account
-  if (!(await logIn(t, 'mixer'))) return;
-  const app = t.context.app;
-
-  // open EditStreamInfo window
-  await focusMain(t);
-  await app.client.click('button=Go Live');
-
-  // set stream info, and start stream
-  await focusChild(t);
-  await fillForm(t, 'editStreamForm', {
-    stream_title: 'SLOBS Test Stream',
-    game: 'PLAYERUNKNOWN\'S BATTLEGROUNDS',
-  });
-
-  await app.client.click('button=Confirm & Go Live');
-
-  // check we're streaming
-  await focusMain(t);
-  await app.client.waitForExist('button=End Stream', 10 * 1000);
-  t.pass();
-});
-
-
-// TODO: find the reason why the auth-token for Youtube get stale very fast
+// TODO: the auth-token for Youtube get stale very fast
 // test('Streaming to Youtube', async t => {
 //
 //   // login into the account
