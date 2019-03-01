77179b4416d18761e3d0ea242ff8775862ce2859
Alexander Nazarov <kadabrik91@gmail.com>
2019-02-28 17:04:35 -0800

    themes test

--- test/themes.ts
+++ test/themes.ts
@@ -1,18 +1,16 @@
-import { ISceneCollectionsServiceApi } from '../app/services/scene-collections';
-import { focusLibrary, test, useSpectron } from './helpers/spectron';
-import { getClient } from './helpers/api-client';
+import { focusLibrary, focusMain, test, useSpectron } from './helpers/spectron';
 import { logIn } from './helpers/spectron/user';
 import { sleep } from './helpers/sleep';
+import { FormMonkey } from './helpers/form-monkey';
 
 useSpectron({ appArgs: '--nosync' });
 
 const OVERLAY_NAME = 'Talon Stream Package by VBI';
-const OVERLAY_URL =
-  'https://cdn.streamlabs.com/marketplace/overlays/7684923/ddcf3ea/ddcf3ea.overlay';
 const OVERLAY_SCENES = ['Starting Soon', 'Be Right Back', 'Stream Ending', 'Intermission', 'Main'];
 
 test('Installing a theme', async (t: any) => {
   const { app } = t.context;
+  const formMonkey = new FormMonkey(t);
 
   await logIn(t);
 
@@ -21,25 +19,18 @@ test('Installing a theme', async (t: any) => {
 
   await focusLibrary(t);
 
-  await app.client.waitForExist('.market-item', 10000);
-  await sleep(99999);
-  await app.client.setValue('input', 'abc');
-  await sleep(99999);
+  // search a theme
+  await formMonkey.setInputValue('input', OVERLAY_NAME);
+  // the input field has a debounce search
+  await sleep(15000);
+  // wait items load
   await app.client.click('.market-item');
+  // install overlay
+  await app.client.click('button=Install Overlay');
 
-
-  await sleep(99999);
-  // This is all we can test, that webview is there
-  t.true(
-    await app.client.isExisting(
-      '.overlays-container webview[src^="https://streamlabs.com/library"]'
-    )
-  );
-
-  // Install theme manually, as embedded library doesn't allow us to interact with elements
-  await installOverlay(OVERLAY_NAME, OVERLAY_URL);
-
-  await app.client.click('button=Editor');
+  // wait for installation complete
+  await focusMain(t);
+  await app.client.waitForExist('.studio-page', 10000);
 
   // Should've loaded the overlay as a new scene collection
   t.true(await app.client.isExisting(`span=${OVERLAY_NAME}`));
@@ -54,11 +45,3 @@ test('Installing a theme', async (t: any) => {
     t.true(await app.client.isExisting(`span.item-title=${source}`), `Source ${source}`);
   }
 });
-
-const installOverlay = async (name: string, url: string): Promise<void> => {
-  const api = await getClient();
-  const sceneCollectionService = api.getResource<
-    ISceneCollectionsServiceApi & { installOverlay: (url: string, name: string) => Promise<void> }
-  >('SceneCollectionsService');
-  await sceneCollectionService.installOverlay(OVERLAY_URL, OVERLAY_NAME);
-};
