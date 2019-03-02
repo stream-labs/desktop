0496d17b257adc12fc18fe6dfa17d532f423fbc7
Alexander Nazarov <kadabrik91@gmail.com>
2019-03-01 16:02:28 -0800

    add audio service

---
+++ app/services/api/external-api/audio/audio.ts
@@ -0,0 +1,66 @@
+import {
+  AudioSource as InternalAudioSource,
+  AudioService as InternalAudioService,
+} from 'services/audio';
+import { Inject } from 'util/injector';
+import { Singleton } from 'services/api/external-api';
+import { ServiceHelper } from 'services/stateful-service';
+import { ISerializable } from '../../rpc-api';
+import * as obs from '../../../../../obs-api';
+
+@Singleton()
+export class AudioService {
+  @Inject() protected audioService: InternalAudioService;
+
+  getSource(sourceId: string): AudioSource {
+    const source = this.audioService.getSource(sourceId);
+    return source ? new AudioSource(sourceId) : null;
+  }
+
+  getSources(): AudioSource[] {
+    return this.audioService.getSources().map(source => this.getSource(source.sourceId));
+  }
+}
+
+export interface IFader {
+  db: number;
+  deflection: number;
+  mul: number;
+}
+
+export interface IAudioSourceModel {
+  sourceId: string;
+  fader: IFader;
+  audioMixers: number;
+  monitoringType: obs.EMonitoringType;
+  forceMono: boolean;
+  syncOffset: number;
+  muted: boolean;
+  mixerHidden: boolean;
+}
+
+@ServiceHelper()
+export class AudioSource implements ISerializable {
+  @Inject() private audioService: InternalAudioService;
+  private audioSource: InternalAudioSource;
+
+  constructor(private sourceId: string) {
+    this.audioSource = this.audioService.getSource(sourceId);
+  }
+
+  /**
+   * serialize source
+   */
+  getModel(): IAudioSourceModel {
+    return {
+      sourceId: this.audioSource.sourceId,
+      fader: this.audioSource.fader,
+      audioMixers: this.audioSource.audioMixers,
+      monitoringType: this.audioSource.monitoringType,
+      forceMono: this.audioSource.forceMono,
+      syncOffset: this.audioSource.syncOffset,
+      muted: this.audioSource.muted,
+      mixerHidden: this.audioSource.mixerHidden,
+    };
+  }
+}
