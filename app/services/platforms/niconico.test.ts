3e5ea7001db2dc65ecedeca43ef3df63fe6a2e80
berlysia <berlysia@gmail.com>
2019-03-11 20:22:16 +0900

    test(niconico): jest化

--- app/services/platforms/niconico.test.ts
+++ app/services/platforms/niconico.test.ts
@@ -1,222 +1,187 @@
-import test from 'ava';
-import * as Proxyquire from 'proxyquire';
-import * as sinon from 'sinon';
 import { sleep } from 'util/sleep';
+import { createSetupFunction } from 'util/test-setup';
 
-const proxyquire = Proxyquire.noCallThru();
-
-function createInject(mockServices: any) {
-  return function Inject(serviceName?: string) {
-    return function (target: Object, key: string) {
-      Object.defineProperty(target, key, {
-        get() {
-          const name = serviceName || key.charAt(0).toUpperCase() + key.slice(1);
-          const serviceInstance = mockServices[name];
-          if (!serviceInstance) throw new Error(`no mock defined for "${name}"`);
-          return serviceInstance;
-        }
-      });
-    };
-  };
-}
-
-const getStub = (injectTable = {}) => ({
-  '../../util/injector': {
-    Inject: createInject({
-      StreamingService: {
-        streamingStatusChange: {
-          subscribe() {}
-        }
+const setup = createSetupFunction({
+  injectee: {
+    HostsService: {},
+    SettingsService: {},
+    UserService: {},
+    StreamingService: {
+      streamingStatusChange: {
+        subscribe() {},
       },
-      ...injectTable
-    })
+    },
+    WindowsService: {},
   },
-  '../../util/sleep': {
+});
+
+beforeEach(() => {
+  jest.resetModules();
+
+  jest.mock('services/stateful-service');
+  jest.mock('util/injector');
+  jest.mock('services/streaming', () => ({}));
+  jest.mock('services/user', () => ({}));
+  jest.mock('services/settings', () => ({}));
+  jest.mock('services/windows', () => ({}));
+  jest.mock('util/sleep', () => ({
     sleep: () => sleep(0),
-  },
-  '../streaming': {},
-  '../user': {},
-  '../settings': {},
-  'services/windows': {},
+  }));
 });
 
-test('get instance', t => {
-  const m = proxyquire('./niconico', getStub());
-  t.truthy(m.NiconicoService.instance);
+test('get instance', () => {
+  setup();
+  const { NiconicoService } = require('./niconico');
+  expect(NiconicoService.instance).toBeInstanceOf(NiconicoService);
 });
 
-test('setupStreamSettingsで番組がない場合', async t => {
-  const m = proxyquire('./niconico', getStub());
-  const { instance } = m.NiconicoService;
-  const fetchLiveProgramInfoStub = sinon.stub();
-  fetchLiveProgramInfoStub.returns(Promise.resolve({}));
-  instance.fetchLiveProgramInfo = fetchLiveProgramInfoStub;
+test('setupStreamSettingsで番組がない場合', async () => {
+  setup();
+  const { NiconicoService } = require('./niconico');
+  const { instance } = NiconicoService;
 
-  const result = await instance.setupStreamSettings()
-  t.falsy(result.url, '空の配信設定を得る');
-  t.false(result.asking, 'ポップアップは出さない');
-  t.true(fetchLiveProgramInfoStub.calledTwice, 'リトライするので2回呼ぶ')
+  instance.fetchLiveProgramInfo = jest.fn().mockResolvedValue({});
+
+  const result = await instance.setupStreamSettings();
+  expect(result.url).toEqual('');
+  expect(result.asking).toBe(false);
+  expect(instance.fetchLiveProgramInfo).toHaveBeenCalledTimes(2);
 });
 
-test('setupStreamSettingsで番組がひとつある場合', async t => {
-  const updatePlatformChannelIdStub = sinon.stub();
-  const getSettingsFormDataStub = sinon.stub();
-  const setSettingsStub = sinon.stub();
+test('setupStreamSettingsで番組がひとつある場合', async () => {
+  const updatePlatformChannelId = jest.fn();
+  const getSettingsFormData = jest.fn();
+  const setSettings = jest.fn();
 
-  getSettingsFormDataStub.returns([
+  getSettingsFormData.mockReturnValue([
     {
       nameSubCategory: 'Untitled',
-      parameters: [
-        { name: 'service', value: '' },
-        { name: 'server', value: '' },
-        { name: 'key', value: '' }
-      ]
-    }
+      parameters: [{ name: 'service', value: '' }, { name: 'server', value: '' }, { name: 'key', value: '' }],
+    },
   ]);
 
-  const injectMap = {
+  const injectee = {
     UserService: {
-      updatePlatformChannelId: updatePlatformChannelIdStub
+      updatePlatformChannelId,
     },
     SettingsService: {
-      getSettingsFormData: getSettingsFormDataStub,
-      setSettings: setSettingsStub
-    }
+      getSettingsFormData,
+      setSettings,
+    },
   };
 
-  const m = proxyquire('./niconico', getStub(injectMap));
+  setup({ injectee });
+  const { NiconicoService } = require('./niconico');
+  const { instance } = NiconicoService;
 
-  const { instance } = m.NiconicoService;
-
-  const fetchLiveProgramInfoStub = sinon.stub();
-  instance.fetchLiveProgramInfo = fetchLiveProgramInfoStub;
-  fetchLiveProgramInfoStub.returns(Promise.resolve({
+  instance.fetchLiveProgramInfo = jest.fn().mockResolvedValue({
     channelId: {
       url: 'url1',
       key: 'key1',
       bitrate: 'bitrate1',
-    }
-  }));
+    },
+  });
 
   const result = await instance.setupStreamSettings();
-  t.is(result.url, 'url1', '配信設定を受け取れる');
-  t.true(updatePlatformChannelIdStub.calledOnceWith('channelId'), 'チャンネルIDをUsersServiceに通知する');
-  t.true(setSettingsStub.args[0][0] === 'Stream', '配信設定を更新する');
-
-  const wroteSettings = setSettingsStub.args[0][1];
-  const params = wroteSettings[0].parameters;
-  t.is(params.find((x: any) => x.name === 'service').value, 'niconico ニコニコ生放送');
-  t.is(params.find((x: any) => x.name === 'server').value, 'url1');
-  t.is(params.find((x: any) => x.name === 'key').value, 'key1');
+  expect(result.url).toBe('url1');
+  expect(updatePlatformChannelId).toHaveBeenCalledTimes(1);
+  expect(updatePlatformChannelId).toHaveBeenLastCalledWith('channelId');
+  expect(setSettings).toHaveBeenCalledTimes(1);
+  expect(setSettings.mock.calls[0]).toMatchSnapshot();
 });
 
-test('setupStreamSettingsで番組取得にリトライで成功する場合', async t => {
-  const updatePlatformChannelIdStub = sinon.stub();
-  const getSettingsFormDataStub = sinon.stub();
-  const setSettingsStub = sinon.stub();
+test('setupStreamSettingsで番組取得にリトライで成功する場合', async () => {
+  const updatePlatformChannelId = jest.fn();
+  const getSettingsFormData = jest.fn();
+  const setSettings = jest.fn();
 
-  getSettingsFormDataStub.returns([
+  getSettingsFormData.mockReturnValue([
     {
       nameSubCategory: 'Untitled',
-      parameters: [
-        { name: 'service', value: '' },
-        { name: 'server', value: '' },
-        { name: 'key', value: '' }
-      ]
-    }
+      parameters: [{ name: 'service', value: '' }, { name: 'server', value: '' }, { name: 'key', value: '' }],
+    },
   ]);
 
-  const injectMap = {
-    UserService: {
-      updatePlatformChannelId: updatePlatformChannelIdStub
-    },
-    SettingsService: {
-      getSettingsFormData: getSettingsFormDataStub,
-      setSettings: setSettingsStub
-    }
+  const injectee = {
+    UserService: { updatePlatformChannelId: updatePlatformChannelId },
+    SettingsService: { getSettingsFormData: getSettingsFormData, setSettings: setSettings },
   };
 
-  const m = proxyquire('./niconico', getStub(injectMap));
+  setup({ injectee });
+  const { NiconicoService } = require('./niconico');
+  const { instance } = NiconicoService;
 
-  const { instance } = m.NiconicoService;
-
-  const fetchLiveProgramInfoStub = sinon.stub();
-  instance.fetchLiveProgramInfo = fetchLiveProgramInfoStub;
-  fetchLiveProgramInfoStub.onCall(0).returns(Promise.resolve({}));
-  fetchLiveProgramInfoStub.returns(Promise.resolve({
-    channelId: {
-      url: 'url1',
-      key: 'key1',
-      bitrate: 'bitrate1',
-    }
-  }));
+  instance.fetchLiveProgramInfo = jest
+    .fn()
+    .mockReturnValueOnce(Promise.resolve({}))
+    .mockResolvedValue({
+      channelId: {
+        url: 'url1',
+        key: 'key1',
+        bitrate: 'bitrate1',
+      },
+    });
 
   const result = await instance.setupStreamSettings();
-  t.is(result.url, 'url1', '配信設定を受け取れる');
-  t.true(updatePlatformChannelIdStub.calledOnceWith('channelId'), 'チャンネルIDをUsersServiceに通知する');
-  t.true(setSettingsStub.args[0][0] === 'Stream', '配信設定を更新する');
-
-  const wroteSettings = setSettingsStub.args[0][1];
-  const params = wroteSettings[0].parameters;
-  t.is(params.find((x: any) => x.name === 'service').value, 'niconico ニコニコ生放送');
-  t.is(params.find((x: any) => x.name === 'server').value, 'url1');
-  t.is(params.find((x: any) => x.name === 'key').value, 'key1');
+  expect(result).toEqual({
+    asking: false,
+    url: 'url1',
+    key: 'key1',
+    bitrate: 'bitrate1',
+  });
+  expect(updatePlatformChannelId).toHaveBeenCalledTimes(1);
+  expect(updatePlatformChannelId).toHaveBeenLastCalledWith('channelId');
+  expect(setSettings).toHaveBeenCalledTimes(1);
+  expect(setSettings.mock.calls[0]).toMatchSnapshot();
 });
 
-test('setupStreamSettingsで番組が複数ある場合', async t => {
-  const updatePlatformChannelIdStub = sinon.stub();
-  const getSettingsFormDataStub = sinon.stub();
-  const setSettingsStub = sinon.stub();
-  const showWindowStub = sinon.stub();
+test('setupStreamSettingsで番組が複数ある場合', async () => {
+  const updatePlatformChannelId = jest.fn();
+  const getSettingsFormData = jest.fn();
+  const setSettings = jest.fn();
+  const showWindow = jest.fn();
 
-  getSettingsFormDataStub.returns([
+  getSettingsFormData.mockReturnValue([
     {
       nameSubCategory: 'Untitled',
-      parameters: [
-        { name: 'service', value: '' },
-        { name: 'server', value: '' },
-        { name: 'key', value: '' }
-      ]
-    }
+      parameters: [{ name: 'service', value: '' }, { name: 'server', value: '' }, { name: 'key', value: '' }],
+    },
   ]);
 
-  const injectMap = {
+  const injectee = {
     UserService: {
-      updatePlatformChannelId: updatePlatformChannelIdStub
+      updatePlatformChannelId: updatePlatformChannelId,
     },
     SettingsService: {
-      getSettingsFormData: getSettingsFormDataStub,
-      setSettings: setSettingsStub
+      getSettingsFormData: getSettingsFormData,
+      setSettings: setSettings,
     },
     WindowsService: {
-      showWindow: showWindowStub
-    }
-  };
-
-  const m = proxyquire('./niconico', getStub(injectMap));
-
-  const { instance } = m.NiconicoService;
-
-  const fetchLiveProgramInfoStub = sinon.stub();
-  instance.fetchLiveProgramInfo = fetchLiveProgramInfoStub;
-  const info = {
-    channelId1: {
-      url: 'url1',
-      key: 'key1',
-      bitrate: 'bitrate1',
+      showWindow: showWindow,
     },
-    channelId2: {
-      url: 'url2',
-      key: 'key2',
-      bitrate: 'bitrate2',
-    }
   };
-  fetchLiveProgramInfoStub.returns(Promise.resolve(info));
+
+  setup({ injectee });
+  const { NiconicoService } = require('./niconico');
+  const { instance } = NiconicoService;
+
+  const info = {
+    channelId1: { url: 'url1', key: 'key1', bitrate: 'bitrate1' },
+    channelId2: { url: 'url2', key: 'key2', bitrate: 'bitrate2' },
+  };
+  instance.fetchLiveProgramInfo = jest.fn().mockResolvedValue(info);
 
   const result = await instance.setupStreamSettings();
-  t.deepEqual(showWindowStub.args[0][0].queryParams, info, 'ポップアップに番組設定を渡す');
-  t.falsy(result.url, '空の配信設定を受け取る');
-  t.true(result.asking, 'ポップアップを出す');
-  t.true(updatePlatformChannelIdStub.notCalled, 'チャンネルIDは通知しない');
-  t.true(setSettingsStub.notCalled, '配信設定は更新しない');
+  expect(result).toMatchInlineSnapshot(`
+Object {
+  "asking": true,
+  "bitrate": undefined,
+  "key": "",
+  "url": "",
+}
+`);
+  expect(showWindow.mock.calls[0]).toMatchSnapshot();
+
+  expect(updatePlatformChannelId).not.toHaveBeenCalled();
+  expect(setSettings).not.toHaveBeenCalled();
 });
