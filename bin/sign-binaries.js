c2a7db20c0483acfa2add8a675225c8e769a1c24
Andy Creeth <acreeth@gmail.com>
2019-03-11 11:34:34 -0700

    update everything for new cert

--- bin/sign-binaries.js
+++ bin/sign-binaries.js
@@ -1,27 +1,36 @@
 const shell = require('shelljs');
 const fs = require('fs');
 const path = require('path');
-const signtool = require('signtool')
+const signtool = require('signtool');
 
 const osn = './node_modules/obs-studio-node/';
 const crash_handler = './node_modules/crash-handler/';
 
-function parseFiles(directory) {
-	const files = fs.readdirSync(directory);
-	files.forEach(file => {
-		const fullPath = path.join(directory, file);
-		if(fs.statSync(fullPath).isDirectory() && file != './data/obs-plugins/win-capture/') {
-			parseFiles(fullPath);
-		} else {
-			const absolutePath = path.resolve(fullPath);
-			const ext = path.extname(absolutePath)
-			if(ext === '.dll' || ext === '.node' || ext === '.exe') {
-				console.log('Signing ' + absolutePath);
-				signtool.sign(absolutePath, {certificate: process.env.CSC_LINK, password: process.env.CSC_KEY_PASSWORD});
-			}
-		}			
-	})
+async function parseFiles(directory) {
+  const files = fs.readdirSync(directory);
+
+  for (const file of files) {
+    const fullPath = path.join(directory, file);
+
+    if (fs.statSync(fullPath).isDirectory() && fullPath !== 'node_modules\\obs-studio-node\\data\\obs-plugins\\win-capture') {
+      await parseFiles(fullPath);
+    } else {
+      const absolutePath = path.resolve(fullPath);
+      const ext = path.extname(absolutePath)
+
+      if (ext === '.dll' || ext === '.node' || ext === '.exe') {
+        console.log('Signing ' + absolutePath);
+        await signtool.sign(absolutePath, {
+          subject: 'Streamlabs (General Workings, Inc.)',
+          rfcTimestamp: 'http://timestamp.digicert.com'
+        });
+      }
+    }
+  }
 }
 
-parseFiles(osn);
-parseFiles(crash_handler);
+(async function() {
+  await parseFiles(osn);
+  await parseFiles(crash_handler);
+})();
+
