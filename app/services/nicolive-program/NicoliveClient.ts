827430af9f5a58557e19d0891d427b5bf478cb89
berlysia <berlysia@gmail.com>
2019-03-12 23:01:45 +0900

    feat(nicolive): 型定義を追加

--- app/services/nicolive-program/NicoliveClient.ts
+++ app/services/nicolive-program/NicoliveClient.ts
@@ -1,4 +1,14 @@
 import { remote, ipcRenderer } from 'electron';
+import {
+  ProgramSchedules,
+  ProgramInfo,
+  Segment,
+  Extension,
+  OperatorComment,
+  Statistics,
+  NicoadStatistics,
+  Communities,
+} from './ResponseTypes';
 const { BrowserWindow } = remote;
 
 export enum CreateResult {
@@ -12,7 +22,9 @@ export enum EditResult {
   OTHER = 'OTHER',
 }
 
-interface HeaderSeed { [key: string]: string }
+interface HeaderSeed {
+  [key: string]: string;
+}
 
 export class NicoliveClient {
   static live2BaseURL = 'https://live2.nicovideo.jp';
@@ -20,11 +32,11 @@ export class NicoliveClient {
   static nicoadBaseURL = 'https://api.nicoad.nicovideo.jp';
   private static frontendID = 134;
 
-  static isProgramPage(url: string) {
+  static isProgramPage(url: string): boolean {
     return /^https?:\/\/live2\.nicovideo\.jp\/watch\/lv\d+/.test(url);
   }
 
-  static isMyPage(url: string) {
+  static isMyPage(url: string): boolean {
     const urlObj = new URL(url);
     return (
       /^https?:$/.test(urlObj.protocol) &&
@@ -33,14 +45,11 @@ export class NicoliveClient {
     );
   }
 
-  static isAllowedURL(url: string) {
+  static isAllowedURL(url: string): boolean {
     return /^https?:\/\/live2?.nicovideo.jp\//.test(url);
   }
 
-  private static createRequest(
-    method: 'GET' | 'POST' | 'PUT' | 'DELETE',
-    requestInit: RequestInit
-  ): RequestInit {
+  private static createRequest(method: 'GET' | 'POST' | 'PUT' | 'DELETE', requestInit: RequestInit): RequestInit {
     return {
       method,
       mode: 'cors',
@@ -49,104 +58,102 @@ export class NicoliveClient {
     };
   }
 
-  private get(url: string | URL, options: RequestInit = {}) {
-    return fetch(url.toString(), NicoliveClient.createRequest('GET', options)).then(res => res.json());
+  private async get(url: string | URL, options: RequestInit = {}) {
+    const res = await fetch(url.toString(), NicoliveClient.createRequest('GET', options));
+    return res.json();
   }
 
-  private post(url: string | URL, options: RequestInit = {}) {
-    return fetch(url.toString(), NicoliveClient.createRequest('POST', options)).then(res => res.json());
+  private async post(url: string | URL, options: RequestInit = {}) {
+    const res = await fetch(url.toString(), NicoliveClient.createRequest('POST', options));
+    return res.json();
   }
 
-  private put(url: string | URL, options: RequestInit = {}) {
-    return fetch(url.toString(), NicoliveClient.createRequest('PUT', options)).then(res => res.json());
+  private async put(url: string | URL, options: RequestInit = {}) {
+    const res = await fetch(url.toString(), NicoliveClient.createRequest('PUT', options));
+    return res.json();
   }
 
-  async fetchProgramSchedules(headers?: HeaderSeed) {
-    return this.get(
-      `${NicoliveClient.live2BaseURL}/unama/tool/v1/program_schedules`,
-      { headers }
-    );
+  /** ユーザごとの番組スケジュールを取得 */
+  async fetchProgramSchedules(headers?: HeaderSeed): Promise<ProgramSchedules> {
+    return this.get(`${NicoliveClient.live2BaseURL}/unama/tool/v1/program_schedules`, { headers });
   }
 
-  async fetchProgram(programID: string, headers?: HeaderSeed) {
-    return this.get(
-      `${NicoliveClient.live2BaseURL}/watch/${programID}/programinfo`,
-      { headers }
-    );
+  /** 番組情報を取得 */
+  async fetchProgram(programID: string, headers?: HeaderSeed): Promise<ProgramInfo> {
+    return this.get(`${NicoliveClient.live2BaseURL}/watch/${programID}/programinfo`, { headers });
   }
 
-  async startProgram(programID: string, headers?: HeaderSeed) {
-    return this.put(
-      `${NicoliveClient.live2BaseURL}/watch/${programID}/segment`,
-      { headers: { 'Content-Type': 'application/json', ...headers }, body: JSON.stringify({ state: 'on_air' }) }
-    );
+  /** 番組を開始 */
+  async startProgram(programID: string, headers?: HeaderSeed): Promise<Segment> {
+    return this.put(`${NicoliveClient.live2BaseURL}/watch/${programID}/segment`, {
+      headers: { 'Content-Type': 'application/json', ...headers },
+      body: JSON.stringify({ state: 'on_air' }),
+    });
   }
 
-  async endProgram(programID: string, headers?: HeaderSeed) {
-    return this.put(
-      `${NicoliveClient.live2BaseURL}/watch/${programID}/segment`,
-      { headers: { 'Content-Type': 'application/json', ...headers }, body: JSON.stringify({ state: 'end' }) }
-    );
+  /** 番組を終了 */
+  async endProgram(programID: string, headers?: HeaderSeed): Promise<Segment> {
+    return this.put(`${NicoliveClient.live2BaseURL}/watch/${programID}/segment`, {
+      headers: { 'Content-Type': 'application/json', ...headers },
+      body: JSON.stringify({ state: 'end' }),
+    });
   }
 
-  async extendProgram(programID: string, minutes: number = 30, headers?: HeaderSeed) {
-    return this.post(
-      `${NicoliveClient.live2BaseURL}/watch/${programID}/extension`,
-      { headers: { 'Content-Type': 'application/json', ...headers }, body: JSON.stringify({ minutes })}
-    );
+  /** 番組を延長 */
+  async extendProgram(programID: string, minutes: number = 30, headers?: HeaderSeed): Promise<Extension> {
+    return this.post(`${NicoliveClient.live2BaseURL}/watch/${programID}/extension`, {
+      headers: { 'Content-Type': 'application/json', ...headers },
+      body: JSON.stringify({ minutes }),
+    });
   }
 
+  /** 運営コメントを送信 */
   async sendOperatorComment(
     programID: string,
-    { text, isPermanent }: { text: string, isPermanent?: boolean },
+    { text, isPermanent }: { text: string; isPermanent?: boolean },
     headers?: HeaderSeed
-  ) {
-    return this.put(
-      `${NicoliveClient.live2BaseURL}/watch/${programID}/operator_comment`,
-      {
-        headers: { 'Content-Type': 'application/json', ...headers },
-        body: JSON.stringify({ text, isPermanent }),
-      }
-    );
+  ): Promise<OperatorComment> {
+    return this.put(`${NicoliveClient.live2BaseURL}/watch/${programID}/operator_comment`, {
+      headers: { 'Content-Type': 'application/json', ...headers },
+      body: JSON.stringify({ text, isPermanent }),
+    });
   }
 
-  async fetchStatistics(programID: string, headers?: HeaderSeed) {
-    return this.get(
-      `${NicoliveClient.live2BaseURL}/watch/${programID}/statistics`,
-      { headers }
-    );
+  /** 統計情報（視聴者とコメント数）を取得 */
+  async fetchStatistics(programID: string, headers?: HeaderSeed): Promise<Statistics> {
+    return this.get(`${NicoliveClient.live2BaseURL}/watch/${programID}/statistics`, { headers });
   }
 
   // 関心が別だが他の場所におく程の理由もないのでここにおく
-  async fetchNicoadStatistics(programID: string, headers?: HeaderSeed) {
-    return this.get(
-      `${NicoliveClient.nicoadBaseURL}/v1/live/statusarea/${programID}`,
-      { headers }
-    );
+  /**
+   * ニコニ広告ptとギフトptを取得
+   * 放送開始前は404になる
+   **/
+  async fetchNicoadStatistics(programID: string, headers?: HeaderSeed): Promise<NicoadStatistics> {
+    return this.get(`${NicoliveClient.nicoadBaseURL}/v1/live/statusarea/${programID}`, { headers });
   }
 
   // 関心が別だが他の場所におく程の理由もないのでここにおく
-  async fetchCommunity(communityId: string, headers?: HeaderSeed) {
+  /** コミュニティ情報を取得 */
+  async fetchCommunity(communityId: string, headers?: HeaderSeed): Promise<Communities> {
     const url = new URL(`${NicoliveClient.publicBaseURL}/v1/communities.json`);
     const params = {
-      communityIds: communityId
+      communityIds: communityId,
     };
     for (const [key, value] of Object.entries(params)) {
       url.searchParams.append(key, value);
     }
 
-    return this.get(
-      url,
-      {
-        headers: {
-          ...headers,
-          'X-Frontend-Id': NicoliveClient.frontendID.toString(10)
-        },
-      })
+    return this.get(url, {
+      headers: {
+        ...headers,
+        'X-Frontend-Id': NicoliveClient.frontendID.toString(10),
+      },
+    });
   }
 
-  // webview
-  async createProgram() {
+  /** 番組作成画面を開いて結果を返す */
+  async createProgram(): Promise<CreateResult> {
     const win = new BrowserWindow({ width: 1200, height: 900 });
     return new Promise<CreateResult>((resolve, _reject) => {
       win.on('closed', () => resolve(CreateResult.OTHER));
@@ -167,8 +174,8 @@ export class NicoliveClient {
     });
   }
 
-  // webview
-  async editProgram(programID: string) {
+  /** 番組編集画面を開いて結果を返す */
+  async editProgram(programID: string): Promise<EditResult> {
     const win = new BrowserWindow({ width: 1200, height: 900 });
     return new Promise<EditResult>((resolve, _reject) => {
       win.on('closed', () => resolve(EditResult.OTHER));
---
+++ app/services/nicolive-program/ResponseTypes.ts
@@ -0,0 +1,231 @@
+export type ProgramSchedules =
+  | {
+      data: {
+        /** 番組ID */
+        nicoliveProgramId: string;
+        /** テスト開始時刻 */
+        testBeginAt: number;
+        /** 放送開始時刻 */
+        onAirBeginAt: number;
+        /** 放送終了予定時刻 */
+        onAirEndAt: number;
+        /** 配信コミュニティ・チャンネルID */
+        socialGroupId: string;
+        /** 配信状態 */
+        status: 'reserved' | 'test' | 'onAir' | 'end';
+      }[];
+      meta: {
+        statusCode: 200;
+        errorCode: 'OK';
+      };
+    }
+  | {
+      meta: {
+        status: number;
+        errorCode: string;
+      };
+      data?: { message: string };
+    };
+
+export type ProgramInfo =
+  | {
+      meta: {
+        status: 200;
+        errorCode: 'OK';
+      };
+      data: {
+        /** 番組の状態 */
+        status: 'test' | 'onAir' | 'end';
+        /** 番組の配信者に関係する情報 */
+        socialGroup:
+          | {
+              providerType: 'community';
+              /** コミュニティ名 */
+              name: string;
+              /** コミュニティID */
+              id: string;
+            }
+          | {
+              providerType: 'channel';
+              /** チャンネル名 */
+              name: string;
+              /** チャンネルID */
+              id: string;
+              /** 配信会社名 */
+              ownerName: string;
+              /** コミュレベル */
+              communityLevel: number;
+            };
+        /** コメントのルーム */
+        rooms: {
+          /** 部屋ID */
+          id: number;
+          /** 部屋名 */
+          name: string;
+          /** コメントサーバーの接続先(WebSocket) */
+          webSocketUri: string;
+          /** コメントサーバーの接続先(XmlSocket) */
+          xmlSocketUri: string;
+          /** スレッドID */
+          threadId: string;
+        }[];
+        /** 番組タイトル */
+        title: string;
+        /** 番組情報 */
+        description: string;
+        /** メンバー限定放送であるか */
+        isMemberOnly: boolean;
+        /** vpos基準時刻 */
+        vposBaseTime: number;
+        /** 開演時間 */
+        beginAt: number;
+        /** 終了時間 */
+        endAt: number;
+        /** 番組のカテゴリ・追加カテゴリ一覧 */
+        categories: string[];
+        /** ニコニ広告許可設定 */
+        isAdsEnabled: boolean;
+        /** 配信者情報 */
+        broadcaster: {
+          /** 配信者/チャンネルのID */
+          id: number;
+          /** 配信者/配信会社名 */
+          name: string;
+        }[];
+      };
+    }
+  | {
+      meta: {
+        status: number;
+        errorCode: string;
+      };
+      data?: { message: string };
+    };
+
+export type Segment =
+  | {
+      meta: {
+        status: 200;
+        errorCode: 'OK';
+      };
+      data: {
+        end_time: number;
+        start_time: number;
+      };
+    }
+  | {
+      meta: {
+        status: number;
+        errorCode: string;
+        errorMessage: string;
+      };
+      data?: { message: string };
+    };
+
+export type Extension =
+  | {
+      meta: {
+        status: 200;
+        errorCode: 'OK';
+      };
+      data: {
+        end_time: number;
+      };
+    }
+  | {
+      meta: {
+        status: number;
+        errorCode: string;
+        errorMessage: string;
+      };
+      data?: { message: string };
+    };
+
+export type OperatorComment =
+  | {
+      meta: {
+        status: 200;
+        errorCode: 'OK';
+      };
+    }
+  | {
+      meta: {
+        status: number;
+        errorCode: string;
+        errorMessage: string;
+      };
+      data?: { message: string };
+    };
+
+export type Statistics =
+  | {
+      meta: {
+        status: 200;
+        errorCode: 'OK';
+      };
+      data: {
+        watchCount: number;
+        commentCount: number;
+      };
+    }
+  | {
+      meta: {
+        status: number;
+        errorCode: string;
+      };
+      data?: { message: string };
+    };
+
+export type NicoadStatistics =
+  | {
+      meta: {
+        status: 200;
+      };
+      data: {
+        totalAdPoint: number;
+        totalGiftPoint: number;
+        conductors: {
+          text: string;
+          url: string;
+        }[];
+      };
+    }
+  | {
+      meta: {
+        status: number;
+        errorCode: string;
+        errorMessage: string;
+      };
+    };
+
+export type Communities =
+  | {
+      meta: {
+        status: 200;
+      };
+      data: {
+        // 使用しそうなものだけ雑に抜粋
+        id: number;
+        name: string;
+        description: 'string';
+        status: 'open' | 'closed';
+        thumbnail_url: {
+          normal: string;
+          small: string;
+        };
+        thumbnails: {
+          '1024x1024': string;
+          '512x512': string;
+          '256x256': string;
+          '128x128': string;
+          '64x64': string;
+        };
+      }[];
+    }
+  | {
+      meta: {
+        status: number;
+        'error-code': string;
+        'error-message': string;
+      };
+    };
