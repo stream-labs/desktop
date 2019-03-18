adcfb404397fbc20f47ae2613326d556ed372cce
berlysia <berlysia@gmail.com>
2019-03-18 16:28:10 +0900

    feat(nicolive): メッセージボックス表示を追加

--- app/components/nicolive-area/NicoliveArea.vue.ts
+++ app/components/nicolive-area/NicoliveArea.vue.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import { Component, Watch } from 'vue-property-decorator';
 import { Inject } from 'util/injector';
 import { NicoliveProgramService } from 'services/nicolive-program/nicolive-program';
+import { remote } from 'electron';
+import { $t } from 'services/i18n';
 
 @Component({
   components: {},
@@ -29,8 +31,21 @@ export default class NicolivePanelRoot extends Vue {
       this.isFetching = true;
       return await this.nicoliveProgramService.fetchProgram();
     } catch (e) {
-      // TODO
       console.warn(e);
+      // TODO: 翻訳
+      // TODO: エラー理由を見て出し分ける
+      await new Promise(resolve => {
+        remote.dialog.showMessageBox(
+          remote.getCurrentWindow(),
+          {
+            type: 'warning',
+            message: 'ニコニコ生放送にて番組が作成されていません。\n［番組作成］ボタンより、番組を作成してください。',
+            buttons: [$t('common.ok')],
+            noLink: true,
+          },
+          done => resolve(done)
+        );
+      });
     } finally {
       this.isFetching = false;
     }
@@ -78,7 +93,23 @@ export default class NicolivePanelRoot extends Vue {
   async endProgram() {
     try {
       this.isEnding = true;
-      return await this.nicoliveProgramService.endProgram();
+      const isOk = await new Promise(resolve => {
+        // TODO: 翻訳
+        remote.dialog.showMessageBox(
+          remote.getCurrentWindow(),
+          {
+            type: 'warning',
+            message: '番組を終了しますか？',
+            buttons: ['終了する', $t('common.cancel')],
+            noLink: true,
+          },
+          idx => resolve(idx === 0)
+        );
+      });
+
+      if (isOk) {
+        return await this.nicoliveProgramService.endProgram();
+      }
     } catch (e) {
       // TODO
       console.warn(e);
