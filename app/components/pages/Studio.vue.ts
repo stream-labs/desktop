8f9181c63c5b74954a677874a77f85b338569a2e
Sean Beyer <sean.beyer@streamlabs.com>
2019-03-21 14:57:55 -0700

    Remove unused import

--- app/components/pages/Studio.vue.ts
+++ app/components/pages/Studio.vue.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import { Component, Watch } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
 import { CustomizationService } from 'services/customization';
 import StudioEditor from 'components/StudioEditor.vue';
 import StudioControls from 'components/StudioControls.vue';
