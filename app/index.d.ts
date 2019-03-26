5ee39a1329439e2bf53bba9248ff15695d234753
Piotr Kucharski <kuhasuki@gmail.com>
2019-03-26 16:14:30 -0700

    Face Mask Subscriptions and Bits (#1516)
    * Fix download progress calculations
    
    * Check for face mask plugin dll after checking if face masks are enabled
    
    * Reduce cognitive complexity
    
    * apply suggested changes, use guard statements
    
    * Make code more consistent
    
    * Add sl beaker
    
    * Declare beaker
    
    * Add FM button to topnav
    
    * Make fm service more useable from child window
    
    * add fm settings window
    
    * WIP fm settings window
    
    * Add beaker
    
    * Only show FM button to logged in users
    
    * Update fm settings form WIP
    
    * Add more data to FM settings form
    
    * Add new features to FM service
    
    * Update beaker version
    
    * Add counter to FM setting for sub mask selection
    
    * Update settings page
    
    * Begin adding bits and subs support
    
    * Add bits socket event
    
    * Encode alert jank into type system
    
    * Add support for subscription and bits events
    
    * Update vue version to 2.6.6 to fix duplicate vue instances bug
    
    * new yarn
    
    * Tidy up the settings form
    
    * Update settings form
    
    * Clean up face mask service add settings update method
    
    * Add more socket evets
    
    * Update icon for face masks
    
    * Fix old code
    
    * Maybe shut the robot up
    
    * flatten deps and fix prod builds
    
    * flatten vee-validate
    
    * flatten vue-class-component
    
    * flatten vue-color
    
    * flatten vue-property-decorator
    
    * Appease robot overlords
    
    * Superficial change
    
    * Refactor
    
    * Code cleanup
    
    * Move facemask service into a folder
    
    * Import interfaces differently
    
    * Fix automatic filter application
    
    * Fix bug with normal bits events
    
    * Bandaid fix for switching scene collections
    
    * More bandaids
    
    * Validate webcam required correctly
    
    * Fix ui bug
    
    * increase node mem for all webpack commands
    
    * Add semicolon
    
    * Fix lint
    
    * v-tooltip dep
    
    * vue-js-modal
    
    * try forcing resolutions for vue-slider-component

--- app/index.d.ts
+++ app/index.d.ts
@@ -70,6 +70,7 @@ declare module 'vue-resize';
 declare module 'serve-handler';
 declare module 'v-selectpage';
 declare module '*.m.less';
+declare module 'streamlabs-beaker';
 
 // uncomment to allow TS to import components without type definitions
 // webpack still checks the module existence
