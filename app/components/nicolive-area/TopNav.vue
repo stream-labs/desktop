fba374ca552f84d034fa8a46dd00d32d0810df5d
berlysia <berlysia@gmail.com>
2019-04-01 19:27:48 +0900

    feat(nicolive): 関連リンクを追加

--- app/components/nicolive-area/TopNav.vue
+++ app/components/nicolive-area/TopNav.vue
@@ -3,6 +3,9 @@
     <div v-if="hasProgram">
       <button @click="fetchProgram" :disabled="isFetching">番組取得</button>
       <button @click="editProgram" :disabled="isEditing">番組編集</button>
+      <a @click.prevent="openInDefaultBrowser($event)" :href="twitterShareURL">share in twitter</a>
+      <a @click.prevent="openInDefaultBrowser($event)" :href="contentTreeURL">コンテンツツリー</a>
+      <a @click.prevent="openInDefaultBrowser($event)" :href="creatorsProgramURL">クリエイター奨励プログラム</a>
     </div>
   </div>
 </template>
