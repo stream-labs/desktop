974ff4b04ac91c434aba105a6bc46558545d16b6
berlysia <berlysia@gmail.com>
2019-04-02 19:18:30 +0900

    featnicolive): 予約番組の開始までのカウントダウンを表示

--- app/components/nicolive-area/ToolBar.vue
+++ app/components/nicolive-area/ToolBar.vue
@@ -5,6 +5,9 @@
     <button @click="extendProgram" :disabled="autoExtensionEnabled || isExtending || !isProgramExtendable">
       30分延長
     </button>
+    <div v-if="programStatus === 'reserved'">
+      番組開始まで {{ format(-programCurrentTime) }}
+    </div>
     <span>自動延長 <input type="checkbox" :checked="autoExtensionEnabled" @click="toggleAutoExtension" /></span>
   </div>
 </template>
