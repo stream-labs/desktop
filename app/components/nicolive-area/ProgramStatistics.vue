e81a6c6f21c38d88d7540d7f9663420276efe781
yusukess <43235200+yusukess@users.noreply.github.com>
2019-04-04 17:36:50 +0900

    追加: 番組統計エリアを追加

---
+++ app/components/nicolive-area/ProgramStatistics.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <span>視聴者数: {{ viewers }}</span>
+            <span>コメント数: {{ comments }}</span>
+            <span>ニコニ広告pt: {{ adPoint }}</span>
+            <span>ギフトpt: {{ giftPoint }}</span>
+        </div>
+        <div>
+            <a @click.prevent="openInDefaultBrowser($event)" :href="twitterShareURL">share in twitter</a>
+            <a @click.prevent="openInDefaultBrowser($event)" :href="contentTreeURL">コンテンツツリー</a>
+            <a @click.prevent="openInDefaultBrowser($event)" :href="creatorsProgramURL">クリエイター奨励プログラム</a>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" src="./ProgramStatistics.vue.ts"></script>
--- app/components/nicolive-area/TopNav.vue.ts
+++ app/components/nicolive-area/TopNav.vue.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
-import * as moment from 'moment';
 import { Inject } from 'util/injector';
 import { NicoliveProgramService } from 'services/nicolive-program/nicolive-program';
 import { remote } from 'electron';
@@ -13,66 +12,6 @@ export default class TopNav extends Vue {
 
   get hasProgram(): boolean {
     return this.nicoliveProgramService.hasProgram;
-  }
-
-  get programID(): string {
-    return this.nicoliveProgramService.state.programID;
-  }
-
-  get programStatus() { // 推論させる
-    return this.nicoliveProgramService.state.status;
-  }
-
-  get contentTreeURL(): string {
-    return `https://commons.nicovideo.jp/tree/${this.programID}`;
-  }
-
-  get creatorsProgramURL(): string {
-    return `https://commons.nicovideo.jp/cpp/application/?site_id=nicolive&creation_id=${this.programID}`;
-  }
-
-  get twitterShareURL(): string {
-    const content = this.twitterShareContent();
-    const url = new URL('https://twitter.com/intent/tweet');
-    url.searchParams.append('text', content.text);
-    url.searchParams.append('url', content.url);
-    return url.toString();
-  }
-
-  private twitterShareContent(): { text: string, url: string } {
-    const title = this.nicoliveProgramService.state.title;
-    const url = `https://live.nicovideo.jp/watch/${this.programID}?ref=sharetw`;
-    const time = this.nicoliveProgramService.state.startTime;
-    const formattedTime = moment.unix(time).format('YYYY/MM/DD HH:mm');
-
-    if (this.programStatus === 'reserved' || this.programStatus === 'test') {
-      return {
-        text: `【ニコ生(${formattedTime}開始)】${title}`,
-        url,
-      };
-    }
-
-    if (this.programStatus === 'onAir') {
-      return {
-        text: `【ニコ生配信中】${title}`,
-        url,
-      };
-    }
-
-    if (this.programStatus === 'end') {
-      return {
-        text: `【ニコ生タイムシフト視聴中(${formattedTime}放送)】${title}`,
-        url,
-      };
-    }
-  }
-
-  openInDefaultBrowser(event: MouseEvent): void {
-    const href = (event.currentTarget as HTMLAnchorElement).href;
-    const url = new URL(href);
-    if (/^https?/.test(url.protocol)) {
-      remote.shell.openExternal(url.toString());
-    }
   }
 
   isFetching: boolean = false;
