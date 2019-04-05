pajenterprise/streamlabs-obs

feat(settings): display tabs for subcategories in Advanced output

app/components/obs/inputs/GenericFormGroups.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="form-groups">
+  <GenericFormTabbed v-model="value" v-if="isTabbed" @input="onInputHandler" />
+  <div class="form-groups" v-else>
     <div
       class="section"
       v-for="(formGroup, groupIndex) in value"
       :key="formGroup.nameSubCategory + groupIndex"
       v-if="hasAnyVisibleSettings(formGroup)"
-      ref="container">
-
+      ref="container"
+    >
       <h2
         class="section-title section-title--dropdown"
         v-if="formGroup.nameSubCategory !== 'Untitled'"
-        @click="toggleGroup(groupIndex)">
-        <i class="fa fa-plus section-title__icon"  v-show="collapsedGroups[groupIndex]"></i>
+        @click="toggleGroup(groupIndex);"
+      >
+        <i class="fa fa-plus section-title__icon" v-show="collapsedGroups[groupIndex]"></i>
         <i class="fa fa-minus section-title__icon" v-show="!collapsedGroups[groupIndex]"></i>
         {{ $t(formGroup.nameSubCategory) }}
       </h2>
@@ -32,7 +34,7 @@
 <script lang="ts" src="./GenericFormGroups.vue.ts"></script>
 
 <style lang="less">
-@import "../../../styles/index";
+@import '../../../styles/index';
 
 .expand-enter {
   max-height: 0;
@@ -41,7 +43,7 @@
 
 .expand-enter-active,
 .expand-leave-active {
-  transition: all .5s ease;
+  transition: all 0.5s ease;
 }
 
 .expand-enter-to {
@@ -51,7 +53,7 @@
 
 .expand-leave,
 .expand-leave-to {
-  transition: all .275s ease;
+  transition: all 0.275s ease;
 }
 
 .expand-leave {

stream-labs/desktop

app/components/obs/inputs/GenericFormGroups.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="form-groups">
+  <AdvancedOutputTabs v-model="value" v-if="isAdvancedOutput" @input="onInputHandler" />
+  <div class="form-groups" v-else>
     <div
       class="section"
       v-for="(formGroup, groupIndex) in value"
       :key="formGroup.nameSubCategory + groupIndex"
       v-if="hasAnyVisibleSettings(formGroup)"
-      ref="container">
-
+      ref="container"
+    >
       <h2
         class="section-title section-title--dropdown"
         v-if="formGroup.nameSubCategory !== 'Untitled'"
-        @click="toggleGroup(groupIndex)">
-        <i class="fa fa-plus section-title__icon"  v-show="collapsedGroups[groupIndex]"></i>
+        @click="toggleGroup(groupIndex);"
+      >
+        <i class="fa fa-plus section-title__icon" v-show="collapsedGroups[groupIndex]"></i>
         <i class="fa fa-minus section-title__icon" v-show="!collapsedGroups[groupIndex]"></i>
         {{ $t(formGroup.nameSubCategory) }}
       </h2>
@@ -32,7 +34,7 @@
 <script lang="ts" src="./GenericFormGroups.vue.ts"></script>
 
 <style lang="less">
-@import "../../../styles/index";
+@import '../../../styles/index';
 
 .expand-enter {
   max-height: 0;
@@ -41,7 +43,7 @@
 
 .expand-enter-active,
 .expand-leave-active {
-  transition: all .5s ease;
+  transition: all 0.5s ease;
 }
 
 .expand-enter-to {
@@ -51,7 +53,7 @@
 
 .expand-leave,
 .expand-leave-to {
-  transition: all .275s ease;
+  transition: all 0.275s ease;
 }
 
 .expand-leave {
023db780ec9e4e0fb2493328bfe5f142e7027c0e
Adrian Perez <adrian@adrianperez.org>
2019-03-22 15:51:05 -0700

    feat(settings): display tabs for subcategories in Advanced output

--- app/components/obs/inputs/GenericFormGroups.vue.ts
+++ app/components/obs/inputs/GenericFormGroups.vue.ts
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import GenericForm from './GenericForm.vue';
+import AdvancedOutputTabs from './AdvancedOutputTabs.vue';
 import { Component, Prop } from 'vue-property-decorator';
 import { ISettingsSubCategory } from '../../../services/settings';
 
 @Component({
-  components: { GenericForm },
+  components: { AdvancedOutputTabs, GenericForm },
 })
 export default class GenericFormGroups extends Vue {
   @Prop()
@@ -24,5 +25,16 @@ export default class GenericFormGroups extends Vue {
     return !!category.parameters.find(setting => {
       return setting.visible;
     });
+  }
+
+  get isAdvancedOutput() {
+    // TODO: improve this check after backend changes required to enable ECategoryType detection
+    return (
+      this.value[0] &&
+      this.value[0].parameters.length &&
+      this.value[0].parameters[0].value === 'Advanced' &&
+      this.value[1] &&
+      this.value[1].nameSubCategory === 'Streaming'
+    );
   }
 }
