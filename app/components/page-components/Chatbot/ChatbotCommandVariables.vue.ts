a202ea5c1809aaf33fe6ecf82be8d6ffa9af1d1d
AnkhHeart <yours-truly@hotmail.be>
2019-04-07 22:21:07 +0200

    Command Variable Filtering
    - Command Variable Filtering

--- app/components/page-components/Chatbot/ChatbotCommandVariables.vue.ts
+++ app/components/page-components/Chatbot/ChatbotCommandVariables.vue.ts
@@ -1,9 +1,52 @@
 import ChatbotBase from 'components/page-components/Chatbot/ChatbotBase.vue';
 import { Component } from 'vue-property-decorator';
+import { Accordion, Badge } from 'streamlabs-beaker';
+import mapValues from 'lodash/mapValues';
+import pickBy from 'lodash/pickBy';
+@Component({
+  components: { Accordion, Badge },
+})
+export default class ChatbotCommandVariables extends ChatbotBase {
+  searchQuery = '';
 
-export default class ChatbotDefaultCommands extends ChatbotBase {
-  get variables() {
-    return this.chatbotApiService.Commands.state.commandVariablesResponse;
+  get filteredVariables() {
+    const grouped = {};
+    const arr = this.chatbotApiService.Commands.state.commandVariablesResponse as any[];
+
+    for (const variable of arr) {
+      for (const tag of variable.tags) {
+        if (!grouped[tag]) {
+          grouped[tag] = [];
+          grouped[tag].push(variable);
+        } else {
+          grouped[tag].push(variable);
+        }
+      }
+    }
+
+    const filteredVariables = mapValues(grouped, (section, slug) => {
+      return pickBy(
+        mapValues(section, (variable: any) => {
+          const found =
+            slug.toLowerCase().indexOf(this.searchQuery.toLowerCase()) > -1 ||
+            variable.variable.toLowerCase().indexOf(this.searchQuery.toLowerCase()) > -1 ||
+            variable.example.toLowerCase().indexOf(this.searchQuery.toLowerCase()) > -1 ||
+            variable.result.toLowerCase().indexOf(this.searchQuery.toLowerCase()) > -1;
+          return found ? variable : undefined;
+        }),
+        (x, y) => {
+          return x !== undefined;
+        },
+      );
+    });
+
+    const remaining = pickBy(filteredVariables, (section, slug) => {
+      return Object.keys(section).length !== 0;
+    });
+
+    console.log(remaining);
+
+    return remaining;
   }
 
   mounted() {
