3dd5a2638d23b44afa76a374a86c297410eb4f76
yusukess <43235200+yusukess@users.noreply.github.com>
2019-04-09 19:25:08 +0900

    開閉ボタンにスタイルを適用

--- app/components/nicolive-area/NicoliveArea.vue
+++ app/components/nicolive-area/NicoliveArea.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="root">
     <button @click="onToggle" class="nicolive-area-toggle-button" :class="{ 'nicolive-area--opened': opened }">
+      <ControlsArrow />
     </button>
-    <div class="nicolive-area" v-if="opened">
+    <div class="nicolive-area-container" v-if="opened">
       <top-nav />
       <template v-if="hasProgram">
         <program-info />
@@ -22,21 +23,53 @@
 <script lang="ts" src="./NicoliveArea.vue.ts"></script>
 
 <style lang="less" scoped>
+@import "../../styles/_colors";
+@import "../../styles/mixins";
+
 .root {
   display: flex;
   height: 100%;
+  border-left: 1px solid @bg-secondary;
 }
 
 .nicolive-area-toggle-button {
-  display: block;
-  height: 100%;
-  width: 8px;
-  // debug
-  background-color: white;
+  width: 16px;
+  height: 180px;
+  margin: auto 8px auto auto;
+  background-color: @bg-primary;
+  border-width: 1px 1px 1px 0;
+  border-style: solid;
+  border-color: @bg-secondary;
+
+  > svg {
+    width: 10px;
+    height: 140px;
+    fill: @bg-secondary;
+    transition: .5s;
+    transform: rotate(180deg);
+  }
+
+  &:hover {
+    background-color: @bg-secondary;
+
+    > svg {
+      fill: @text-primary;
+    }
+  }
+
+  &.nicolive-area--opened {
+    > svg {
+      transform: rotate(0deg);
+    }
+  }
 }
 
-.nicolive-area {
-  flex-basis: fill;
-  width: 360px;
+.nicolive-area-container {
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  align-items: center;
+  background-color: @bg-primary;
 }
 </style>
