8862d636e4844cce029a5a4df297d57c3fb0660f
Andy Creeth <acreeth@gmail.com>
2019-04-09 16:03:54 -0700

    hotkeys settings performance

--- app/components/Hotkeys.vue
+++ app/components/Hotkeys.vue
@@ -1,17 +1,19 @@
 <template>
 <div>
-  <hotkey-group
-    :hotkeys="hotkeySet.general"/>
-  <hotkey-group
-    v-for="(scenesHotkeys, sceneId) in hotkeySet.scenes"
-    :key="sceneId"
-    :title="scenesService.getScene(sceneId).name"
-    :hotkeys="scenesHotkeys"/>
-  <hotkey-group
-    v-for="(sourceHotkeys, sourceId) in hotkeySet.sources"
-    :key="sourceId"
-    :title="sourcesService.getSource(sourceId).name"
-    :hotkeys="sourceHotkeys"/>
+  <div v-if="hotkeySet">
+    <hotkey-group
+      :hotkeys="hotkeySet.general"/>
+    <hotkey-group
+      v-for="(scenesHotkeys, sceneId) in hotkeySet.scenes"
+      :key="sceneId"
+      :title="scenesService.getScene(sceneId).name"
+      :hotkeys="scenesHotkeys"/>
+    <hotkey-group
+      v-for="(sourceHotkeys, sourceId) in hotkeySet.sources"
+      :key="sourceId"
+      :title="sourcesService.getSource(sourceId).name"
+      :hotkeys="sourceHotkeys"/>
+    </div>
 </div>
 </template>
 
