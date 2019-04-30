b54a49bd1e02fd2558ac4e639cc150e938597bd8
Sean Beyer <sean.beyer@streamlabs.com>
2019-04-30 11:50:36 -0700

    Port to beaker slider

--- app/components/shared/inputs/SliderInput.vue
+++ app/components/shared/inputs/SliderInput.vue
@@ -6,33 +6,17 @@
     data-type="slider"
     :data-name="options.name"
   >
-    <vue-slider
-      class="slider w-form-group__input"
+    <slider
+      style="width: 100%;"
       :value="localValue"
       @input="value => updateLocalValue(value)"
-      :max="options.max"
+      :max="max"
       :min="options.min"
       :interval="options.interval"
       :tooltip="options.displayValue || 'always'"
-      :speed="0"
-      :height="4"
-      :formatter="formatter"
-      :piecewise="options.piecewise || (options.interval && options.interval >= 1)"
-      :piecewiseLabel="options.piecewiseLabel"
+      :suffix="usePercentages ? '%' : ''"
       :data="options.data"
       ref="slider"
-      :piecewiseStyle="{
-        position: 'absolute',
-        backgroundColor: sliderColor[theme],
-        height: '2px',
-        width: '2px',
-        'borderRadius': '1px',
-        top: '12px'
-    }"
-      :labelStyle="{ color: sliderColor[theme]  }"
-      :piecewiseActiveStyle="{ backgroundColor: '#3c4c53' }"
-      :sliderStyle="options.sliderStyle"
-      :dotSize="options.dotSize"
     />
     <input
       v-if="options.hasValueBox && !options.usePercentages"
@@ -58,66 +42,5 @@
 
 .slider-container.has-tooltip {
   padding-bottom: 16px;
-}
-
-.slider-input {
-  .margin-left(3);
-  width: 60px;
-}
-
-.slider {
-  background: transparent;
-  .padding-v-sides();
-  .padding-h-sides(@0) !important;
-  margin: 0;
-  flex-grow: 1;
-  height: auto;
-
-  &:hover {
-    .vue-slider-tooltip {
-      color: var(--paragraph) !important;
-    }
-  }
-}
-
-.vue-slider {
-  background-color: var(--slider-bg) !important;
-}
-
-.vue-slider-process {
-  background-color: var(--slider-progress) !important;
-}
-
-.vue-slider-dot {
-  &:after {
-    content: '';
-    display: block;
-    position: absolute;
-    left: 5px;
-    top: 5px;
-    width: 5px;
-    height: 5px;
-    border-radius: 50%;
-    background-color: var(--slider-progress);
-  }
-}
-
-.vue-slider-tooltip {
-  background-color: transparent !important;
-  border: none !important;
-  color: var(--title) !important;
-  font-size: 13px !important;
-  top: 40px !important;
-  .transition;
-
-  &:before {
-    display: none;
-  }
-}
-
-.vue-slider-piecewise {
-  .vue-slider-piecewise-dot {
-    display: none !important;
-  }
 }
 </style>
