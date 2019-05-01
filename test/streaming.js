fc9ed653b7f752e2c15faedd2c6fcb95604c4ddc
Alexander Nazarov <kadabrik91@gmail.com>
2019-05-01 15:47:53 -0700

    reveal mixer tests

--- test/streaming.js
+++ test/streaming.js
@@ -3,8 +3,9 @@ import { useSpectron, focusMain, focusChild } from './helpers/spectron/index';
 import { setFormInput } from './helpers/spectron/forms';
 import { fillForm } from './helpers/form-monkey';
 import { logIn } from './helpers/spectron/user';
+import { sleep } from './helpers/sleep';
 
-useSpectron({ appArgs: '--nosync' });
+useSpectron({ appArgs: '--nosync', pauseIfFailed: true });
 
 test('Streaming to Twitch without auth', async t => {
   if (!process.env.SLOBS_TEST_STREAM_KEY) {
@@ -88,31 +89,30 @@ test('Streaming to Facebook', async t => {
   t.pass();
 });
 
-// TODO: Mixer blocks account
-// test('Streaming to Mixer', async t => {
-//
-//   // login into the account
-//   if (!(await logIn(t, 'mixer'))) return;
-//   const app = t.context.app;
-//
-//   // open EditStreamInfo window
-//   await focusMain(t);
-//   await app.client.click('button=Go Live');
-//
-//   // set stream info, and start stream
-//   await focusChild(t);
-//   await fillForm(t, 'form[name=editStreamForm]', {
-//     stream_title: 'SLOBS Test Stream',
-//     game: 'PLAYERUNKNOWN\'S BATTLEGROUNDS',
-//   });
-//
-//   await app.client.click('button=Confirm & Go Live');
-//
-//   // check we're streaming
-//   await focusMain(t);
-//   await app.client.waitForExist('button=End Stream', 10 * 1000);
-//   t.pass();
-// });
+test('Streaming to Mixer', async t => {
+
+  // login into the account
+  if (!(await logIn(t, 'mixer'))) return;
+  const app = t.context.app;
+
+  // open EditStreamInfo window
+  await focusMain(t);
+  await app.client.click('button=Go Live');
+
+  // set stream info, and start stream
+  await focusChild(t);
+  await fillForm(t, 'form[name=editStreamForm]', {
+    stream_title: 'SLOBS Test Stream',
+    game: 'PLAYERUNKNOWN\'S BATTLEGROUNDS',
+  });
+
+  await app.client.click('button=Confirm & Go Live');
+
+  // check we're streaming
+  await focusMain(t);
+  await app.client.waitForExist('button=End Stream', 10 * 1000);
+  t.pass();
+});
 
 test('Streaming to Youtube', async t => {
 
