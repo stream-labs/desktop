b5cd9e6a1d9dc3d79c9717e4552d1c086d645018
Sean Beyer <sean.beyer@streamlabs.com>
2019-05-08 16:29:16 -0700

    Fix tab reactivity

--- app/components/obs/inputs/GenericFormGroups.vue.ts
+++ app/components/obs/inputs/GenericFormGroups.vue.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import GenericForm from './GenericForm.vue';
 import AdvancedOutputTabs from './AdvancedOutputTabs.vue';
-import { Component, Prop } from 'vue-property-decorator';
+import { Component, Prop, Watch } from 'vue-property-decorator';
 import { ISettingsSubCategory, SettingsService } from '../../../services/settings';
 import { Inject } from 'util/injector';
 
@@ -16,21 +16,26 @@ export default class GenericFormGroups extends Vue {
 
   collapsedGroups: Dictionary<boolean> = {};
 
+  isAdvancedOutput = this.settingsService.isTabbedForm(this.categoryName) || false;
+
   toggleGroup(index: string) {
     this.$set(this.collapsedGroups, index, !this.collapsedGroups[index]);
   }
 
   onInputHandler() {
     this.$emit('input', this.value);
+
+    this.$nextTick(this.updateIsAdvancedOutput);
+  }
+
+  @Watch('categoryName')
+  updateIsAdvancedOutput() {
+    this.isAdvancedOutput = this.settingsService.isTabbedForm(this.categoryName);
   }
 
   hasAnyVisibleSettings(category: ISettingsSubCategory) {
     return !!category.parameters.find(setting => {
       return setting.visible;
     });
-  }
-
-  get isAdvancedOutput() {
-    return this.settingsService.isTabbedForm(this.categoryName);
   }
 }
