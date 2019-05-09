4a3009cf3a3cd98b6ee2caa87b935ef81eadb0c6
Alexander Nazarov <kadabrik91@gmail.com>
2019-05-09 13:48:05 -0700

    run full tests

--- .buildkite/run-tests.ps1
+++ .buildkite/run-tests.ps1
@@ -1,21 +1,14 @@
+echo "IP for RDP connections:" $(Invoke-RestMethod ipinfo.io/ip)
+
 yarn install
 yarn compile
-yarn test --match="Main and child window visibility"
-echo 'jobsdone';
+yarn test-flaky
+
 #Restart-Computer
 $exitCode = $LastExitCode;
 
 echo 'Start shutdown job';
-#Get-ScheduledJob | Where-Object {$_.Name -eq "RestartJob"} | Unregister-ScheduledJob -Force
-#(Register-ScheduledJob -Name "RestartJob" -ScriptBlock {
-#  $agentPid = (Get-Process | Where-Object {$_.ProcessName -eq "buildkite-agent"}).Id
-#  $wshell = New-Object -ComObject wscript.shell;
-#  $wshell.AppActivate($agentPid)
-#  Sleep 1
-#  $wshell.SendKeys("^(C)")
-#  Sleep 5
-#  Restart-Computer;
-#}).StartJob()
+
 Start-Process powershell -ArgumentList '-file ".buildkite/reboot.ps1"'
 
 echo 'Done';
