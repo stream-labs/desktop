fccb404a6e32f80019b5ef7db69f1d224e1bc370
Alexander Nazarov <kadabrik91@gmail.com>
2019-05-09 10:59:49 -0700

    add restart script

--- .buildkite/run-tests.ps1
+++ .buildkite/run-tests.ps1
@@ -3,4 +3,16 @@
 #yarn test --match="Main and child window visibility"
 echo 'jobsdone';
 #Restart-Computer
-exit $LastExitCode
+$exitCode = $LastExitCode;
+
+
+Start-Job -Name "RestartPC" -ScriptBlock {
+  $wshell = New-Object -ComObject wscript.shell;
+  $wshell.AppActivate("C:\buildkite-agent\bin\buildkite-agent.exe")
+  Sleep 1
+  $wshell.SendKeys("^(C)")
+  Sleep 5
+  Restart-Computer
+}
+
+exit $exitCode
