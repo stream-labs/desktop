8ec30b1b6ac0732bf57fa82b9dfd2ebb20c5cb98
Sean Beyer <sean.beyer@streamlabs.com>
2019-05-13 16:41:42 -0700

    Use Beaker Slider (#1677)
    * Port to beaker slider
    
    * Update beaker
    
    * Use vue-slider 2.8
    
    * Remove slider tests from widget tests

--- app/components/shared/inputs/SliderInput.vue.ts
+++ app/components/shared/inputs/SliderInput.vue.ts
@@ -1,6 +1,6 @@
-import VueSlider from 'vue-slider-component';
 import { debounce } from 'lodash-decorators';
 import { Component, Prop, Watch } from 'vue-property-decorator';
+import { Slider } from 'streamlabs-beaker';
 import { BaseInput } from './BaseInput';
 import { CustomizationService } from 'services/customization';
 import ResizeSensor from 'css-element-queries/src/ResizeSensor';
@@ -9,7 +9,7 @@ import { ISliderMetadata } from './index';
 import { isString } from 'util';
 
 @Component({
-  components: { VueSlider },
+  components: { Slider },
 })
 export default class SliderInput extends BaseInput<number, ISliderMetadata> {
   @Inject() customizationService: CustomizationService;
@@ -17,26 +17,26 @@ export default class SliderInput extends BaseInput<number, ISliderMetadata> {
   @Prop() readonly value: number;
   @Watch('value')
   private syncLocalValue(newVal: number) {
-    this.localValue = newVal;
+    this.localValue = this.usePercentages ? newVal * 100 : newVal;
   }
 
   @Prop() readonly title: string;
   @Prop() readonly metadata: ISliderMetadata;
 
-  usePercentages: boolean;
-  interval: number;
+  usePercentages: boolean = this.options.usePercentages || false;
+  interval: number = this.options.usePercentages
+    ? this.options.interval * 100 || 1
+    : this.options.interval || 1;
   isFullyMounted = false;
 
   // The displaying value on and within the ui components.
-  localValue: number | string = this.value || 0;
+  localValue: number | string = this.options.usePercentages
+    ? this.value * 100 || this.min
+    : this.value || this.min || 0;
 
   $refs: { slider: any };
 
   mounted() {
-    // setup defaults
-    this.interval = this.options.interval || 1;
-    this.usePercentages = this.options.usePercentages || false;
-
     // Hack to prevent transitions from messing up slider width
     setTimeout(() => this.onResizeHandler(), 500);
     new ResizeSensor(this.$el, () => this.onResizeHandler());
@@ -58,6 +58,14 @@ export default class SliderInput extends BaseInput<number, ISliderMetadata> {
       this.localValue = parsedValue;
       this.updateValue(parsedValue);
     }
+  }
+
+  get min() {
+    return this.usePercentages ? this.options.min * 100 : this.options.min;
+  }
+
+  get max() {
+    return this.usePercentages ? this.options.max * 100 : this.options.max;
   }
 
   @debounce(100)
@@ -87,13 +95,8 @@ export default class SliderInput extends BaseInput<number, ISliderMetadata> {
 
   // Javascript precision is weird
   roundNumber(num: number) {
-    return parseFloat(num.toFixed(6));
-  }
-
-  formatter(value: number) {
-    let formattedValue = String(value);
-    if (this.usePercentages) formattedValue = `${Math.round(value * 100)}%`;
-    return formattedValue;
+    const val = this.usePercentages ? num / 100 : num;
+    return parseFloat(val.toFixed(6));
   }
 
   @debounce(500)
