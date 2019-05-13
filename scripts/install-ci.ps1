dc873d1896a96e273a280d0b35d1778144cd46aa
Alexander Nazarov <kadabrik91@gmail.com>
2019-05-13 13:51:34 -0700

    finalize

--- scripts/install-ci.ps1
+++ scripts/install-ci.ps1
@@ -1,5 +1,5 @@
 # Run this script as administrator to setup enviroment on new CI machine:
-# powershell install.ps1 your_buildkite_token your_buildkite_token your_password
+# powershell install.ps1 your_azure_token host_user host_password
 
 $token=$args[0]
 $username=$args[1]
@@ -8,7 +8,7 @@ $password=$args[2]
 $agentPath = "C:\agent\run.cmd"
 
 if (-Not($token) -Or -Not($username) -Or -Not($password)) {
-  echo "Provide a buildkite token, system user name and password";
+  echo "Provide a token, system user name and password";
   echo "Installation canceled";
   exit;
 }
@@ -34,18 +34,27 @@ choco install yarn
 echo "Install Git for Windows"
 choco install git.install
 
-echo "Install Buildkite Agent"
-$env:buildkiteAgentToken = $token
-Set-ExecutionPolicy Bypass -Scope Process -Force
-iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/buildkite/agent/master/install.ps1'))
+echo "Donwload and install Azure Agent"
+cd /
+Remove-Item agent -Recurse -ErrorAction Ignore
+mkdir agent ; cd agent;
+Invoke-WebRequest -Uri https://vstsagentpackage.azureedge.net/agent/2.150.3/vsts-agent-win-x64-2.150.3.zip -OutFile "$PWD\agent.zip"
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD\agent.zip", "$PWD")
 
+echo "Configure Azure Agent"
+.\config --unattended --url https://dev.azure.com/streamlabs --auth pat --token $token --agent "$env:computername $(Get-Random)"
+
+# Azure Agent has --AutoLogon option to add Anget to autostartup
+# But it doesn't allow to pass any arguments to the Agent
+# So call own implementation of AutoLogon here
 echo "Setup auto-login when system starts"
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String
 Set-ItemProperty $RegPath "DefaultUsername" -Value $username -type String
 Set-ItemProperty $RegPath "DefaultPassword" -Value "$password" -type String
 
-echo "Add Buildkite agent to startup"
-Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name 'StartBuildkite' -Value "$agentPath --once";
+# -once argument gracefuly stops Agent after first job
+echo "Add agent to startup"
+Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name 'StartAsureAgent' -Value "$agentPath --once";
 
 echo "Installation completed. Restart PC to take effect"
