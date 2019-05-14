fc8f3e0b45bb5ec26433ef634d0d9645f6d80971
Alexander Nazarov <kadabrik91@gmail.com>
2019-05-14 15:56:42 -0700

    reduce video tests

--- test/settings/video.ts
+++ test/settings/video.ts
@@ -5,53 +5,47 @@ import { setFormDropdown } from '../helpers/spectron/forms';
 
 useSpectron();
 
-let i = 20;
+test('Populates video settings', async t => {
+  const form = new FormMonkey(t);
+  const assertOptions = createOptionsAssertion(t, form);
 
-while (i--) {
+  const { app } = t.context;
 
-  test('Populates video settings' + i, async t => {
-    const form = new FormMonkey(t);
-    const assertOptions = createOptionsAssertion(t, form);
+  await focusMain(t);
+  await app.client.click('.top-nav .icon-settings');
 
-    const { app } = t.context;
+  await focusChild(t);
+  await app.client.click('li=Video');
 
-    await focusMain(t);
-    await app.client.click('.top-nav .icon-settings');
-
-    await focusChild(t);
-    await app.client.click('li=Video');
-
-    await assertOptions(
-      '[data-name=ScaleType]',
+  await assertOptions(
+    '[data-name=ScaleType]',
+    'Bicubic (Sharpened scaling, 16 samples)',
+    [
+      'Bilinear (Fastest, but blurry if scaling)',
       'Bicubic (Sharpened scaling, 16 samples)',
-      [
-        'Bilinear (Fastest, but blurry if scaling)',
-        'Bicubic (Sharpened scaling, 16 samples)',
-        'Lanczos (Sharpened scaling, 32 samples)',
-      ].map(addTrailingSpace),
-    );
+      'Lanczos (Sharpened scaling, 32 samples)',
+    ].map(addTrailingSpace),
+  );
 
-    await assertOptions(
-      '[data-name=FPSType]',
-      'Common FPS Values',
-      ['Common FPS Values', 'Integer FPS Value', 'Fractional FPS Value'].map(addTrailingSpace),
-    );
+  await assertOptions(
+    '[data-name=FPSType]',
+    'Common FPS Values',
+    ['Common FPS Values', 'Integer FPS Value', 'Fractional FPS Value'].map(addTrailingSpace),
+  );
 
-    await assertOptions(
-      '[data-name=FPSCommon]',
-      '30',
-      ['10', '20', '24 NTSC', '29.97', '30', '48', '59.94', '60'].map(addTrailingSpace),
-    );
+  await assertOptions(
+    '[data-name=FPSCommon]',
+    '30',
+    ['10', '20', '24 NTSC', '29.97', '30', '48', '59.94', '60'].map(addTrailingSpace),
+  );
 
-    // Only test resolution on CI, as this is hardware-specific
-    // if (process.env.CI) {
-    //   for (const res of ['1024x768']) {
-    //     await t.notThrowsAsync(
-    //       setFormDropdown(t, 'Base (Canvas) Resolution', '1024x768'),
-    //       `Resolution ${res} was not found`,
-    //     );
-    //   }
-    // }
-  });
-
-}
+  // Only test resolution on CI, as this is hardware-specific
+  // if (process.env.CI) {
+  //   for (const res of ['1024x768']) {
+  //     await t.notThrowsAsync(
+  //       setFormDropdown(t, 'Base (Canvas) Resolution', '1024x768'),
+  //       `Resolution ${res} was not found`,
+  //     );
+  //   }
+  // }
+});
