f16c0d636fb5b0b57744784bf7fe916f07d95111
Pablo <pabloronan@gmail.com>
2019-05-21 16:39:37 -0700

    Add CMake install (#1718)

--- scripts/install-ci.ps1
+++ scripts/install-ci.ps1
@@ -31,6 +31,9 @@ choco install yarn
 echo "Install Git for Windows"
 choco install git.install
 
+echo "Install CMake"
+choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System'
+
 echo "Install Visual Studio 2017 Build Tools"
 choco install visualstudio2017buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools;includeRecommended"
 
