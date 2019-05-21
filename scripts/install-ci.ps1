9146e38621cb9ae38835869e07387e6c7721158b
Pablo <pabloronan@gmail.com>
2019-05-21 15:49:47 -0700

    Add specific workload for Visual Studio build tools (#1729)
    * Remove Visual Studio build tools from ci script
    
    We need the full installation of Visual Studio to compile obs-studio-node in order to compile it in the CI machines
    
    * Add install of Visual Studio Build Tools with specific workload

--- scripts/install-ci.ps1
+++ scripts/install-ci.ps1
@@ -32,7 +32,7 @@ echo "Install Git for Windows"
 choco install git.install
 
 echo "Install Visual Studio 2017 Build Tools"
-choco install choco install visualstudio2017buildtools
+choco install visualstudio2017buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools;includeRecommended"
 
 echo "Donwload and install Azure Agent"
 cd /
