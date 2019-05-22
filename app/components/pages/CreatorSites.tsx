535bd8f5676491e102180caf91d768042ab58d70
Adrian Perez <adrian@adrianperez.org>
2019-05-22 16:32:53 -0700

    feat: creator sites integration

--- app/components/pages/CreatorSites.tsx
+++ app/components/pages/CreatorSites.tsx
@@ -1,14 +1,57 @@
+import electron, { WebviewTag } from 'electron';
+import url from 'url';
+import { Loading } from 'streamlabs-beaker';
 import TsxComponent from 'components/tsx-component';
 import { Component } from 'vue-property-decorator';
 import { UserService } from 'services/user';
 import { Inject } from 'services/core/injector';
+import { HostsService } from 'services/hosts';
+import { GuestApiService } from 'services/guest-api';
+import Utils from 'services/utils';
 
 @Component({})
 export default class CreatorSites extends TsxComponent<{}> {
   @Inject() userService: UserService;
+  @Inject() hostsService: HostsService;
+  @Inject() guestApiService: GuestApiService;
+
+  $refs: {
+    creatorSitesWebview: WebviewTag;
+  };
+
+  /**
+   * SPAs loaded into webview can't rely on dom-ready for knowing when to show them
+   * without flashes or loaders. This is part of an API for the site itself to notify SLOBS
+   * when it's ready which contributes to a better UX.
+   */
+  remoteAppReady: boolean = false;
+
+  mounted() {
+    this.$refs.creatorSitesWebview.addEventListener('did-finish-load', () => {
+      this.guestApiService.exposeApi(this.$refs.creatorSitesWebview.getWebContents().id, {
+        remoteAppReady: async () => {
+          this.remoteAppReady = true;
+        },
+      });
+
+      if (Utils.isDevMode()) {
+        this.$refs.creatorSitesWebview.openDevTools();
+      }
+    });
+
+    // Open StreamPress popups (like Publish opening new tab) in the user's default browser
+    this.$refs.creatorSitesWebview.addEventListener('new-window', evt => {
+      const { shell } = electron.remote;
+      const protocol = url.parse(evt.url).protocol;
+      if (protocol === 'http:' || protocol === 'https:') {
+        shell.openExternal(evt.url);
+      }
+    });
+  }
 
   get creatorSitesUrl() {
-    return this.userService.dashboardUrl('editor');
+    return `https://beta.streamlabs.com/editor/slobs?auth_token=${this.userService.apiToken}`;
+    //return `http://${this.hostsService.streamlabs}/editor/slobs/${this.userService.apiToken}`;
   }
 
   get containerStyles() {
@@ -23,6 +66,8 @@ export default class CreatorSites extends TsxComponent<{}> {
 
   get webviewStyles() {
     return {
+      // We can't mess with `display` per Electron docs, it doesn't trigger network either if we do
+      visibility: this.remoteAppReady ? 'visible' : 'hidden',
       position: 'absolute',
       top: '-2px',
       right: 0,
@@ -31,20 +76,37 @@ export default class CreatorSites extends TsxComponent<{}> {
     };
   }
 
+  get loadingStyles() {
+    return {
+      display: this.remoteAppReady ? 'none' : 'block',
+    };
+  }
+
+  get loadingMessages() {
+    return ['Loading resources...', 'Setting up layouts...', 'Loading files...'];
+  }
+
   render(h: Function) {
+    if (!this.userService.isLoggedIn) {
+      return;
+    }
+
+    const ua = `SLOBS ${electron.remote.process.env.SLOBS_VERSION}`;
+
     return (
       <div>
         <div class="creator-sites-container" style={this.containerStyles}>
-          {this.userService.isLoggedIn() ? (
-            <webview
-              class="creator-sites"
-              ref="creatorSitesWebview"
-              src={this.creatorSitesUrl}
-              style={this.webviewStyles}
-            />
-          ) : (
-            undefined
-          )}
+          <webview
+            class="creator-sites"
+            ref="creatorSitesWebview"
+            src={this.creatorSitesUrl}
+            style={this.webviewStyles}
+            useragent={ua}
+            preload="bundles/guest-api"
+          />
+          <div style={this.loadingStyles}>
+            <Loading loadingStrs={this.loadingMessages} />
+          </div>
         </div>
       </div>
     );
