ffe1ab6748d1dff0aaa67633d973af12d44dcfd9
Alexander Nazarov <kadabrik91@gmail.com>
2019-05-30 16:09:01 -0700

    Allow removing sources from the mixer

---
+++ app/services/editor-commands/commands/remove-source.ts
@@ -0,0 +1,38 @@
+import { Command } from './command';
+import { Inject } from 'services/core/injector';
+import { ISourceAddOptions, SourcesService, TSourceType } from 'services/sources';
+
+/**
+ * Removes the source
+ * Works for global sources only (with channel > 0)
+ */
+export class RemoveSourceCommand extends Command {
+  @Inject() private sourcesService: SourcesService;
+
+  private name: string;
+  private type: TSourceType;
+  private addSourceOptions: ISourceAddOptions<any>;
+
+  constructor(private sourceId: string) {
+    super();
+  }
+
+  get description() {
+    return `Remove source ${this.name}`;
+  }
+
+  execute() {
+    const source = this.sourcesService.getSource(this.sourceId);
+    this.name = source.name;
+    this.type = source.type;
+    this.addSourceOptions = {
+      sourceId: source.sourceId,
+      channel: source.channel,
+    };
+    this.sourcesService.removeSource(this.sourceId);
+  }
+
+  rollback() {
+    this.sourcesService.createSource(this.name, this.type, {}, this.addSourceOptions);
+  }
+}
