394b35ed68b63725681acce19d4b9eee71927abf
Alexander Nazarov <kadabrik91@gmail.com>
2019-06-05 18:41:30 -0700

    fix

--- test/helpers/widget-helpers.ts
+++ test/helpers/widget-helpers.ts
@@ -26,11 +26,11 @@ export async function waitForWidgetSettingsSync(t: TExecutionContext, fn: Functi
     // start listen widgets SettingsUpdate events
     const listener = await getListener();
 
-    // catching events for 15s
-    listener.pipe(first()).subscribe(ev => resolve(ev));
-
     // execute code
     await fn();
+
+    // catching events for 15s
+    listener.pipe(first()).subscribe(ev => resolve(ev));
 
     await sleep(15000);
 
