153224fe1e3612258e3847c3d874a6520b156aea
Alexander Nazarov <kadabrik91@gmail.com>
2019-06-12 22:17:35 -0700

    replace merge to pull

--- test/screentest/runner.js
+++ test/screentest/runner.js
@@ -55,10 +55,11 @@ function checkoutBranch(branchName) {
   if (!fs.existsSync(branchPath)) fs.mkdirSync(branchPath);
   const checkoutTarget = branchName === 'current' ? commitSHA : branchName;
   rimraf.sync(CONFIG.compiledTestsDist);
+  exec(`git reset --hard`);
   exec(`git checkout ${checkoutTarget}`);
   if (branchName !== CONFIG.baseBranch) {
     // the base branch may have changes, so merge it
-    exec(`git merge origin/${CONFIG.baseBranch}`);
+    exec(`git pull origin ${CONFIG.baseBranch}`);
   }
   exec('yarn install --frozen-lockfile --check-files');
   exec('yarn compile:ci');
