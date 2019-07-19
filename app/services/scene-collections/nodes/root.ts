068fb8ce7677905ceccd5c300e9f364423603332
Andy Creeth <acreeth@gmail.com>
2019-07-19 15:55:15 -0700

    store base resolution in scene collections and auto switch

--- app/services/scene-collections/nodes/root.ts
+++ app/services/scene-collections/nodes/root.ts
@@ -3,8 +3,14 @@ import { SourcesNode } from './sources';
 import { ScenesNode } from './scenes';
 import { TransitionsNode } from './transitions';
 import { HotkeysNode } from './hotkeys';
+import { Inject } from 'services/core';
+import { VideoService } from 'services/video';
 
 interface ISchema {
+  baseResolution: {
+    width: number;
+    height: number;
+  };
   sources: SourcesNode;
   scenes: ScenesNode;
   hotkeys?: HotkeysNode;
@@ -13,7 +19,9 @@ interface ISchema {
 
 // This is the root node of the config file
 export class RootNode extends Node<ISchema, {}> {
-  schemaVersion = 2;
+  schemaVersion = 3;
+
+  @Inject() videoService: VideoService;
 
   async save(): Promise<void> {
     const sources = new SourcesNode();
@@ -31,10 +39,13 @@ export class RootNode extends Node<ISchema, {}> {
       scenes,
       transitions,
       hotkeys,
+      baseResolution: this.videoService.baseResolution,
     };
   }
 
   async load(): Promise<void> {
+    this.videoService.setBaseResolution(this.data.baseResolution);
+
     await this.data.transitions.load();
     await this.data.sources.load({});
     await this.data.scenes.load({});
@@ -47,6 +58,8 @@ export class RootNode extends Node<ISchema, {}> {
   migrate(version: number) {
     if (version === 1) {
       this.data.transitions = this.data['transition'];
+    } else if (version === 2) {
+      this.data.baseResolution = this.videoService.baseResolution;
     }
   }
 }
