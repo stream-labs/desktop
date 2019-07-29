905b5299516eb6b641f6da7f2ad081133d6cae1a
Alexander Nazarov <kadabrik91@gmail.com>
2019-07-29 14:56:05 -0700

    transitions tests

---
+++ test/api/transitions.ts
@@ -0,0 +1,24 @@
+import { useSpectron, test } from '../helpers/spectron';
+import { getClient } from '../helpers/api-client';
+import { TransitionsService } from 'services/api/external-api/transitions';
+
+useSpectron({ restartAppAfterEachTest: false });
+
+test('Transitions', async t => {
+  const api = await getClient();
+  const transitionsService = api.getResource<TransitionsService>('TransitionsService');
+
+  // test enable studio mode
+  let eventWatcher = api.watchForEvents(['TransitionService.studioModeChanged']);
+  transitionsService.enableStudioMode();
+  await eventWatcher.waitForAll();
+  t.true(eventWatcher.receivedEvents[0].data);
+  t.true(transitionsService.getModel().studioMode);
+
+  // test disable studio mode
+  eventWatcher = api.watchForEvents(['TransitionService.studioModeChanged']);
+  transitionsService.disableStudioMode();
+  await eventWatcher.waitForAll();
+  t.false(eventWatcher.receivedEvents[0].data);
+  t.false(transitionsService.getModel().studioMode);
+});
