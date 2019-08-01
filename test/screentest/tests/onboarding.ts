0a28537054d78c1e82b81a57f089bfa6085a6684
Alexander Nazarov <kadabrik91@gmail.com>
2019-08-01 16:29:43 -0700

    Debug tests

--- test/screentest/tests/onboarding.ts
+++ test/screentest/tests/onboarding.ts
@@ -18,18 +18,22 @@ test('Onboarding steps', async t => {
 
   await logIn(t, 'twitch', null, false, true);
   await sleep(1000);
+  console.log('skip auth');
   await app.client.click('p=Skip');
 
   await app.client.waitForVisible('h2=Start Fresh', 15000);
   await makeScreenshots(t, 'Start fresh or import from OBS');
+  console.log('click start fresh');
   await app.client.click('h2=Start Fresh');
 
   await app.client.waitForVisible('h1=Add a Theme');
   await makeScreenshots(t, 'Add a Theme');
+  console.log('click skip theme');
   await app.client.click('p=Skip');
 
   await app.client.waitForVisible('h1=Optimize');
   await makeScreenshots(t, 'Before optimize');
+  console.log('click start optimize');
   await app.client.click('button=Start');
   await app.client.waitForVisible('h1=Optimizing... 33%');
   await makeScreenshots(t, 'Optimization progress');
@@ -50,17 +54,20 @@ test('OBS Importer', async t => {
   spawnSync(_7z, ['x', obsCacheZipPath, `-o${cacheDir}`]);
 
   // skip auth
+  console.log('click skip auth');
   await client.click('p=Skip');
   await sleep(1000);
 
   // import from OBS
   await client.waitForVisible('h2=Import from OBS');
   await makeScreenshots(t, 'Import button');
+  console.log('import from obs');
   await client.click('h2=Import from OBS');
 
   // benefits page
   await client.waitForVisible('h1=A few benefits of using Streamlabs OBS');
   await makeScreenshots(t, 'Benefits');
+  console.log('click complete');
   await client.click('button=Complete');
 
   // success?
