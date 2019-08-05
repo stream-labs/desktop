c908ca83d9c17426b196def3774d677e8fba2a5f
Alexander Nazarov <kadabrik91@gmail.com>
2019-08-05 11:02:29 -0700

    Test the EditStream window for network errors (#1962)

--- test/streaming.js
+++ test/streaming.js
@@ -1,10 +1,17 @@
-import { useSpectron, focusMain, focusChild, test } from './helpers/spectron/index';
+import {
+  useSpectron,
+  focusMain,
+  focusChild,
+  test,
+  skipCheckingErrorsInLog
+} from './helpers/spectron/index';
 import { setFormInput } from './helpers/spectron/forms';
 import { fillForm, FormMonkey } from './helpers/form-monkey';
 import { logIn } from './helpers/spectron/user';
 import { setOutputResolution } from './helpers/spectron/output';
 const moment = require('moment');
 import { sleep } from './helpers/sleep';
+import { fetchMock, resetFetchMock } from './helpers/spectron/network';
 
 
 useSpectron({ appArgs: '--nosync' });
@@ -211,4 +218,27 @@ schedulingPlatforms.forEach(platform => {
     }
 
   });
+});
+
+
+
+test('Go live error', async t => {
+
+  // login into the account
+  if (!(await logIn(t, 'twitch'))) return;
+  const app = t.context.app;
+
+  // simulate issues with the twitch api
+  await fetchMock(t, /api\.twitch\.tv/, 404);
+  skipCheckingErrorsInLog();
+
+  // open EditStreamInfo window
+  await app.client.click('button=Go Live');
+  await focusChild(t);
+
+  // check that the error text is shown
+  await app.client.waitForVisible('a=just go live.');
+
+  await resetFetchMock(t);
+  t.pass();
 });
