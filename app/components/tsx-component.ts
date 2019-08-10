0f019711daee29ceb553f29dcac74879da6a01b7
DESKTOP-FGF6CBD\Peter <peter.kucharski@streamlabs.com>
2019-08-09 17:04:15 -0700

    Add class option to tsx component

--- app/components/tsx-component.ts
+++ app/components/tsx-component.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue';
 
 export default abstract class TsxComponent<TProps> extends Vue {
-  private vueTsxProps: Readonly<{ slot?: string; ref?: string }> & Readonly<TProps>;
+  private vueTsxProps: Readonly<{ slot?: string; ref?: string; class?: string }> & Readonly<TProps>;
 }
