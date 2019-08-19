8ac1419b4a5fb6793bb9b5566f60d24b0efa2315
Andy Creeth <acreeth@gmail.com>
2019-08-19 10:52:00 -0700

    add tweet when going live

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -6,6 +6,7 @@ export const notes: IPatchNotes = {
   showChest: false,
   notes: [
     'Added a new and improved design. Navigation that was previously on the top of the window has been made slimmer and moved to the side. Stay tuned for more aesthetic changes coming soon!',
+    'Added a new feature that allows you to link your Twitter account and send a Tweet while going live.',
     'Added a new flow for first time streamers that helps you get set up even faster.',
     'Added the "Interact" feature to the browser source, which allows you to click, scroll, and type inside a browser source.',
     'Lots of bug and crash fixes',
