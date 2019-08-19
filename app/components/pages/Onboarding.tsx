db9786102e4d602a74b12dec462110612169c9fc
Sean Beyer <sean.beyer@streamlabs.com>
2019-08-19 09:51:59 -0700

    Fix regressions
    Onboarding can now point to optimize only step, nav doesn't navigate when not logged in

--- app/components/pages/Onboarding.tsx
+++ app/components/pages/Onboarding.tsx
@@ -12,6 +12,7 @@ import ThemeSelector from './onboarding-steps/ThemeSelector';
 import { IncrementalRolloutService, EAvailableFeatures } from 'services/incremental-rollout';
 import { UserService } from 'services/user';
 import styles from './Onboarding.m.less';
+import { throttleSetter } from 'lodash-decorators';
 
 @Component({})
 export default class OnboardingPage extends TsxComponent<{}> {
@@ -125,11 +126,27 @@ export default class OnboardingPage extends TsxComponent<{}> {
     );
   }
 
+  optimizePage(h: Function) {
+    return (
+      <div>
+        <div class={styles.container}>
+          <Optimize
+            continue={this.complete.bind(this)}
+            setProcessing={this.setProcessing.bind(this)}
+          />
+        </div>
+      </div>
+    );
+  }
+
   render(h: Function) {
     const steps = this.steps(h);
 
     if (this.onboardingService.options.isLogin) {
       return this.loginPage(h);
+    }
+    if (this.onboardingService.options.isOptimize) {
+      return this.optimizePage(h);
     }
 
     return (
