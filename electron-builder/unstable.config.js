0066b792dda6331d6f09d866649b7827246d06bc
berlysia <berlysia@gmail.com>
2019-08-22 21:12:19 +0900

    build: unstable版のビルド設定を新設する

---
+++ electron-builder/unstable.config.js
@@ -0,0 +1,7 @@
+const config = require('./stable.config.js');
+
+config.appId = 'jp.nicovideo.nair-unstable';
+config.productName = 'N Air Unstable';
+config.extraMetadata.name = 'n-air-app-unstable';
+
+module.exports = config;
