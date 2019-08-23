579cc7f666221aec96f6daefa81a5d10657f582b
berlysia <berlysia@gmail.com>
2019-08-24 01:33:44 +0900

    build: リリースノート生成スクリプトを独立

---
+++ bin/release/generatePatchNote.js
@@ -0,0 +1,110 @@
+// @ts-check
+
+const OctoKit = require('@octokit/rest');
+const sh = require('shelljs');
+const inq = require('inquirer');
+const colors = require('colors/safe');
+const {
+  log,
+  info,
+  error,
+  input,
+  confirm,
+  executeCmd,
+} = require('./scripts/prompt');
+const {
+  checkEnv,
+  getTagCommitId,
+} = require('./scripts/util');
+const {
+  generateNewVersion,
+  readPatchNoteFile,
+  writePatchNoteFile,
+  collectPullRequestMerges,
+} = require('./scripts/patchNote');
+
+async function generatePatchNote({
+  patchNoteFileName,
+  publicRelease,
+  githubTokenForReadPullRequest
+}) {
+  info('checking current tag ...');
+  const previousTag = executeCmd('git describe --tags --abbrev=0').stdout.trim();
+  info(`current tag: ${previousTag}`);
+
+  info(`checking ${patchNoteFileName} ...`);
+  const previousPatchNote = readPatchNoteFile(patchNoteFileName);
+  if (previousPatchNote) {
+    info('patch-note.txt found');
+    log(`${previousPatchNote.version}\n${previousPatchNote.lines.join('\n')}`);
+
+    if (!await confirm('overwrite?', false)) {
+      sh.exit(1);
+    }
+  }
+
+  const defaultVersion = generateNewVersion(previousTag, !publicRelease);
+  const newVersion = await input('What should the new version number be?', defaultVersion);
+
+  if (getTagCommitId(`v${newVersion}`)) {
+    error(`version ${newVersion} has already been released`);
+    sh.exit(1);
+  }
+
+  const prMerges = await collectPullRequestMerges(
+    {
+      octokit: new OctoKit({
+        baseUrl: 'https://api.github.com',
+        auth: `token ${githubTokenForReadPullRequest}`,
+      }),
+      owner: 'n-air-app',
+      repo: 'n-air-app',
+    },
+    previousTag
+  );
+
+  const directCommits = executeCmd(`git log --no-merges --first-parent --pretty=format:"%s (%t)" ${previousTag}..`, {
+    silent: true,
+  }).stdout;
+  const directCommitsNotes = directCommits ? `\nDirect Commits:\n${directCommits}` : '';
+  const newNotes = `${prMerges}${directCommitsNotes}`;
+
+  writePatchNoteFile(patchNoteFileName, newVersion, newNotes);
+  info(`generated ${patchNoteFileName}:`);
+  log(newNotes);
+
+  info(`Please edit ${patchNoteFileName} if needed.`);
+}
+
+if (!module.parent) {
+  (async () => {
+    info(colors.magenta('|------------------------------|'));
+    info(colors.magenta('| N Air Release Note Generator |'));
+    info(colors.magenta('|------------------------------|'));
+
+    checkEnv('NAIR_GITHUB_TOKEN');
+
+    const baseDir = executeCmd('git rev-parse --show-cdup', { silent: true }).stdout.trim();
+    const patchNoteFileName = `${baseDir}patch-note.txt`;
+
+    const { releaseEnv } = await inq.prompt({
+      type: 'list',
+      name: 'releaseEnv',
+      message: 'What environment do you want to release?',
+      choices: ['internal', 'public'],
+    });
+
+    // const { releaseChannel } = await inq.prompt({
+    //   type: 'list',
+    //   name: 'releaseChannel',
+    //   message: 'What channel do you want to release?',
+    //   choices: ['unstable', 'stable'],
+    // });
+
+    await generatePatchNote({
+      patchNoteFileName,
+      publicRelease: releaseEnv === 'public',
+      githubTokenForReadPullRequest: process.env.NAIR_GITHUB_TOKEN,
+    });
+  })();
+}
