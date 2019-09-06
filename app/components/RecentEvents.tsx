3471a050f0c6566655dfee8de9ccff90f0ca5e15
Andy Creeth <acreeth@gmail.com>
2019-09-06 13:45:27 -0700

    fix duplicate keys in events

--- app/components/RecentEvents.tsx
+++ app/components/RecentEvents.tsx
@@ -139,7 +139,7 @@ export default class RecentEvents extends TsxComponent<{}> {
         {!!this.recentEvents.length &&
           this.recentEvents.map(event => (
             <EventCell
-              key={event.hash}
+              key={`${event.hash}${event.created_at || event.iso8601Created || event.createdAt}`}
               event={event}
               repeatAlert={this.repeatAlert.bind(this)}
               eventString={this.eventString.bind(this)}
