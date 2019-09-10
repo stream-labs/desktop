3ca72e640b143e776b5fa9eedeb93984543c92e2
Andy Creeth <acreeth@gmail.com>
2019-09-10 12:57:07 -0700

    remove store log

--- app/app.ts
+++ app/app.ts
@@ -217,8 +217,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     I18nService.setVuei18nInstance(i18n);
 
-    console.log(store);
-
     // create a root Vue component
     const windowId = Utils.getCurrentUrlParams().windowId;
     const vm = new Vue({
