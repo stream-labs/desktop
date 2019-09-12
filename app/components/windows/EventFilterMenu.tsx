0ed2e6ad27f1da38e6f87ef4b3a2712fe5eb712a
DESKTOP-FGF6CBD\Peter <peter.kucharski@streamlabs.com>
2019-09-12 16:11:06 -0700

    Add crude UI for managing event filtering

--- app/components/windows/EventFilterMenu.tsx
+++ app/components/windows/EventFilterMenu.tsx
@@ -5,37 +5,141 @@ import TsxComponent from 'components/tsx-component';
 import { $t } from 'services/i18n';
 import { WindowsService } from 'services/windows';
 import { RecentEventsService } from 'services/recent-events';
+import { BoolInput, NumberInput } from 'components/shared/inputs/inputs';
 
 @Component({})
 export default class EventFilterMenu extends TsxComponent<{}> {
   @Inject() windowsService: WindowsService;
   @Inject() recentEventsService: RecentEventsService;
 
-  reset() {
-    return;
-  }
-
   cancel() {
     this.windowsService.closeChildWindow();
   }
 
-  get filters() {
-    console.log(this.recentEventsService.state.filterConfig);
-    return this.recentEventsService.state.filterConfig;
+  get mainFilters() {
+    return this.recentEventsService.platformFilters.main;
+  }
+
+  get subFilters() {
+    return this.recentEventsService.platformFilters.sub;
+  }
+
+  get resubFilters() {
+    return this.recentEventsService.platformFilters.resub;
+  }
+
+  get subsEnabled() {
+    return this.subFilters.hasOwnProperty('subscription') && this.subFilters['subscription'].value;
+  }
+
+  get resubsEnabled() {
+    return this.resubFilters.hasOwnProperty('resub') && this.resubFilters['resub'].value;
+  }
+
+  updateFilter(filter: string, value: any) {
+    this.recentEventsService.updateFilterPreference(filter, value);
   }
 
   render(h: Function) {
     return (
       <ModalLayout customControls showControls={false}>
         <div slot="content">
-          {Object.keys(this.filters).map(filter => (
-            <p>{filter}</p>
-          ))}
+          <div>
+            {Object.keys(this.mainFilters).map(filter => (
+              <div>
+                <BoolInput
+                  value={this.mainFilters[filter].value}
+                  metadata={{ title: $t(this.mainFilters[filter].name) }}
+                  onInput={(value: boolean) => this.updateFilter(filter, value)}
+                />
+              </div>
+            ))}
+          </div>
+          <div>
+            {Object.keys(this.subFilters).map(
+              filter =>
+                filter === 'subscription' && (
+                  <div>
+                    <BoolInput
+                      value={this.subFilters[filter].value}
+                      metadata={{ title: $t(this.subFilters[filter].name) }}
+                      onInput={(value: boolean) => this.updateFilter(filter, value)}
+                    />
+                  </div>
+                ),
+            )}
+            {Object.keys(this.subFilters).map(
+              filter =>
+                filter !== 'subscription' &&
+                this.subsEnabled && (
+                  <div>
+                    <BoolInput
+                      value={this.subFilters[filter].value}
+                      metadata={{ title: $t(this.subFilters[filter].name) }}
+                      onInput={(value: boolean) => this.updateFilter(filter, value)}
+                    />
+                  </div>
+                ),
+            )}
+          </div>
+          <div>
+            {Object.keys(this.resubFilters).map(
+              filter =>
+                filter === 'resub' && (
+                  <div>
+                    <BoolInput
+                      value={this.resubFilters[filter].value}
+                      metadata={{ title: $t(this.resubFilters[filter].name) }}
+                      onInput={(value: boolean) => this.updateFilter(filter, value)}
+                    />
+                  </div>
+                ),
+            )}
+            {Object.keys(this.resubFilters).map(
+              filter =>
+                filter !== 'resub' &&
+                this.resubsEnabled &&
+                filter !== 'filter_subscription_minimum_months' &&
+                filter !== 'filter_subscription_minimum_enabled' && (
+                  <div>
+                    <BoolInput
+                      value={this.resubFilters[filter].value}
+                      metadata={{ title: $t(this.resubFilters[filter].name) }}
+                      onInput={(value: boolean) => this.updateFilter(filter, value)}
+                    />
+                  </div>
+                ),
+            )}
+            {Object.keys(this.resubFilters).map(
+              filter =>
+                filter === 'filter_subscription_minimum_enabled' &&
+                this.resubsEnabled && (
+                  <div>
+                    <BoolInput
+                      value={this.resubFilters[filter].value}
+                      metadata={{ title: $t(this.resubFilters[filter].name) }}
+                      onInput={(value: boolean) => this.updateFilter(filter, value)}
+                    />
+                  </div>
+                ),
+            )}
+            {Object.keys(this.resubFilters).map(
+              filter =>
+                filter === 'filter_subscription_minimum_months' &&
+                this.resubsEnabled && (
+                  <div>
+                    <p>{this.resubFilters[filter].name}</p>
+                    <NumberInput
+                      value={this.resubFilters[filter].value}
+                      metadata={{ min: 1, max: 120, isInteger: true }}
+                      onInput={(value: number) => this.updateFilter(filter, value)}
+                    />
+                  </div>
+                ),
+            )}
+          </div>
         </div>
         <div slot="controls">
-          <button class="button button--default" onClick={this.reset}>
-            {$t('Reset')}
-          </button>
           <button class="button button--action" onClick={this.cancel}>
             {$t('Done')}
           </button>
