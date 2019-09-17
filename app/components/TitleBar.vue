2da76c2cd370f584b17c10bc3517f148ac96fa18
Sean Beyer <sean.beyer@streamlabs.com>
2019-09-18 06:22:32 +0900

    Restyle title bar and news banner (#2103)
    * Restyle title bar and news banner
    
    * Remove dummy banner
    
    * Remove border

--- app/components/TitleBar.vue
+++ app/components/TitleBar.vue
@@ -23,7 +23,7 @@
   align-items: center;
   height: 30px;
   flex: 0 0 30px;
-  border-bottom: 1px solid var(--border);
+  background: var(--titlebar);
 }
 
 .titlebar-icon {
@@ -35,6 +35,7 @@
   flex-grow: 1;
   padding-left: 10px;
   -webkit-app-region: drag;
+  color: var(--icon);
 }
 
 .titlebar-actions {
