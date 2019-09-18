506598dbd678cf8bd4f05f6eeaf594b36d939d08
DESKTOP-FGF6CBD\Peter <peter.kucharski@streamlabs.com>
2019-09-17 17:13:04 -0700

    Refactor filters component slightly

--- app/components/windows/EventFilterMenu.tsx
+++ app/components/windows/EventFilterMenu.tsx
@@ -41,8 +41,103 @@ export default class EventFilterMenu extends TsxComponent<{}> {
     return this.resubFilters.hasOwnProperty('resub') && this.resubFilters['resub'].value;
   }
 
-  updateFilter(filter: string, value: any) {
+  updateFilter(filter: string, value: boolean | number) {
     this.recentEventsService.updateFilterPreference(filter, value);
+  }
+
+  renderBooleanInput(
+    h: Function,
+    key: string,
+    filter: { name: string; value: boolean },
+    header: boolean = false,
+  ) {
+    return (
+      <BoolInput
+        value={filter.value}
+        metadata={{ title: $t(filter.name) }}
+        onInput={(value: boolean) => this.updateFilter(key, value)}
+        class={header ? styles.categoryHeader : ''}
+      />
+    );
+  }
+
+  renderGeneralFilters(h: Function) {
+    return (
+      <div class={styles.halfWidth}>
+        <h2>{$t('General')}</h2>
+        {Object.keys(this.mainFilters).map(filter => (
+          <div>{this.renderBooleanInput(h, filter, this.mainFilters[filter])}</div>
+        ))}
+      </div>
+    );
+  }
+
+  renderSubFilters(h: Function) {
+    return (
+      <div class={styles.halfWidth}>
+        {Object.keys(this.subFilters).map(
+          filter =>
+            filter === 'subscription' && (
+              <div>{this.renderBooleanInput(h, filter, this.subFilters[filter], true)}</div>
+            ),
+        )}
+        {Object.keys(this.subFilters).map(
+          filter =>
+            filter !== 'subscription' &&
+            this.subsEnabled && (
+              <div>{this.renderBooleanInput(h, filter, this.subFilters[filter])}</div>
+            ),
+        )}
+      </div>
+    );
+  }
+
+  renderResubFilters(h: Function) {
+    return (
+      <div class={styles.fullWidth}>
+        {Object.keys(this.resubFilters).map(
+          filter =>
+            filter === 'resub' && (
+              <div>{this.renderBooleanInput(h, filter, this.resubFilters[filter], true)}</div>
+            ),
+        )}
+        <div class={styles.resubOptions}>
+          {Object.keys(this.resubFilters).map(
+            filter =>
+              filter !== 'resub' &&
+              this.resubsEnabled &&
+              filter !== 'filter_subscription_minimum_months' &&
+              filter !== 'filter_subscription_minimum_enabled' && (
+                <div>{this.renderBooleanInput(h, filter, this.resubFilters[filter])}</div>
+              ),
+          )}
+        </div>
+        <div class={styles.minimum}>
+          {Object.keys(this.resubFilters).map(
+            filter =>
+              filter === 'filter_subscription_minimum_enabled' &&
+              this.resubsEnabled && (
+                <div>{this.renderBooleanInput(h, filter, this.resubFilters[filter])}</div>
+              ),
+          )}
+          {Object.keys(this.minMonthsFilter).map(
+            filter =>
+              filter === 'filter_subscription_minimum_months' &&
+              this.resubsEnabled && (
+                <div class={styles.monthsInputContainer}>
+                  <NumberInput
+                    value={this.minMonthsFilter[filter].value}
+                    metadata={{ min: 1, max: 120, isInteger: true }}
+                    onInput={(value: number) => this.updateFilter(filter, value)}
+                    class={styles.monthsInput}
+                  />
+                  <p class={styles.monthsLabel}>{this.minMonthsFilter[filter].name}</p>
+                </div>
+              ),
+          )}
+        </div>
+      </div>
+    );
   }
 
   render(h: Function) {
@@ -50,111 +145,10 @@ export default class EventFilterMenu extends TsxComponent<{}> {
       <ModalLayout customControls showControls={false}>
         <div slot="content" class={styles.flexColumn}>
           <div class={styles.flexRow}>
-            <div class={styles.halfWidth}>
-              <h2>{$t('General')}</h2>
-              {Object.keys(this.mainFilters).map(filter => (
-                <div>
-                  <BoolInput
-                    value={this.mainFilters[filter].value}
-                    metadata={{ title: $t(this.mainFilters[filter].name) }}
-                    onInput={(value: boolean) => this.updateFilter(filter, value)}
-                  />
-                </div>
-              ))}
-            </div>
-            <div class={styles.halfWidth}>
-              {Object.keys(this.subFilters).map(
-                filter =>
-                  filter === 'subscription' && (
-                    <div>
-                      <BoolInput
-                        value={this.subFilters[filter].value}
-                        metadata={{ title: $t(this.subFilters[filter].name) }}
-                        onInput={(value: boolean) => this.updateFilter(filter, value)}
-                        class={styles.categoryHeader}
-                      />
-                    </div>
-                  ),
-              )}
-              {Object.keys(this.subFilters).map(
-                filter =>
-                  filter !== 'subscription' &&
-                  this.subsEnabled && (
-                    <div>
-                      <BoolInput
-                        value={this.subFilters[filter].value}
-                        metadata={{ title: $t(this.subFilters[filter].name) }}
-                        onInput={(value: boolean) => this.updateFilter(filter, value)}
-                      />
-                    </div>
-                  ),
-              )}
-            </div>
+            {this.renderGeneralFilters(h)}
+            {this.renderSubFilters(h)}
           </div>
-          <div class={styles.flexRow}>
-            <div class={styles.fullWidth}>
-              {Object.keys(this.resubFilters).map(
-                filter =>
-                  filter === 'resub' && (
-                    <div>
-                      <BoolInput
-                        value={this.resubFilters[filter].value}
-                        metadata={{ title: $t(this.resubFilters[filter].name) }}
-                        onInput={(value: boolean) => this.updateFilter(filter, value)}
-                        class={styles.categoryHeader}
-                      />
-                    </div>
-                  ),
-              )}
-              <div class={styles.resubOptions}>
-                {Object.keys(this.resubFilters).map(
-                  filter =>
-                    filter !== 'resub' &&
-                    this.resubsEnabled &&
-                    filter !== 'filter_subscription_minimum_months' &&
-                    filter !== 'filter_subscription_minimum_enabled' && (
-                      <div>
-                        <BoolInput
-                          value={this.resubFilters[filter].value}
-                          metadata={{ title: $t(this.resubFilters[filter].name) }}
-                          onInput={(value: boolean) => this.updateFilter(filter, value)}
-                        />
-                      </div>
-                    ),
-                )}
-              </div>
-              <div class={styles.minimum}>
-                {Object.keys(this.resubFilters).map(
-                  filter =>
-                    filter === 'filter_subscription_minimum_enabled' &&
-                    this.resubsEnabled && (
-                      <div>
-                        <BoolInput
-                          value={this.resubFilters[filter].value}
-                          metadata={{ title: $t(this.resubFilters[filter].name) }}
-                          onInput={(value: boolean) => this.updateFilter(filter, value)}
-                        />
-                      </div>
-                    ),
-                )}
-                {Object.keys(this.minMonthsFilter).map(
-                  filter =>
-                    filter === 'filter_subscription_minimum_months' &&
-                    this.resubsEnabled && (
-                      <div class={styles.monthsInputContainer}>
-                        <NumberInput
-                          value={this.minMonthsFilter[filter].value}
-                          metadata={{ min: 1, max: 120, isInteger: true }}
-                          onInput={(value: number) => this.updateFilter(filter, value)}
-                          class={styles.monthsInput}
-                        />
-                        <p class={styles.monthsLabel}>{this.minMonthsFilter[filter].name}</p>
-                      </div>
-                    ),
-                )}
-              </div>
-            </div>
-          </div>
+          <div class={styles.flexRow}>{this.renderResubFilters(h)}</div>
         </div>
         <div slot="controls">
           <button class="button button--action" onClick={this.cancel}>
