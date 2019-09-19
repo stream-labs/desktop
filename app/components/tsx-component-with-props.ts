159adbc9143e416a678711e57a1d5cc2d2564966
Alexander Nazarov <kadabrik91@gmail.com>
2019-09-18 17:46:48 -0700

    clean

--- app/components/tsx-component-with-props.ts
+++ app/components/tsx-component-with-props.ts
@@ -2,12 +2,11 @@ import Vue from 'vue';
 
 export function createProps<TProps extends new () => any>(propsClass: TProps): string[] {
   const props = new propsClass();
-  console.log('register props', Object.keys(props));
   return Object.keys(props);
 }
 
 export abstract class TsxComponent<TProps> extends Vue {
-  protected vueTsxProps: Readonly<{ slot?: string; ref?: string; class?: string; key?: string }> &
+  private vueTsxProps: Readonly<{ slot?: string; ref?: string; class?: string; key?: string }> &
     Readonly<TProps>;
 
   get props(): TProps {
--- app/components/tsx-component.ts
+++ app/components/tsx-component.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 
 export default abstract class TsxComponent<TProps> extends Vue {
-  protected vueTsxProps: Readonly<{ slot?: string; ref?: string; class?: string; key?: string }> &
+  private vueTsxProps: Readonly<{ slot?: string; ref?: string; class?: string; key?: string }> &
     Readonly<TProps>;
 }
