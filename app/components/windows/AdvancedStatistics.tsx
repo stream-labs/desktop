62c5614c9623e71977d414cb365af8be0afa16c0
DESKTOP-FGF6CBD\Peter <peter.kucharski@streamlabs.com>
2019-10-08 15:56:41 -0700

    Refactor render function to be shorter

--- app/components/windows/AdvancedStatistics.tsx
+++ app/components/windows/AdvancedStatistics.tsx
@@ -126,24 +126,76 @@ export default class AdvancedStatistics extends TsxComponent<{}> {
     }
   }
 
+  statusBar(h: Function) {
+    return (
+      <div>
+        <h2>Status</h2>
+        <div
+          class={cx(
+            styles.status,
+            this.status.type === 'error' ? styles.error : '',
+            this.status.type === 'warning' ? styles.warning : '',
+            this.status.type === 'success' ? styles.success : '',
+          )}
+        >
+          {this.statusIcon(h)}
+          <span>{this.status.description}</span>
+        </div>
+      </div>
+    );
+  }
+
+  notificationsArea(h: Function) {
+    return (
+      <div class={styles.section}>
+        <h2>Performance Notifications</h2>
+        <div class={styles.notificationContainer}>
+          {this.notifications.map(notification => (
+            <div class={styles.notification}>
+              {this.iconForNotificationBySubType(h, notification)}
+              <div class="message">{notification.message}</div>
+              <div class="date">{this.moment(notification.date)}</div>
+            </div>
+          ))}
+          {this.notifications.length === 0 && (
+            <div class={styles.notificationEmpty}>
+              <div class="message">{$t('You do not have any notifications')}</div>
+            </div>
+          )}
+        </div>
+        <p class={styles.description}>
+          {$t(
+            'When Streamlabs OBS detects performance issues with your stream, such as dropped frames, lagged frames, or a stream disconnection, troubleshooting notifications will be sent to you here.',
+          )}
+        </p>
+      </div>
+    );
+  }
+
+  iconForNotificationBySubType(h: Function, notification: INotification) {
+    return (
+      <div class="icon">
+        {notification.subType === ENotificationSubType.DISCONNECTED && (
+          <i class={cx('icon-disconnected', styles.errorText)} />
+        )}
+        {notification.subType === ENotificationSubType.SKIPPED && (
+          <i class={cx('icon-reset icon-skipped-frame', styles.warningText)} />
+        )}
+        {notification.subType === ENotificationSubType.LAGGED && (
+          <i class={cx('icon-time', styles.warningText)} />
+        )}
+        {notification.subType === ENotificationSubType.DROPPED && (
+          <i class={cx('icon-back-alt icon-down-arrow', styles.warningText)} />
+        )}
+      </div>
+    );
+  }
+
   render(h: Function) {
     return (
       <ModalLayout showControls={false}>
         <div slot="content" class={styles.container}>
-          <div>
-            <h2>Status</h2>
-            <div
-              class={cx(
-                styles.status,
-                this.status.type === 'error' ? styles.error : '',
-                this.status.type === 'warning' ? styles.warning : '',
-                this.status.type === 'success' ? styles.success : '',
-              )}
-            >
-              {this.statusIcon(h)}
-              <span>{this.status.description}</span>
-            </div>
-          </div>
+          {this.statusBar(h)}
           <div>
             <h2>Live Stats</h2>
             <div class={styles.statsRow}>
@@ -151,41 +203,7 @@ export default class AdvancedStatistics extends TsxComponent<{}> {
               <GlobalSyncStatus />
             </div>
           </div>
-          <div class={styles.section}>
-            <h2>Performance Notifications</h2>
-            <div class={styles.notificationContainer}>
-              {this.notifications.map(notification => (
-                <div class={styles.notification}>
-                  <div class="icon">
-                    {notification.subType === ENotificationSubType.DISCONNECTED && (
-                      <i class={cx('icon-disconnected', styles.errorText)} />
-                    )}
-                    {notification.subType === ENotificationSubType.SKIPPED && (
-                      <i class={cx('icon-reset icon-skipped-frame', styles.warningText)} />
-                    )}
-                    {notification.subType === ENotificationSubType.LAGGED && (
-                      <i class={cx('icon-time', styles.warningText)} />
-                    )}
-                    {notification.subType === ENotificationSubType.DROPPED && (
-                      <i class={cx('icon-back-alt icon-down-arrow', styles.warningText)} />
-                    )}
-                  </div>
-                  <div class="message">{notification.message}</div>
-                  <div class="date">{this.moment(notification.date)}</div>
-                </div>
-              ))}
-              {this.notifications.length === 0 && (
-                <div class={styles.notificationEmpty}>
-                  <div class="message">{$t('You do not have any notifications')}</div>
-                </div>
-              )}
-            </div>
-            <p class={styles.description}>
-              {$t(
-                'When Streamlabs OBS detects performance issues with your stream, such as dropped frames, lagged frames, or a stream disconnection, troubleshooting notifications will be sent to you here.',
-              )}
-            </p>
-          </div>
+          {this.notificationsArea(h)}
         </div>
       </ModalLayout>
     );
