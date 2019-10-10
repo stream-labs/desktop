45599c62d7465c4e9b1e4044f06d3ce68682557d
Andy Creeth <acreeth@gmail.com>
2019-10-10 09:32:34 -0700

    add global settings search to patch notes

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -9,6 +9,7 @@ export const notes: IPatchNotes = {
     'Selective recording allows you to hide certain sources in your recordings that are present in your live stream. Enable it by clicking the icon above the sources list.',
     'Added a button to open up the Streamlabs dashboard in a web browser.',
     'Added a recording timer.',
+    'Added search functionality to the settings window.',
     'Resolved an incompatibility with new drivers for certain webcams.',
     'Lots of bug and crash fixes',
   ],
