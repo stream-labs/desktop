e3484b879180584d7d742d37d439b356f1362e96
berlysia <berlysia@gmail.com>
2019-10-11 17:50:28 +0900

    fix: プレビュー非表示状態の外観を修正

--- app/components/pages/Studio.vue
+++ app/components/pages/Studio.vue
@@ -1,17 +1,17 @@
 <template>
 <div class="studio-page" data-test="Studio">
-  <div class="studio-mode-container" ref="studioModeContainer" :class="{ stacked }">
+  <div v-if="previewEnabled" class="studio-mode-container" ref="studioModeContainer" :class="{ stacked }">
     <studio-mode-controls v-if="studioMode" :stacked="stacked" />
     <div
       class="studio-display-container"
       :class="{ stacked }">
-      <studio-editor v-if="previewEnabled" class="studio-output-display" />
+      <studio-editor class="studio-output-display" />
       <div v-if="studioMode" class="studio-mode-display-container">
         <display class="studio-mode-display" :paddingSize="10" />
       </div>
     </div>
   </div>
-  <div v-if="!previewEnabled" class="no-preview">
+  <div v-else class="no-preview">
     <div class="message">
       {{ $t('scenes.previewIsDisabledInPerformanceMode') }}
       <div class="button button--action button--sm" @click="enablePreview">{{ $t('scenes.disablePerformanceMode') }}</div>
@@ -65,6 +65,7 @@
   flex-grow: 1;
   background-color: @bg-tertiary;
   display: flex;
+  align-items: center;
   justify-content: center;
 
   .message {
