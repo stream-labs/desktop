7d5ea56477611fd27458d58bd572f686e03dbe4f
DESKTOP-FGF6CBD\Peter <peter.kucharski@streamlabs.com>
2019-10-30 17:40:18 -0700

    Change how performance stats are rendered in the footer

--- app/components/StudioFooter.vue
+++ app/components/StudioFooter.vue
@@ -11,15 +11,14 @@
     </div>
     <i
       v-bind:class="['icon-leaderboard-4', 'metrics-icon', performanceIconClassName]"
-      @mouseover="metricsShown = true"
-      @mouseleave="metricsShown = false"
       @click="openMetricsWindow"
     />
+    <performance-metrics mode="limited" class="performance-metrics" />
     <global-sync-status v-if="loggedIn && !mediaBackupOptOut" />
     <notifications-area class="notifications-area flex--grow"/>
-    <transition name="slide">
+    <!-- <transition name="slide">
       <performance-metrics v-if="metricsShown" class="performance-metrics" />
-    </transition>
+    </transition> -->
   </div>
 
   <div class="nav-right">
@@ -212,22 +211,8 @@
 }
 
 .performance-metrics {
-  position: absolute;
+  position: relative;
+  display: inline-flex;
   background: var(--section) !important;
-  left: 50px;
-  z-index: 100;
-}
-
-// performance metrics transition
-
-.slide-enter,
-.slide-leave-to {
-  transform: translateX(50px);
-  opacity: 0;
-}
-
-.slide-enter-active,
-.slide-leave-active {
-  transition: all 0.5s ease;
 }
 </style>
