1a2a8a3db2a8fd0ae2b22ed9352c09958c550c05
Alexander Nazarov <alexzerg91@gmail.com>
2019-12-11 15:34:10 -0800

    remove comments

--- strict-null-check.config.js
+++ strict-null-check.config.js
@@ -1,5 +1,19 @@
 const merge = require('webpack-merge');
 const baseConfig = require('./base.config.js');
+const path = require('path');
+const fs = require('fs');
+
+// read all files eligible for the strictNulls checking
+const filesPath = 'strict-null-check-files';
+const files = fs.readdirSync(filesPath);
+const tsFiles = [];
+const tsxFiles = [];
+files.forEach(file => {
+  const json = JSON.parse(fs.readFileSync(`${filesPath}/${file}`));
+  if (json.ts) tsFiles.push(...json.ts);
+  if (json.tsx) tsxFiles.push(...json.tsx);
+});
+
 
 module.exports = merge.smart(baseConfig, {
   entry: {
@@ -8,6 +22,7 @@ module.exports = merge.smart(baseConfig, {
     'guest-api': './guest-api'
   },
 
+  mode: 'development',
   watchOptions: { ignored: /node_modules/ },
 
   optimization: {
@@ -21,11 +36,32 @@ module.exports = merge.smart(baseConfig, {
         loader: 'awesome-typescript-loader',
         options: {
           useCache: true,
-          reportFiles: ['app/services/core/*.ts'],
+          reportFiles: [
+            ...tsFiles
+          ],
           strictNullChecks: true
         },
         exclude: /node_modules|vue\/src/
       },
-    ]
+      {
+        test: /\.tsx$/,
+        include: path.resolve(__dirname, 'app/components'),
+        loader: [
+          'babel-loader',
+          {
+            loader: 'awesome-typescript-loader',
+            options: {
+              forceIsolatedModules: true,
+              reportFiles: [
+                ...tsxFiles
+              ],
+              configFileName: 'tsxconfig.json',
+              instance: 'tsx-loader'
+            }
+          }
+        ],
+        exclude: /node_modules/,
+      },
+    ],
   }
 });
