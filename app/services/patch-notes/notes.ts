bbdf091ccd15ab3cf1c93062766ef023e0735450
Andy Creeth <acreeth@gmail.com>
2020-01-15 08:44:56 -0800

    patch notes 0.20.0

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,15 +1,13 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.19.0',
-  title: 'Redesigned YouTube and Stats',
+  version: '0.20.0',
+  title: 'Introducing the Layout Editor',
   showChest: false,
   notes: [
-    "We've redesigned the YouTube experience from the ground up to support live events and the live Studio",
-    "The stats window has been redesigned to give you a realtime picture of your stream's health, and allows you to pin individual stats to the bottom of the main window.",
-    'Day mode has been overhauled to provide better contrast and readability',
-    'Added the ability to completely hide the preview display in the editor',
-    'Settings search now additionally searches hotkeys',
+    'Our new Layout Editor feature lets you completely customize the editor tab. Choose what panels you want to see, and where the should go. Complete control is finally here. Click the Layout Editor icon in the bottom left to get started.',
+    "The game capture now has an 'auto' mode that takes the guesswork out of capturing games. It will automatically capture most popular games without need any additional settings.",
+    'Multistream is now available to users logged in via YouTube',
     'Lots of bug and crash fixes',
   ],
 };
