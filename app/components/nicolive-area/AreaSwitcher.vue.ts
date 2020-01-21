c6f581e98715419c02be07faceaecb3aafa67cf9
berlysia <berlysia@gmail.com>
2020-01-21 22:05:56 +0900

    chore(nicolive): console.logを除去

--- app/components/nicolive-area/AreaSwitcher.vue.ts
+++ app/components/nicolive-area/AreaSwitcher.vue.ts
@@ -19,9 +19,7 @@ export default class AreaSwitcher extends Vue {
   activeContent: IArea = this.contents.find(c => c.defaultSelected) ?? this.contents[0];
 
   select(slotName: string) {
-    console.log('select', slotName);
     this.activeContent = this.contents.find(c => c.slotName === slotName)!;
-    console.log(JSON.parse(JSON.stringify(this.activeContent)));
   }
 
 }
