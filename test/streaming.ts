4818b6d9d42a1644bc1d8ab66d9019f1f3bf6fc8
Alexander Nazarov <alexzerg91@gmail.com>
2020-01-23 15:39:37 -0800

    fix test

--- test/streaming.ts
+++ test/streaming.ts
@@ -338,12 +338,11 @@ test('Go live error', async t => {
   await resetFetchMock(t);
   await focusChild(t);
   await app.client.click('a=fetching the information again');
-  await app.client.waitForVisible('button=Confirm & Go Live');
+  await app.client.waitForEnabled('button=Confirm & Go Live');
 
   // test the case when the channel info has been successful fetched but can't be updated
   await fetchMock(t, /api\.twitch\.tv/, 404);
   await focusChild(t);
-  await app.client.waitForVisible('button=Confirm & Go Live');
   await click(t, 'button=Confirm & Go Live');
   await app.client.waitForVisible('a=just go live.');
 
