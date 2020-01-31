28315521cca415ea890b2a46560cbc96dec56839
berlysia <berlysia@gmail.com>
2020-01-31 19:37:59 +0900

    feat(nicolive): ギフトメッセージの仕様を反映

--- app/components/nicolive-area/comment/GiftComment.vue.ts
+++ app/components/nicolive-area/comment/GiftComment.vue.ts
@@ -10,9 +10,9 @@ export default class GiftComment extends Vue {
 
   get computedContent() {
     const parsed = parseContent(this.chat.value);
-    const [itemId, userId, advertiserName, point, message, itemName, contributionRank] = parsed.values;
-    const contributionMessage = contributionRank ? `（第 ${contributionRank} 位）` : '';
-    return `${advertiserName} さんが「${itemName}」を贈りました。「${message}」${contributionMessage}`;
+    const [_itemId, _userId, advertiserName, point, _message, itemName, contributionRank] = parsed.values;
+    const contributionMessage = contributionRank ? `【ギフト貢献第${contributionRank}位】 ` : '';
+    return `${contributionMessage}${advertiserName}さんが「${itemName}（${point}pt）」を贈りました`;
   }
 
   get computedTitle() {
