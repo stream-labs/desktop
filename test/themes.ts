b833a4814034902368bdf365a35afb22e266e0c4
Alexander Nazarov <alexzerg91@gmail.com>
2020-02-19 15:43:11 -0800

    add logs to themes test

--- test/themes.ts
+++ test/themes.ts
@@ -4,7 +4,7 @@ import { sleep } from './helpers/sleep';
 import { FormMonkey } from './helpers/form-monkey';
 import { sceneExisting } from './helpers/spectron/scenes';
 
-useSpectron({ pauseIfFailed: true });
+useSpectron();
 
 const OVERLAY_NAME = 'Portals';
 const OVERLAY_SCENES = ['Live Scene', 'Starting Soon', 'Be Right Back', 'Offline'];
@@ -17,21 +17,27 @@ test('Installing a theme', async (t: any) => {
 
   // await app.client.waitForExist('button=themes', 5000, true);
   await app.client.click('div[title=Themes]');
+  console.log('themes clicked');
 
   await focusLibrary(t);
+  console.log('library focused');
 
   // search a theme
   await formMonkey.setInputValue('input', OVERLAY_NAME);
+  console.log('input typed');
   // the input field has a debounce search
   await sleep(2000);
   // wait items load
   await app.client.click('.market-item');
+  console.log('item clicked');
   // install overlay
   await app.client.click('button=Install Overlay');
+  console.log('install clicked');
 
   // wait for installation complete
   await focusMain(t);
   await app.client.waitForExist('.editor-page', 60000);
+  console.log('install completed');
 
   // Should've loaded the overlay as a new scene collection
   t.true(await app.client.isExisting(`span=${OVERLAY_NAME}`));
