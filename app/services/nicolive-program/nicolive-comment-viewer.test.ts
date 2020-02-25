e52686755226cd80ea76a45b603717e8dbc46427
berlysia <berlysia@gmail.com>
2020-02-25 23:24:08 +0900

    test(nicolive): エラー・切断時のメッセージを検証

--- app/services/nicolive-program/nicolive-comment-viewer.test.ts
+++ app/services/nicolive-program/nicolive-comment-viewer.test.ts
@@ -19,6 +19,7 @@ test('接続先情報が来たら接続する', () => {
   const stateChange = new Subject();
   const clientSubject = new Subject();
   jest.doMock('./MessageServerClient', () => ({
+    ...jest.requireActual('./MessageServerClient'),
     MessageServerClient: class MessageServerClient {
       connect() {
         return clientSubject;
@@ -93,4 +94,176 @@ test('/disconnectが流れてきたらunsubscribeする', () => {
 
   clientSubject.next({ chat: { premium: 2, content: '/disconnect' } });
   expect(unsubscribe).toHaveBeenCalledTimes(2);
+});
+
+function connectionSetup() {
+  const stateChange = new Subject();
+  const clientSubject = new Subject();
+  jest.doMock('./MessageServerClient', () => ({
+    ...jest.requireActual('./MessageServerClient'),
+    MessageServerClient: class MessageServerClient {
+      connect() {
+        return clientSubject;
+      }
+      requestLatestMessages() {}
+    },
+  }));
+  setup({ injectee: { NicoliveProgramService: { stateChange } } });
+
+  const { NicoliveCommentViewerService } = require('./nicolive-comment-viewer');
+  const instance = NicoliveCommentViewerService.instance as NicoliveCommentViewerService;
+
+  stateChange.next({ roomURL: 'https://example.com', roomThreadID: '175622' });
+
+  return {
+    instance,
+    clientSubject,
+  };
+}
+
+test('chatメッセージはstateに保持する', () => {
+  jest.spyOn(Date, 'now').mockImplementation(() => 1582175622000);
+  const { instance, clientSubject } = connectionSetup();
+
+  clientSubject.next({
+    chat: {
+      content: 'yay',
+    },
+  });
+  clientSubject.next({
+    chat: {
+      content: 'foo',
+    },
+  });
+
+  // bufferTime tweaks
+  clientSubject.complete();
+
+  expect(instance.state.messages).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "seqId": 0,
+        "type": "normal",
+        "value": Object {
+          "content": "yay",
+        },
+      },
+      Object {
+        "seqId": 1,
+        "type": "normal",
+        "value": Object {
+          "content": "foo",
+        },
+      },
+      Object {
+        "seqId": 2,
+        "type": "n-air-emulated",
+        "value": Object {
+          "content": "サーバーとの接続が終了しました",
+          "date": 1582175622,
+        },
+      },
+    ]
+  `);
+});
+
+test('接続エラー時にメッセージを表示する', () => {
+  jest.spyOn(Date, 'now').mockImplementation(() => 1582175622000);
+  const { instance, clientSubject } = connectionSetup();
+
+  const error = new Error('yay');
+
+  clientSubject.error(error);
+
+  // bufferTime tweaks
+  clientSubject.complete();
+
+  expect(instance.state.messages).toMatchInlineSnapshot(`
+                          Array [
+                            Object {
+                              "seqId": 0,
+                              "type": "n-air-emulated",
+                              "value": Object {
+                                "content": "エラーが発生しました: yay",
+                                "date": 1582175622,
+                              },
+                            },
+                            Object {
+                              "seqId": 1,
+                              "type": "n-air-emulated",
+                              "value": Object {
+                                "content": "サーバーとの接続が終了しました",
+                                "date": 1582175622,
+                              },
+                            },
+                          ]
+                `);
+});
+
+test('スレッドの参加失敗時にメッセージを表示する', () => {
+  jest.spyOn(Date, 'now').mockImplementation(() => 1582175622000);
+  const { instance, clientSubject } = connectionSetup();
+
+  clientSubject.next({
+    thread: {
+      resultcode: 1,
+    },
+  });
+
+  // bufferTime tweaks
+  clientSubject.complete();
+
+  expect(instance.state.messages).toMatchInlineSnapshot(`
+        Array [
+          Object {
+            "seqId": 0,
+            "type": "n-air-emulated",
+            "value": Object {
+              "content": "スレッドへの参加に失敗しました",
+              "date": 1582175622,
+            },
+          },
+          Object {
+            "seqId": 1,
+            "type": "n-air-emulated",
+            "value": Object {
+              "content": "サーバーとの接続が終了しました",
+              "date": 1582175622,
+            },
+          },
+        ]
+    `);
+});
+
+test('スレッドからの追い出し発生時にメッセージを表示する', () => {
+  jest.spyOn(Date, 'now').mockImplementation(() => 1582175622000);
+  const { instance, clientSubject } = connectionSetup();
+
+  clientSubject.next({
+    leave_thread: {},
+  });
+
+  // bufferTime tweaks
+  clientSubject.complete();
+
+  expect(instance.state.messages).toMatchInlineSnapshot(`
+        Array [
+          Object {
+            "seqId": 0,
+            "type": "n-air-emulated",
+            "value": Object {
+              "content": "スレッドから追い出されました",
+              "date": 1582175622,
+            },
+          },
+          Object {
+            "seqId": 1,
+            "type": "n-air-emulated",
+            "value": Object {
+              "content": "サーバーとの接続が終了しました",
+              "date": 1582175622,
+            },
+          },
+        ]
+    `);
 });
