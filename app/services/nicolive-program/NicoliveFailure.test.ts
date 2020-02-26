82d196bc30bb38d2963ee915515b5895ab907b27
berlysia <berlysia@gmail.com>
2020-02-27 00:09:33 +0900

    feat(nicolive): 4xx,5xxエラーの文言を400,500にフォールバックする

---
+++ app/services/nicolive-program/NicoliveFailure.test.ts
@@ -0,0 +1,69 @@
+type NicoliveFailureType = typeof import('./NicoliveFailure').NicoliveFailure;
+
+afterEach(() => {
+  jest.resetModules();
+});
+
+test('4xxで未定義文言だったら400にフォールバックする', async () => {
+  const showMessageBox = jest.fn().mockImplementation((_window, _option, callback) => {
+    callback();
+  });
+  jest.doMock('electron', () => ({
+    remote: {
+      dialog: {
+        showMessageBox,
+      },
+      getCurrentWindow: () => {},
+    },
+  }));
+  jest.doMock('services/i18n', () => ({
+    $t: jest.fn().mockImplementation((key, { fallback } = {}) => {
+      const keys = key.split('.');
+      const code = keys[keys.length - 2];
+      const value = keys[keys.length - 1];
+      if (code === '400') return value;
+      return fallback;
+    }),
+  }));
+
+  const m = require('./NicoliveFailure');
+  const NicoliveFailure = m.NicoliveFailure as NicoliveFailureType;
+  const openErrorDialogFromFailure = m.openErrorDialogFromFailure;
+  const failure = NicoliveFailure.fromClientError('method', { ok: false, value: { meta: { status: 403 } } });
+
+  await openErrorDialogFromFailure(failure);
+  expect(showMessageBox.mock.calls[0][1].title).toBe('title');
+  expect(showMessageBox.mock.calls[0][1].message).toBe('message');
+});
+
+test('5xxで未定義文言だったら500にフォールバックする', async () => {
+  const showMessageBox = jest.fn().mockImplementation((_window, _option, callback) => {
+    callback();
+  });
+  jest.doMock('electron', () => ({
+    remote: {
+      dialog: {
+        showMessageBox,
+      },
+      getCurrentWindow: () => {},
+    },
+  }));
+  jest.doMock('services/i18n', () => ({
+    $t: jest.fn().mockImplementation((key, { fallback } = {}) => {
+      const keys = key.split('.');
+      const code = keys[keys.length - 2];
+      const value = keys[keys.length - 1];
+      if (code === '500') return value;
+      return fallback;
+    }),
+  }));
+
+  const m = require('./NicoliveFailure');
+  const NicoliveFailure = m.NicoliveFailure as NicoliveFailureType;
+  const openErrorDialogFromFailure = m.openErrorDialogFromFailure;
+  const failure = NicoliveFailure.fromClientError('method', { ok: false, value: { meta: { status: 503 } } });
+
+  await openErrorDialogFromFailure(failure);
+  expect(showMessageBox.mock.calls[0][1].title).toBe('title');
+  expect(showMessageBox.mock.calls[0][1].message).toBe('message');
+});
