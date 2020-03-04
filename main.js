ee97d8e3607399fac5b3aac3dab1a8481940bc47
Andy Creeth <acreeth@gmail.com>
2020-03-04 14:46:57 -0800

    remove extra log set

--- main.js
+++ main.js
@@ -28,11 +28,6 @@ const overlay = require('@streamlabs/game-overlay');
 // We use a special cache directory for running tests
 if (process.env.SLOBS_CACHE_DIR) {
   app.setPath('appData', process.env.SLOBS_CACHE_DIR);
-  electronLog.transports.file.file = path.join(
-    process.env.SLOBS_CACHE_DIR,
-    'slobs-client',
-    'log.log'
-  );
 }
 
 app.setPath('userData', path.join(app.getPath('appData'), 'slobs-client'));
