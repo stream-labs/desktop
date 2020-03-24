2ace7cd959d880fac22ee6b36f255bc4f828d2a4
Sean Beyer <sean.beyer@streamlabs.com>
2020-03-23 17:06:38 -0700

    Add state interface

--- app/services/community-hub/index.ts
+++ app/services/community-hub/index.ts
@@ -4,12 +4,18 @@ import { HostsService } from 'services/hosts';
 import { Inject } from 'services/core/injector';
 import { I18nService } from 'services/i18n';
 
-interface ICommunityHubState {}
+interface ICommunityHubState {
+  friends: Array<{ id: string; username: string; avatar: string; status: string }>;
+  chatrooms: Array<{ id: string; name: string; members: Array<string> }>;
+}
 
 export class CommunityHubService extends StatefulService<ICommunityHubState> {
   @Inject() private hostsService: HostsService;
   @Inject() private userService: UserService;
   @Inject() private i18nService: I18nService;
 
-  static initialState: ICommunityHubState = {};
+  static initialState: ICommunityHubState = {
+    friends: [],
+    chatrooms: [],
+  };
 }
