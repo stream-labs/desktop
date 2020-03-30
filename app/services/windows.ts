a3db2d7f7f3a3a97da757a082082895b2b921561
Andy Creeth <acreeth@gmail.com>
2020-03-30 13:37:47 -0700

    Release 0.1.0-mac

--- app/services/windows.ts
+++ app/services/windows.ts
@@ -173,7 +173,7 @@ export class WindowsService extends StatefulService<IWindowsState> {
       isShown: true,
       hideStyleBlockers: true,
       hideChat: false,
-      title: `Streamlabs OBS - ${remote.process.env.SLOBS_VERSION} - Private Beta`,
+      title: `Streamlabs OBS - ${remote.process.env.SLOBS_VERSION}`,
     },
     child: {
       componentName: '',
