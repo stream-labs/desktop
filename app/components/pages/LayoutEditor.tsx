a0b9a766ca0f8d3976a6e77e19f8b920fd1047bb
Sean Beyer <sean.beyer@streamlabs.com>
2020-04-03 15:45:41 -0700

    Fix prime flow and website element focusing

--- app/components/pages/LayoutEditor.tsx
+++ app/components/pages/LayoutEditor.tsx
@@ -26,6 +26,7 @@ export default class LayoutEditor extends TsxComponent {
 
   private highlightedSlot: LayoutSlot = null;
   private showModal = false;
+  canDragSlot = true;
 
   mounted() {
     if (this.slottedElements[ELayoutElement.Browser]) {
@@ -212,7 +213,7 @@ export default class LayoutEditor extends TsxComponent {
       <div
         class={this.classForSlot(slot)}
         id={slot}
-        draggable={this.elementInSlot(slot)}
+        draggable={this.elementInSlot(slot) && this.canDragSlot}
         ondragenter={(): unknown => (this.highlightedSlot = slot)}
         ondragexit={(): unknown => (this.highlightedSlot = null)}
         onDragend={(e: MouseEvent) =>
@@ -224,6 +225,8 @@ export default class LayoutEditor extends TsxComponent {
           <TextInput
             class={styles.urlTextBox}
             vModel={this.browserUrl}
+            onFocus={() => (this.canDragSlot = false)}
+            onBlur={() => (this.canDragSlot = true)}
             metadata={{ placeholder: $t('Enter Target URL') }}
           />
         )}
