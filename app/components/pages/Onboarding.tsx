03375223c4417a0003531465254c4c92dc2bedc9
Sean Beyer <sean.beyer@streamlabs.com>
2020-04-28 15:33:01 -0700

    Move step handling to OnboardingService

--- app/components/pages/Onboarding.tsx
+++ app/components/pages/Onboarding.tsx
@@ -3,62 +3,18 @@ import { Onboarding } from 'streamlabs-beaker';
 import TsxComponent from 'components/tsx-component';
 import { OnboardingService } from 'services/onboarding';
 import { Inject } from 'services/core/injector';
-import { IncrementalRolloutService, EAvailableFeatures } from 'services/incremental-rollout';
-import { SceneCollectionsService } from 'services/scene-collections';
-import {
-  Connect,
-  ObsImport,
-  StreamlabsFeatures,
-  HardwareSetup,
-  ThemeSelector,
-  Optimize,
-  FacebookPageCreation,
-  Multistream,
-  MacPermissions,
-} from './onboarding-steps';
-import { UserService } from 'services/user';
-import { $t } from 'services/i18n';
 import styles from './Onboarding.m.less';
-import { RestreamService } from 'services/restream';
-import { OS } from 'util/operating-systems';
-
-interface IOnboardingStep {
-  element: JSX.Element;
-  disableControls: boolean;
-  hideSkip: boolean;
-  hideButton: boolean;
-  requiresHack?: boolean;
-}
 
 @Component({})
 export default class OnboardingPage extends TsxComponent<{}> {
   @Inject() onboardingService: OnboardingService;
-  @Inject() incrementalRolloutService: IncrementalRolloutService;
-  @Inject() userService: UserService;
-  @Inject() sceneCollectionsService: SceneCollectionsService;
-  @Inject() restreamService: RestreamService;
 
   currentStep = 1;
   importedFromObs = false;
   processing = false;
-  fbSetupEnabled: boolean = null;
-
-  checkFbPageEnabled() {
-    if (this.fbSetupEnabled !== null || !this.userService.isLoggedIn) return;
-    // This will do a second unnecessary fetch, but it's the only
-    // way to be sure we have fetched features
-    this.incrementalRolloutService.fetchAvailableFeatures().then(() => {
-      if (this.incrementalRolloutService.featureIsEnabled(EAvailableFeatures.facebookOnboarding)) {
-        this.fbSetupEnabled = true;
-      } else {
-        this.fbSetupEnabled = false;
-      }
-    });
-  }
 
   continue(importedObs?: boolean) {
     if (importedObs != null) this.importedFromObs = importedObs;
-    this.checkFbPageEnabled();
     this.proceed();
   }
 
@@ -90,163 +46,27 @@ export default class OnboardingPage extends TsxComponent<{}> {
   }
 
   get steps() {
-    const steps: IOnboardingStep[] = [];
-
-    if (process.platform === OS.Mac) {
-      steps.push({
-        element: (
-          <MacPermissions
-            slot={(steps.length + 1).toString()}
-            continue={this.continue.bind(this)}
-          />
-        ),
-        disableControls: false,
-        hideSkip: true,
-        hideButton: true,
-      });
-    }
-
-    steps.push({
-      element: <Connect slot={(steps.length + 1).toString()} continue={this.continue.bind(this)} />,
-      disableControls: false,
-      hideSkip: true,
-      hideButton: true,
-    });
-
-    steps.push({
-      element: (
-        <ObsImport
-          slot={(steps.length + 1).toString()}
-          continue={this.continue.bind(this)}
-          setProcessing={this.setProcessing.bind(this)}
-        />
-      ),
-      disableControls: true,
-      hideSkip: true,
-      hideButton: true,
-    });
-
-    if (this.importedFromObs) {
-      steps.push({
-        element: <StreamlabsFeatures slot={(steps.length + 1).toString()} />,
-        disableControls: false,
-        hideSkip: true,
-        hideButton: false,
-        requiresHack: true,
-      });
-    } else {
-      steps.push({
-        element: <HardwareSetup slot={(steps.length + 1).toString()} />,
-        disableControls: false,
-        hideSkip: false,
-        hideButton: false,
-        requiresHack: true,
-      });
-    }
-
-    if (this.noExistingSceneCollections) {
-      steps.push({
-        element: (
-          <ThemeSelector
-            slot={(steps.length + 1).toString()}
-            continue={this.continue.bind(this)}
-            setProcessing={this.setProcessing.bind(this)}
-          />
-        ),
-        disableControls: false,
-        hideSkip: false,
-        hideButton: true,
-      });
-    }
-
-    if (this.onboardingService.isTwitchAuthed) {
-      steps.push({
-        element: (
-          <Optimize
-            slot={(steps.length + 1).toString()}
-            continue={this.continue.bind(this)}
-            setProcessing={this.setProcessing.bind(this)}
-          />
-        ),
-        disableControls: false,
-        hideSkip: false,
-        hideButton: true,
-      });
-
-      // steps.push({
-      //   element: (
-      //     <Multistream slot={(steps.length + 1).toString()} continue={() => this.continue()} />
-      //   ),
-      //   disableControls: false,
-      //   hideSkip: false,
-      //   hideButton: true,
-      // });
-    }
-
-    if (this.onboardingService.isFacebookAuthed && this.fbSetupEnabled) {
-      steps.push({
-        element: (
-          <FacebookPageCreation
-            slot={(steps.length + 1).toString()}
-            continue={this.continue.bind(this)}
-          />
-        ),
-        disableControls: false,
-        hideSkip: false,
-        hideButton: true,
-      });
-    }
-
-    return steps;
+    return this.onboardingService.views.steps;
   }
 
-  get loginPage() {
-    return (
-      <div>
-        <div class={styles.container}>
-          <Connect continue={this.complete.bind(this)} />
+  get singletonStep() {
+    if (this.onboardingService.views.singletonStep) {
+      const Component = this.onboardingService.views.singletonStep;
+      return (
+        <div>
+          <div class={styles.container}>
+            <Component
+              continue={this.complete.bind(this)}
+              setProcessing={this.setProcessing.bind(this)}
+            />
+          </div>
         </div>
-      </div>
-    );
-  }
-
-  get optimizePage() {
-    return (
-      <div>
-        <div class={styles.container}>
-          <Optimize
-            continue={this.complete.bind(this)}
-            setProcessing={this.setProcessing.bind(this)}
-          />
-        </div>
-      </div>
-    );
-  }
-
-  get hardwarePage() {
-    return (
-      <div>
-        <div class={styles.container}>
-          <HardwareSetup />
-          <button class="button button--action" onClick={this.complete}>
-            {$t('Complete')}
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  get noExistingSceneCollections() {
-    return (
-      this.sceneCollectionsService.loadableCollections.length === 1 &&
-      this.sceneCollectionsService.loadableCollections[0].auto
-    );
+      );
+    }
   }
 
   render() {
-    if (this.onboardingService.options.isLogin) return this.loginPage;
-    if (this.onboardingService.options.isOptimize) return this.optimizePage;
-    if (this.onboardingService.options.isHardware) return this.hardwarePage;
+    if (this.singletonStep) return this.singletonStep;
 
     return (
       <div>
--- app/services/onboarding.ts
+++ app/services/onboarding.ts
@@ -1,17 +1,90 @@
-import { StatefulService, mutation } from './core/stateful-service';
-import { NavigationService } from './navigation';
-import { UserService } from './user';
-import { Inject } from './core/injector';
+import { StatefulService, mutation } from 'services/core/stateful-service';
+import { NavigationService } from 'services/navigation';
+import { UserService } from 'services/user';
+import { Inject, ViewHandler } from 'services/core/';
+import { SceneCollectionsService } from 'services/scene-collections';
+import * as onboardingSteps from 'components/pages/onboarding-steps';
+import TsxComponent from 'components/tsx-component';
+import { OS } from 'util/operating-systems';
 
-type TOnboardingStep =
-  | 'Connect'
-  | 'OptimizeA'
-  | 'OptimizeB'
-  | 'OptimizeC'
-  | 'OptimizeBrandDevice'
-  | 'SceneCollectionsImport'
-  | 'ObsImport'
-  | 'FacebookPageCreation';
+enum EOnboardingSteps {
+  MacPermissions = 'MacPermissions',
+  Connect = 'Connect',
+  ObsImport = 'ObsImport',
+  StreamlabsFeatures = 'StreamlabsFeatures',
+  HardwareSetup = 'HardwareSetup',
+  ThemeSelector = 'ThemeSelector',
+  Optimize = 'Optimize',
+  Multistream = 'Multistream',
+  FacebookPageCreation = 'FacebookPageCreation',
+}
+
+const ONBOARDING_STEPS = {
+  [EOnboardingSteps.MacPermissions]: {
+    element: onboardingSteps.MacPermissions,
+    disableControls: false,
+    hideSkip: true,
+    hideButton: true,
+  },
+  [EOnboardingSteps.Connect]: {
+    element: onboardingSteps.Connect,
+    disableControls: false,
+    hideSkip: true,
+    hideButton: true,
+  },
+  [EOnboardingSteps.ObsImport]: {
+    element: onboardingSteps.ObsImport,
+    disableControls: true,
+    hideSkip: true,
+    hideButton: true,
+  },
+  [EOnboardingSteps.StreamlabsFeatures]: {
+    element: onboardingSteps.StreamlabsFeatures,
+    disableControls: false,
+    hideSkip: true,
+    hideButton: false,
+    requiresHack: true,
+  },
+  [EOnboardingSteps.HardwareSetup]: {
+    element: onboardingSteps.HardwareSetup,
+    disableControls: false,
+    hideSkip: false,
+    hideButton: false,
+    requiresHack: true,
+  },
+  [EOnboardingSteps.ThemeSelector]: {
+    element: onboardingSteps.ThemeSelector,
+    disableControls: false,
+    hideSkip: false,
+    hideButton: true,
+  },
+  [EOnboardingSteps.Optimize]: {
+    element: onboardingSteps.Optimize,
+    disableControls: false,
+    hideSkip: false,
+    hideButton: true,
+  },
+  [EOnboardingSteps.Multistream]: {
+    element: onboardingSteps.Multistream,
+    disableControls: false,
+    hideSkip: false,
+    hideButton: true,
+  },
+  [EOnboardingSteps.FacebookPageCreation]: {
+    element: onboardingSteps.FacebookPageCreation,
+    disableControls: false,
+    hideSkip: false,
+    hideButton: true,
+  },
+};
+
+interface IOnboardingStep {
+  element: typeof TsxComponent;
+  disableControls: boolean;
+  hideSkip: boolean;
+  hideButton: boolean;
+  requiresHack?: boolean;
+}
 
 interface IOnboardingOptions {
   isLogin: boolean; // When logging into a new account after onboarding
@@ -23,17 +96,44 @@ interface IOnboardingOptions {
 
 interface IOnboardingServiceState {
   options: IOnboardingOptions;
+  importedFromObs: boolean;
+  existingSceneCollections: boolean;
 }
 
-// Represents a single step in the onboarding flow.
-// Implemented as a linked list.
-interface IOnboardingStep {
-  // Whether this step should run.  The service is
-  // passed in as an argument.
-  isEligible: (service: OnboardingService) => boolean;
+class OnboardingViews extends ViewHandler<IOnboardingServiceState> {
+  get singletonStep() {
+    if (this.state.options.isLogin) return onboardingSteps.Connect;
+    if (this.state.options.isOptimize) return onboardingSteps.Optimize;
+    if (this.state.options.isHardware) return onboardingSteps.HardwareSetup;
+  }
 
-  // The next step in the flow
-  next?: TOnboardingStep;
+  get steps() {
+    const steps: IOnboardingStep[] = [];
+
+    if (process.platform === OS.Mac) {
+      steps.push(ONBOARDING_STEPS[EOnboardingSteps.MacPermissions]);
+    }
+
+    steps.push(ONBOARDING_STEPS[EOnboardingSteps.Connect]);
+    steps.push(ONBOARDING_STEPS[EOnboardingSteps.ObsImport]);
+
+    if (this.state.importedFromObs) {
+      steps.push(ONBOARDING_STEPS[EOnboardingSteps.StreamlabsFeatures]);
+    } else {
+      steps.push(ONBOARDING_STEPS[EOnboardingSteps.HardwareSetup]);
+    }
+
+    if (!this.state.existingSceneCollections) {
+      steps.push(ONBOARDING_STEPS[EOnboardingSteps.ThemeSelector]);
+    }
+
+    if (this.getServiceViews(UserService).isTwitchAuthed) {
+      steps.push(ONBOARDING_STEPS[EOnboardingSteps.Optimize]);
+      steps.push(ONBOARDING_STEPS[EOnboardingSteps.Multistream]);
+    }
+
+    return steps;
+  }
 }
 
 export class OnboardingService extends StatefulService<IOnboardingServiceState> {
@@ -44,20 +144,48 @@ export class OnboardingService extends StatefulService<IOnboardingServiceState> 
       isSecurityUpgrade: false,
       isHardware: false,
     },
+    importedFromObs: false,
+    existingSceneCollections: false,
   };
 
   localStorageKey = 'UserHasBeenOnboarded';
 
   @Inject() navigationService: NavigationService;
   @Inject() userService: UserService;
+  @Inject() sceneCollectionsService: SceneCollectionsService;
 
   @mutation()
   SET_OPTIONS(options: Partial<IOnboardingOptions>) {
     Object.assign(this.state.options, options);
   }
 
+  @mutation()
+  SET_OBS_IMPORTED(val: boolean) {
+    this.state.importedFromObs = val;
+  }
+
+  @mutation()
+  SET_EXISTING_COLLECTIONS(val: boolean) {
+    this.state.existingSceneCollections = val;
+  }
+
+  get views() {
+    return new OnboardingViews(this.state);
+  }
+
   get options() {
     return this.state.options;
+  }
+
+  get existingSceneCollections() {
+    return !(
+      this.sceneCollectionsService.loadableCollections.length === 1 &&
+      this.sceneCollectionsService.loadableCollections[0].auto
+    );
+  }
+
+  init() {
+    this.SET_EXISTING_COLLECTIONS(this.existingSceneCollections);
   }
 
   // A login attempt is an abbreviated version of the onboarding process,
