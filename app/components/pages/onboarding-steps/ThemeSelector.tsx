5ebb2e6f6a9ce3187bacff3f79ff421b031ee2e7
Sean Beyer <sean.beyer@streamlabs.com>
2020-05-12 16:26:11 -0700

    Add theme detail panel

--- app/components/pages/onboarding-steps/ThemeSelector.tsx
+++ app/components/pages/onboarding-steps/ThemeSelector.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames';
 import { Component } from 'vue-property-decorator';
 import { ProgressBar } from 'streamlabs-beaker';
 import TsxComponent, { createProps } from 'components/tsx-component';
@@ -6,6 +7,7 @@ import { Inject } from 'services';
 import { SceneCollectionsService } from 'services/scene-collections';
 import commonStyles from './Common.m.less';
 import styles from './ThemeSelector.m.less';
+import { throttleSetter } from 'lodash-decorators';
 
 class ThemeSelectorProps {
   continue: () => void = () => {};
@@ -17,6 +19,7 @@ export default class ObsImport extends TsxComponent<ThemeSelectorProps> {
   @Inject() sceneCollectionsService: SceneCollectionsService;
 
   installing = false;
+  showDetail: string = null;
   progress = 0;
 
   get themesMetadata() {
@@ -25,33 +28,58 @@ export default class ObsImport extends TsxComponent<ThemeSelectorProps> {
         title: 'Borderline [Red Yellow] - by Nerd or Die',
         url: 'https://cdn.streamlabs.com/marketplace/overlays/7684923/ea91062/ea91062.overlay',
         thumbnail: 'borderline',
+        detail: {},
       },
       {
         title: 'Dark Matter by VBI',
         url: 'https://cdn.streamlabs.com/marketplace/overlays/7684923/3205db0/3205db0.overlay',
         thumbnail: 'darkmatter',
+        detail: {},
       },
       {
         title: 'Geometic Madness',
         url: 'https://cdn.streamlabs.com/marketplace/overlays/2116872/17f7cb5/17f7cb5.overlay',
         thumbnail: 'geometric',
+        detail: {},
       },
       {
         title: 'Nexus',
         url: 'https://cdn.streamlabs.com/marketplace/overlays/7684923/dd96270/dd96270.overlay',
         thumbnail: 'nexus',
+        detail: {},
       },
       {
         title: 'Relative Minds',
         url: 'https://cdn.streamlabs.com/marketplace/overlays/7684923/0d2e611/0d2e611.overlay',
         thumbnail: 'relativeminds',
+        detail: {},
       },
       {
         title: 'Facebook Gaming Pure Hexagons',
         url: 'https://cdn.streamlabs.com/marketplace/overlays/8062844/4a0582e/4a0582e.overlay',
         thumbnail: 'purehexagon',
+        detail: {},
       },
     ];
+  }
+
+  get detailInfo() {
+    const index = this.themesMetadata.findIndex(data => data.title === this.showDetail);
+    if (index === -1) return null;
+    return { index, ...this.themesMetadata[index] };
+  }
+
+  get filteredMetadata() {
+    if (!this.detailInfo) return this.themesMetadata;
+    if ([2, 5].includes(this.detailInfo.index)) {
+      return [this.themesMetadata[0], this.themesMetadata[3]];
+    } else {
+      return [this.themesMetadata[2], this.themesMetadata[5]];
+    }
+  }
+
+  focusTheme(title: string) {
+    this.showDetail = title;
   }
 
   async installTheme(url: string, name: string) {
@@ -74,8 +102,8 @@ export default class ObsImport extends TsxComponent<ThemeSelectorProps> {
         <div>
           {!this.installing ? (
             <div class={styles.container}>
-              {this.themesMetadata.map(theme => (
-                <div class={styles.cell} onClick={() => this.installTheme(theme.url, theme.title)}>
+              {this.filteredMetadata.map(theme => (
+                <div class={styles.cell} onClick={() => this.focusTheme(theme.title)}>
                   <img
                     class={styles.thumbnail}
                     src={require(`../../../../media/images/onboarding/${theme.thumbnail}.png`)}
@@ -83,6 +111,17 @@ export default class ObsImport extends TsxComponent<ThemeSelectorProps> {
                   <div class={styles.title}>{theme.title}</div>
                 </div>
               ))}
+              {this.showDetail && (
+                <div
+                  class={cx(
+                    styles.detailPanel,
+                    [2, 5].includes(this.detailInfo.index) ? styles.right : styles.left,
+                  )}
+                  onClick={() => this.focusTheme(null)}
+                >
+                  {/* <img /> */}
+                </div>
+              )}
             </div>
           ) : (
             <ProgressBar progressComplete={Math.floor(this.progress * 100)} />
