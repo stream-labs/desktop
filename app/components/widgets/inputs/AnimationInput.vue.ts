aeddf5a926d16ff45af4b79507a4b208afe7701d
Trevor Lemeron <trevorrl@me.com>
2020-06-25 17:32:28 -0400

    first try at proper fix/refactor, removed comments

--- app/components/widgets/inputs/AnimationInput.vue.ts
+++ app/components/widgets/inputs/AnimationInput.vue.ts
@@ -5,21 +5,69 @@ import { BaseInput } from 'components/shared/inputs/BaseInput';
 import { IAnimationMetadata } from './index';
 import { $t } from 'services/i18n';
 
-// const COMMON_ANIMATIONS = [
-//   'bounce',
-//   'flash',
-//   'pulse',
-//   'rubberBand',
-//   'shake',
-//   'headShake',
-//   'swing',
-//   'tada',
-//   'wobble',
-//   'jello',
-//   'flip',
-//   'hinge',
-//   'jackInTheBox',
-// ];
+const eventInAnimations = () => [
+  { title: $t('Bounce'), value: 'bounce' },
+  { title: $t('Bounce In'), value: 'bounceIn' },
+  { title: $t('Bounce In Down'), value: 'bounceInDown' },
+  { title: $t('Bounce In Left'), value: 'bounceInLeft' },
+  { title: $t('Bounce In Right'), value: 'bounceInRight' },
+  { title: $t('Bounce In Up'), value: 'bounceInUp' },
+  { title: $t('Fade In'), value: 'fadeIn' },
+  { title: $t('Fade In Down'), value: 'fadeInDown' },
+  { title: $t('Fade In Down Big'), value: 'fadeInDownBig' },
+  { title: $t('Fade In Left'), value: 'fadeInLeft' },
+  { title: $t('Fade In Left Big'), value: 'fadeInLeftBig' },
+  { title: $t('Fade In Right'), value: 'fadeInRight' },
+  { title: $t('Fade In Up'), value: 'fadeInUp' },
+  { title: $t('Fade In Up Big'), value: 'fadeInUpBig' },
+  { title: $t('Light Speed In'), value: 'lightSpeedIn' },
+  { title: $t('Flash'), value: 'flash' },
+  { title: $t('Pulse'), value: 'pulse' },
+  { title: $t('Rubberband'), value: 'rubberband' },
+  { title: $t('Shake'), value: 'shake' },
+  { title: $t('Swing'), value: 'swing' },
+  { title: $t('Tada'), value: 'tada' },
+  { title: $t('Wobble'), value: 'wobble' },
+  { title: $t('Jello'), value: 'jello' },
+  { title: $t('Hinge'), value: 'hinge' },
+  { title: $t('Roll In'), value: 'rollIn' },
+  { title: $t('Rotate In Left'), value: 'rotateInLeft' },
+  { title: $t('Rotate In Right'), value: 'rotateInRight' },
+  { title: $t('Zoom In'), value: 'zoomIn' },
+  { title: $t('Zoom In Down'), value: 'zoomInDown' },
+  { title: $t('Zoom In Left'), value: 'zoomInLeft' },
+  { title: $t('Zoom In Right'), value: 'zoomInRight' },
+  { title: $t('Zoom In Up'), value: 'zoomInUp' },
+  { title: $t('Slide In Down'), value: 'slideInDown' },
+  { title: $t('Slide In Left'), value: 'slideInLeft' },
+  { title: $t('Slide In Right'), value: 'slideInRight' },
+];
+
+const eventOutAnimations = () => [
+  { title: $t('Bounce Out'), value: 'bounceOut' },
+  { title: $t('Bounce Out Down'), value: 'bounceOutDown' },
+  { title: $t('Bounce Out Left'), value: 'bounceOutLeft' },
+  { title: $t('Bounce Out Right'), value: 'bounceOutRight' },
+  { title: $t('Bounce Out Up'), value: 'bounceOutUp' },
+  { title: $t('Fade Out'), value: 'fadeOut' },
+  { title: $t('Fade Out Down'), value: 'fadeOutDown' },
+  { title: $t('Fade Out Down Big'), value: 'fadeOutDownBig' },
+  { title: $t('Fade Out Left'), value: 'fadeOutLeft' },
+  { title: $t('Fade Out Left Big'), value: 'fadeOutLeftBig' },
+  { title: $t('Fade Out Right'), value: 'fadeOutRight' },
+  { title: $t('Fade Out Up'), value: 'fadeOutUp' },
+  { title: $t('Fade Out Up Big'), value: 'fadeOutUpBig' },
+  { title: $t('Light Speed Out'), value: 'lightSpeedOut' },
+  { title: $t('Hinge'), value: 'hinge' },
+  { title: $t('Roll Out'), value: 'rollOut' },
+  { title: $t('Rotate Out Left'), value: 'rotateOutLeft' },
+  { title: $t('Rotate Out Right'), value: 'rotateOutRight' },
+  { title: $t('Zoom Out'), value: 'zoomOut' },
+  { title: $t('Zoom Out Down'), value: 'zoomOutDown' },
+  { title: $t('Zoom Out Left'), value: 'zoomOutLeft' },
+  { title: $t('Zoom Out Right'), value: 'zoomOutRight' },
+  { title: $t('Zoom Out Up'), value: 'zoomOutUp' },
+];
 
 const inAnimations = () => [
   { title: $t('Bounce In'), value: 'bounceIn' },
@@ -116,37 +164,18 @@ export default class AnimationInput extends BaseInput<string, IAnimationMetadata
   readonly title: string;
 
   get listInputMetadata() {
-    const animations = [];
 
-    if (this.options.filter === 'in') {
-      return { ...this.options, options: inAnimations() };
-    } else if (this.options.filter === 'out') {
-      return { ...this.options, options: outAnimations() };
-    } else if (this.options.filter === 'text') {
-      return { ...this.options, options: textAnimations() };
+    switch (this.options.filter) {
+      case 'in':
+        return { ...this.options, options: inAnimations() };
+      case 'out':
+        return { ...this.options, options: outAnimations() };
+      case 'text':
+        return { ...this.options, options: textAnimations() };
+      case 'eventIn':
+        return { ...this.options, options: eventInAnimations() };
+      case 'eventOut':
+        return { ...this.options, options: eventOutAnimations() };
     }
-
-    return {
-      ...this.options,
-    };
   }
 }
-
-
-// get listInputMetadata() {
-//   const animations = [].concat(COMMON_ANIMATIONS);
-//   if (this.options.filter === 'in') {
-//     animations.push(...IN_ANIMATIONS);
-//   } else if (this.options.filter === 'out') {
-//     animations.push(...OUT_ANIMATIONS);
-//   } else {
-//     animations.push(...IN_ANIMATIONS, ...OUT_ANIMATIONS);
-//   }
-//   if (this.options.filter === 'text') {
-//     return { ...this.options, options: textAnimations() };
-//   }
-//   return {
-//     ...this.options,
-//     options: animations.map(animationName => ({ title: animationName, value: animationName })),
-//   };
-// }
