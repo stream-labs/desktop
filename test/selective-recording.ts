c34e7c35c2920ed710f6fcdd87e760e520f633e9
Alexander Nazarov <alexzerg91@gmail.com>
2020-07-13 21:35:00 -0700

    fix selective recording

--- test/selective-recording.ts
+++ test/selective-recording.ts
@@ -42,10 +42,9 @@ test('Selective Recording', async t => {
 
   // Start recording and wait
   await client.click('.record-button');
-  await sleep(2000);
 
   // Ensure recording indicator is active
-  t.true(await client.isExisting('.record-button.active'));
+  await client.waitForVisible('.record-button.active', 15000);
 
   // Stop recording
   await client.click('.record-button');
