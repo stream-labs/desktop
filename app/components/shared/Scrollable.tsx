166fbdcb564bf5e02545a75e7955972493d945a9
Sean Beyer <sean.beyer@streamlabs.com>
2020-08-11 13:33:12 -0700

    Add Scrollable component

--- app/components/shared/Scrollable.tsx
+++ app/components/shared/Scrollable.tsx
@@ -1,8 +1,24 @@
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-vue';
-import TsxComponent from 'components/tsx-component';
+import TsxComponent, { createProps } from 'components/tsx-component';
+import { Component } from 'vue-property-decorator';
 
-export default class Scrollable extends TsxComponent {
+class ScrollableProps {
+  className?: string = '';
+}
+
+@Component({ props: createProps(ScrollableProps) })
+export default class Scrollable extends TsxComponent<ScrollableProps> {
   render() {
-    return <OverlayScrollbarsComponent>{this.$slots.default}</OverlayScrollbarsComponent>;
+    return (
+      <OverlayScrollbarsComponent
+        options={{
+          className: this.props.className,
+          resize: 'both',
+          scrollbars: { clickScrolling: true },
+        }}
+      >
+        {this.$slots.default}
+      </OverlayScrollbarsComponent>
+    );
   }
 }
