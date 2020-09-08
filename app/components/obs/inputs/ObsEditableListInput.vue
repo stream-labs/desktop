7969946676ac00c68661372487a3e6b7ca8fc84e
gettinToasty <sean.beyer@streamlabs.com>
2020-09-08 15:27:25 -0700

    Fix ObsEditableListInput (#2926)

--- app/components/obs/inputs/ObsEditableListInput.vue
+++ app/components/obs/inputs/ObsEditableListInput.vue
@@ -1,41 +1,43 @@
 <template>
-<div class="input-container">
-  <div class="input-label">
-    <label>
-      {{ value.description }}
-    </label>
-  </div>
-  <div class="input-wrapper editable-list">
-    <div class="editable-list__bar">
-      <i
-        @click="menu.popup()"
-        class="icon-add icon-button editable-list__control"/>
-      <i
-        @click="handleRemove"
-        class="icon-subtract icon-button editable-list__control"/>
-      <i
-        @click="handleEdit"
-        class="icon-settings icon-button editable-list__control"/>
+  <div class="input-container">
+    <div class="input-label">
+      <label>
+        {{ value.description }}
+      </label>
     </div>
-    <selector
-      class="editable-list__list"
-      :items="list"
-      :activeItems="activeItem ? [activeItem] : []"
-      @select="handleSelect"
-      @sort="handleSort"/>
+    <div class="input-wrapper editable-list">
+      <div class="editable-list__bar">
+        <i @click="menu.popup()" class="icon-add icon-button editable-list__control" />
+        <i @click="handleRemove" class="icon-subtract icon-button editable-list__control" />
+        <i @click="handleEdit" class="icon-settings icon-button editable-list__control" />
+      </div>
+      <selector
+        class="editable-list__list"
+        :items="list"
+        :activeItems="activeItem ? [activeItem] : []"
+        @select="handleSelect"
+        @sort="handleSort"
+      />
+    </div>
   </div>
-</div>
 </template>
 
 <script lang="ts" src="./ObsEditableListInput.vue.ts"></script>
 
 <style lang="less" scoped>
-@import "../../../styles/index";
+@import '../../../styles/index';
+
+.editable-list {
+  display: flex;
+  flex-direction: column;
+  height: 150px;
+}
 
 .editable-list__list {
   background: var(--input-bg);
   border-color: var(--button);
   border: 0;
+  flex-grow: 1;
 }
 
 .editable-list__bar {
