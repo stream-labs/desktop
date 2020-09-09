e55a1889cbf95b3875df869e54b9570d47f24024
Sean Beyer <sean.beyer@streamlabs.com>
2020-09-09 11:36:01 -0700

    Revert "Fix FFZ scroll"
    This reverts commit 2a1978b34919c7fc1a85faa9b61f5054a54bcd67.

--- app/components/windows/FFZSettings.vue
+++ app/components/windows/FFZSettings.vue
@@ -1,23 +1,19 @@
 <template>
-  <modal-layout :show-controls="false">
-    <div slot="content" class="ffz-settings">
-      <webview
-        class="ffz-settings-container"
-        id="ffz-settings"
-        ref="ffzSettings"
-        :src="popoutURL"
+<modal-layout :show-controls="false">
+  <div slot="content" class="ffz-settings">
+    <webview
+      class="ffz-settings-container"
+      id="ffz-settings"
+      ref="ffzSettings"
+      :src="popoutURL"
       />
-    </div>
-  </modal-layout>
+  </div>
+</modal-layout>
 </template>
 
 <script lang="ts" src="./FFZSettings.vue.ts"></script>
 
-<style lang="less" scoped>
-.ffz-settings {
-  height: 100%;
-}
-
+<style lang="less">
 .ffz-settings-container {
   width: 100%;
   height: 100%;
