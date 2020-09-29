f9d36e7687c850c1ba131f5a9e06ff0cc0d611ba
Andy Creeth <acreeth@gmail.com>
2020-09-29 16:54:20 -0700

    hotkeys performance part 1

--- app/components/windows/settings/HotkeyGroup.tsx
+++ app/components/windows/settings/HotkeyGroup.tsx
@@ -7,11 +7,16 @@ import TsxComponent, { createProps } from 'components/tsx-component';
 class HotkeyGroupProps {
   hotkeys: IHotkey[] = [];
   title: string = null;
+  isSearch: boolean = false;
 }
 
 @Component({ props: createProps(HotkeyGroupProps) })
 export default class HotkeyGroup extends TsxComponent<HotkeyGroupProps> {
-  collapsed = false;
+  collapsed = true;
+
+  mounted() {
+    console.log(this.props);
+  }
 
   get header() {
     return this.props.title ? (
@@ -19,11 +24,17 @@ export default class HotkeyGroup extends TsxComponent<HotkeyGroupProps> {
         class="section-title section-title--dropdown"
         onClick={() => (this.collapsed = !this.collapsed)}
       >
-        {this.collapsed ? <i class="fa fa-plus section-title__icon" /> : null}
-        {!this.collapsed ? <i class="fa fa-minus section-title__icon" /> : null}
+        {this.isCollapsible && this.collapsed ? <i class="fa fa-plus section-title__icon" /> : null}
+        {this.isCollapsible && !this.collapsed ? (
+          <i class="fa fa-minus section-title__icon" />
+        ) : null}
         {this.props.title}
       </h2>
     ) : null;
+  }
+
+  get isCollapsible() {
+    return this.props.title && !this.props.isSearch;
   }
 
   render() {
@@ -31,16 +42,20 @@ export default class HotkeyGroup extends TsxComponent<HotkeyGroupProps> {
       <div class="section">
         {this.header}
         <transition name="expand">
-          <div
-            style={this.collapsed ? { display: 'none' } : null}
-            class={cx({ 'section-content--opened': !!this.props.title }, 'section-content')}
-          >
-            {this.props.hotkeys.map(hotkey => (
-              <div key={hotkey.actionName + hotkey.sceneId + hotkey.sceneItemId + hotkey.sourceId}>
-                <Hotkey hotkey={hotkey} />
-              </div>
-            ))}
-          </div>
+          {(!this.isCollapsible || !this.collapsed) && (
+            <div
+              // style={this.collapsed ? { display: 'none' } : null}
+              class={cx({ 'section-content--opened': !!this.props.title }, 'section-content')}
+            >
+              {this.props.hotkeys.map(hotkey => (
+                <div
+                  key={hotkey.actionName + hotkey.sceneId + hotkey.sceneItemId + hotkey.sourceId}
+                >
+                  <Hotkey hotkey={hotkey} />
+                </div>
+              ))}
+            </div>
+          )}
         </transition>
       </div>
     );
