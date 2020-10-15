b6b87d370212e4ba6918095de826968f4b1a644e
Sean Beyer <sean.beyer@streamlabs.com>
2020-10-15 15:56:02 -0700

    Add guard clause to base element interval

--- app/components/editor/elements/BaseElement.tsx
+++ app/components/editor/elements/BaseElement.tsx
@@ -16,6 +16,7 @@ export default class BaseElement extends TsxComponent {
     this.height = this.$el.getBoundingClientRect().height;
     this.width = this.$el.getBoundingClientRect().width;
     window.setInterval(() => {
+      if (!this.$el?.getBoundingClientRect) return;
       this.height = this.$el.getBoundingClientRect().height;
       this.width = this.$el.getBoundingClientRect().width;
     }, 500);
