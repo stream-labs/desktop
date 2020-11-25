77379880c25fe588935eda4e880efe6a61540f43
Florin Petrovici <florin0x01@gmail.com>
2020-11-25 23:39:01 +0200

    Get version of obsnode from non standard resources.json - wip

--- app/app.ts
+++ app/app.ts
@@ -29,6 +29,7 @@ import { UserService, setSentryContext } from 'services/user';
 import { getResource } from 'services';
 import * as obs from '../obs-api';
 import path from 'path';
+import fs from 'fs';
 import util from 'util';
 import uuid from 'uuid/v4';
 import Blank from 'components/windows/Blank.vue';
@@ -73,6 +74,16 @@ if (isProduction) {
 
 let usingSentry = false;
 const windowId = Utils.getWindowId();
+
+const jsonData = fs.readFileSync('../scripts/repositories.json');
+const root = JSON.parse(jsonData.toString());
+const dependencies = root.root;
+const obsEntry = dependencies.filter(
+  (dependency: { name: string }) => dependency.name === 'obs-studio-node',
+)[0];
+const obsnodeVersion = obsEntry.version;
+
+console.log('OBS node version: ', obsnodeVersion);
 
 function wrapLogFn(fn: string) {
   const old: Function = console[fn];
