150eb08ae14dc1c94fec8ae7570dee058e88a3dd
Sean Beyer <sean.beyer@streamlabs.com>
2020-11-30 15:19:09 -0800

    Update Patchnotes v0.26.0

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,11 +1,17 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.23.0',
-  title: 'Additional OBS plugins integration',
+  version: '0.26.0',
+  title: 'Update to Electron 9',
   showChest: false,
   notes: [
-    'Virtual Webcam is now supported, it can be started and stopped directly from the settings',
-    'OpenVR source and motion effect transitions are now available',
+    'Updated frontend system architecture to Electron 9',
+    'Revamped the Go Live flow for Facebook users and multistreamers',
+    'Moved Login/Logout to the Settings Window',
+    'Added Follower Goals for Facebook users',
+    'Updated the Custom Resolution input for easier use',
+    'Fixed a bug related to keyboard shortcuts in the Edit Transform Window',
+    'Fixed a bug where the Scenes list appeared empty in some layouts',
+    'Fixed a bug loading Alert Box settings with a merged account',
   ],
 };
