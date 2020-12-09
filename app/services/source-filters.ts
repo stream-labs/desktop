901e66e81df46e6460089c44e26b1529ce337804
gettinToasty <sean.beyer@streamlabs.com>
2020-12-09 15:10:23 -0800

    Add Visual Presets to filter menu (#3161)
    * Add Visual Presets to filter menu
    
    * Add filters to onboarding
    
    * Fix bug preventing presets being applied during onboarding
    
    * Persist onboarding filters to default sources
    
    * Fix tests

--- app/services/source-filters.ts
+++ app/services/source-filters.ts
@@ -7,6 +7,7 @@ import {
   TObsValue,
   IObsListInput,
 } from 'components/obs/inputs/ObsInput';
+import { metadata } from 'components/shared/inputs';
 
 import { Inject } from './core/injector';
 import { SourcesService } from './sources';
@@ -17,6 +18,8 @@ import { $t } from 'services/i18n';
 import { EOrderMovement } from 'obs-studio-node';
 import { Subject } from 'rxjs';
 import { UsageStatisticsService } from './usage-statistics';
+import { getSharedResource } from 'util/get-shared-resource';
+import { ViewHandler } from 'services/core/stateful-service';
 
 export type TSourceFilterType =
   | 'mask_filter'
@@ -61,6 +64,35 @@ export interface ISourceFilterIdentifier {
   name: string;
 }
 
+class SourceFiltersViews extends ViewHandler<{}> {
+  get presetFilterOptions() {
+    return [
+      { title: $t('None'), value: '' },
+      { title: $t('Grayscale'), value: 'luts\\grayscale.png' },
+      { title: $t('Sepiatone'), value: 'luts\\sepia.png' },
+      { title: $t('Dramatic'), value: 'luts\\gazing.png' },
+      { title: $t('Flashback'), value: 'luts\\muted.png' },
+      { title: $t('Inverted'), value: 'luts\\inverted.png' },
+      { title: $t('Action Movie'), value: 'luts\\cool_tone.png' },
+      { title: $t('Hearth'), value: 'luts\\warm_tone.png' },
+      { title: $t('Wintergreen'), value: 'luts\\green_tone.png' },
+      { title: $t('Heat Map'), value: 'luts\\heat_map.png' },
+      { title: $t('Cel Shade'), value: 'luts\\cel_shade.png' },
+    ];
+  }
+
+  get presetFilterMetadata() {
+    return metadata.list({
+      options: this.presetFilterOptions,
+      title: $t('Visual Presets'),
+    });
+  }
+
+  parsePresetValue(path: string) {
+    return path.match(/luts\\[a-z_]+.png$/)[0];
+  }
+}
+
 export class SourceFiltersService extends Service {
   @Inject() private sourcesService: SourcesService;
   @Inject() private windowsService: WindowsService;
@@ -70,6 +102,10 @@ export class SourceFiltersService extends Service {
   filterRemoved = new Subject<ISourceFilterIdentifier>();
   filterUpdated = new Subject<ISourceFilterIdentifier>();
   filtersReordered = new Subject<void>();
+
+  get views() {
+    return new SourceFiltersViews({});
+  }
 
   getTypesList(): IObsListOption<TSourceFilterType>[] {
     const obsAvailableTypes = obs.FilterFactory.types();
@@ -166,6 +202,9 @@ export class SourceFiltersService extends Service {
     // There is now 2 references to the filter at that point
     // We need to release one
     obsFilter.release();
+    if (this.presetFilter?.name !== '') {
+      this.setOrder(sourceId, '__PRESET', 1);
+    }
     this.filterAdded.next({ sourceId, name: filterName });
 
     if (filterType === 'vst_filter') {
@@ -204,6 +243,22 @@ export class SourceFiltersService extends Service {
         type: obsFilter.id as TSourceFilterType,
         settings: obsFilter.settings,
       }));
+  }
+
+  presetFilter(sourceId: string): ISourceFilter {
+    return this.getFilters(sourceId).find((filter: ISourceFilter) => filter.name === '__PRESET');
+  }
+
+  addPresetFilter(sourceId: string, path: string) {
+    const preset = this.presetFilter(sourceId);
+    if (preset) {
+      this.setPropertiesFormData(sourceId, '__PRESET', [
+        { name: 'image_path', value: getSharedResource(path), options: null, description: null },
+      ]);
+    } else {
+      // Funky name to attempt avoiding namespace collisions with user-set filters
+      this.add(sourceId, 'clut_filter', '__PRESET', { image_path: getSharedResource(path) });
+    }
   }
 
   setVisibility(sourceId: string, filterName: string, visible: boolean) {
