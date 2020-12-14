e26f6f6d872bbc0ffe7fd9fe68a7f714f7d37603
Sean Beyer <sean.beyer@streamlabs.com>
2020-12-14 10:12:21 -0800

    Clean up AdvancedAudio

--- app/services/audio/audio.ts
+++ app/services/audio/audio.ts
@@ -192,7 +192,7 @@ export class AudioService extends StatefulService<IAudioSourcesState> {
       componentName: 'AdvancedAudio',
       title: $t('Advanced Audio Settings'),
       size: {
-        width: 1370,
+        width: 1050,
         height: 600,
       },
     });
