0852d7bba275ab906be304f470a053afbd057f67
Alexander Nazarov <alexzerg91@gmail.com>
2021-02-24 14:08:41 -0800

    remove unfinished components

--- app/components-react/index.ts
+++ app/components-react/index.ts
@@ -1,13 +1,9 @@
 /// <reference path="./index.d.ts" />
 import NameFolder from './windows/NameFolder';
-import GoLiveWindow from './windows/go-live/GoLiveWindow';
-import Playground from './windows/Playground';
 import NewsBanner from './root/NewsBanner';
 
 // list of React components for usage inside Vue components
 export const components = {
   NameFolder,
-  GoLiveWindow,
-  Playground,
   NewsBanner,
 };
