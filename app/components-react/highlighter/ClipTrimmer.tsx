4364fd1e9c459791398779ac925545ed591f7023
Andy Creeth <acreeth@gmail.com>
2021-04-22 17:38:05 -0700

    trimming ui mostly in

--- app/components-react/highlighter/ClipTrimmer.tsx
+++ app/components-react/highlighter/ClipTrimmer.tsx
@@ -1,21 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { IClip, SCRUB_FRAMES, SCRUB_HEIGHT, SCRUB_WIDTH } from 'services/highlighter';
 import times from 'lodash/times';
+import styles from './ClipTrimmer.m.less';
+import cx from 'classnames';
+
+type TDragType = 'start' | 'end';
 
 export default function ClipTrimmer(props: { clip: IClip }) {
   const videoRef = useRef<HTMLVideoElement>(null);
   const timelineRef = useRef<HTMLDivElement>(null);
-  const startTime = props.clip.startTrim;
-  const endTime = props.clip.duration! - props.clip.endTrim;
-  const [currentTime, setCurrentTime] = useState(startTime);
+  const [currentTime, setCurrentTime] = useState(props.clip.startTrim);
   const [scrubFrames, setScrubFrames] = useState<number[]>([]);
+  const [localStartTrim, setLocalStartTrim] = useState(props.clip.startTrim);
+  const [localEndTrim, setLocalEndTrim] = useState(props.clip.endTrim);
   const isPlaying = useRef(false);
   const playPastEnd = useRef(false);
+  const isDragging = useRef<TDragType | null>(null);
+  const isScrubbing = useRef(false);
+
+  const [isPlayingState, setIsPlayingState] = useState(false);
+
+  const endTime = props.clip.duration! - localEndTrim;
 
   function playAt(t: number) {
     if (!videoRef.current) return;
 
     isPlaying.current = true;
+    setIsPlayingState(true);
     videoRef.current.currentTime = t;
     videoRef.current.play();
 
@@ -40,6 +51,7 @@ export default function ClipTrimmer(props: { clip: IClip }) {
     if (!videoRef.current) return;
 
     isPlaying.current = false;
+    setIsPlayingState(false);
     videoRef.current.pause();
   }
 
@@ -47,20 +59,66 @@ export default function ClipTrimmer(props: { clip: IClip }) {
     if (isPlaying.current) {
       stopPlaying();
     } else {
-      playAt(startTime);
+      playAt(localStartTrim);
     }
   }
 
   useEffect(() => {
-    playAt(startTime);
+    playAt(localStartTrim);
 
     return stopPlaying;
   }, []);
 
-  function onTimelineClick(e: React.MouseEvent) {
+  function startDragging(e: React.MouseEvent, type: TDragType) {
+    e.stopPropagation();
+    isDragging.current = type;
+  }
+
+  function onMouseDown() {
+    stopPlaying();
+    isScrubbing.current = true;
+  }
+
+  function onMouseUp(e: React.MouseEvent) {
+    isScrubbing.current = false;
     const timelineWidth = timelineRef.current!.offsetWidth;
     const timelineOffset = timelineRef.current!.getBoundingClientRect().left;
     playAt(((e.clientX - timelineOffset) / timelineWidth) * props.clip.duration!);
+  }
+
+  function onMouseMove(e: React.MouseEvent) {
+    const timelineWidth = timelineRef.current!.offsetWidth;
+
+    if (isDragging.current) {
+      if (isDragging.current === 'start') {
+        const timelineOffset = timelineRef.current!.getBoundingClientRect().left;
+        const time = ((e.clientX - timelineOffset) / timelineWidth) * props.clip.duration!;
+        setLocalStartTrim(time);
+      } else {
+        const timelineOffset = timelineRef.current!.getBoundingClientRect().right;
+        const time = ((timelineOffset - e.clientX) / timelineWidth) * props.clip.duration!;
+        setLocalEndTrim(time);
+      }
+    } else if (isScrubbing.current) {
+      if (!videoRef.current) return;
+      const timelineOffset = timelineRef.current!.getBoundingClientRect().left;
+      const time = ((e.clientX - timelineOffset) / timelineWidth) * props.clip.duration!;
+
+      videoRef.current.currentTime = time;
+      updatePlayhead();
+    }
+  }
+
+  function onMouseLeave() {
+    isDragging.current = null;
+    isScrubbing.current = false;
+  }
+
+  function stopDragging(e: React.MouseEvent) {
+    isDragging.current = null;
+    playAt(localStartTrim);
+    e.stopPropagation();
+    // TODO - dispatch action to highlighter service
   }
 
   const scrubHeight = 80;
@@ -84,7 +142,10 @@ export default function ClipTrimmer(props: { clip: IClip }) {
         ref={videoRef}
         src={props.clip.path}
         width="100%"
-        onEnded={() => (isPlaying.current = false)}
+        onEnded={() => {
+          isPlaying.current = false;
+          setIsPlayingState(false);
+        }}
         onClick={togglePlayPause}
       />
       <div
@@ -100,7 +161,10 @@ export default function ClipTrimmer(props: { clip: IClip }) {
           alignItems: 'center',
           borderRadius: 5,
         }}
-        onClick={onTimelineClick}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseMove={onMouseMove}
+        onMouseLeave={onMouseLeave}
       >
         {scrubFrames.map(frame => {
           return (
@@ -115,42 +179,51 @@ export default function ClipTrimmer(props: { clip: IClip }) {
                 objectFit: 'cover',
                 objectPosition: `-${frame * SCRUB_WIDTH * (scrubHeight / SCRUB_HEIGHT)}px`,
                 borderRadius: 5,
+                pointerEvents: 'none',
               }}
             ></img>
           );
         })}
         <div
           style={{
-            position: 'absolute',
             left: `${(currentTime / props.clip.duration!) * 100}%`,
-            width: 5,
-            height: 100,
-            backgroundColor: 'white',
-            zIndex: 100,
           }}
+          className={cx(styles.clipPlayhead, { [styles.clipPlayheadPlaying]: isPlayingState })}
         ></div>
         <div
           style={{
             position: 'absolute',
             left: 0,
-            width: `${(props.clip.startTrim / props.clip.duration!) * 100}%`,
+            width: `${(localStartTrim / props.clip.duration!) * 100}%`,
             height: 100,
             backgroundColor: 'black',
             opacity: 0.9,
             borderRadius: 5,
           }}
-        ></div>
+        >
+          <div
+            className={styles.clipResizeHandle}
+            onMouseDown={e => startDragging(e, 'start')}
+            onMouseUp={stopDragging}
+          ></div>
+        </div>
         <div
           style={{
             position: 'absolute',
             right: 0,
-            width: `${(props.clip.endTrim / props.clip.duration!) * 100}%`,
+            width: `${(localEndTrim / props.clip.duration!) * 100}%`,
             height: 100,
             backgroundColor: 'black',
             opacity: 0.9,
             borderRadius: 5,
           }}
-        ></div>
+        >
+          <div
+            className={cx(styles.clipResizeHandle, styles.clipResizeHandleRight)}
+            onMouseDown={e => startDragging(e, 'end')}
+            onMouseUp={stopDragging}
+          ></div>
+        </div>
       </div>
     </div>
   );
