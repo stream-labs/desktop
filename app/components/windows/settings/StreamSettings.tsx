0edbdf4aac2e93895ed77d856099137fb8b96f6e
Alexander Nazarov <alexzerg91@gmail.com>
2021-05-05 11:52:13 -0700

    Show integration if primary platform (#3506)

--- app/components/windows/settings/StreamSettings.tsx
+++ app/components/windows/settings/StreamSettings.tsx
@@ -52,7 +52,10 @@ export default class StreamSettings extends TsxComponent {
   private editCustomDestMode: boolean | number = false;
 
   // TODO: fix TikTok merging on the Back End
-  private platforms = this.streamingView.allPlatforms.filter(platform => platform !== 'tiktok');
+  private platforms = this.streamingView.allPlatforms.filter(
+    // show tiktok only if it's a primary platform
+    (platform, ind) => this.userService.views.platform.type === 'tiktok' || platform !== 'tiktok',
+  );
 
   saveObsSettings(obsSettings: ISettingsSubCategory[]) {
     this.streamSettingsService.setObsStreamSettings(obsSettings);
