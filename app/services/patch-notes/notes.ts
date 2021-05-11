f71b8fae9bf4a4b8bd813f02a956da394097e13a
Sean Beyer <sean.beyer@streamlabs.com>
2021-05-11 12:55:23 -0700

    Update Patchnotes v1.1.0

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,14 +1,17 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.0',
-  title: "It's About Time",
+  version: '1.1.0',
+  title: 'A New Look',
   showChest: false,
   notes: [
-    'Improve stability of many VST plugins',
-    'Fixed a bug when editing Slide Show file paths',
-    'Fixed a bug where platform specific streamlabels could break',
-    'Fixed issue where language names were being incorrectly translated to English',
+    'Updated logo and favicon to match new Streamlabs branding',
+    'Added Twitch VOD audio track',
+    'Added ability to login with TikTok',
+    'Added increased reactivity and other UX improvements to the Go Live window',
+    'Fixed several bugs relating to the MiniFeed',
+    'Fixed YouTube thumbnails being re-uploaded in a small resolution',
+    'Fixed FFZ settings not correctly applying',
     'Minor bugfixes and stability improvements',
   ],
 };
