a7ddc539ba3e496966632841281153be70709ee6
Sean Beyer <sean.beyer@streamlabs.com>
2021-05-17 15:44:49 -0700

    Fix tests

--- app/components-react/pages/Loader.tsx
+++ app/components-react/pages/Loader.tsx
@@ -66,7 +66,7 @@ const spinnerSrc = `
 <svg
 version="1.1"
 xmlns="http://www.w3.org/2000/svg"
-xmlns:xlink="http://www.w3.org/1999/xlink"
+xmlnsXlink="http://www.w3.org/1999/xlink"
 viewBox="0 0 28 40"
 >
   <path d="M0 0, l0 4, l0 -4" id="s-bar-y-path"></path>
