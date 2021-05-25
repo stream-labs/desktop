a970ff0f55e6662971b6431be2e778630fed2031
Alexander Nazarov <alexzerg91@gmail.com>
2021-05-24 20:44:56 -0700

    fix_custom_fields

--- app/components-react/windows/go-live/MessageLayout.tsx
+++ app/components-react/windows/go-live/MessageLayout.tsx
@@ -19,7 +19,6 @@ interface IMessageLayoutProps {
 export default function MessageLayout(p: IMessageLayoutProps & HTMLAttributes<unknown>) {
   const [isErrorDetailsShown, setDetailsShown] = useState(false);
   const error = p.error;
-  const message = p.message || error?.message;
   const details = error?.details;
   const type = error ? 'error' : p.type;
 
