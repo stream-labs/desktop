effbbdb3fe5713d49af42e3165f371b0e6c77b11
Sean Beyer <sean.beyer@streamlabs.com>
2021-07-19 12:22:07 -0700

    Change beta to prod

--- app/services/hosts.ts
+++ app/services/hosts.ts
@@ -10,7 +10,7 @@ export class HostsService extends Service {
     if (Util.shouldUseLocalHost()) {
       return 'streamlabs.site';
     }
-    return 'beta.streamlabs.com';
+    return 'streamlabs.com';
   }
 
   get overlays() {
