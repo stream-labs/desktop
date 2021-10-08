9938980e089f7bc45f72104ecbf5d73d4be8a97e
Akihiko Koizuka <koizuka@dwango.co.jp>
2021-10-09 01:02:26 +0900

    add some words to replace_rules / fix tests

--- app/services/nicolive-program/ParaphraseDictionary.test.ts
+++ app/services/nicolive-program/ParaphraseDictionary.test.ts
@@ -1,12 +1,29 @@
-import { ParaphraseDictionary } from './ParaphraseDictionary';
+import { isText, ParaphraseDictionary, replace_rules, replace_text } from './ParaphraseDictionary';
 
-test('PhraseDictionary', async () => {
+describe('ParaphraseDictionary', async () => {
   const dictionary = new ParaphraseDictionary();
-  expect(dictionary.process('https://www.nicovideo.jp')).toBe('URLショウリャク');
+  test('remove URL', () =>
+    expect(dictionary.process('https://www.nicovideo.jp')).toBe('URLショウリャク'));
+
   expect(dictionary.process('8888')).toBe('パチパチパチ');
   expect(dictionary.process('ww')).toBe('ワラワラ');
   expect(dictionary.process('テストw')).toBe('テスト、ワラ');
-  expect(dictionary.process('複数行は\nすべて無視')).toBe('');
-  expect(dictionary.process('初見')).toBe('しょけん');
-  expect(dictionary.process('古参')).toBe('こさん');
+
+  test('remove multiple lines comments', () => {
+    expect(dictionary.process('複数行は\nすべて無視')).toBe('');
+    expect(dictionary.process('今\n北\n産\n業')).toBe('');
+  });
+
+  // 他の設定に影響を受けていないかどうか、全部試してみる
+  const table = replace_rules.elements
+    .filter(elem => isText(elem))
+    .map<[string, string]>((elem: replace_text) => [elem.text, elem.replacement]);
+  test.each(table)('text %s -> %s', (text, replace) =>
+    expect(dictionary.process(text)).toBe(replace),
+  );
+
+  test('match multiple times', () => {
+    const [text, replace] = table[0];
+    expect(dictionary.process(`${text}${text}`)).toBe(`${replace}${replace}`);
+  });
 });
--- app/services/nicolive-program/ParaphraseDictionary.ts
+++ app/services/nicolive-program/ParaphraseDictionary.ts
@@ -1,16 +1,26 @@
-type replace_rules_type = {
-  elements: {
-    note?: string;
-    regularExpression: string;
-    replacement: string;
-  }[];
+export type replace_regular_expression = {
+  note?: string;
+  regularExpression: string;
+  replacement: string;
 };
+export type replace_text = { note?: string; text: string; replacement: string };
+export type replace_item = replace_regular_expression | replace_text;
 
-const replace_rules = require('./replace_rules.json') as replace_rules_type;
+export type replace_rules_type = { elements: replace_item[] };
+
+export const replace_rules = require('./replace_rules.json') as replace_rules_type;
+
+export function isText(r: replace_item): r is replace_text {
+  return (r as unknown as replace_text).text !== undefined;
+}
+
+function escapeStringRegexp(string: string): string {
+  return string.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d');
+}
 
 export class ParaphraseDictionary {
   dictionary = replace_rules.elements.map(e => ({
-    pattern: new RegExp(e.regularExpression, 'i'),
+    pattern: new RegExp(isText(e) ? escapeStringRegexp(e.text) : e.regularExpression, 'gi'),
     to: e.replacement,
   }));
 
