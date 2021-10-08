385b545cc843aceeb23e2a259c76ee6b69be857a
Alexander Nazarov <alexzerg91@gmail.com>
2021-10-08 00:59:50 -0700

    fix colorpicker

--- app/components-react/shared/inputs/ColorInput.tsx
+++ app/components-react/shared/inputs/ColorInput.tsx
@@ -1,17 +1,16 @@
-import React, { RefObject, useEffect, useRef, useState } from 'react';
+import React, { ChangeEvent, RefObject, useEffect, useRef, useState } from 'react';
 import { TSlobsInputProps, useInput } from './inputs';
 import { Button, Col, Collapse, Input, Popover, Row, Select, Tooltip } from 'antd';
 import { SketchPicker } from 'react-color';
 import InputWrapper from './InputWrapper';
 import { omit } from 'lodash';
-import { DownOutlined } from '@ant-design/icons';
 import { getOS, OS } from '../../../util/operating-systems';
 import { $t } from '../../../services/i18n';
 import { loadColorPicker } from '../../../util/slow-imports';
 import { Services } from '../../service-provider';
+import { HexColorPicker } from 'react-colorful';
 import { findDOMNode } from 'react-dom';
-import Animation from 'rc-animate';
-import { CollapseArea } from '../CollapseArea';
+import { getDefined } from '../../../util/properties-type-guards';
 
 export function ColorInput(p: TSlobsInputProps<{}, string>) {
   // const [color, setColor] = useState('#ff0000');
@@ -27,16 +26,17 @@ export function ColorInput(p: TSlobsInputProps<{}, string>) {
   const { wrapperAttrs, inputAttrs } = useInput('color', { ...p, debounce });
   const color = inputAttrs.value;
   const divAttrs = omit(inputAttrs, 'onChange');
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [textInputVal, setTextInputVal] = useState(inputAttrs.value);
 
-  function toggle() {
-    setIsExpanded(!isExpanded);
-  }
+  useEffect(() => {
+    setTextInputVal(inputAttrs.value);
+  }, [inputAttrs.value]);
 
+  // open eydrop picker
   async function eyedrop(e: React.MouseEvent) {
     e.stopPropagation();
     const colorPicker = (await loadColorPicker()).default;
-    Services.UsageStatisticsService.recordFeatureUsage('screenColorPicker');
+    Services.UsageStatisticsService.actions.recordFeatureUsage('screenColorPicker');
     colorPicker.startColorPicker(
       (data: { event: string; hex: string }) => {
         if (data.event === 'mouseClick') {
@@ -48,67 +48,74 @@ export function ColorInput(p: TSlobsInputProps<{}, string>) {
     );
   }
 
+  // find the form element in parents and use it as a popup container
+  const $formRef = useRef<HTMLElement | null>(null);
   const ref: RefObject<Input> = useRef(null);
   useEffect(() => {
     const $input: Element = findDOMNode(ref.current);
-    // const $picker = $input
-    //   .closest('[data-role="input-wrapper"]')!
-    //   .querySelector('.sketch-picker') as HTMLElement;
-    // if ($picker) $picker.style.backgroundColor = 'var(--section-alt)';
-  });
+    const $form = $input.closest('[data-role="form"]') as HTMLElement;
+    $formRef.current = $form;
+  }, []);
+
+  function getPopupContainer() {
+    return getDefined($formRef.current);
+  }
+
+  function onTextInputChange(ev: ChangeEvent<any>) {
+    const color = ev.target.value;
+    setTextInputVal(color);
+    const isValidColor = color.match(/^#(?:[0-9a-fA-F]{3}){1,2}$/);
+    if (!isValidColor) return;
+    inputAttrs.onChange(color.toLowerCase());
+  }
+
+  function onTextInputBlur() {
+    // reset invalid color
+    const validColor = inputAttrs.value;
+    if (textInputVal !== validColor) setTextInputVal(validColor);
+  }
 
   return (
     <InputWrapper {...wrapperAttrs}>
-      <Input
-        {...divAttrs}
-        value={color}
-        contentEditable={false}
-        onClick={toggle}
-        ref={ref}
-        style={{ cursor: 'default', caretColor: 'transparent' }}
-        prefix={
-          <span style={{ width: '22px' }}>
-            <div
-              style={{
-                backgroundColor: color,
-                position: 'absolute',
-                borderRadius: '2px',
-                left: '2px',
-                bottom: '2px',
-                width: '26px',
-                top: '2px',
-              }}
-            />
-          </span>
-        }
-        addonAfter={
-          getOS() === OS.Windows ? (
-            <Button title={$t('Pick Screen Color')} style={{ padding: '4px 9px' }}>
-              <i className="fas fa-eye-dropper" onClick={eyedrop} />
-            </Button>
-          ) : (
-            false
-          )
-        }
-      />
-      <CollapseArea isExpanded={isExpanded}>
-        <SketchPicker
-          color={inputAttrs.value}
-          onChange={color => inputAttrs.onChange && inputAttrs.onChange(color.hex)}
-          disableAlpha={true}
-          styles={{
-            default: {
-              picker: {
-                background: 'var(--section-alt)',
-                color: 'var(--paragraph)',
-              },
-              controls: {
-                color: 'var(--paragraph)',
-              },
-            },
-          }}
+      <Popover
+        content={<HexColorPicker color={inputAttrs.value} onChange={inputAttrs.onChange} />}
+        trigger="click"
+        placement="bottomLeft"
+        getPopupContainer={getPopupContainer}
+        overlayStyle={{ marginTop: '-20px' }}
+      >
+        <Input
+          {...divAttrs}
+          value={textInputVal}
+          onChange={onTextInputChange}
+          onBlur={onTextInputBlur}
+          ref={ref}
+          prefix={
+            <span style={{ width: '22px' }}>
+              <div
+                style={{
+                  backgroundColor: color,
+                  position: 'absolute',
+                  borderRadius: '2px',
+                  left: '2px',
+                  bottom: '2px',
+                  width: '26px',
+                  top: '2px',
+                }}
+              />
+            </span>
+          }
+          addonAfter={
+            getOS() === OS.Windows ? (
+              <Button title={$t('Pick Screen Color')} style={{ padding: '4px 9px' }}>
+                <i className="fas fa-eye-dropper" onClick={eyedrop} />
+              </Button>
+            ) : (
+              false
+            )
+          }
         />
-      </CollapseArea>
+      </Popover>
     </InputWrapper>
   );
 }
