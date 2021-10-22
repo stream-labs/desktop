4c20d71cdd165a8d8a741670c37aee0c9b3156d4
Sean Beyer <sean.beyer@streamlabs.com>
2021-10-22 15:13:37 -0700

    Fix scroll

--- app/components-react/windows/advanced-audio/index.tsx
+++ app/components-react/windows/advanced-audio/index.tsx
@@ -20,7 +20,7 @@ export default function AdvancedAudio() {
         <Menu.Item key="source">{$t('Source Settings')}</Menu.Item>
         <Menu.Item key="global">{$t('Global Settings')}</Menu.Item>
       </Menu>
-      <Scrollable style={{ height: '100%' }} snapToWindowEdge>
+      <Scrollable style={{ height: 'calc(100% - 46px)' }} snapToWindowEdge>
         {activeTab === 'source' && <SourceSettings />}
         {activeTab === 'global' && <GlobalSettings />}
       </Scrollable>
