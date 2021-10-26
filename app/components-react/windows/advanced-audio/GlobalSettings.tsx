33effc2713f2ebf06e076e8fc19a044c6181a190
Sean Beyer <sean.beyer@streamlabs.com>
2021-10-25 17:10:13 -0700

    Add recording tracks input

--- app/components-react/windows/advanced-audio/GlobalSettings.tsx
+++ app/components-react/windows/advanced-audio/GlobalSettings.tsx
@@ -1,18 +1,11 @@
 import React, { useState, useRef, useMemo } from 'react';
 import { Provider } from 'react-redux';
-import {
-  SliderInput,
-  BoolButtonInput,
-  ListInput,
-  SwitchInput,
-  NumberInput,
-} from 'components-react/shared/inputs';
+import { BoolButtonInput, ListInput, SwitchInput } from 'components-react/shared/inputs';
+import InputWrapper from 'components-react/shared/inputs/InputWrapper';
 import Form from 'components-react/shared/inputs/Form';
-import { TObsValue, IObsListInput, IObsInput, TObsFormData } from 'components/obs/inputs/ObsInput';
+import { TObsValue, IObsListInput, IObsInput } from 'components/obs/inputs/ObsInput';
 import { Services } from 'components-react/service-provider';
 import { useVuex } from 'components-react/hooks';
-import { AudioSource } from 'services/audio';
-import { Source } from 'services/sources';
 import { $t } from 'services/i18n';
 import Utils from 'services/utils';
 import styles from './AdvancedAudio.m.less';
@@ -22,27 +15,54 @@ import { store } from '../../store';
 export default function GlobalSettings() {
   const { SettingsService } = Services;
 
-  const { advancedAudioSettings } = useVuex(() => ({
+  const { advancedAudioSettings, isAdvancedOutput, recordingTracks } = useVuex(() => ({
     advancedAudioSettings: SettingsService.views.advancedAudioSettings,
+    isAdvancedOutput: SettingsService.views.isAdvancedOutput,
+    recordingTracks: SettingsService.views.recordingTracks,
   }));
 
   const monitoringDevice = advancedAudioSettings?.parameters[0] as IObsListInput<TObsValue>;
   const audioDucking = advancedAudioSettings?.parameters[1] as IObsInput<boolean>;
+  const recTracks = Utils.numberToBinnaryArray(recordingTracks, 6).reverse();
 
   return (
-    <Provider store={store}>
-      {monitoringDevice && (
-        <ListInput
-          label={monitoringDevice.description}
-          value={monitoringDevice.value}
-          options={monitoringDevice.options.map(opt => ({
-            value: opt.value,
-            label: opt.description,
-          }))}
-        />
-      )}
-      {audioDucking && <SwitchInput label={audioDucking.description} value={audioDucking.value} />}
-      <ObsSettings page="Audio" />
-    </Provider>
+    <>
+      <Form>
+        {monitoringDevice && (
+          <ListInput
+            label={monitoringDevice.description}
+            value={monitoringDevice.value}
+            options={monitoringDevice.options.map(opt => ({
+              value: opt.value,
+              label: opt.description,
+            }))}
+          />
+        )}
+        {audioDucking && (
+          <SwitchInput label={audioDucking.description} value={audioDucking.value} />
+        )}
+        <InputWrapper
+          label={$t('Audio Tracks')}
+          tooltip={$t('Designates if this source is audible in your recorded track(s)')}
+          layout="horizontal"
+          style={{ flexWrap: 'nowrap' }}
+        >
+          <div style={{ display: 'flex' }}>
+            {recTracks?.map((track, i) => (
+              <BoolButtonInput
+                label={String(i + 1)}
+                key={i}
+                value={!!track}
+                checkboxStyles={{ marginRight: '4px' }}
+                name={`flag${track}`}
+              />
+            ))}
+          </div>
+        </InputWrapper>
+      </Form>
+      <Provider store={store}>
+        <ObsSettings page="Audio" />
+      </Provider>
+    </>
   );
 }
