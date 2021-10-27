4509e3a5149c05ce540a180bfa2af8c07c79dba9
Akihiko Koizuka <koizuka@dwango.co.jp>
2021-10-27 12:12:03 +0900

    use nicoaccount's get user-ID API
    to test niconico login session validity

--- app/services/platforms/niconico.ts
+++ app/services/platforms/niconico.ts
@@ -61,41 +61,38 @@ export class NiconicoService extends Service implements IPlatformService {
 
   client: NicoliveClient = new NicoliveClient();
 
-  getUserKey(): Promise<string> {
-    const url = `${this.hostsService.niconicoFlapi}/getuserkey`;
+  async getUserId(): Promise<string> {
+    const url = `${this.hostsService.niconicoAccount}/api/public/v1/user/id.json`;
     const request = new Request(url, { credentials: 'same-origin' });
 
-    return fetch(request)
-      .then(handleErrors)
-      .then(response => response.text())
-      .then(text => {
-        if (text.startsWith('userkey=')) {
-          return text.substr('userkey='.length);
-        }
-        return '';
-      });
-  }
-  isLoggedIn(): Promise<boolean> {
-    return this.getUserKey().then(userkey => userkey !== '');
+    const response = await fetch(request);
+    const response_1 = await handleErrors(response);
+    const json = await response_1.json();
+    if (json.data && json.data.userId) {
+      return json.data.userId;
+    }
+    return '';
   }
 
-  isPremium(token: string): Promise<boolean> {
+  async isLoggedIn(): Promise<boolean> {
+    const id = await this.getUserId();
+    return id !== '';
+  }
+
+  async isPremium(token: string): Promise<boolean> {
     const url = `${this.hostsService.niconicoOAuth}/v1/user/premium.json`;
     const headers = authorizedHeaders(token);
     const request = new Request(url, { headers });
-    return fetch(request)
-      .then(res => res.json())
-      .then(({ data }) => {
-        return data.type === 'premium';
-      });
+    const res = await fetch(request);
+    const { data } = await res.json();
+    return data.type === 'premium';
   }
 
-  logout(): Promise<void> {
+  async logout(): Promise<void> {
     const url = `${this.hostsService.niconicoAccount}/logout`;
     const request = new Request(url, { credentials: 'same-origin' });
-    return fetch(request)
-      .then(handleErrors)
-      .then(() => {});
+    const response = await fetch(request);
+    handleErrors(response);
   }
 
   get authUrl() {
@@ -140,7 +137,7 @@ export class NiconicoService extends Service implements IPlatformService {
       if (this.streamingStatus === EStreamingState.Reconnecting) {
         console.log('reconnecting - checking stream key');
         this.client.fetchBroadcastStream(this.channelId).catch(() => {
-          console.log('niconico programas has ended! stopping streaming.');
+          console.log('niconico programs has ended! stopping streaming.');
           this.streamingService.stopStreaming();
         });
       }
@@ -215,14 +212,14 @@ export class NiconicoService extends Service implements IPlatformService {
   }
 
   // TODO ニコニコOAuthのtoken更新に使う
-  fetchNewToken(): Promise<void> {
+  async fetchNewToken(): Promise<void> {
     const url = `${this.hostsService.niconicoOAuth}/oauth2/token`;
     const headers = authorizedHeaders(this.userService.apiToken);
     const request = new Request(url, { headers });
 
-    return fetch(request)
-      .then(handleErrors)
-      .then(response => response.json())
-      .then(response => this.userService.updatePlatformToken(response.access_token));
+    const response = await fetch(request);
+    const response_1 = await handleErrors(response);
+    const response_2 = await response_1.json();
+    return this.userService.updatePlatformToken(response_2.access_token);
   }
 }
