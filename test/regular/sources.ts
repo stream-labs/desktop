d1ae2d35e9ccae39651b516577df485c2d86e0c7
Alexander Nazarov <alexzerg91@gmail.com>
2021-11-30 22:14:08 -0800

    skip test

--- test/regular/sources.ts
+++ test/regular/sources.ts
@@ -84,7 +84,8 @@ test('Create/Remove Browser Source and view Source Properties', async t => {
   t.pass();
 });
 
-test('Create/Remove Media Source and view Source Properties', async t => {
+// TODO: re-write test for the React version
+test.skip('Create/Remove Media Source and view Source Properties', async t => {
   const sourceName = 'Media Source';
 
   await addSource('Media Source', sourceName);
