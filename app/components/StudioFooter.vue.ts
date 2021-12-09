c6f4a18d6992056a9953e6696f4dfb9f60f74db6
Akihiko Koizuka <koizuka@dwango.co.jp>
2021-12-09 17:06:53 +0900

    StudioFooterからStreamingControllerを分割

--- app/components/StudioFooter.vue.ts
+++ app/components/StudioFooter.vue.ts
@@ -1,89 +1,14 @@
 import Vue from 'vue';
-import { Component, Prop, Watch } from 'vue-property-decorator';
-import { Inject } from '../services/core/injector';
-import { StreamingService, EStreamingState, EReplayBufferState } from '../services/streaming';
-import StartStreamingButton from './StartStreamingButton.vue';
+import { Component, Prop } from 'vue-property-decorator';
+import StreamingController from './StreamingController.vue';
 import PerformanceMetrics from './PerformanceMetrics.vue';
-import NotificationsArea from './NotificationsArea.vue';
-import { UserService } from '../services/user';
-import { CustomizationService } from 'services/customization';
-import { $t } from 'services/i18n';
-import { SettingsService } from 'services/settings';
 
 @Component({
   components: {
-    StartStreamingButton,
+    StreamingController,
     PerformanceMetrics,
-    NotificationsArea,
   },
 })
 export default class StudioFooterComponent extends Vue {
-  @Inject() streamingService: StreamingService;
-  @Inject() userService: UserService;
-  @Inject() customizationService: CustomizationService;
-  @Inject() settingsService: SettingsService;
-
   @Prop() locked: boolean;
-
-  toggleRecording() {
-    this.streamingService.toggleRecording();
-  }
-
-  get recording() {
-    return this.streamingService.isRecording;
-  }
-
-  get streamingStatus() {
-    return this.streamingService.state.streamingStatus;
-  }
-
-  get loggedIn() {
-    return this.userService.isLoggedIn();
-  }
-
-  get replayBufferEnabled() {
-    return this.settingsService.state.Output.RecRB;
-  }
-
-  get replayBufferOffline() {
-    return this.streamingService.state.replayBufferStatus === EReplayBufferState.Offline;
-  }
-
-  get replayBufferStopping() {
-    return this.streamingService.state.replayBufferStatus === EReplayBufferState.Stopping;
-  }
-
-  get replayBufferSaving() {
-    return this.streamingService.state.replayBufferStatus === EReplayBufferState.Saving;
-  }
-
-  toggleReplayBuffer() {
-    if (this.streamingService.state.replayBufferStatus === EReplayBufferState.Offline) {
-      this.streamingService.startReplayBuffer();
-    } else {
-      this.streamingService.stopReplayBuffer();
-    }
-  }
-
-  saveReplay() {
-    this.streamingService.saveReplay();
-  }
-
-  streamingElapsedTime: string = '--:--:--';
-  @Watch('streamingStatus')
-  updateStreamingElapsedTime(): void {
-    if (this.streamingService.state.streamingStatus !== EStreamingState.Live) {
-      this.streamingElapsedTime = '--:--:--';
-      return;
-    }
-
-    this.streamingElapsedTime = this.streamingService.formattedDurationInCurrentStreamingState;
-
-    setTimeout(() => this.updateStreamingElapsedTime(), 200);
-  }
-
-  recordTooltip = $t('streaming.recordTooltip');
-  startReplayBufferTooltip = $t('streaming.startReplayBuffer');
-  stopReplayBufferTooltip = $t('streaming.stopReplayBuffer');
-  saveReplayTooltip = $t('streaming.saveReplay');
 }
