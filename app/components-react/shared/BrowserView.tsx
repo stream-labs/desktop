b32c4fcca6bd96a15b4a3b6f09a279f5beed07ad
Andy Creeth <acreeth@gmail.com>
2021-12-13 17:04:56 -0800

    fix browserview onready handler

--- app/components-react/shared/BrowserView.tsx
+++ app/components-react/shared/BrowserView.tsx
@@ -53,6 +53,7 @@ export default function BrowserView(p: BrowserViewProps) {
 
   useEffect(() => {
     browserView.current = new electron.remote.BrowserView(options);
+    if (p.onReady) p.onReady(browserView.current);
     if (p.setLocale) I18nService.setBrowserViewLocale(browserView.current);
 
     browserView.current.webContents.on('did-finish-load', () => setLoading(false));
@@ -67,8 +68,6 @@ export default function BrowserView(p: BrowserViewProps) {
   }, []);
 
   useEffect(() => {
-    if (!loading && p.onReady) p.onReady(browserView.current);
-
     const resizeInterval = window.setInterval(checkResize, 100);
 
     return () => clearInterval(resizeInterval);
