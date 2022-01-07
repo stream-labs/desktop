b8f8f9968e471eafd4ce4eb56321d62957b7b38a
elmarceloc <73359763+elmarceloc@users.noreply.github.com>
2022-01-06 22:22:23 -0300

    Minor formatting

--- main.js
+++ main.js
@@ -520,6 +520,7 @@ async function startApp() {
     // Vue dev tools appears to cause strange non-deterministic
     // interference with certain NodeJS APIs, expecially asynchronous
     // IO from the renderer process.  Enable at your own risk.
+    
     // const devtoolsInstaller = require('electron-devtools-installer');
     // devtoolsInstaller.default(devtoolsInstaller.VUEJS_DEVTOOLS);
     // setTimeout(() => {
