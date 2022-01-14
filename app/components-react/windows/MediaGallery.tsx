e8667a4726aded30d626686aadffa53f4d47db91
Sean Beyer <sean.beyer@streamlabs.com>
2022-01-14 17:20:50 -0500

    Port vue template to tsx

--- app/components-react/windows/MediaGallery.tsx
+++ app/components-react/windows/MediaGallery.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import electron, { clipboard } from 'electron';
 import * as remote from '@electron/remote';
+import cx from 'classnames';
 import { IMediaGalleryFile, IMediaGalleryInfo } from 'services/media-gallery';
 import { $t } from 'services/i18n';
-import ModalLayout from 'components-react/shared/ModalLayout';
+import { ModalLayout } from 'components-react/shared/ModalLayout';
 import Scrollable from 'components-react/shared/Scrollable';
 import { Services } from 'components-react/service-provider';
 import { useSubscription } from 'components-react/hooks/useSubscription';
+import { useVuex } from 'components-react/hooks';
+import styles from './MediaGallery.m.less';
 
 const getTypeMap = () => ({
   title: {
@@ -57,9 +60,10 @@ export default function MediaGallery() {
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
 
   const typeMap = getTypeMap();
+  const promiseId = WindowsService.state.child.queryParams?.promiseId;
+  const filter = WindowsService.state.child.queryParams?.filter;
 
   useEffect(() => {
-    const filter = WindowsService.state.child.queryParams?.filter;
     if (filter) setType(filter);
     fetchGalleryInfo();
 
@@ -75,6 +79,10 @@ export default function MediaGallery() {
     if (ev.type !== 'streamlabs_prime_subscribe') return;
     fetchGalleryInfo();
   });
+
+  const { isPrime } = useVuex(() => ({
+    isPrime: UserService.views.isPrime,
+  }));
 
   async function fetchGalleryInfo() {
     setGalleryInfo(await MediaGalleryService.fetchGalleryInfo());
@@ -119,131 +127,281 @@ export default function MediaGallery() {
     // });
   }
 
-  // get promiseId() {
-  //   return this.windowsService.state.child.queryParams.promiseId;
-  // }
+  function files() {
+    if (!galleryInfo) return [];
+    return galleryInfo.files.filter(file => {
+      if (category !== 'stock' && file.isStock !== false) return false;
+      if (category === 'stock' && file.isStock === false) return false;
+      return !(type && file.type !== type);
+    });
+  }
 
-  // get files() {
-  //   if (!this.galleryInfo) return [];
-  //   return this.galleryInfo.files.filter(file => {
-  //     if (this.category !== 'stock' && file.isStock !== false) return false;
-  //     if (this.category === 'stock' && file.isStock === false) return false;
-  //     return !(this.type && file.type !== this.type);
-  //   });
-  // }
-  // get title() {
-  //   return this.typeMap.title[this.type] || $t('All Files');
-  // }
-  // get noFilesCopy() {
-  //   return this.typeMap.noFilesCopy[this.type] || $t("You don't have any uploaded files!");
-  // }
-  // get noFilesBtn() {
-  //   return this.typeMap.noFilesBtn[this.type] || $t('Upload A File');
-  // }
-  // get totalUsage() {
-  //   return this.galleryInfo ? this.galleryInfo.totalUsage : 0;
-  // }
-  // get maxUsage() {
-  //   return this.galleryInfo ? this.galleryInfo.maxUsage : 0;
-  // }
-  // get usagePct() {
-  //   return this.galleryInfo ? this.totalUsage / this.maxUsage : 0;
-  // }
-  // get totalUsageLabel() {
-  //   return this.formatBytes(this.totalUsage, 2);
-  // }
-  // get maxUsageLabel() {
-  //   return this.formatBytes(this.maxUsage, 2);
-  // }
-  // onDragOver() {
-  //   this.dragOver = true;
-  // }
-  // onDragEnter() {
-  //   this.dragOver = true;
-  // }
-  // onDragLeave() {
-  //   this.dragOver = false;
-  // }
+  const title = typeMap.title[type] || $t('All Files');
+  const noFilesCopy = typeMap.noFilesCopy[type] || $t("You don't have any uploaded files!");
+  const noFilesBtn = typeMap.noFilesBtn[type] || $t('Upload A File');
+  const totalUsage = galleryInfo?.totalUsage ?? 0;
+  const maxUsage = galleryInfo?.maxUsage ?? 0;
+  const usagePct = galleryInfo ? totalUsage / maxUsage : 0;
+  const totalUsageLabel = formatBytes(totalUsage, 2);
+  const maxUsageLabel = formatBytes(maxUsage, 2);
 
-  // handleFileDrop(e: DragEvent) {
-  //   const mappedFiles = Array.from(e.dataTransfer.files).map(file => file.path);
-  //   this.upload(mappedFiles);
-  //   this.dragOver = false;
-  // }
-  // handleTypeFilter(type: 'audio' | 'image', category: 'stock' | 'uploads') {
-  //   if (type !== this.type || category !== this.category) {
-  //     this.type = type;
-  //     this.category = category;
-  //   }
-  // }
-  // handleBrowseGalleryClick() {
-  //   this.category = 'stock';
-  // }
-  // selectFile(file: IMediaGalleryFile, shouldSelect: boolean = false) {
-  //   if (this.filter && file.type !== this.filter) {
-  //     return this.$toasted.show($t('Not a supported file type'), {
-  //       duration: 1000,
-  //       position: 'top-right',
-  //       className: 'toast-alert',
-  //     });
-  //   }
-  //   this.selectedFile = file;
-  //   if (file.type === 'audio' && !shouldSelect) {
-  //     if (this.audio) this.audio.pause();
-  //     this.audio = new Audio(file.href);
-  //     this.audio.play();
-  //   }
-  //   if (shouldSelect) this.handleSelect();
-  // }
-  // handleSelect() {
-  //   if (!this.selectedFile) return this.windowsService.actions.closeChildWindow();
-  //   if (this.selectedFile.prime && !this.userService.views.isPrime) {
-  //     this.upgradeToPrime();
-  //     return;
-  //   }
-  //   this.mediaGalleryService.resolveFileSelect(this.promiseId, this.selectedFile);
-  //   this.windowsService.actions.closeChildWindow();
-  // }
-  // async handleDelete() {
-  //   if (this.selectedFile) {
-  //     remote.dialog
-  //       .showMessageBox(remote.getCurrentWindow(), {
-  //         title: 'Streamlabs Desktop',
-  //         type: 'warning',
-  //         message: $t('Are you sure you want to delete this file? This action is irreversable.'),
-  //         buttons: [$t('Cancel'), $t('OK')],
-  //       })
-  //       .then(async ({ response }) => {
-  //         if (!response || !this.selectedFile) return;
-  //         this.galleryInfo = await this.mediaGalleryService.deleteFile(this.selectedFile);
-  //         this.selectedFile = null;
-  //       });
-  //   }
-  // }
-  // async handleDownload() {
-  //   const { filePath } = await remote.dialog.showSaveDialog(remote.getCurrentWindow(), {
-  //     defaultPath: this.selectedFile.filename,
-  //   });
-  //   if (!this.selectedFile) return;
-  //   this.setBusy($t('Downloading...'));
-  //   await this.mediaGalleryService.downloadFile(filePath, this.selectedFile);
-  //   this.setNotBusy();
-  // }
+  function onDragEnter(e: React.DragEvent) {
+    e.preventDefault();
+    setDragOver(true);
+  }
+  function onDragLeave(e: React.DragEvent) {
+    e.preventDefault();
+    setDragOver(false);
+  }
 
-  // async handleCopy(href: string) {
-  //   try {
-  //     await clipboard.writeText(href);
-  //     this.$toasted.show($t('URL Copied'), {
-  //       duration: 1000,
-  //       position: 'top-right',
-  //       className: 'toast-success',
-  //     });
-  //   } catch (e: unknown) {
-  //     this.$toasted.show($t('Failed to copy URL'), {
-  //       duration: 1000,
-  //       position: 'top-right',
-  //       className: 'toast-alert',
-  //     });
-  //   }
-  // }
+  function handleFileDrop(e: React.DragEvent) {
+    e.preventDefault();
+    if (!e.dataTransfer?.files) return;
+    const mappedFiles = Array.from(e.dataTransfer.files).map(file => file.path);
+    upload(mappedFiles);
+    setDragOver(false);
+  }
+
+  function handleTypeFilter(t: 'audio' | 'image', c: 'stock' | 'uploads') {
+    if (t !== type) setType(t);
+    if (c !== category) setCategory(c);
+  }
+
+  function selectFile(file: IMediaGalleryFile, shouldSelect: boolean = false) {
+    if (filter && file.type !== filter) {
+      // return this.$toasted.show($t('Not a supported file type'), {
+      //   duration: 1000,
+      //   position: 'top-right',
+      //   className: 'toast-alert',
+      // });
+    }
+    setSelectedFile(file);
+    if (file.type === 'audio' && !shouldSelect) {
+      if (audio) audio.pause();
+      setAudio(new Audio(file.href));
+      audio?.play();
+    }
+    if (shouldSelect) handleSelect();
+  }
+
+  function handleSelect() {
+    if (!selectedFile) return WindowsService.actions.closeChildWindow();
+    if (selectedFile.prime && !isPrime) {
+      upgradeToPrime();
+      return;
+    }
+    MediaGalleryService.actions.resolveFileSelect(promiseId, selectedFile);
+    WindowsService.actions.closeChildWindow();
+  }
+
+  async function handleDelete() {
+    if (selectedFile) {
+      remote.dialog
+        .showMessageBox(remote.getCurrentWindow(), {
+          title: 'Streamlabs Desktop',
+          type: 'warning',
+          message: $t('Are you sure you want to delete this file? This action is irreversable.'),
+          buttons: [$t('Cancel'), $t('OK')],
+        })
+        .then(async ({ response }) => {
+          if (!response || !selectedFile) return;
+          setGalleryInfo(await MediaGalleryService.actions.return.deleteFile(selectedFile));
+          setSelectedFile(null);
+        });
+    }
+  }
+
+  async function handleDownload() {
+    if (!selectedFile) return;
+    const { filePath } = await remote.dialog.showSaveDialog(remote.getCurrentWindow(), {
+      defaultPath: selectedFile.filename,
+    });
+    if (!filePath) return;
+    // setBusy($t('Downloading...'));
+    await MediaGalleryService.actions.return.downloadFile(filePath, selectedFile);
+    // this.setNotBusy();
+  }
+
+  async function handleCopy(href: string) {
+    try {
+      await clipboard.writeText(href);
+      // this.$toasted.show($t('URL Copied'), {
+      //   duration: 1000,
+      //   position: 'top-right',
+      //   className: 'toast-success',
+      // });
+    } catch (e: unknown) {
+      // this.$toasted.show($t('Failed to copy URL'), {
+      //   duration: 1000,
+      //   position: 'top-right',
+      //   className: 'toast-alert',
+      // });
+    }
+  }
+
+  return (
+    <ModalLayout onOk={handleSelect}>
+      <div
+        className={styles.container}
+        onDragEnter={onDragEnter}
+        onDragOver={onDragEnter}
+        onDrop={handleFileDrop}
+      >
+        <div className="flex" style={{ height: '100%' }}>
+          <div className={styles.leftPanel}>
+            <div className={styles.dropzone} onClick={openFilePicker}>
+              <i className="icon-cloud-backup" />
+              {$t('Drag & Drop Upload')}
+            </div>
+            {['uploads', 'stock'].map(cat => (
+              <ul key={cat} className={styles.navList}>
+                <div className={styles.listTitle}>
+                  {cat === 'stock' ? $t('Stock Files') : $t('My Uploads')}
+                </div>
+                <li
+                  className={cx(styles.listItem, {
+                    [styles.active]: type === null && cat === category,
+                  })}
+                  onClick={() => handleTypeFilter(null, cat)}
+                >
+                  <i className="fa fa-file" />
+                  {$t('All Files')}
+                </li>
+                <li
+                  className={cx(styles.listItem, {
+                    [styles.active]: type === 'image' && cat === category,
+                  })}
+                  onClick={() => handleTypeFilter('image', cat)}
+                >
+                  <i className="icon-image" />
+                  {$t('Images')}
+                </li>
+                <li
+                  className={cx(styles.listItem, {
+                    [styles.active]: type === 'audio' && cat === category,
+                  })}
+                  onClick={() => handleTypeFilter('audio', cat)}
+                >
+                  <i className="icon-music" />
+                  {$t('Sounds')}
+                </li>
+              </ul>
+            ))}
+            <div>
+              <div>
+                {totalUsageLabel} / {maxUsageLabel}
+              </div>
+              <div className={cx(styles.progressSlider, 'radius')}>
+                <div
+                  style={{ width: `${usagePct * 100}%` }}
+                  className={cx(styles.progressSliderFill, 'radius')}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles.rightPanel}>
+            <h4>{title}</h4>
+            <div className={styles.toolbar}>
+              <i className="icon-cloud-backup" onClick={openFilePicker} />
+              <i
+                className={cx('icon-trash', {
+                  [styles.disabled]: !selectedFile || (selectedFile && selectedFile.isStock),
+                })}
+                onClick={handleDelete}
+              />
+              <i
+                className={cx('fa fa-download', { [styles.disabled]: !selectFile })}
+                onClick={handleDownload}
+              ></i>
+            </div>
+            {dragOver && (
+              <div
+                onDragOver={onDragEnter}
+                onDragLeave={onDragLeave}
+                className={cx(styles.dragOverlay, 'radius')}
+              />
+            )}
+            {busy && <div className={styles.busyOverlay} />}
+            {files().length > 0 && (
+              <Scrollable className={styles.uploadsManagerList}>
+                {files().map(file => (
+                  <li
+                    key={file.href}
+                    className={cx(styles.uploadManagerItem, 'radius', {
+                      [styles.selected]: selectedFile && selectedFile.href === file.href,
+                    })}
+                    onClick={e => selectFile(file, false, e)}
+                    onDoubleClick={e => selectFile(file, true, e)}
+                  >
+                    {file.type === 'image' && /\.webm$/.test(file.href) && (
+                      <video
+                        autoplay
+                        muted
+                        loop
+                        src={file.href}
+                        style={{ height: '100%', width: '100%' }}
+                      />
+                    )}
+                    {file.type == 'image' && !/\.webm$/.test(file.href) && (
+                      <div
+                        className={styles.imagePreview}
+                        style={{ backgroundImage: `url(${file.href})` }}
+                      />
+                    )}
+                    {file.type == 'audio' && (
+                      <i
+                        className="icon-music"
+                        style={{
+                          height: '132px',
+                          lineHeight: '132px',
+                          fontSize: '28px',
+                          textAlign: 'center',
+                          display: 'block',
+                        }}
+                      />
+                    )}
+                    {!file.prime && (
+                      <i
+                        className="icon-copy"
+                        // v-tooltip.left="$t('Copy URL')"
+                        onClick={() => handleCopy(file.href)}
+                      />
+                    )}
+                    <div
+                      className={cx(styles.uploadFooter, { [styles.image]: file.type === 'image' })}
+                    >
+                      <div className={styles.uploadSize}>
+                        {file.size ? formatBytes(file.size) : ' '}
+                      </div>
+                      <div className={styles.uploadTitle}>{file.filename}</div>
+                      {file.prime && (
+                        <div className={styles.uploadPrime}>
+                          {$t('Prime')}
+                          <i className="icon-prime" />
+                        </div>
+                      )}
+                    </div>
+                  </li>
+                ))}
+              </Scrollable>
+            )}
+
+            {files().length < 1 && (
+              <div className={styles.emptyBox}>
+                <div>{noFilesCopy}</div>
+                <div>
+                  <button onClick={openFilePicker} className="button">
+                    {noFilesBtn}
+                  </button>
+                  <button onClick={() => setCategory('stock')} className="button">
+                    {$t('Browse the Gallery')}
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </ModalLayout>
+  );
 }
