30e73919cb267fa17fe04ae7ea2b4ae12da90c61
gettinToasty <sean.beyer@streamlabs.com>
2022-01-18 15:06:42 -0800

    Redesign Prime onboarding step (#3988)
    * Redesign Prime onboarding step
    
    * Fix tests
    
    * Fix autoconfig test
    
    * Fix autoconfig test again

--- test/regular/obs-importer.ts
+++ test/regular/obs-importer.ts
@@ -39,13 +39,13 @@ test('OBS Importer', async t => {
   await logIn(t, 'twitch', { prime: false }, false, true);
   await sleep(1000);
   await (await t.context.app.client.$('span=Skip')).click();
-  await (await t.context.app.client.$('div=Choose Starter')).click();
 
   // import from OBS
   await click('div=Import from OBS Studio');
   await click('div=Start');
 
   await (await t.context.app.client.$('button=Skip')).click();
+  await (await t.context.app.client.$('div=Choose Free')).click();
 
   await waitForDisplayed('.scene-collections-wrapper');
 
