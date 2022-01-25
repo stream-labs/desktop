6c9f10d0b774703d8edca49d9c4a5d11a2a5809f
Sean Beyer <sean.beyer@streamlabs.com>
2022-01-25 11:46:11 -0800

    Add Superchat Goal

---
+++ app/services/widgets/settings/superchat-goal.ts
@@ -0,0 +1,26 @@
+import { GenericGoalService } from './generic-goal';
+import { WidgetDefinitions, WidgetType } from 'services/widgets';
+import { WIDGET_INITIAL_STATE } from './widget-settings';
+import { InheritMutations } from 'services/core/stateful-service';
+
+@InheritMutations()
+export class SuperchatGoalService extends GenericGoalService {
+  static initialState = WIDGET_INITIAL_STATE;
+
+  getApiSettings() {
+    return {
+      type: WidgetType.SuperchatGoal,
+      url: WidgetDefinitions[WidgetType.SuperchatGoal].url(this.getHost(), this.getWidgetToken()),
+      previewUrl: `https://${this.getHost()}/widgets/superchat-goal?token=${this.getWidgetToken()}`,
+      dataFetchUrl: `https://${this.getHost()}/api/v5/slobs/widget/superchatgoal/settings`,
+      settingsSaveUrl: `https://${this.getHost()}/api/v5/slobs/widget/superchatgoal/settings`,
+      goalUrl: `https://${this.getHost()}/api/v5/slobs/widget/superchatgoal`,
+      settingsUpdateEvent: 'superchatGoalSettingsUpdate',
+      goalCreateEvent: 'superchatGoalStart',
+      goalResetEvent: 'superchatGoalEnd',
+      hasTestButtons: true,
+      customCodeAllowed: true,
+      customFieldsAllowed: true,
+    };
+  }
+}
