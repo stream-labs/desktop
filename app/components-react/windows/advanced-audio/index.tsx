9d9160826c579d47c89471013866fba3fd33c2fd
Sean Beyer <sean.beyer@streamlabs.com>
2022-01-25 15:06:14 -0800

    Add error surfacing for same vod and stream track

--- app/components-react/windows/advanced-audio/index.tsx
+++ app/components-react/windows/advanced-audio/index.tsx
@@ -1,13 +1,34 @@
-import React, { useState } from 'react';
-import { Menu } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Menu, message } from 'antd';
 import { ModalLayout } from 'components-react/shared/ModalLayout';
 import Scrollable from 'components-react/shared/Scrollable';
+import { Services } from 'components-react/service-provider';
+import { useVuex } from 'components-react/hooks';
 import SourceSettings from './SourceSettings';
 import GlobalSettings from './GlobalSettings';
 import { $t } from 'services/i18n';
 
 export default function AdvancedAudio() {
   const [activeTab, setActiveTab] = useState('source');
+  const { SettingsService } = Services;
+
+  const { streamTrack, vodTrack } = useVuex(() => ({
+    streamTrack: SettingsService.views.streamTrack,
+    vodTrack: SettingsService.views.vodTrack,
+  }));
+
+  useEffect(() => {
+    if (streamTrack === vodTrack) {
+      message.error(
+        $t(
+          'Your Stream and VOD Track are the same. This may result in unexpected audio behavior, go to Global Settings to change.',
+        ),
+        0,
+      );
+    } else {
+      message.destroy();
+    }
+  }, [streamTrack, vodTrack]);
 
   return (
     <ModalLayout hideFooter>
