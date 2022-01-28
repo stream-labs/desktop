59fec0024125094504c3f24c719f81a04132a622
Akihiko Koizuka <koizuka@dwango.co.jp>
2022-01-28 21:28:40 +0900

    BottomLine: fix missing Component decorator

--- app/components/BottomLine.vue.ts
+++ app/components/BottomLine.vue.ts
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import { Inject } from 'services/core/injector';
 import { NicoliveProgramService } from 'services/nicolive-program/nicolive-program';
+import { Component } from 'vue-property-decorator';
 
+@Component({})
 export default class BottomLine extends Vue {
   @Inject()
   nicoliveProgramService: NicoliveProgramService;
