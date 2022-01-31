63eaae38afd5c7a595d478ad9fe1e3e0f803aa89
gettinToasty <sean.beyer@streamlabs.com>
2022-01-31 10:14:47 -0800

    Port Media Gallery to React (#3998)
    * Begin port
    
    * Use useSubscription
    
    * Port vue template to tsx
    
    * Fix strict null errors
    
    * Update toasts with antd messages
    
    * Fix imports
    
    * Refactor files function
    
    * Style MediaGallery
    
    * Fix drag n drop and use confirmAsync
    
    * Surface ux error if upload fails
    
    * Fix spacing of single row cards

--- app/app-services.ts
+++ app/app-services.ts
@@ -157,6 +157,7 @@ import { MediaGalleryService } from './services/media-gallery';
 import { RecentEventsService } from 'services/recent-events';
 import { AudioService } from './services/audio';
 import { SourceFiltersService } from './services/source-filters';
+import { WebsocketService } from './services/websocket';
 import { SpinWheelService } from 'services/widgets/settings/spin-wheel';
 import { LayoutService } from './services/layout';
 
@@ -205,6 +206,7 @@ export const AppServices = {
   PlatformAppsService,
   AudioService,
   SourceFiltersService,
+  WebsocketService,
   SpinWheelService,
   LayoutService,
 };
