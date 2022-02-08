3d0ee4c4194f0bb12d484475ddc16c679bce0461
Sean Beyer <sean.beyer@streamlabs.com>
2022-02-08 12:37:43 -0800

    Fix max handling on syncOffset

--- app/components-react/windows/advanced-audio/SourceSettings.tsx
+++ app/components-react/windows/advanced-audio/SourceSettings.tsx
@@ -235,7 +235,7 @@ function PanelForm(p: { source: AudioSource }) {
         label={$t('Sync Offset')}
         value={syncOffset}
         name="syncOffset"
-        onInput={value => handleSettingsChange('syncOffset', value)}
+        onChange={value => handleSettingsChange('syncOffset', value)}
         tooltip={$t('Time it takes between sound occuring and being broadcast (ms)')}
         min={-950}
         max={20000}
