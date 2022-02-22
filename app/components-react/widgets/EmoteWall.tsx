01ad13ad81a04856139a37c6c79aab0a66f17cb5
Trevor Lemeron <trevorrl@me.com>
2022-02-22 12:56:15 -0500

    port emote wall widget to react (#4030)
    * emote wall react port
    
    * remove unused import
    
    * better styling
    
    * fix eslint
    
    * another eslint fix lol
    
    * refactor to remove before/after patch
    
    * get rid of duplicated code
    
    * change max
    
    * fix eslint
    
    * fix code style
    
    * remove updateCombRequired
    
    * remove unused imports
    
    * fix default transform
    
    * eslint
    
    Co-authored-by: Alexander Nazarov <alexzerg91@gmail.com>

---
+++ app/components-react/widgets/EmoteWall.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { IWidgetState, useWidget, WidgetModule } from './common/useWidget';
+import { WidgetLayout } from './common/WidgetLayout';
+import InputWrapper from '../shared/inputs/InputWrapper';
+import { $t } from '../../services/i18n';
+import { createBinding, SliderInput, SwitchInput } from '../shared/inputs';
+import { IEmoteWallSettings } from 'services/widgets/settings/emote-wall';
+import { metadata } from '../shared/inputs/metadata';
+
+interface IEmoteWallState extends IWidgetState {
+  data: {
+    settings: {
+      combo_count: number;
+      combo_required: boolean;
+      combo_timeframe: number; // milliseconds
+      emote_animation_duration: number; // milliseconds
+      emote_scale: number;
+      enabled: boolean;
+      ignore_duplicates: boolean;
+    };
+  };
+}
+
+export function EmoteWall() {
+  const { isLoading, bind, isComboRequired } = useEmoteWall();
+  const secondsFormatter = metadata.seconds({ min: 1000, max: 60000 });
+
+  // use 1 column layout
+  return (
+    <WidgetLayout>
+      {!isLoading && (
+        <>
+          <SwitchInput label={$t('Enabled')} {...bind.enabled} />
+          <SliderInput
+            label={$t('Duration')}
+            {...secondsFormatter}
+            {...bind.emote_animation_duration}
+          />
+          <SliderInput label={$t('Emote Scale')} min={1} max={10} {...bind.emote_scale} />
+
+          <SwitchInput label={$t('Combo Required')} {...bind.combo_required} />
+          {isComboRequired && (
+            <InputWrapper nowrap={true}>
+              <SliderInput label={$t('Combo Count')} min={2} max={100} {...bind.combo_count} />
+              <SliderInput
+                label={$t('Combo Timeframe')}
+                {...secondsFormatter}
+                {...bind.combo_timeframe}
+              />
+            </InputWrapper>
+          )}
+
+          <SwitchInput label={$t('Ignore Duplicates')} {...bind.ignore_duplicates} />
+        </>
+      )}
+    </WidgetLayout>
+  );
+}
+
+export class EmoteWallModule extends WidgetModule<IEmoteWallState> {
+  bind = createBinding(
+    () => this.settings,
+    statePatch => this.updateSettings(statePatch),
+  );
+
+  get isComboRequired() {
+    return this.settings?.combo_required;
+  }
+}
+
+function useEmoteWall() {
+  return useWidget<EmoteWallModule>();
+}
