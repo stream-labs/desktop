5da2c2e3941ce504dcaa317115234bb2b78cf500
Alexander Nazarov <alexzerg91@gmail.com>
2022-03-30 19:13:37 -0700

    Revert "export injectors"
    This reverts commit d684f63ad71aeecf5e0f10e0056927999b59d66e.

--- app/components-react/root/ReactRoot.tsx
+++ app/components-react/root/ReactRoot.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
-import { ReactModules } from 'slap';
 
 /**
  * Creates a root React component with integrated Redux store
@@ -10,9 +9,7 @@ export function createRoot(ChildComponent: (props: any) => JSX.Element) {
   return function ReactRoot(childProps: Object) {
     return (
       <Provider store={store}>
-        <ReactModules>
-          <ChildComponent {...childProps} />
-        </ReactModules>
+        <ChildComponent {...childProps} />
       </Provider>
     );
   };
