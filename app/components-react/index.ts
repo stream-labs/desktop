675ee4384973948371618ec337891753f2674412
Sean Beyer <sean.beyer@streamlabs.com>
2022-04-27 13:12:18 -0700

    Port Notifications window

--- app/components-react/index.ts
+++ app/components-react/index.ts
@@ -44,6 +44,7 @@ import Projector from './windows/Projector';
 import SceneSelector from './editor/elements/SceneSelectorElement';
 import AddSource from './windows/AddSource';
 import SideNav from './sidebar/SideNav';
+import Notifications from './windows/Notifications';
 
 // list of React components to be used inside Vue components
 export const components = {
@@ -92,4 +93,5 @@ export const components = {
   SceneSelector: createRoot(SceneSelector),
   AddSource,
   SideNav,
+  Notifications,
 };
