afd77edb66df829e300bfa6ab598bbd2a588ae48
Sean Beyer <sean.beyer@streamlabs.com>
2022-05-12 10:01:12 -0700

    Fix reactivity for app popout

--- app/components-react/pages/PlatformAppMainPage.tsx
+++ app/components-react/pages/PlatformAppMainPage.tsx
@@ -3,13 +3,17 @@ import { EAppPageSlot } from 'services/platform-apps';
 import { $t } from 'services/i18n';
 import { Services } from 'components-react/service-provider';
 import PlatformAppPageView from 'components-react/shared/PlatformAppPageView';
+import { useVuex } from 'components-react/hooks';
 
 export default function PlatformAppMainPage(p: { params: { appId: string } }) {
   const { PlatformAppsService } = Services;
   const pageSlot = EAppPageSlot.TopNav;
 
-  const app = PlatformAppsService.views.getApp(p.params.appId);
-  const poppedOut = !!app?.poppedOutSlots.find(slot => slot === pageSlot);
+  const { poppedOut } = useVuex(() => ({
+    poppedOut: PlatformAppsService.views
+      .getApp(p.params.appId)
+      ?.poppedOutSlots.find(slot => slot === pageSlot),
+  }));
 
   return (
     <div style={{ height: '100%', width: '100%' }}>
