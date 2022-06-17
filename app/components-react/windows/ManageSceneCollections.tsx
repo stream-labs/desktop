d0a0cf84459336a02eced904e6f3176e1b1bbb03
Sean Beyer <sean.beyer@streamlabs.com>
2022-06-17 13:58:03 -0700

    Add styling

--- app/components-react/windows/ManageSceneCollections.tsx
+++ app/components-react/windows/ManageSceneCollections.tsx
@@ -6,10 +6,11 @@ import cx from 'classnames';
 import { ModalLayout } from 'components-react/shared/ModalLayout';
 import { Services } from 'components-react/service-provider';
 import { alertAsync, promptAsync } from 'components-react/modals';
+import Scrollable from 'components-react/shared/Scrollable';
 import { $t } from 'services/i18n';
 import { ISceneCollectionsManifestEntry } from 'services/scene-collections';
 import { byOS, getOS, OS } from 'util/operating-systems';
-import Scrollable from 'components-react/shared/Scrollable';
+import styles from './ManageSceneCollections.m.less';
 
 const { Sider, Content } = Layout;
 const { Search } = Input;
@@ -60,30 +61,30 @@ export default function ManageSceneCollections() {
 
   return (
     <ModalLayout onCancel={close}>
-      <Layout>
-        <Sider>
+      <Layout style={{ height: '100%' }}>
+        <Sider width={300}>
           <div>{$t('Your Scene Collections:')}</div>
           <Search placeholder={$t('Search Scene Collections')} onSearch={setQuery} allowClear />
-          <Scrollable>
+          <Scrollable style={{ height: '100%' }}>
             {filteredCollections().map((collection, i) => (
-              <CollectionNode collection={collection} recentlyUpdated={i < 2} />
+              <CollectionNode collection={collection} recentlyUpdated={i < 2} key={collection.id} />
             ))}
           </Scrollable>
         </Sider>
-        <Content>
-          <div>{$t('Add New Scene Collection')}</div>
-          <div>
-            <button onClick={create}>
-              <i className="icon-paper" />
+        <Content style={{ paddingLeft: '24px' }}>
+          <div>{$t('Add New Scene Collection:')}</div>
+          <div className={styles.buttonContainer}>
+            <button onClick={create} className={cx('button', styles.button)}>
+              <i className="icon-stream-labels" />
               <strong>{$t('New')}</strong>
               <p>{$t('Start fresh and build from scratch')}</p>
             </button>
-            <button onClick={importFromObs}>
-              <i className="icon-upload" />
+            <button onClick={importFromObs} className={cx('button', styles.button)}>
+              <i className="icon-cloud-backup" />
               <strong>{$t('Import')}</strong>
               <p>{$t('Load existing scenes from OBS')}</p>
             </button>
-            <button onClick={goToThemes}>
+            <button onClick={goToThemes} className={cx('button', styles.button, styles.lg)}>
               <div>
                 <strong>{$t('Template')}</strong>
                 <p>{$t('Choose a template from our theme library')}</p>
@@ -154,25 +155,29 @@ function CollectionNode(p: {
   }
 
   return (
-    <div onDoubleClick={makeActive}>
+    <div
+      onDoubleClick={makeActive}
+      className={cx(styles.collectionNode, { [styles.active]: isActive })}
+    >
       <span>
         <i className={cx('fab', byOS({ [OS.Windows]: 'fa-windows', [OS.Mac]: 'fa-apple' }))} />
         {p.collection.name}
       </span>
-      {isActive && <span>Active</span>}
-      {p.recentlyUpdated && <span>Updated {modified}</span>}
-      <Tooltip title={$t('Rename')}>
-        <i className="icon-edit" onClick={rename} />
-      </Tooltip>
-      {!duplicating && (
-        <Tooltip title={$t('Duplicate')}>
-          <i className="icon-copy" onClick={duplicate} />
+      {p.recentlyUpdated && <span className={styles.whisper}>Updated {modified}</span>}
+      <div className={styles.editIcons}>
+        <Tooltip title={$t('Rename')}>
+          <i className="icon-edit" onClick={rename} />
         </Tooltip>
-      )}
-      {duplicating && <i className="fa fa-spinner fa-pulse" />}
-      <Tooltip title={$t('Delete')}>
-        <i className="icon-trash" onClick={remove} />
-      </Tooltip>
+        {!duplicating && (
+          <Tooltip title={$t('Duplicate')}>
+            <i className="icon-copy" onClick={duplicate} />
+          </Tooltip>
+        )}
+        {duplicating && <i className="fa fa-spinner fa-pulse" />}
+        <Tooltip title={$t('Delete')}>
+          <i className="icon-trash" onClick={remove} />
+        </Tooltip>
+      </div>
     </div>
   );
 }
