42925768f255365c390f582d057271ec46a31a61
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2022-08-23 11:49:54 -0400

    Added tab menu to News & Notifications window.

--- app/components-react/windows/NotificationsAndNews.tsx
+++ app/components-react/windows/NotificationsAndNews.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import cx from 'classnames';
+import { Menu } from 'antd';
 import { useRenderInterval, useVuex } from 'components-react/hooks';
 import { ModalLayout } from 'components-react/shared/ModalLayout';
 import { Services } from 'components-react/service-provider';
@@ -9,12 +10,18 @@ import { INotification } from 'services/notifications';
 import { $t } from 'services/i18n';
 import styles from './NotificationsAndNews.m.less';
 
+enum NotificationTab {
+  News = 'News',
+  Notifications = 'Notifications',
+}
+
 export default function NotificationsAndNews() {
   const { NotificationsService } = Services;
 
   // Re-render every minute to refresh timestamps
   useRenderInterval(() => {}, 60 * 1000);
   useEffect(() => () => NotificationsService.actions.markAllAsRead(), []);
+  const [selectedTab, setSelectedTab] = useState(NotificationTab.News);
 
   const { notificationGroups, notificationsCount } = useVuex(() => ({
     notificationGroups: {
@@ -23,6 +30,12 @@ export default function NotificationsAndNews() {
     },
     notificationsCount: NotificationsService.views.getAll().length,
   }));
+
+  useEffect(() => {
+    if (!!notificationGroups && notificationGroups.unread.length > 0) {
+      setSelectedTab(NotificationTab.Notifications);
+    }
+  }, [notificationGroups]);
 
   function onNotificationClickHandler(id: number) {
     NotificationsService.actions.applyAction(id);
@@ -35,7 +48,18 @@ export default function NotificationsAndNews() {
   return (
     <ModalLayout hideFooter>
       <Scrollable style={{ height: '100%' }}>
-        NEWWWWW
+        <Menu
+          mode="horizontal"
+          selectedKeys={[selectedTab]}
+          onClick={() => {
+            console.log('firing');
+          }}
+          className={styles.menu}
+        >
+          {Object.values(NotificationTab).map(tabName => (
+            <Menu.Item key={tabName}>{tabName}</Menu.Item>
+          ))}
+        </Menu>
         {!notificationsCount && <h4>{$t("You don't have any notifications")}</h4>}
         {Object.keys(notificationGroups).map((groupName: string) => (
           <div key={groupName}>
