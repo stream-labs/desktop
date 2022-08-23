877cb39d4ca353a3ae39c367bf30077f6cfdda9f
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2022-08-23 12:44:26 -0400

    Tabs show news and notifications when clicked.

--- app/components-react/windows/Notifications.tsx
+++ app/components-react/windows/Notifications.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import moment from 'moment';
 import cx from 'classnames';
 import { useRenderInterval, useVuex } from 'components-react/hooks';
-import { ModalLayout } from 'components-react/shared/ModalLayout';
 import { Services } from 'components-react/service-provider';
 import Scrollable from 'components-react/shared/Scrollable';
 import { INotification } from 'services/notifications';
@@ -33,36 +32,34 @@ export default function Notifications() {
   }
 
   return (
-    <ModalLayout hideFooter>
-      <Scrollable style={{ height: '100%' }}>
-        {!notificationsCount && <h4>{$t("You don't have any notifications")}</h4>}
+    <Scrollable style={{ height: '100%' }}>
+      {!notificationsCount && <h4>{$t("You don't have any notifications")}</h4>}
 
-        {Object.keys(notificationGroups).map((groupName: string) => (
-          <div key={groupName}>
-            {notificationGroups[groupName].length > 0 && (
-              <h4>{groupName === 'unread' ? $t('New Notifications') : $t('Log')}</h4>
-            )}
-            {notificationGroups[groupName].map((notify: INotification) => (
-              <div
-                key={notify.id}
-                onClick={() => onNotificationClickHandler(notify.id)}
-                className={cx(styles.notification, {
-                  [styles.unread]: notify.unread,
-                  [styles.hasAction]: notify.action,
-                })}
-                data-name={notify.action && 'hasAction'}
-              >
-                <div className="icon">
-                  {notify.type === 'INFO' && <span className="fa fa-info-circle" />}
-                  {notify.type === 'WARNING' && <span className="fa fa-warning" />}
-                </div>
-                <div className="message">{notify.message}</div>
-                <div className={styles.date}>{momentize(notify.date)}</div>
+      {Object.keys(notificationGroups).map((groupName: string) => (
+        <div key={groupName}>
+          {notificationGroups[groupName].length > 0 && (
+            <h4>{groupName === 'unread' ? $t('New Notifications') : $t('Log')}</h4>
+          )}
+          {notificationGroups[groupName].map((notify: INotification) => (
+            <div
+              key={notify.id}
+              onClick={() => onNotificationClickHandler(notify.id)}
+              className={cx(styles.notification, {
+                [styles.unread]: notify.unread,
+                [styles.hasAction]: notify.action,
+              })}
+              data-name={notify.action && 'hasAction'}
+            >
+              <div className="icon">
+                {notify.type === 'INFO' && <span className="fa fa-info-circle" />}
+                {notify.type === 'WARNING' && <span className="fa fa-warning" />}
               </div>
-            ))}
-          </div>
-        ))}
-      </Scrollable>
-    </ModalLayout>
+              <div className="message">{notify.message}</div>
+              <div className={styles.date}>{momentize(notify.date)}</div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </Scrollable>
   );
 }
--- app/components-react/windows/NotificationsAndNews.tsx
+++ app/components-react/windows/NotificationsAndNews.tsx
@@ -1,92 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import moment from 'moment';
 import cx from 'classnames';
 import { Menu } from 'antd';
-import { useRenderInterval, useVuex } from 'components-react/hooks';
+import { useVuex } from 'components-react/hooks';
 import { ModalLayout } from 'components-react/shared/ModalLayout';
 import { Services } from 'components-react/service-provider';
-import Scrollable from 'components-react/shared/Scrollable';
-import { INotification } from 'services/notifications';
+import News from './News';
+import Notifications from './Notifications';
 import { $t } from 'services/i18n';
 import styles from './NotificationsAndNews.m.less';
-
-enum NotificationTab {
-  News = 'News',
-  Notifications = 'Notifications',
-}
 
 export default function NotificationsAndNews() {
   const { NotificationsService } = Services;
 
-  // Re-render every minute to refresh timestamps
-  useRenderInterval(() => {}, 60 * 1000);
-  useEffect(() => () => NotificationsService.actions.markAllAsRead(), []);
-  const [selectedTab, setSelectedTab] = useState(NotificationTab.News);
+  const tabNames = {
+    news: $t('News'),
+    notifications: $t('Notifications'),
+  };
 
-  const { notificationGroups, notificationsCount } = useVuex(() => ({
+  const [selectedTab, setSelectedTab] = useState(tabNames.news);
+
+  const { notificationGroups } = useVuex(() => ({
     notificationGroups: {
       unread: NotificationsService.views.getUnread(),
-      read: NotificationsService.views.getRead(),
     },
-    notificationsCount: NotificationsService.views.getAll().length,
   }));
 
   useEffect(() => {
     if (!!notificationGroups && notificationGroups.unread.length > 0) {
-      setSelectedTab(NotificationTab.Notifications);
+      setSelectedTab(tabNames.notifications);
     }
   }, [notificationGroups]);
 
-  function onNotificationClickHandler(id: number) {
-    NotificationsService.actions.applyAction(id);
-  }
-
-  function momentize(time: number): string {
-    return moment(time).fromNow();
-  }
-
   return (
     <ModalLayout hideFooter>
-      <Scrollable style={{ height: '100%' }}>
+      <div className={styles.notificationsMenu}>
         <Menu
           mode="horizontal"
           selectedKeys={[selectedTab]}
-          onClick={() => {
-            console.log('firing');
+          onClick={({ key }: { key: string }) => {
+            setSelectedTab(key);
           }}
-          className={styles.menu}
         >
-          {Object.values(NotificationTab).map(tabName => (
+          {Object.values(tabNames).map(tabName => (
             <Menu.Item key={tabName}>{tabName}</Menu.Item>
           ))}
         </Menu>
-        {!notificationsCount && <h4>{$t("You don't have any notifications")}</h4>}
-        {Object.keys(notificationGroups).map((groupName: string) => (
-          <div key={groupName}>
-            {notificationGroups[groupName].length > 0 && (
-              <h4>{groupName === 'unread' ? $t('New Notifications') : $t('Log')}</h4>
-            )}
-            {notificationGroups[groupName].map((notify: INotification) => (
-              <div
-                key={notify.id}
-                onClick={() => onNotificationClickHandler(notify.id)}
-                className={cx(styles.notification, {
-                  [styles.unread]: notify.unread,
-                  [styles.hasAction]: notify.action,
-                })}
-                data-name={notify.action && 'hasAction'}
-              >
-                <div className="icon">
-                  {notify.type === 'INFO' && <span className="fa fa-info-circle" />}
-                  {notify.type === 'WARNING' && <span className="fa fa-warning" />}
-                </div>
-                <div className="message">{notify.message}</div>
-                <div className={styles.date}>{momentize(notify.date)}</div>
-              </div>
-            ))}
-          </div>
-        ))}
-      </Scrollable>
+      </div>
+      {selectedTab === tabNames.notifications ? <Notifications /> : <News />}
     </ModalLayout>
   );
 }
