a0715bc80ce5f034a107dea363b717a84c150857
Sean Beyer <sean.beyer@streamlabs.com>
2022-10-14 14:14:49 -0700

    Fix typing issue

--- app/components-react/shared/inputs/metadata.ts
+++ app/components-react/shared/inputs/metadata.ts
@@ -1,5 +1,4 @@
 import { Rule } from 'antd/lib/form';
-import { TInputType } from './inputs';
 
 /**
  * Metadata generator for inputs
@@ -7,21 +6,21 @@ import { TInputType } from './inputs';
  */
 export const metadata = {
   any: (options: IAnyMetadata) => options,
-  text: (options: ITextMetadata) => ({ ...options, type: 'text' as TInputType }),
-  number: (options: INumberMetadata) => ({ ...options, type: 'number' as TInputType }),
-  slider: (options: ISliderMetadata) => ({ ...options, type: 'slider' as TInputType }),
-  bool: (options: ITextBoolMetadata) => ({ ...options, type: 'bool' as TInputType }),
-  list: <T>(options: IListMetadata<T>) => ({ ...options, type: 'list' as TInputType }),
+  text: (options: ITextMetadata) => ({ ...options, type: 'text' }),
+  number: (options: INumberMetadata) => ({ ...options, type: 'number' }),
+  slider: (options: ISliderMetadata) => ({ ...options, type: 'slider' }),
+  bool: (options: ITextBoolMetadata) => ({ ...options, type: 'bool' }),
+  list: <T>(options: IListMetadata<T>) => ({ ...options, type: 'list' }),
   autocomplete: <T>(options: IListMetadata<T>) => ({
     ...options,
-    type: 'autocomplete' as TInputType,
+    type: 'autocomplete',
   }),
   seconds: (options: ISliderMetadata) => ({
     min: 0,
     step: 1000,
     tipFormatter: (ms: number) => `${ms / 1000}s`,
     ...options,
-    type: 'seconds',
+    type: 'slider',
   }),
 };
 
@@ -36,7 +35,7 @@ interface IBaseMetadata {
   label?: string;
   tooltip?: string;
   required?: boolean;
-  type?: TInputType;
+  type?: string;
   rules?: Rule[];
   onChange?: (value: unknown) => void;
   children?: Dictionary<TInputMetadata<unknown>>;
