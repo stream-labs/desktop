7139ac894ef13560ed66ce515b924ac599789230
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2022-10-20 09:39:51 -0400

    Removed unusused functions.

--- app/components-react/widgets/CustomWidget.tsx
+++ app/components-react/widgets/CustomWidget.tsx
@@ -22,29 +22,4 @@ export function CustomWidget() {
   );
 }
 
-export class CustomWidgetModule extends WidgetModule<ICustomWidgetState> {
-  patchAfterFetch(data: any): ICustomWidgetState {
-    return {
-      ...data,
-      settings: {
-        ...data.settings,
-        custom_enabled: data.settings.custom_enabled,
-        custom_html: data.settings.custom_html,
-        custom_css: data.settings.custom_css,
-        custom_js: data.settings.custom_js,
-        custom_json: data.settings.custom_json,
-      },
-    };
-  }
-
-  patchBeforeSend(settings: ICustomWidgetState['data']['settings']): any {
-    return {
-      ...settings,
-      custom_enabled: settings.custom_enabled,
-      custom_html: settings.custom_html,
-      custom_css: settings.custom_css,
-      custom_js: settings.custom_js,
-      custom_json: settings.custom_json,
-    };
-  }
-}
+export class CustomWidgetModule extends WidgetModule<ICustomWidgetState> {}
