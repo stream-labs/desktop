3ea650bdda713691a51c7925b9572315c145eb0a
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2022-11-03 15:57:17 -0400

    SubMenu component typing fix.

--- app/components-react/shared/SubMenu.tsx
+++ app/components-react/shared/SubMenu.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Menu, MenuItemProps, SubMenuProps } from 'antd';
 
-type ISubMenuProps = Omit<SubMenuProps & MenuItemProps, 'title'> & {
+interface ISubMenuProps extends SubMenuProps, MenuItemProps {
   title?: string;
-};
+}
 
 export default function SubMenu(p: ISubMenuProps) {
   const { title } = p;
   return (
     <div title={title} className="submenu-wrapper">
-      <Menu.SubMenu {...p} title={title}>
-        {p.children}
-      </Menu.SubMenu>
+      <Menu.SubMenu {...p}>{p.children}</Menu.SubMenu>
     </div>
   );
 }
