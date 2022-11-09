1324c3c7fcec2e289d0b9ced19f6b3e1ab3c4bc6
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2022-11-09 13:53:57 -0500

    Fix closed menu dashboard icon position.

--- app/components-react/shared/SubMenu.tsx
+++ app/components-react/shared/SubMenu.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { Menu, MenuItemProps, SubMenuProps } from 'antd';
 
 interface ISubMenuProps extends SubMenuProps, MenuItemProps {
   title?: string;
+  style?: CSSProperties;
 }
 
 export default function SubMenu(p: ISubMenuProps) {
   const { title } = p;
   return (
-    <div title={title} className="submenu-wrapper">
+    <div title={title} className="submenu-wrapper" style={p.style}>
       <Menu.SubMenu {...p}>{p.children}</Menu.SubMenu>
     </div>
   );
