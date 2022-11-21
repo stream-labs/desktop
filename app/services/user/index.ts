4c70a67d7b1bb79545aa78320351e4a1bff0a06d
Andy Creeth <acreeth@gmail.com>
2022-11-21 13:32:16 -0800

    implement relogin behavior foir slid

--- app/services/user/index.ts
+++ app/services/user/index.ts
@@ -115,6 +115,12 @@ interface ILinkedPlatformsResponse {
   streamlabs_account?: ILinkedPlatform;
   user_id: number;
   created_at: string;
+
+  /**
+   * When the server sends this back as true, we must force
+   * the user to reauthenticate.
+   */
+  force_login_required: boolean;
 }
 
 export type LoginLifecycleOptions = {
@@ -440,7 +446,7 @@ export class UserService extends PersistentStatefulService<IUserServiceState> {
       if (!allPlatforms.includes(this.state.auth.primaryPlatform)) return;
 
       const service = getPlatformService(this.state.auth.primaryPlatform);
-      return this.login(service, this.state.auth);
+      return this.login(service, this.state.auth, true);
     }
   }
 
@@ -511,10 +517,16 @@ export class UserService extends PersistentStatefulService<IUserServiceState> {
     });
   }
 
+  /**
+   * Updates linked platforms and returns true if we need to force log out the user
+   * @returns `true` if the user should be force logged out
+   */
   async updateLinkedPlatforms() {
     const linkedPlatforms = await this.fetchLinkedPlatforms();
 
     if (!linkedPlatforms) return;
+
+    if (linkedPlatforms.force_login_required) return true;
 
     if (linkedPlatforms.user_id) {
       this.writeUserIdFile(linkedPlatforms.user_id);
@@ -785,32 +797,58 @@ export class UserService extends PersistentStatefulService<IUserServiceState> {
     this.onboardingService.start({ isLogin: true });
   }
 
+  /**
+   * Lets the user know we have forced them to relogin, and will
+   */
+  clearForceLoginStatus() {
+    if (!this.state.auth || !this.state.auth.apiToken) return;
+
+    const host = this.hostsService.streamlabs;
+    const headers = authorizedHeaders(this.apiToken);
+    const url = `https://${host}/api/v5/slobs/clear-force-login-status`;
+    const request = new Request(url, { headers, method: 'POST' });
+
+    return jfetch<unknown>(request);
+  }
+
   @RunInLoadingMode()
-  private async login(service: IPlatformService, auth?: IUserAuth) {
+  private async login(service: IPlatformService, auth?: IUserAuth, isOnStartup = false) {
     if (!auth) auth = this.state.auth;
     this.LOGIN(auth);
     this.VALIDATE_LOGIN(true);
     this.setSentryContext();
     this.userLogin.next(auth);
 
-    const [validateLoginResult, validatePlatformResult] = await Promise.all([
-      this.validateLogin(),
+    const forceRelogin = await this.updateLinkedPlatforms();
+
+    if (isOnStartup && forceRelogin) {
+      try {
+        await this.clearForceLoginStatus();
+
+        this.SET_IS_RELOG(true);
+        this.LOGOUT();
+        await remote.dialog.showMessageBox({
+          title: 'Streamlabs Desktop',
+          message: $t(
+            'Your login has expired. Please reauthenticate to continue using Streamlabs Desktop.',
+          ),
+        });
+        this.showLogin();
+        return;
+      } catch (e: unknown) {
+        console.error('Error forcing relog');
+        // Intentional that if something goes wrong here, we continue as normal,
+        // since otherwise the user might get stuck in a relog loop.
+      }
+    }
+
+    const [validatePlatformResult] = await Promise.all([
       service.validatePlatform(),
-      this.updateLinkedPlatforms(),
       this.refreshUserInfo(),
       this.sceneCollectionsService.setupNewUser(),
       this.setPrimeStatus(),
     ]);
     this.subscribeToSocketConnection();
-
-    if (!validateLoginResult) {
-      this.logOut();
-      remote.dialog.showMessageBox({
-        title: 'Streamlabs Desktop',
-        message: $t('You have been logged out'),
-      });
-      return;
-    }
 
     // Currently we treat generic errors as success
     if (validatePlatformResult === EPlatformCallResult.TwitchTwoFactor) {
