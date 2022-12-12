747c830de3e8a6f6a4416801ca6d09437980fdd6
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2022-12-12 15:43:08 -0800

    Refactor to always show editor menu item when side nav is open, as well as show custom editor screens in collapsed menu.

--- app/components-react/sidebar/EditorTabs.tsx
+++ app/components-react/sidebar/EditorTabs.tsx
@@ -4,6 +4,7 @@ import styles from './SideNav.m.less';
 import MenuItem from 'components-react/shared/MenuItem';
 import { useVuex } from 'components-react/hooks';
 import cx from 'classnames';
+import { EMenuItemKey, ENavName } from 'services/side-nav';
 
 interface IEditorTabs {
   type?: 'root' | 'submenu';
@@ -13,7 +14,16 @@ export default function EditorTabs(p: IEditorTabs) {
   const { NavigationService, SideNavService, LayoutService } = Services;
   const { type = 'root' } = p;
 
-  const { currentMenuItem, setCurrentMenuItem, studioTabs, isOpen } = useVuex(() => ({
+  const {
+    currentMenuItem,
+    setCurrentMenuItem,
+    studioTabs,
+    isOpen,
+    showCustomEditor,
+    toggleSidebarSubmenu,
+    toggleMenuItem,
+    editorToggled,
+  } = useVuex(() => ({
     currentMenuItem:
       SideNavService.views.currentMenuItem === 'editor'
         ? 'default'
@@ -22,6 +32,11 @@ export default function EditorTabs(p: IEditorTabs) {
     studioTabs: LayoutService.views.studioTabs,
     compactView: SideNavService.views.compactView,
     isOpen: SideNavService.views.isOpen,
+    showCustomEditor: SideNavService.views.showCustomEditor,
+    toggleSidebarSubmenu: SideNavService.actions.toggleSidebarSubmenu,
+    toggleMenuItem: SideNavService.actions.toggleMenuItem,
+    editorToggled: SideNavService.views.getMenuItemData(ENavName.TopNav, EMenuItemKey.Editor)
+      ?.isActive,
   }));
 
   function navigateToStudioTab(tabId: string, trackingTarget: string, key: string) {
@@ -29,20 +44,33 @@ export default function EditorTabs(p: IEditorTabs) {
       NavigationService.actions.navigate('Studio', { trackingTarget });
       LayoutService.actions.setCurrentTab(tabId);
       setCurrentMenuItem(key);
+
+      // make sure custom editor setting is toggled on
+      // if the active editor screen is not the default editor screen
+      if (tabId !== 'default' && !showCustomEditor) {
+        toggleSidebarSubmenu(true);
+      } else if (tabId === 'default' && !editorToggled) {
+        toggleMenuItem(ENavName.TopNav, EMenuItemKey.Editor, true);
+      }
     }
   }
+
+  const rootTabs = editorToggled ? studioTabs : studioTabs.filter(tab => tab.key !== 'default');
 
   // if closed, show editor tabs in sidenav when tab is toggled on
   // show all editor tabs in submenu
   // don't translate tab title because the user has set it
   return type === 'root' ? (
     <>
-      {studioTabs.map(tab => (
+      {rootTabs.map(tab => (
         <MenuItem
           key={tab.key}
           className={cx(
             !isOpen && styles.closed,
-            (currentMenuItem === tab.key || currentMenuItem === `sub-${tab.key}`) && styles.active,
+            (currentMenuItem === EMenuItemKey.Editor ||
+              currentMenuItem === tab.key ||
+              currentMenuItem === `sub-${tab.key}`) &&
+              styles.active,
           )}
           title={tab.title}
           icon={<i className={tab.icon} />}
