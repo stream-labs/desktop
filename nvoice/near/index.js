0e563d27ae946a39f8b5ba6989b9aa7a8867165b
Akihiko Koizuka <koizuka@dwango.co.jp>
2023-01-13 20:53:15 +0900

    目パチ(5秒間隔・一回瞬き)

--- nvoice/near/index.js
+++ nvoice/near/index.js
@@ -1,21 +1,31 @@
-// load images?
-
 const href = window.location.href;
 const params = new URLSearchParams(href.split('?')[1]);
-const host = `http://localhost:${params.get('port') || 3000}`;
-// eslint-disable-next-line no-undef
-const socket = io(host);
+let socket;
+try {
+  const port = params.get('port');
+  if (port && parseInt(port, 10) !== 0) {
+    const host = `http://localhost:${port}`;
+    // eslint-disable-next-line no-undef
+    socket = io(host);
+  } else {
+    console.log('offline mode.');
+  }
+} catch (e) {
+  console.log(e);
+}
 
-const debug = document.getElementById('debug');
-// debug.textContent = 'host: ' + host;
+const debug = document.getElementById('debug'); // DEBUG時に使う用
 
 const image = document.getElementById('image');
+const eyes = document.getElementById('eyes');
 
 let active = false;
 function setActive(a) {
   if (active !== a) {
     active = a;
-    socket.emit('active', active);
+    if (socket) {
+      socket.emit('active', active);
+    }
   }
 }
 
@@ -25,12 +35,36 @@ addEventListener('visibilitychange', () => {
   setActive(document.visibilityState === 'visible');
 });
 
+const IMAGE_FILENAMES = {
+  default: 'default.png',
+
+  a: 'a.png',
+
+  i: 'i.png',
+  I: 'i.png',
+
+  u: 'u.png',
+  U: 'u.png',
+  w: 'u.png',
+
+  e: 'e.png',
+
+  o: 'o.png',
+
+  m: 'm.png',
+  p: 'm.png',
+  b: 'm.png',
+  'silE': 'm.png',
+};
+const DEFAULT_COOL_TIME_MS = 1000;
+
 let t;
 function timer_set() {
-  t = setTimeout(1000, () => {
-    image.src = 'default.png';
+  timer_reset();
+  t = setTimeout(() => {
+    image.src = IMAGE_FILENAMES.default;
     t = undefined;
-  })
+  }, DEFAULT_COOL_TIME_MS);
 }
 function timer_reset() {
   if (t) {
@@ -39,40 +73,38 @@ function timer_reset() {
   }
 }
 
-socket.on('phoneme', (phoneme) => {
-  if (!active) {
-    return;
-  }
-  console.log('phoneme', phoneme);
-  switch (phoneme) {
-    case 'a':
-      image.src = 'a.png';
-      break;
-    case 'i':
-    case 'I':
-      image.src = 'i.png';
-      break;
-    case 'u':
-    case 'U':
-    case 'w':
-      image.src = 'u.png';
-      break;
-    case 'e':
-      image.src = 'e.png';
-      break;
-    case 'o':
-      image.src = 'o.png';
-      break;
-    case 'm':
-    case 'p':
-    case 'b':
-      image.src = 'm.png';
-      break;
+if (socket) {
+  socket.on('phoneme', (phoneme) => {
+    if (!active) {
+      return;
+    }
+    console.log('phoneme', phoneme);
+    if (IMAGE_FILENAMES[phoneme]) {
+      image.src = IMAGE_FILENAMES[phoneme];
+    }
+    timer_set();
+  });
+}
 
-    case 'silE':
-      image.src = 'm.png';
-      break;
-  }
-  timer_set();
-});
+let blinkIndex = -1;
+const blinkSequence = ['A', 'B', 'C', 'B', 'A'];
+const BLINK_FRAME_MS = 100;
+const BLINK_INTERVAL_MS = 5000;
 
+setInterval(() => {
+  if (active) {
+    const blink = () => {
+      const isDefault = image.src.endsWith('default.png');
+      ++blinkIndex;
+      if (blinkIndex < blinkSequence.length) {
+        eyes.src = `SD_${isDefault ? 'default' : 'read'}_${blinkSequence[blinkIndex]}.png`;
+        eyes.hidden = false;
+        setTimeout(blink, BLINK_FRAME_MS);
+      } else {
+        eyes.hidden = true;
+        blinkIndex = -1;
+      }
+    };
+    setTimeout(blink, BLINK_FRAME_MS);
+  }
+}, BLINK_INTERVAL_MS);
