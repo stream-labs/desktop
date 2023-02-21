1b97267c33d2ad351907c3cbf675375a30bbfd32
Sean Beyer <sean.beyer@streamlabs.com>
2023-02-21 14:23:30 -0800

    Fix onboarding skip

--- test/helpers/modules/onboarding.ts
+++ test/helpers/modules/onboarding.ts
@@ -9,6 +9,6 @@ export async function skipOnboarding() {
     await clickIfDisplayed('div=Start Fresh');
     await clickButton('Skip');
     await clickIfDisplayed('button=Skip');
-    await clickIfDisplayed('div=Current Plan');
+    await clickIfDisplayed('div=Choose Starter');
   });
 }
