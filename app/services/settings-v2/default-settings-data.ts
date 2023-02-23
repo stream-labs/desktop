00d4bc954ad03c7b7da73f8823b88a125b254630
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2023-02-23 12:13:04 -0500

    Streamline obs imports for default settings data.

--- app/services/settings-v2/default-settings-data.ts
+++ app/services/settings-v2/default-settings-data.ts
@@ -1,20 +1,4 @@
-import * as obs from 'obs-studio-node';
-
-export type TDefaultVideoSetting = 'default' | 'defaultVertical';
-
-export const horizontalDisplayData = {
-  fpsNum: 120,
-  fpsDen: 2,
-  baseWidth: 3840,
-  baseHeight: 2160,
-  outputWidth: 1920,
-  outputHeight: 1080,
-  outputFormat: obs.EVideoFormat.I420,
-  colorspace: obs.EColorSpace.CS709,
-  range: obs.ERangeType.Full,
-  scaleType: obs.EScaleType.Lanczos,
-  fpsType: obs.EFPSType.Fractional,
-};
+import { EVideoFormat, EColorSpace, ERangeType, EScaleType, EFPSType } from 'obs-studio-node';
 
 export const verticalDisplayData = {
   fpsNum: 60,
@@ -23,9 +7,9 @@ export const verticalDisplayData = {
   baseHeight: 700,
   outputWidth: 400,
   outputHeight: 700,
-  outputFormat: obs.EVideoFormat.I420,
-  colorspace: obs.EColorSpace.CS709,
-  range: obs.ERangeType.Full,
-  scaleType: obs.EScaleType.Lanczos,
-  fpsType: obs.EFPSType.Fractional,
+  outputFormat: EVideoFormat.I420,
+  colorspace: EColorSpace.CS709,
+  range: ERangeType.Full,
+  scaleType: EScaleType.Lanczos,
+  fpsType: EFPSType.Fractional,
 };
