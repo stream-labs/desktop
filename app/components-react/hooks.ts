8a1facf8817a4ea671f4c1b4eba32538b9eeddda
Sean Beyer <sean.beyer@streamlabs.com>
2023-04-10 16:19:40 -0700

    Add params for one-off windows

--- app/components-react/hooks.ts
+++ app/components-react/hooks.ts
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback, useRef, useMemo } from 'react'
 import debounce from 'lodash/debounce';
 import { StatefulService } from '../services/core';
 import { Services } from './service-provider';
+import Util from 'services/utils';
 
 /**
  * Creates a reactive state for a React component based on Vuex store
@@ -130,5 +131,14 @@ export function usePromise<TPromiseResult>(
 export function useChildWindowParams(key?: string) {
   const { WindowsService } = Services;
   const params = useMemo(() => WindowsService.getChildWindowQueryParams(), []);
+  return key ? params[key] : params;
+}
+
+export function useOneOffWindowParams(key?: string) {
+  const { WindowsService } = Services;
+  const params = useMemo(() => {
+    const windowId = Util.getCurrentUrlParams().windowId;
+    return WindowsService.getWindowOptions(windowId);
+  }, []);
   return key ? params[key] : params;
 }
