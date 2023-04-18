596e55cc72ef7c7a3715d3358066bc4579f17bcf
gettinToasty <sean.beyer@streamlabs.com>
2023-04-18 09:33:25 -0700

    Revert "Revert "Streaming refactor for backend changes (#4497)" (#4526)" (#4527)
    This reverts commit 48f62491f47f3487b65c214508fa4e8474ea2221.

--- app/services/platforms/trovo.ts
+++ app/services/platforms/trovo.ts
@@ -13,6 +13,7 @@ import { platformAuthorizedRequest } from './utils';
 import { IGoLiveSettings } from '../streaming';
 import { getDefined } from '../../util/properties-type-guards';
 import Utils from '../utils';
+import { TDisplayType } from 'services/settings-v2';
 
 interface ITrovoServiceState extends IPlatformState {
   settings: ITrovoStartStreamOptions;
@@ -78,17 +79,22 @@ export class TrovoService
     return this.userService.state.auth?.platforms?.trovo?.username || '';
   }
 
-  async beforeGoLive(goLiveSettings: IGoLiveSettings) {
+  async beforeGoLive(goLiveSettings: IGoLiveSettings, context?: TDisplayType) {
     const trSettings = getDefined(goLiveSettings.platforms.trovo);
     const key = this.state.streamKey;
     if (!this.streamingService.views.isMultiplatformMode) {
-      this.streamSettingsService.setSettings({
-        streamType: 'rtmp_custom',
-        key,
-        server: this.rtmpServer,
-      });
+      this.streamSettingsService.setSettings(
+        {
+          streamType: 'rtmp_custom',
+          key,
+          server: this.rtmpServer,
+        },
+        context,
+      );
     }
     await this.putChannelInfo(trSettings);
+
+    this.setPlatformContext('trovo');
   }
 
   fetchNewToken(): Promise<void> {
