6c39a3ded8aed6bf48099e59955bf1dee6cbfe77
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2023-06-12 17:14:51 -0400

    Code cleanup, editor commands.

--- app/services/editor-commands/commands/remove-item.ts
+++ app/services/editor-commands/commands/remove-item.ts
@@ -6,9 +6,7 @@ import { Source } from 'services/sources';
 import { ReorderNodesCommand, EPlaceType } from './reorder-nodes';
 import { $t } from 'services/i18n';
 import { ISceneItemSettings } from 'services/api/external-api/scenes';
-import { DualOutputService } from 'services/dual-output';
-import { TDisplayType, VideoSettingsService } from 'services/settings-v2';
-import { SceneCollectionsService } from 'services/scene-collections';
+import { VideoSettingsService } from 'services/settings-v2';
 
 // Removing and recreating a source is a very complex event.
 // We can save a lot of time by leveraging the scene collection system.
@@ -26,24 +24,15 @@ class SourceReviver extends SourcesNode {
 
 export class RemoveItemCommand extends Command {
   @Inject() private scenesService: ScenesService;
-  @Inject() private dualOutputService: DualOutputService;
   @Inject() private videoSettingsService: VideoSettingsService;
-  @Inject() private sceneCollectionsService: SceneCollectionsService;
 
   private sceneId: string;
   private sourceId: string;
   private sourceReviver: SourceReviver;
 
   private reorderNodesSubcommand: ReorderNodesCommand;
-  private reorderDualOutputNodesSubcommand: ReorderNodesCommand;
 
   private settings: ISceneItemSettings;
-
-  // the dual output node is the correllating node in a scene
-  // that has both horizontal and vertical nodes
-  private dualOutputNodeId: string;
-  private dualOutputNodeSettings: ISceneItemSettings;
-  private dualOutputNodeDisplay: TDisplayType;
 
   constructor(private sceneItemId: string) {
     super();
@@ -73,36 +62,6 @@ export class RemoveItemCommand extends Command {
     );
     this.reorderNodesSubcommand.execute();
 
-    // If the scene has vertical nodes, remove the corresponding vertical node
-    // if (this.removeVertical && this.dualOutputService.views.hasNodeMap(this.sceneId)) {
-    //   this.dualOutputNodeDisplay = this.dualOutputService.views.getVerticalNodeId(this.sceneItemId)
-    //     ? 'horizontal'
-    //     : 'vertical';
-
-    //   this.dualOutputNodeId =
-    //     this.dualOutputNodeDisplay === 'horizontal'
-    //       ? this.dualOutputService.views.getVerticalNodeId(this.sceneItemId)
-    //       : this.dualOutputService.views.getHorizontalNodeId(this.sceneItemId, this.sceneId);
-
-    //   const dualOutputSceneItem = this.scenesService.views.getSceneItem(this.dualOutputNodeId);
-    //   this.dualOutputNodeSettings = dualOutputSceneItem.getSettings();
-
-    //   this.reorderDualOutputNodesSubcommand = new ReorderNodesCommand(
-    //     scene.getSelection(this.dualOutputNodeId),
-    //     void 0,
-    //     EPlaceType.After,
-    //   );
-
-    //   this.reorderDualOutputNodesSubcommand.execute();
-
-    //   dualOutputSceneItem.remove();
-
-    //   const nodeToRemoveId =
-    //     this.dualOutputNodeDisplay === 'horizontal' ? this.sceneItemId : this.dualOutputNodeId;
-
-    //   this.sceneCollectionsService.removeNodeMapEntry(nodeToRemoveId, this.sceneId);
-    // }
-
     // If this was the last item using this source, the underlying source
     // will automatically be removed. In this case, we need to store enough
     // information to bring it back intio existence in the rollback function.
@@ -118,61 +77,12 @@ export class RemoveItemCommand extends Command {
   }
 
   async rollback() {
-    // because this command removes both the horizontal and vertical scene nodes
-    // if this command has been used to remove a scene, the vertical nodes are
-    // if (!this.sourceId) return;
-
     if (this.sourceReviver) {
       await this.sourceReviver.load({});
     }
 
     const scene = this.scenesService.views.getScene(this.sceneId);
 
-    // if (this.removeVertical && this.dualOutputService.views.hasNodeMap(this.sceneId)) {
-    //   // if the scene has vertical node items, restore both the horizontal and vertical nodes
-    //   const horizontalNodeId =
-    //     this.dualOutputNodeDisplay === 'horizontal' ? this.sceneItemId : this.dualOutputNodeId;
-    //   const verticalNodeId =
-    //     this.dualOutputNodeDisplay === 'horizontal' ? this.dualOutputNodeId : this.sceneItemId;
-
-    //   const horizontalSettings =
-    //     this.dualOutputNodeDisplay === 'horizontal' ? this.settings : this.dualOutputNodeSettings;
-    //   const verticalSettings =
-    //     this.dualOutputNodeDisplay === 'horizontal' ? this.dualOutputNodeSettings : this.settings;
-
-    //   // vertical scene item
-    //   const verticalItem = scene.addSource(this.sourceId, { id: verticalNodeId, select: false });
-    //   const verticalContext = this.videoSettingsService.contexts.vertical;
-    //   verticalItem.setSettings({
-    //     ...verticalSettings,
-    //     output: verticalContext,
-    //     display: 'vertical',
-    //   });
-
-    //   // horizontal scene item
-    //   const horizontalItem = scene.addSource(this.sourceId, {
-    //     id: horizontalNodeId,
-    //     select: false,
-    //   });
-    //   const horizontalContext = this.videoSettingsService.contexts.horizontal;
-    //   horizontalItem.setSettings({
-    //     ...horizontalSettings,
-    //     output: horizontalContext,
-    //     display: 'horizontal',
-    //   });
-
-    //   // reorder both
-    //   this.reorderNodesSubcommand.rollback();
-    //   this.reorderDualOutputNodesSubcommand.rollback();
-
-    //   // restore entry to node map
-    //   this.sceneCollectionsService.createNodeMapEntry(
-    //     this.sceneId,
-    //     horizontalNodeId,
-    //     verticalNodeId,
-    //   );
-    // } else {
-    // otherwise, just create horizontal item
     const item = scene.addSource(this.sourceId, { id: this.sceneItemId, select: false });
     const display = item.display ?? 'horizontal';
     const context = this.videoSettingsService.contexts[display];
@@ -183,6 +93,5 @@ export class RemoveItemCommand extends Command {
     });
 
     this.reorderNodesSubcommand.rollback();
-    // }
   }
 }
