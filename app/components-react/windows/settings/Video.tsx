a5e62ae36174a8f89d835a0e2d865207f9cdd7cf
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2023-07-12 17:10:43 -0400

    Unsubscribe from login subscription.

--- app/components-react/windows/settings/Video.tsx
+++ app/components-react/windows/settings/Video.tsx
@@ -390,9 +390,10 @@ class VideoSettingsModule {
   handleAuth() {
     Services.WindowsService.actions.closeChildWindow();
     this.userService.actions.showLogin();
-    Services.OnboardingService.onboardingCompleted.subscribe(() => {
+    const onboardingCompleted = Services.OnboardingService.onboardingCompleted.subscribe(() => {
       Services.DualOutputService.actions.setdualOutputMode();
       Services.SettingsService.actions.showSettings('Video');
+      onboardingCompleted.unsubscribe();
     });
   }
 }
