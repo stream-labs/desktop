a151c05a78e67740f7bf4ce306b57e71cb25b724
yusukess <yusukes@me.com>
2023-07-28 18:50:18 +0900

    ユーザー情報パネル

--- app/components/windows/UserInfo.vue.ts
+++ app/components/windows/UserInfo.vue.ts
@@ -1,22 +1,22 @@
-import ModalLayout from "components/ModalLayout.vue";
+import ModalLayout from 'components/ModalLayout.vue';
 import { Subscription } from 'rxjs';
-import { Inject } from "services/core";
-import { ChatMessage } from "services/nicolive-program/ChatMessage";
-import { ChatComponentType } from "services/nicolive-program/ChatMessage/ChatComponentType";
-import { KonomiTag, NicoliveClient } from "services/nicolive-program/NicoliveClient";
-import { WrappedChat, WrappedChatWithComponent } from "services/nicolive-program/WrappedChat";
-import { KonomiTagsService } from "services/nicolive-program/konomi-tags";
-import { NicoliveCommentViewerService } from "services/nicolive-program/nicolive-comment-viewer";
-import { NicoliveProgramService } from "services/nicolive-program/nicolive-program";
-import { WindowsService } from "services/windows";
-import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Inject } from 'services/core';
+import { ChatMessage } from 'services/nicolive-program/ChatMessage';
+import { ChatComponentType } from 'services/nicolive-program/ChatMessage/ChatComponentType';
+import { KonomiTag, NicoliveClient } from 'services/nicolive-program/NicoliveClient';
+import { WrappedChat, WrappedChatWithComponent } from 'services/nicolive-program/WrappedChat';
+import { KonomiTagsService } from 'services/nicolive-program/konomi-tags';
+import { NicoliveCommentViewerService } from 'services/nicolive-program/nicolive-comment-viewer';
+import { NicoliveProgramService } from 'services/nicolive-program/nicolive-program';
+import { WindowsService } from 'services/windows';
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
 import CommonComment from '../nicolive-area/comment/CommonComment.vue';
 import EmotionComment from '../nicolive-area/comment/EmotionComment.vue';
 import GiftComment from '../nicolive-area/comment/GiftComment.vue';
 import NicoadComment from '../nicolive-area/comment/NicoadComment.vue';
 import SystemMessage from '../nicolive-area/comment/SystemMessage.vue';
-import { getDisplayName } from "services/nicolive-program/ChatMessage/getDisplayName";
+import { getDisplayName } from 'services/nicolive-program/ChatMessage/getDisplayName';
 
 const componentMap: { [type in ChatComponentType]: Vue.Component } = {
   common: CommonComment,
@@ -26,7 +26,6 @@ const componentMap: { [type in ChatComponentType]: Vue.Component } = {
   system: SystemMessage,
 };
 
-
 @Component({
   components: {
     ModalLayout,
@@ -35,7 +34,7 @@ const componentMap: { [type in ChatComponentType]: Vue.Component } = {
     GiftComment,
     EmotionComment,
     SystemMessage,
-  }
+  },
 })
 export default class UserInfo extends Vue {
   @Inject() private nicoliveCommentViewerService: NicoliveCommentViewerService;
@@ -47,14 +46,30 @@ export default class UserInfo extends Vue {
   private myKonomiTags: KonomiTag[] = [];
   private rawKonomiTags: KonomiTag[] = [];
 
+  private cleanup: () => void = undefined;
+  isLatestVisible = true;
 
   mounted() {
     this.myKonomiTags = [];
     this.rawKonomiTags = [];
     this.isFollowing = false;
 
+    const sentinelEl = this.$refs.sentinel as HTMLElement;
+    const ioCallback: IntersectionObserverCallback = entries => {
+      this.isLatestVisible = entries[entries.length - 1].isIntersecting;
+    };
+    const ioOptions = {
+      rootMargin: '0px',
+      threshold: 0,
+    };
+    const io = new IntersectionObserver(ioCallback, ioOptions);
+    io.observe(sentinelEl);
+    this.cleanup = () => {
+      io.unobserve(sentinelEl);
+    };
+
     this.konomiTagsSubscription = this.konomiTagsService.stateChange.subscribe({
-      next: (state) => {
+      next: state => {
         this.myKonomiTags = state.loggedIn ? state.loggedIn.konomiTags : [];
         this.updateKonomiTags();
       },
@@ -62,17 +77,22 @@ export default class UserInfo extends Vue {
     // ユーザー情報ウィンドウを開く度に自分の好みタグも更新する(自分の好みタグが変わっている可能性があるため)
     this.konomiTagsService.fetch();
 
-    this.nicoliveProgramService.client.fetchKonomiTags(this.userId).then((tags) => {
+    this.nicoliveProgramService.client.fetchKonomiTags(this.userId).then(tags => {
       this.rawKonomiTags = tags;
       this.updateKonomiTags();
     });
-    this.nicoliveProgramService.client.fetchUserFollow(this.userId).then((following) => {
+    this.nicoliveProgramService.client.fetchUserFollow(this.userId).then(following => {
       this.isFollowing = following;
     });
   }
 
   beforeDestroy() {
     this.konomiTagsSubscription.unsubscribe();
+
+    if (this.cleanup) {
+      this.cleanup();
+      this.cleanup = undefined;
+    }
   }
 
   get userIconURL(): string {
@@ -99,28 +119,36 @@ export default class UserInfo extends Vue {
    * this.konomiTags に自分の好みタグと共通のものを先頭に括り出しcommon: trueにして、残りを common: falseで連結してセットする
    */
   private updateKonomiTags() {
-    const [same, other] = this.rawKonomiTags.reduce((acc, tag) => {
-      if (this.myKonomiTags.some((myTag) => myTag.tag_id.value === tag.tag_id.value)) {
-        acc[0].push(tag.name);
-      } else {
-        acc[1].push(tag.name);
-      }
-      return acc;
-    }, [[], []] as [string[], string[]]);
+    const [same, other] = this.rawKonomiTags.reduce(
+      (acc, tag) => {
+        if (this.myKonomiTags.some(myTag => myTag.tag_id.value === tag.tag_id.value)) {
+          acc[0].push(tag.name);
+        } else {
+          acc[1].push(tag.name);
+        }
+        return acc;
+      },
+      [[], []] as [string[], string[]],
+    );
 
     this.konomiTags = [
-      ...same.map((name) => ({ name, common: true })),
-      ...other.map((name) => ({ name, common: false })),
+      ...same.map(name => ({ name, common: true })),
+      ...other.map(name => ({ name, common: false })),
     ];
   }
 
   componentMap = componentMap;
 
   get comments(): WrappedChatWithComponent[] {
-    const comments = this.nicoliveCommentViewerService.items.filter((item) => {
+    const comments = this.nicoliveCommentViewerService.items.filter(item => {
       return item.value.user_id === this.userId;
     });
     return comments;
+  }
+
+  scrollToLatest() {
+    const scrollEl = this.$refs.scroll as HTMLElement;
+    scrollEl.scrollTop = scrollEl.scrollHeight;
   }
 
   getDisplayName(chat: WrappedChat): string {
