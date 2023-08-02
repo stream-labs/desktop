606d201d7ff0e853807fea6312f5a5db2ad72efa
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2023-08-02 15:15:00 -0400

    Apply optimizer settings to both contexts after running optimizer.

--- app/services/dual-output/dual-output.ts
+++ app/services/dual-output/dual-output.ts
@@ -513,6 +513,10 @@ export class DualOutputService extends PersistentStatefulService<IDualOutputServ
     this.SET_VIDEO_SETTING(setting, display);
   }
 
+  updateVideoSettings(settings: IVideoInfo, display: TDisplayType = 'horizontal') {
+    this.UPDATE_VIDEO_SETTING(settings, display);
+  }
+
   /**
    * Update loading state to show loading animation
    */
@@ -571,6 +575,11 @@ export class DualOutputService extends PersistentStatefulService<IDualOutputServ
       ...this.state.videoSettings[display],
       ...setting,
     };
+  }
+
+  @mutation()
+  private UPDATE_VIDEO_SETTING(setting: IVideoInfo, display: TDisplayType = 'vertical') {
+    this.state.videoSettings[display] = { ...setting };
   }
 
   @mutation()
