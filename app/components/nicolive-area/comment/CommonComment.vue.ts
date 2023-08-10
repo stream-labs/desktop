09200dc51633e87aab8ec49e7049c863c0cdc978
Akihiko Koizuka <koizuka@dwango.co.jp>
2023-08-10 17:57:30 +0900

    すでに表示した(ピン留め)コメントのユーザーアイコンの変化に追従

--- app/components/nicolive-area/comment/CommonComment.vue.ts
+++ app/components/nicolive-area/comment/CommonComment.vue.ts
@@ -1,4 +1,4 @@
-import { Component, Prop } from 'vue-property-decorator';
+import { Component, Prop, Watch } from 'vue-property-decorator';
 import { CommentBase } from './CommentBase';
 import { NicoliveClient } from 'services/nicolive-program/NicoliveClient';
 import { remote } from 'electron';
@@ -9,7 +9,18 @@ export default class CommonComment extends CommentBase {
   @Prop() speaking: boolean;
   @Prop() nameplateHint: boolean;
 
-  userIconURL = NicoliveClient.getUserIconURL(this.chat.value.user_id, `${this.chat.value.thread}`);
+  userIconURL: string = '';
+
+  updateUserIconURL() {
+    this.userIconURL = NicoliveClient.getUserIconURL(this.chat.value.user_id, `${this.chat.value.thread}`);
+  }
+  @Watch('chat') chatChanged() {
+    this.updateUserIconURL();
+  }
+  mounted() {
+    this.updateUserIconURL();
+  }
+
   defaultUserIconURL = NicoliveClient.defaultUserIconURL;
 
   openInDefaultBrowser(event: MouseEvent): void {
