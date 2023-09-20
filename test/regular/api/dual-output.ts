3531ec68215e93f26d3f35846d18600e85169778
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2023-09-20 09:53:29 -0400

    Restore auto config to onboarding tests.

--- test/regular/api/dual-output.ts
+++ test/regular/api/dual-output.ts
@@ -1,7 +1,11 @@
 import { DualOutputService } from 'services/dual-output';
 import { getApiClient } from '../../helpers/api-client';
 import { test, useWebdriver, TExecutionContext } from '../../helpers/webdriver';
-import { ScenesService } from 'services/scenes/scenes';
+import { AutoConfigService } from 'services/auto-config';
+import { ScenesService } from 'services/scenes';
+import { VideoSettingsService } from 'services/settings-v2/video';
+import { logIn } from '../../helpers/modules/user';
+import { SettingsService } from 'services/settings';
 
 useWebdriver();
 
@@ -19,4 +23,43 @@ test.skip('Dual output display toggles filter source selector data', async (t: T
   const scenesService = client.getResource<ScenesService>('ScenesService');
   const horizontalNodeIds = dualOutputService.views.horizontalNodeIds;
   const verticalNodeIds = dualOutputService.views.verticalNodeIds;
+});
+
+/**
+ * Dual output auto-config
+ */
+test('Auto config works with Dual Output Scene Collections', async (t: TExecutionContext) => {
+  await logIn('twitch');
+
+  const client = await getApiClient();
+  const dualOutputService = client.getResource<DualOutputService>('DualOutputService');
+  const videoSettingsService = client.getResource<VideoSettingsService>('VideoSettingsService');
+  const autoConfigService = client.getResource<AutoConfigService>('AutoConfigService');
+  const settingsService = client.getResource<SettingsService>('SettingsService');
+
+  dualOutputService.setdualOutputMode(true);
+  videoSettingsService.setVideoSetting('baseWidth', 852, 'horizontal');
+  videoSettingsService.setVideoSetting('baseHeight', 480, 'horizontal');
+  videoSettingsService.setVideoSetting('baseWidth', 720, 'vertical');
+  videoSettingsService.setVideoSetting('baseHeight', 1280, 'vertical');
+
+  await autoConfigService.start();
+
+  // const horizontalBaseWidth = videoSettingsService.contexts.horizontal.video.baseWidth;
+  // const verticalBaseHeight = videoSettingsService.contexts.vertical.video.baseHeight;
+
+  const horizontalBaseWidthState = videoSettingsService.state.horizontal.baseWidth;
+  const verticalBaseHeightState = videoSettingsService.state.vertical.baseHeight;
+
+  const [baseWidth, baseHeight] = settingsService.views.values.Video.Base.split('x');
+  const [outputWidth, outputHeight] = settingsService.views.values.Video.Output.split('x');
+
+  // console.log('horizontalBaseWidth ', horizontalBaseWidth);
+  // console.log('verticalBaseHeight ', verticalBaseHeight);
+  console.log('horizontalBaseWidthState ', horizontalBaseWidthState);
+  console.log('verticalBaseHeightState ', verticalBaseHeightState);
+  console.log('baseWidth ', baseWidth);
+  console.log('baseHeight ', baseHeight);
+  // t.deepEqual(horizontalBaseWidth, 1920);
+  // t.deepEqual(verticalBaseHeight, 1080);
 });
