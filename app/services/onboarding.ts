8cd0421088eccda5fbae09ce603461141056d71a
Adrian Perez <adrianperez.deb@gmail.com>
2023-10-30 10:54:18 -0700

    feat: make Mac skippable for testing

--- app/services/onboarding.ts
+++ app/services/onboarding.ts
@@ -44,7 +44,7 @@ export const ONBOARDING_STEPS = () => ({
     hideButton: true,
     isPreboarding: true,
     cond: isMac,
-    isSkippable: false,
+    isSkippable: true,
   },
   [EOnboardingSteps.StreamingOrRecording]: {
     component: 'StreamingOrRecording',
