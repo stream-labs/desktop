3f22b43ba3b15c8e7eec65bc6f2036919294e69a
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2023-11-03 10:02:52 -0400

    Refactor recording signal handling.

--- app/services/streaming/streaming-api.ts
+++ app/services/streaming/streaming-api.ts
@@ -90,9 +90,7 @@ export interface IStreamingServiceState {
   streamingStatus: EStreamingState;
   streamingStatusTime: string;
   recordingStatus: ERecordingState;
-  verticalRecordingStatus: ERecordingState;
   recordingStatusTime: string;
-  verticalRecordingStatusTime: string;
   replayBufferStatus: EReplayBufferState;
   replayBufferStatusTime: string;
   selectiveRecording: boolean;
