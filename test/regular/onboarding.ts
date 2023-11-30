1ceee1ecb4b55865441de2b9b87866c0fbe6617b
Adrian Perez <adrianperez.deb@gmail.com>
2023-11-30 14:35:11 -0800

    test(onboarding): add and fix onboarding tests

--- test/regular/onboarding.ts
+++ test/regular/onboarding.ts
@@ -1,60 +1,240 @@
 import { test, useWebdriver } from '../helpers/webdriver';
 import { logIn } from '../helpers/webdriver/user';
 import { sleep } from '../helpers/sleep';
-import { click, focusMain, isDisplayed } from '../helpers/modules/core';
+import {
+  click,
+  clickIfDisplayed,
+  focusMain,
+  isDisplayed,
+  waitForDisplayed,
+} from '../helpers/modules/core';
 
+// not a react hook
+// eslint-disable-next-line react-hooks/rules-of-hooks
 useWebdriver({ skipOnboarding: false });
 
-test('Go through the onboarding and autoconfig', async t => {
+test('Go through onboarding login and signup', async t => {
   const app = t.context.app;
   await focusMain();
 
   if (!(await isDisplayed('h2=Live Streaming'))) return;
+
   await click('h2=Live Streaming');
+  await click('h2=Beginner');
   await click('button=Continue');
 
-  // Wait for the auth screen to appear
-  await (await app.client.$('button=Twitch')).isExisting();
+  t.true(await isDisplayed('h1=Sign Up'), 'Shows signup page by default');
+  t.true(await isDisplayed('button=Create a Streamlabs ID'), 'Has a create Streamlabs ID button');
+
+  // Click on Login on the signup page, then wait for the auth screen to appear
+  await click('a=Login');
+  await isDisplayed('button=Log in with Twitch');
+
+  t.truthy(
+    await Promise.all(
+      ['Twitch', 'YouTube', 'Facebook'].map(async platform =>
+        (await app.client.$(`button=Log in with ${platform}`)).isExisting(),
+      ),
+    ),
+    'Shows login buttons for Twitch, YouTube, and Facebook',
+  );
+
+  t.truthy(
+    await Promise.all(
+      ['Trovo', 'TikTok', 'Dlive', 'NimoTV'].map(async platform =>
+        (await app.client.$(`aria/Login with ${platform}`)).isExisting(),
+      ),
+    ),
+    'Shows icon buttons for Trovo, TikTok, Dlive, and NimoTV',
+  );
+
+  t.true(await isDisplayed('a=Sign up'), 'Has a link to go back to Sign Up');
+});
+
+test('Go through onboarding as beginner user', async t => {
+  const app = t.context.app;
+  await focusMain();
+
+  if (!(await isDisplayed('h2=Live Streaming'))) return;
+
+  await click('h2=Live Streaming');
+  // Choose Beginner onboarding
+  await click('h2=Beginner');
+  await click('button=Continue');
+
+  // Click on Login on the signup page, then wait for the auth screen to appear
+  await click('a=Login');
+  await isDisplayed('button=Log in with Twitch');
 
   await logIn(t, 'twitch', { prime: false }, false, true);
   await sleep(1000);
 
-  if (await (await t.context.app.client.$('span=Skip')).isExisting()) {
-    await (await t.context.app.client.$('span=Skip')).click();
-    await sleep(1000);
-  }
+  // We seem to skip the login step after login internally
+  await clickIfDisplayed('button=Skip');
 
   // Don't Import from OBS
-  if (await (await t.context.app.client.$('div=Start Fresh')).isExisting()) {
-    await (await t.context.app.client.$('div=Start Fresh')).click();
-    await sleep(1000);
-  }
+  await clickIfDisplayed('div=Start Fresh');
 
   // Skip hardware config
-  if (await (await t.context.app.client.$('button=Skip')).isExisting()) {
-    await (await t.context.app.client.$('button=Skip')).click();
-    await sleep(1000);
-  }
+  await waitForDisplayed('h1=Set Up Mic and Webcam');
+  await clickIfDisplayed('button=Skip');
 
   // Skip picking a theme
-  if (await (await t.context.app.client.$('button=Skip')).isExisting()) {
-    await (await t.context.app.client.$('button=Skip')).click();
-    await sleep(1000);
-  }
+  await waitForDisplayed('h1=Add an Overlay');
+  await clickIfDisplayed('button=Skip');
+
+  // Skip purchasing prime
+  // TODO: is this timeout because of autoconfig?
+  await waitForDisplayed('div=Choose Starter', { timeout: 60000 });
+  await click('div=Choose Starter');
+
+  // Click Get Started after seeing tips
+  t.true(
+    await isDisplayed('span=Set yourself up for success with our getting started guide'),
+    'Shows beginner tips',
+  );
+  await clickIfDisplayed('button=Get Started');
+
+  await waitForDisplayed('span=Sources', { timeout: 60000 });
+
+  // success?
+  t.true(await (await app.client.$('span=Sources')).isDisplayed(), 'Sources selector is visible');
+});
+
+// TODO: this is the same as beginner as of the current flow, aside page diffs just asserts tips are different
+test('Go through onboarding as intermediate user', async t => {
+  const app = t.context.app;
+  await focusMain();
+
+  if (!(await isDisplayed('h2=Live Streaming'))) return;
+
+  await click('h2=Live Streaming');
+  // Choose Intermediate onboarding
+  await click('h2=Intermediate');
+  await click('button=Continue');
+
+  // Click on Login on the signup page, then wait for the auth screen to appear
+  await click('a=Login');
+  await isDisplayed('button=Log in with Twitch');
+
+  await logIn(t, 'twitch', { prime: false }, false, true);
+  await sleep(1000);
+
+  // We seem to skip the login step after login internally
+  await clickIfDisplayed('button=Skip');
+
+  // Don't Import from OBS
+  await clickIfDisplayed('div=Start Fresh');
+
+  // Skip hardware config
+  await waitForDisplayed('h1=Set Up Mic and Webcam');
+  await clickIfDisplayed('button=Skip');
+
+  // Skip picking a theme
+  await waitForDisplayed('h1=Add an Overlay');
+  await clickIfDisplayed('button=Skip');
+
+  // Skip purchasing prime
+  // TODO: is this timeout because of autoconfig?
+  await waitForDisplayed('div=Choose Starter', { timeout: 60000 });
+  await click('div=Choose Starter');
+
+  // Click Get Started after seeing tips
+  t.true(
+    await isDisplayed('span=Set up your alerts and widgets on Streamlabs Dashboard'),
+    'Shows intermediate tips',
+  );
+  await clickIfDisplayed('button=Get Started');
+
+  await waitForDisplayed('span=Sources', { timeout: 60000 });
+
+  // success?
+  // prettier-ignore
+  t.true(await (await app.client.$('span=Sources')).isDisplayed(), 'Sources selector is visible');
+});
+
+test('Go through onboarding as advanced user', async t => {
+  const app = t.context.app;
+  await focusMain();
+
+  if (!(await isDisplayed('h2=Live Streaming'))) return;
+
+  await click('h2=Live Streaming');
+  // Choose Advanced onboarding
+  await click('h2=Advanced');
+  await click('button=Continue');
+
+  // Don't Import from OBS
+  await clickIfDisplayed('div=Start Fresh');
+
+  // Skip hardware config
+  await waitForDisplayed('h1=Set Up Mic and Webcam');
+  await clickIfDisplayed('button=Skip');
+
+  // Skip purchasing prime
+  // TODO: is this timeout because of autoconfig?
+  await waitForDisplayed('div=Choose Starter', { timeout: 60000 });
+  await click('div=Choose Starter');
+
+  await waitForDisplayed('span=Sources', { timeout: 60000 });
+
+  // success?
+  // prettier-ignore
+  t.true(await (await app.client.$('span=Sources')).isDisplayed(), 'Sources selector is visible');
+});
+
+// TODO: this test is the same as beginner except with autoconfig, make specific assertions here once re-enabled
+test.skip('Go through the onboarding and autoconfig', async t => {
+  const app = t.context.app;
+  await focusMain();
+
+  if (!(await isDisplayed('h2=Live Streaming'))) return;
+
+  await click('h2=Live Streaming');
+  await click('h2=Beginner');
+  await click('button=Continue');
+
+  // Click on Login on the signup page, then wait for the auth screen to appear
+  await click('a=Login');
+  // prettier-ignore
+  await (await app.client.$('button=Log in with Twitch')).isExisting();
+
+  await logIn(t, 'twitch', { prime: false }, false, true);
+  await sleep(1000);
+
+  // We seem to skip the login step after login internally
+  await clickIfDisplayed('button=Skip');
+
+  // Don't Import from OBS
+  await clickIfDisplayed('div=Start Fresh');
+
+  // Skip hardware config
+  await waitForDisplayed('h1=Set Up Mic and Webcam');
+  await clickIfDisplayed('button=Skip');
+
+  // Skip picking a theme
+  await waitForDisplayed('h1=Add an Overlay');
+  await clickIfDisplayed('button=Skip');
 
   // Temporarily skip auto config test until migration to new API
   // Start auto config
   // t.true(await (await app.client.$('button=Start')).isExisting());
   // await (await app.client.$('button=Start')).click();
 
-  await (await t.context.app.client.$('div=Choose Starter')).waitForDisplayed({ timeout: 60000 });
   // Skip purchasing prime
-  if (await (await t.context.app.client.$('div=Choose Starter')).isExisting()) {
-    await (await t.context.app.client.$('div=Choose Starter')).click();
-    await sleep(1000);
-  }
+  // TODO: is this timeout because of autoconfig?
+  await waitForDisplayed('div=Choose Starter', { timeout: 60000 });
+  await click('div=Choose Starter');
 
-  await (await app.client.$('span=Sources')).waitForDisplayed({ timeout: 60000 });
+  // Click Get Started after seeing tips
+  t.true(
+    await isDisplayed('span=Set yourself up for success with our getting started guide'),
+    'Shows beginner tips',
+  );
+  await clickIfDisplayed('button=Get Started');
+
+  await waitForDisplayed('span=Sources', { timeout: 60000 });
+
   // success?
-  t.true(await (await app.client.$('span=Sources')).isDisplayed(), 'Sources selector is visible');
+  t.true(await(await app.client.$('span=Sources')).isDisplayed(), 'Sources selector is visible');
 });
