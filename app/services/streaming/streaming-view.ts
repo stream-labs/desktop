820031fcc77bdc8da8528053ddc02cfb9e363357
Adrian Perez <adrian@adrianperez.org>
2024-02-08 10:33:59 -0800

    fix(streaming): disable multistream check for enabling a primary platform when restoring settings

--- app/services/streaming/streaming-view.ts
+++ app/services/streaming/streaming-view.ts
@@ -302,8 +302,9 @@ export class StreamInfoView<T extends Object> extends ViewHandler<T> {
     const savedGoLiveSettings = this.streamSettingsView.state.goLiveSettings;
 
     // TODO: this should be done as a migration, if needed
+    // TODO: we would ideally run this only if restream can be enabled, but multistream tests fail if we get that specific
     const areNoPlatformsEnabled = () => Object.values(platforms).every(p => !p.enabled);
-    if (!this.restreamView.canEnableRestream && areNoPlatformsEnabled()) {
+    if (areNoPlatformsEnabled()) {
       const primaryPlatform = this.userView.auth?.primaryPlatform;
       if (primaryPlatform) {
         platforms[primaryPlatform].enabled = true;
