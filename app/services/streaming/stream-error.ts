e54d142b0e105063311116f8bdc9c6e35effba18
Adrian Perez <adrian@adrianperez.org>
2024-08-23 13:10:33 -0700

    fix(streaming): unknown error regression

--- app/services/streaming/stream-error.ts
+++ app/services/streaming/stream-error.ts
@@ -375,8 +375,32 @@ export function formatUnknownErrorMessage(
   } else if (typeof info === 'object') {
     if (info?.error) {
       try {
-        const error = typeof info.error === 'object' ? info.error : JSON.parse(info.error);
-        const platform = capitalize(error?.platform);
+        // TODO: we wanna refactor this, at least extract, and we should definitely fix types in the future
+        // ref: IOBSOutputSignalInfo
+        let error;
+        let platform;
+
+        if (typeof info.error === 'string') {
+          /*
+           * Try to parse error as JSON as originally done, however, if it's just a string
+           * (such as in the case of invalid path and many other unknown -4 errors we've
+           * swallowed, use that message instead.
+           * We intentionally skip all the logic below this point
+           */
+          try {
+            error = JSON.parse(info.error) as { platform: string };
+            platform = error.platform ? capitalize(error?.platform) : undefined;
+          } catch {
+            return obsStringErrorAsMessages(info);
+          }
+        } else {
+          error = (info.error as any) as { code: string; message: string; platform: string };
+          platform = capitalize(error.platform);
+        }
+
+        /* All of the below deals with correct JSON, which is what we were doing before and where the platform
+         * errors are?
+         */
         const unknownError = errorTypes['UNKNOWN_STREAMING_ERROR_WITH_MESSAGE'];
 
         const userMessage = platform
@@ -429,5 +453,30 @@ export function formatUnknownErrorMessage(
     user: messages.user.join('. '),
     report: messages.user.join('. '),
     details,
+  };
+}
+
+function obsStringErrorAsMessages(info: { error: string; code: number }) {
+  const error = { message: info.error, code: info.code };
+
+  const diagText = `${error.code} Error: ${error.message}`;
+  let userText;
+
+  // TODO: This doesn't work across locales, I know we want this custom text but it's complicating stuff
+  const invalidPath = /Unable to write to (.+). Make sure you're using a recording path which your user account is allowed to write to/;
+  if (invalidPath.test(error.message)) {
+    userText = `${error.message}\n\n${$t(
+      'Go to Settings -> Output -> Recording -> Recording Path if you need to change this location.',
+    )}`;
+  } else {
+    // Forward all errors without parsed `details` field (not sure where OBS sends JSON as info.error)
+    userText = `${$t(
+      'An error occurred with the output. Please check your streaming and recording settings.',
+    )}\n\n${error.message}`;
+  }
+
+  return {
+    user: userText,
+    report: diagText,
   };
 }
