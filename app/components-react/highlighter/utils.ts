26873890925fa884e3ed349cd6f496deaa9a3be9
marvinoffers <hi@marvinoffers.de>
2024-09-12 11:54:51 +0200

    added yesterday to ordering

--- app/components-react/highlighter/utils.ts
+++ app/components-react/highlighter/utils.ts
@@ -5,9 +5,9 @@ export const isAiClip = (clip: TClip): clip is IAiClip => clip.source === 'AiCli
 
 export function groupStreamsByTimePeriod(streams: { id: string; date: string }[]) {
   const now = moment();
-
   const groups: { [key: string]: typeof streams } = {
     Today: [],
+    Yesterday: [],
     'This week': [],
     'Last week': [],
     'This month': [],
@@ -19,6 +19,8 @@ export function groupStreamsByTimePeriod(streams: { id: string; date: string }[]
     const streamDate = moment(stream.date);
     if (streamDate.isSame(now, 'day')) {
       groups['Today'].push(stream);
+    } else if (streamDate.isSame(now.clone().subtract(1, 'day'), 'day')) {
+      groups['Yesterday'].push(stream);
     } else if (streamDate.isSame(now, 'week')) {
       groups['This week'].push(stream);
     } else if (streamDate.isSame(now.clone().subtract(1, 'week'), 'week')) {
