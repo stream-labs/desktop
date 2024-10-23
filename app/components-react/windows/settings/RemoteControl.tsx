4f6f6389bb13afd9aeb756d64115d243bbc799ba
Sean Beyer <sean.beyer@streamlabs.com>
2024-10-23 14:59:52 -0700

    Add section to RemoteControl for third party use

--- app/components-react/windows/settings/RemoteControl.tsx
+++ app/components-react/windows/settings/RemoteControl.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ObsSettingsSection } from './ObsSettings';
 import { $t } from '../../../services/i18n';
 import { Services } from '../../service-provider';
-import { SwitchInput } from '../../shared/inputs';
+import { SwitchInput, TextInput } from '../../shared/inputs';
 import { IConnectedDevice } from 'services/api/remote-control-api';
 import styles from './RemoteControl.m.less';
 import { useRealmObject } from 'components-react/hooks/realm';
 import { useVuex } from 'components-react/hooks';
 
 export function RemoteControlSettings() {
-  const { RemoteControlService, UserService } = Services;
+  const { RemoteControlService, UserService, TcpServerService } = Services;
 
   const connectedDevices = useRealmObject(RemoteControlService.connectedDevices).devices;
   const enabled = useRealmObject(RemoteControlService.state).enabled;
 
-  const { isLoggedIn } = useVuex(() => ({ isLoggedIn: UserService.views.isLoggedIn }));
+  const { isLoggedIn, websocketsEnabled } = useVuex(() => ({
+    isLoggedIn: UserService.views.isLoggedIn,
+    websocketsEnabled: TcpServerService.state.websockets.enabled,
+  }));
 
   function handleToggle() {
     if (enabled) {
@@ -24,48 +27,103 @@ export function RemoteControlSettings() {
     }
   }
 
+  function handleSocket() {
+    if (websocketsEnabled) {
+      TcpServerService.actions.disableWebsocketsRemoteConnections();
+    } else {
+      TcpServerService.actions.enableWebsoketsRemoteConnections();
+    }
+  }
+
+  function getIPAddresses() {
+    return TcpServerService.getIPAddresses()
+      .filter(address => !address.internal)
+      .map(address => address.address)
+      .join(', ');
+  }
+
   function disconnectDevice(device: IConnectedDevice) {
     RemoteControlService.actions.disconnectDevice(device.socketId);
   }
 
   return (
-    <ObsSettingsSection>
-      <div>
-        {$t(
-          'The free Streamlabs Controller app allows you to control Streamlabs Desktop from your iOS or Android device. You must be logged in to use this feature.',
-        )}
-        <br />
-        <br />
-      </div>
+    <>
+      <ObsSettingsSection>
+        <div>
+          {$t(
+            'The free Streamlabs Controller app allows you to control Streamlabs Desktop from your iOS or Android device. You must be logged in to use this feature.',
+          )}
+          <br />
+          <br />
+        </div>
 
-      <div>
-        {isLoggedIn && (
+        <div>
+          {isLoggedIn && (
+            <SwitchInput
+              label={$t('Allow Controller app connections')}
+              onInput={handleToggle}
+              value={enabled}
+              layout="horizontal"
+            />
+          )}
+
+          {enabled && (
+            <div style={{ paddingBottom: 8 }}>
+              <span>{$t('Connected Devices')}</span>
+              {connectedDevices.length < 1 && (
+                <span className={styles.whisper}>{$t('No devices connected')}</span>
+              )}
+              {connectedDevices.map(device => (
+                <div className={styles.device}>
+                  <span>{device.deviceName}</span>
+                  <span className={styles.disconnect} onClick={() => disconnectDevice(device)}>
+                    {$t('Disconnect')}
+                  </span>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </ObsSettingsSection>
+      <ObsSettingsSection>
+        <div>
+          {$t(
+            'Some third party applications connect to Streamlabs Desktop via websockets connection. Toggle this to allow such connections and display connection info.',
+          )}
+          <br />
+          <span style={{ color: 'var(--info)', display: 'inline' }}>
+            <i className="icon-error" />
+            &nbsp;
+            {$t('Warning: Displaying this portion on stream may leak sensitive information.')}
+          </span>
+          <br />
+          <br />
+        </div>
+        <div>
           <SwitchInput
-            label={$t('Allow remote connections')}
-            onInput={handleToggle}
-            value={enabled}
+            label={$t('Allow third party connections')}
+            onInput={handleSocket}
+            value={websocketsEnabled}
             layout="horizontal"
           />
-        )}
-
-        {enabled && (
-          <div style={{ paddingBottom: 8 }}>
-            <span>{$t('Connected Devices')}</span>
-            {connectedDevices.length < 1 && (
-              <span className={styles.whisper}>{$t('No devices connected')}</span>
-            )}
-            {connectedDevices.map(device => (
-              <div className={styles.device}>
-                <span>{device.deviceName}</span>
-                <span className={styles.disconnect} onClick={() => disconnectDevice(device)}>
-                  {$t('Disconnect')}
-                </span>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </ObsSettingsSection>
+          {websocketsEnabled && (
+            <div className={styles.websocketsForm}>
+              <TextInput label={$t('IP Addresses')} value={getIPAddresses()} readOnly />
+              <TextInput
+                label={$t('Port')}
+                value={`${TcpServerService.state.websockets.port}`}
+                readOnly
+              />
+              <TextInput
+                label={$t('API Token')}
+                value={UserService.views.auth?.apiToken}
+                readOnly
+              />
+            </div>
+          )}
+        </div>
+      </ObsSettingsSection>
+    </>
   );
 }
 
