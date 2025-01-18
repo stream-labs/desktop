7066d2c0af4c07cf0a4af6c757c0a77c36db8585
Adrian Perez <adrian@adrianperez.org>
2025-01-17 16:06:01 -0800

    fix: restore disabled multistream check

--- app/services/streaming/streaming-view.ts
+++ app/services/streaming/streaming-view.ts
@@ -162,13 +162,12 @@ export class StreamInfoView<T extends Object> extends ViewHandler<T> {
    * Returns if the user can or should use the restream service
    */
   get isMultiplatformMode(): boolean {
-    return false;
-    // if (this.isDualOutputMode) return false;
-    // return (
-    //   this.protectedModeEnabled &&
-    //   (this.enabledPlatforms.length > 1 ||
-    //     this.settings.customDestinations.filter(dest => dest.enabled).length > 0)
-    // );
+    if (this.isDualOutputMode) return false;
+    return (
+      this.protectedModeEnabled &&
+      (this.enabledPlatforms.length > 1 ||
+        this.settings.customDestinations.filter(dest => dest.enabled).length > 0)
+    );
   }
 
   /**
