a0a6154ebfca6e2ea81200b6aa429320a16dbe10
gettinToasty <sean.beyer@streamlabs.com>
2025-02-05 11:43:38 -0800

    Revert "fix remove need of stream id (#5289)" (#5294)
    This reverts commit 1d616494157a54695c0e4aad7f03047abdbad5ce.

--- app/components-react/highlighter/PreviewModal.tsx
+++ app/components-react/highlighter/PreviewModal.tsx
@@ -13,6 +13,10 @@ export default function PreviewModal({
   close: () => void;
   streamId: string | undefined;
 }) {
+  if (streamId === undefined) {
+    close();
+    console.error('streamId is required');
+  }
   const { HighlighterService } = Services;
   const clips = HighlighterService.getClips(HighlighterService.views.clips, streamId);
   const { intro, outro } = HighlighterService.views.video;
