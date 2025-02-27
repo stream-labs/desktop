1fa202d4ce5390284bb6f350be65fab242c4c3c1
marvinoffers <hi@marvinoffers.de>
2025-02-27 22:31:40 +0100

    [Highlighter] - Educational UI (#5313)
    * init carousel
    
    * moved to own component
    
    * finished base carousel
    
    * slid-title
    
    * only show language ui if language is not en-US
    
    * adjusted stream card
    
    * finetuning
    
    * fixed open link
    
    * added translation
    
    * streamcard to lisr
    
    * wrapped global css changes in class

--- app/components-react/windows/go-live/AiHighlighterToggle.tsx
+++ app/components-react/windows/go-live/AiHighlighterToggle.tsx
@@ -6,7 +6,8 @@ import { Services } from 'components-react/service-provider';
 import Highlighter from 'components-react/pages/Highlighter';
 import { useVuex } from 'components-react/hooks';
 import { DownOutlined, UpOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { Button, Carousel } from 'antd';
+import EducationCarousel from 'components-react/highlighter/EducationCarousel';
 
 export default function AiHighlighterToggle({
   game,
@@ -65,7 +66,16 @@ export default function AiHighlighterToggle({
           <div className={styles.aiHighlighterBox}>
             <div className={styles.headlineWrapper} onClick={() => setIsExpanded(!isExpanded)}>
               <h3 style={{ margin: 0, fontSize: '14px', fontWeight: 300, color: '#BDC2C4' }}>
-                Streaming <span style={{ fontWeight: 700 }}>Fortnite</span>? Try AI Highlighter!
+                {!useHighlighter ? (
+                  <>
+                    {' '}
+                    Streaming <span style={{ fontWeight: 700 }}>Fortnite</span>? Try AI Highlighter!{' '}
+                  </>
+                ) : (
+                  <>
+                    <span style={{ fontWeight: 700 }}>Ai Highlighter requirements</span>
+                  </>
+                )}
               </h3>
               {isExpanded ? (
                 <UpOutlined style={{ color: '#BDC2C4' }} />
@@ -73,42 +83,53 @@ export default function AiHighlighterToggle({
                 <DownOutlined style={{ color: '#BDC2C4' }} />
               )}
             </div>
-            {isExpanded ? (
+            {isExpanded && (
               <>
                 <div className={styles.expandedWrapper}>
-                  <div className={styles.toggleTextWrapper}>
-                    <div>
-                      <h2 style={{ fontSize: '16px', fontWeight: 600 }}>
-                        Auto-create
-                        <br /> highlights
-                      </h2>
-                      <div className={styles.betaTag}>Beta</div>
+                  {!useHighlighter ? (
+                    <div
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        paddingRight: '32px',
+                        paddingLeft: '32px',
+                        height: '100%',
+                      }}
+                    >
+                      <div>
+                        <h2 style={{ fontSize: '16px', fontWeight: 600 }}>
+                          Auto-create
+                          <br /> highlights
+                        </h2>
+                        <div className={styles.betaTag}>Beta</div>
+                      </div>
+                      <div className={styles.image}></div>
                     </div>
-                    {highlighterVersion !== '' ? (
-                      <SwitchInput
-                        style={{ padding: 0, margin: 0, marginLeft: '-40px', width: '100%' }}
-                        value={useHighlighter}
-                        label=""
-                        onChange={() => HighlighterService.actions.toggleAiHighlighter()}
-                      />
-                    ) : (
-                      <Button
-                        style={{ width: 'fit-content' }}
-                        size="small"
-                        type="primary"
-                        onClick={() => {
-                          HighlighterService.installAiHighlighter();
-                        }}
-                      >
-                        Install AI Highlighter
-                      </Button>
-                    )}
-                  </div>
-                  <div className={styles.image}></div>
+                  ) : (
+                    <EducationCarousel />
+                  )}
+
+                  {highlighterVersion !== '' ? (
+                    <SwitchInput
+                      style={{ width: '80px', margin: 0 }}
+                      value={useHighlighter}
+                      label=""
+                      onChange={() => HighlighterService.actions.toggleAiHighlighter()}
+                    />
+                  ) : (
+                    <Button
+                      style={{ width: 'fit-content' }}
+                      size="small"
+                      type="primary"
+                      onClick={() => {
+                        HighlighterService.installAiHighlighter();
+                      }}
+                    >
+                      Install AI Highlighter
+                    </Button>
+                  )}
                 </div>
               </>
-            ) : (
-              <></>
             )}
           </div>
         </div>
