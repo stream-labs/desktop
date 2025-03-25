18b955138cbc10f87fae7fc313ee9f8e722a02e5
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2025-03-25 12:20:21 -0400

    Add Advanced Replay Buffer to test.

--- test/regular/replay-buffer.ts
+++ test/regular/replay-buffer.ts
@@ -1,24 +1,30 @@
 import { readdir } from 'fs-extra';
-import { test, useWebdriver } from '../helpers/webdriver';
+import { ITestContext, test, useWebdriver } from '../helpers/webdriver';
 import { sleep } from '../helpers/sleep';
 import {
   setOutputResolution,
   setTemporaryRecordingPath,
   showSettingsWindow,
 } from '../helpers/modules/settings/settings';
-import { click, isDisplayed } from '../helpers/modules/core';
+import { click, clickButton, focusMain, isDisplayed, select } from '../helpers/modules/core';
 import {
   saveReplayBuffer,
   startReplayBuffer,
   stopReplayBuffer,
 } from '../helpers/modules/replay-buffer';
+import { ExecutionContext } from 'ava';
+import { setFormDropdown } from '../helpers/webdriver/forms';
 
+// not a react hook
+// eslint-disable-next-line react-hooks/rules-of-hooks
 useWebdriver();
 
-test('Replay Buffer', async t => {
-  const tmpDir = await setTemporaryRecordingPath();
-  await setOutputResolution('100x100');
-
+async function recordHighlight(
+  t: ExecutionContext<ITestContext>,
+  tmpDir: string,
+  numFiles: number,
+  message: string,
+) {
   // record a fragment
   await startReplayBuffer();
   await saveReplayBuffer();
@@ -27,13 +33,49 @@ test('Replay Buffer', async t => {
   // Check that the replay-buffer file has been created
   await sleep(3000);
   const files = await readdir(tmpDir);
-  t.is(files.length, 1);
+  t.is(files.length, numFiles, message);
+}
+
+async function toggleReplayBuffer(advanced: boolean = false) {
+  await showSettingsWindow('Output', async () => {
+    if (advanced) {
+      await setFormDropdown('Output Mode', 'Advanced');
+      await clickButton('Replay Buffer');
+      await click(await select('div[data-name="RecRB"]'));
+    } else {
+      await setFormDropdown('Output Mode', 'Simple');
+      await click('label=Enable Replay Buffer');
+    }
+
+    await sleep(3000);
+    await clickButton('Done');
+    await focusMain();
+  });
+}
+
+test('Replay Buffer', async t => {
+  const tmpDir = await setTemporaryRecordingPath();
+  await setOutputResolution('100x100');
+
+  // Simple Replay Buffer
+  await recordHighlight(t, tmpDir, 1, 'Simple Replay Buffer recorded highlight');
 
   // disable replay buffer
-  await showSettingsWindow('Output', async () => {
-    await click('label=Enable Replay Buffer');
-  });
+  await toggleReplayBuffer();
 
   // check Start Replay Buffer is not visible
-  t.false(await isDisplayed('button .icon-replay-buffer'));
+  t.false(await isDisplayed('button .icon-replay-buffer'), 'Simple Replay Buffer stopped');
+
+  // Advanced Replay Buffer
+  await setTemporaryRecordingPath(true, tmpDir);
+  await toggleReplayBuffer(true);
+  await recordHighlight(t, tmpDir, 2, 'Advanced Replay Buffer recorded highlight');
+  await toggleReplayBuffer(true);
+
+  // check Start Replay Buffer is not visible
+  t.false(await isDisplayed('button .icon-replay-buffer'), 'Advanced Replay Buffer stopped');
+
+  // // Switch back to Simple Replay Buffer
+  await toggleReplayBuffer();
+  await recordHighlight(t, tmpDir, 3, 'Switches between Simple and Advanced Replay Buffer');
 });
